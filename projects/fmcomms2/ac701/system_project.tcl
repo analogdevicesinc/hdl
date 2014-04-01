96761467254c629b80a153f401c2fd54592316d3
ATofan <alexandru.tofan@analog.com>
2014-04-01 15:35:44 +0300

    FMCOMMS2 AC701 Project
    Not tested - must program Vadj on board

---
+++ projects/fmcomms2/ac701/system_project.tcl
@@ -0,0 +1,15 @@
+
+
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project.tcl
+
+adi_project_create fmcomms2_ac701
+adi_project_files fmcomms2_ac701 [list \
+  "system_top.v" \
+  "system_constr.xdc"\
+  "$ad_hdl_dir/projects/common/ac701/ac701_system_constr.xdc" ]
+
+adi_project_run fmcomms2_ac701
+
+
