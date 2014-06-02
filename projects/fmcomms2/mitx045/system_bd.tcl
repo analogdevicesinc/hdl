f217139770d5edfd814ccaff050173b38a84a158
Adrian Costina <adrian.costina@analog.com>
2014-06-02 14:06:10 +0300

    fmcomms2: added project for mini_itx, xc7z045 version

---
+++ projects/fmcomms2/mitx045/system_bd.tcl
@@ -0,0 +1,3 @@
+
+source $ad_hdl_dir/projects/common/mitx045/mitx045_system_bd.tcl
+source ../common/fmcomms2_bd.tcl
---
+++ projects/fmcomms2/mitx045/system_project.tcl
@@ -0,0 +1,16 @@
+
+
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project.tcl
+
+adi_project_create fmcomms2_mitx045
+adi_project_files fmcomms2_mitx045 [list \
+  "system_top.v" \
+  "system_constr.xdc"\
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/mitx045/mitx045_system_constr.xdc" ]
+
+adi_project_run fmcomms2_mitx045
+
+
