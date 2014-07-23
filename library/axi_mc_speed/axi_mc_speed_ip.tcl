54b2cd74bfde4125a28aebd0154c2454c298b147
Adrian Costina <adrian.costina@analog.com>
2014-07-23 11:58:50 +0300

    motor_control: cores modified so they can compile with the new common files

--- library/axi_mc_speed/axi_mc_speed_ip.tcl
+++ library/axi_mc_speed/axi_mc_speed_ip.tcl
@@ -10,8 +10,10 @@ adi_ip_files axi_mc_speed [list \
   "$ad_hdl_dir/library/common/up_delay_cntrl.v" \
   "$ad_hdl_dir/library/common/up_clock_mon.v" \
   "$ad_hdl_dir/library/common/up_drp_cntrl.v" \
-  "$ad_hdl_dir/library/common/up_delay_cntrl.v" \
+  "$ad_hdl_dir/library/common/up_xfer_cntrl.v" \
+  "$ad_hdl_dir/library/common/up_xfer_status.v" \
   "$ad_hdl_dir/library/common/up_adc_common.v" \
+  "$ad_hdl_dir/library/common/up_adc_channel.v" \
   "debouncer.v" \
   "speed_detector.v" \
   "delay_30_degrees.v" \
