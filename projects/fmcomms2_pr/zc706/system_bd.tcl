17122661d29c149051e539586c303f87a84387a5
Istvan Csomortani <istvan.csomortani@analog.com>
2014-09-01 18:49:55 +0300

    fmcomms2_pr: Fix file path on ZC706/system_bd.tcl

--- projects/fmcomms2_pr/zc706/system_bd.tcl
+++ projects/fmcomms2_pr/zc706/system_bd.tcl
@@ -1,5 +1,5 @@
 
 source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
 source $ad_hdl_dir/projects/fmcomms2/common/fmcomms2_bd.tcl
-source $ad_hdl_dir/projects/fmcomms2_rp/common/prcfg_setup.tcl
+source $ad_hdl_dir/projects/fmcomms2_pr/common/prcfg_setup.tcl
 
