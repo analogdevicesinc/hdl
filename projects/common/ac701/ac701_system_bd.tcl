54053e0d87ce486da365a003c666c1fead03e0ad
Lars-Peter Clausen <lars@metafoo.de>
2014-09-30 19:36:03 +0200

    common: Set cpu interconnect strategy to minimize area
    There will rarely be concurrent access to the peripheral control bus
    interconnect, so there is no need to optimize for performace. Setting the
    interconnect strategy to minimize area can reduce the resource usage by
    ~90%.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- projects/common/ac701/ac701_system_bd.tcl
+++ projects/common/ac701/ac701_system_bd.tcl
@@ -94,9 +94,11 @@ set_property -dict [list CONFIG.XML_INPUT_FILE {ac701_system_mig.prj}] $axi_ddr_
 # instance: axi interconnect (lite)
 set axi_cpu_aux_interconnect [create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_cpu_aux_interconnect]
 set_property -dict [list CONFIG.NUM_MI {8}] $axi_cpu_aux_interconnect
+set_property -dict [list CONFIG.STRATEGY {1}] $axi_cpu_aux_interconnect
 
 set axi_cpu_interconnect [create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_cpu_interconnect]
 set_property -dict [list CONFIG.NUM_MI {7}] $axi_cpu_interconnect
+set_property -dict [list CONFIG.STRATEGY {1}] $axi_cpu_interconnect
 
 # instance: axi interconnect
 
