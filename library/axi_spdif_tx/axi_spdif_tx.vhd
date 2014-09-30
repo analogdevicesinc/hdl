f60e112b50ef63eb77a088ffdfc8e4a456708e53
Lars-Peter Clausen <lars@metafoo.de>
2014-09-30 11:32:28 +0200

    axi_spdif: Don't use non-static expressions in port assignments
    Fixes a warning from the tools.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/axi_spdif_tx/axi_spdif_tx.vhd
+++ library/axi_spdif_tx/axi_spdif_tx.vhd
@@ -120,8 +120,10 @@ architecture IMP of axi_spdif_tx is
 	signal conf_ratio : std_logic_vector(7 downto 0);
 	signal conf_tinten, conf_txdata, conf_txen : std_logic;
 	signal channel : std_logic;
+	signal enable : boolean;
 
 	signal fifo_data_out : std_logic_vector(31 downto 0);
+	signal fifo_data_ack : std_logic;
 	signal fifo_reset : std_logic;
 	signal tx_fifo_stb : std_logic;
 
@@ -135,6 +137,8 @@ architecture IMP of axi_spdif_tx is
 begin
 
 	fifo_reset <= not conf_txdata;
+	enable <= conf_txdata = '1';
+	fifo_data_ack <= channel and sample_data_ack;
 
 	streaming_dma_gen: if C_DMA_TYPE = 0 generate
 		fifo: entity axi_streaming_dma_tx_fifo
@@ -146,14 +150,14 @@ begin
 				clk		=> S_AXI_ACLK,
 				resetn		=> S_AXI_ARESETN,
 				fifo_reset	=> fifo_reset,
-				enable		=> conf_txdata = '1',
+				enable		=> enable,
 				S_AXIS_ACLK	=> S_AXIS_ACLK,
 				S_AXIS_TREADY	=> S_AXIS_TREADY,
 				S_AXIS_TDATA	=> S_AXIS_TDATA,
 				S_AXIS_TVALID	=> S_AXIS_TLAST,
 				S_AXIS_TLAST	=> S_AXIS_TVALID,
 
-				out_ack		=> channel and sample_data_ack,
+				out_ack		=> fifo_data_ack,
 				out_data	=> fifo_data_out
 			);
 	end generate;
@@ -175,12 +179,12 @@ begin
 				clk		=> S_AXI_ACLK,
 				resetn		=> S_AXI_ARESETN,
 				fifo_reset	=> fifo_reset,
-				enable		=> conf_txdata = '1',
+				enable		=> enable,
 
 				in_data		=> wr_data,
 				in_stb		=> tx_fifo_stb,
 
-				out_ack		=> channel and sample_data_ack,
+				out_ack		=> fifo_data_ack,
 				out_data	=> fifo_data_out,
 
 				dclk		=> DMA_REQ_ACLK,
