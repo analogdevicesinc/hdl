c4feab5884adf5f3d1cc2503e77065488ae012ac
Lars-Peter Clausen <lars@metafoo.de>
2014-09-30 10:47:32 +0200

    axi_dmac: Hide fifo_wr bus when source type is not the fifo interface
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/axi_dmac/axi_dmac_ip.tcl
+++ library/axi_dmac/axi_dmac_ip.tcl
@@ -57,8 +57,6 @@ adi_set_bus_dependency "s_axis" "s_axis" \
 	"(spirit:decode(id('MODELPARAM_VALUE.C_DMA_TYPE_SRC')) = 1)"
 adi_set_bus_dependency "m_axis" "m_axis" \
 	"(spirit:decode(id('MODELPARAM_VALUE.C_DMA_TYPE_DEST')) = 1)"
-adi_set_ports_dependency "fifo_wr" \
-	"(spirit:decode(id('MODELPARAM_VALUE.C_DMA_TYPE_SRC')) = 2)"
 adi_set_ports_dependency "fifo_rd" \
 	"(spirit:decode(id('MODELPARAM_VALUE.C_DMA_TYPE_DEST')) = 2)"
 
@@ -82,5 +80,7 @@ set_property physical_name {fifo_wr_clk} [ipx::get_port_map CLK [ipx::get_bus_in
 ipx::add_bus_parameter {ASSOCIATED_BUSIF} [ipx::get_bus_interface fifo_wr_clock [ipx::current_core]]
 set_property value {fifo_wr} [ipx::get_bus_parameter ASSOCIATED_BUSIF [ipx::get_bus_interface fifo_wr_clock [ipx::current_core]]]
 
-ipx::save_core [ipx::current_core]
+adi_set_bus_dependency "fifo_wr" "fifo_wr" \
+	"(spirit:decode(id('MODELPARAM_VALUE.C_DMA_TYPE_SRC')) = 2)"
 
+ipx::save_core [ipx::current_core]
