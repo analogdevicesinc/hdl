d0a70380bf48aadf5c1186a6f79086c3b33dc1e2
Rejeesh Kutty <rejeesh.kutty@analog.com>
2014-10-30 11:19:55 -0400

    kcu105: lutram constraints for ies

--- projects/daq2/kcu105/system_project.tcl
+++ projects/daq2/kcu105/system_project.tcl
@@ -10,7 +10,8 @@ adi_project_files daq2_kcu105 [list \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "$ad_hdl_dir/projects/common/kcu105/kcu105_system_constr.xdc" ]
+  "$ad_hdl_dir/projects/common/kcu105/kcu105_system_constr.xdc" \
+  "$ad_hdl_dir/projects/common/kcu105/kcu105_system_lutram_constr.xdc" ]
 
 set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/kcu105/kcu105_system_constr.xdc]
 set_property PROCESSING_ORDER EARLY [get_files system_constr.xdc]
