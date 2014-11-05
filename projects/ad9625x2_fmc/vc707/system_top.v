fcd015305c4973670e39cdbf2a6f2f5098652777
Adrian Costina <adrian.costina@analog.com>
2014-11-05 17:19:35 +0200

    ad9625x2_fmc: Updated vc707 project to include linear flash

--- projects/ad9625x2_fmc/vc707/system_top.v
+++ projects/ad9625x2_fmc/vc707/system_top.v
@@ -77,6 +77,13 @@ module system_top (
 
   fan_pwm,
 
+  linear_flash_addr,
+  linear_flash_adv_ldn,
+  linear_flash_ce_n,
+  linear_flash_oen,
+  linear_flash_wen,
+  linear_flash_dq_io,
+
   gpio_lcd,
   gpio_led,
   gpio_sw,
@@ -171,6 +178,13 @@ module system_top (
   inout             mdio_mdio;
 
   output            fan_pwm;
+
+  output  [26:1]    linear_flash_addr;
+  output            linear_flash_adv_ldn;
+  output            linear_flash_ce_n;
+  output            linear_flash_oen;
+  output            linear_flash_wen;
+  inout   [15:0]    linear_flash_dq_io;
 
   output  [  6:0]   gpio_lcd;
   output  [  7:0]   gpio_led;
@@ -398,6 +412,12 @@ module system_top (
     .ddr3_ras_n (ddr3_ras_n),
     .ddr3_reset_n (ddr3_reset_n),
     .ddr3_we_n (ddr3_we_n),
+    .linear_flash_addr (linear_flash_addr),
+    .linear_flash_adv_ldn (linear_flash_adv_ldn),
+    .linear_flash_ce_n (linear_flash_ce_n),
+    .linear_flash_oen (linear_flash_oen),
+    .linear_flash_wen (linear_flash_wen),
+    .linear_flash_dq_io(linear_flash_dq_io),
     .gpio_ad9625_i (gpio_i),
     .gpio_ad9625_o (gpio_o),
     .gpio_ad9625_t (gpio_t),
