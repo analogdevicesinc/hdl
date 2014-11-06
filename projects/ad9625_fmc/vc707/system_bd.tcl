0ffb1095d9108a2f2014bd330bad9d307b057749
Istvan Csomortani <istvan.csomortani@analog.com>
2014-11-06 14:59:08 +0200

    Revert "ad9625_vc707: Fix source file definition"
    This reverts commit 6c80d88a0cad85b027208b3e82a6937b190d20d6.

--- projects/ad9625_fmc/vc707/system_bd.tcl
+++ projects/ad9625_fmc/vc707/system_bd.tcl
@@ -1,6 +1,6 @@
 
 source $ad_hdl_dir/projects/common/vc707/vc707_system_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/sys_axi_dmafifo.tcl
+source $ad_hdl_dir/projects/common/xilinx/sys_dmafifo.tcl
 source ../common/ad9625_fmc_bd.tcl
 
 
