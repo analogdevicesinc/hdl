c72a04cecf767599a29b403072fd9a1d7bbbc261
Rejeesh Kutty <rejeesh.kutty@analog.com>
2014-11-07 15:17:00 -0500

    fmcomms7: initial updates

--- projects/fmcomms7/zc706/system_project.tcl
+++ projects/fmcomms7/zc706/system_project.tcl
@@ -4,9 +4,9 @@
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
 
-adi_project_create daq2_zc706
-adi_project_files daq2_zc706 [list \
-  "../common/daq2_spi.v" \
+adi_project_create fmcomms7_zc706
+adi_project_files fmcomms7_zc706 [list \
+  "../common/fmcomms7_spi.v" \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
@@ -15,6 +15,6 @@ adi_project_files daq2_zc706 [list \
 set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc]
 set_property PROCESSING_ORDER EARLY [get_files system_constr.xdc]
 
-adi_project_run daq2_zc706
+adi_project_run fmcomms7_zc706
 
 
