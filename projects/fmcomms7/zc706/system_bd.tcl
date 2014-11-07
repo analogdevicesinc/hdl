0560bd5f995370bdb05bcf9f04973b819cc401fa
Rejeesh Kutty <rejeesh.kutty@analog.com>
2014-11-07 15:16:43 -0500

    fmcomms7: initial updates

--- projects/fmcomms7/zc706/system_bd.tcl
+++ projects/fmcomms7/zc706/system_bd.tcl
@@ -1,5 +1,5 @@
 
 source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
 source $ad_hdl_dir/projects/common/zc706/zc706_system_plddr3.tcl
-source ../common/daq2_bd.tcl
+source ../common/fmcomms7_bd.tcl
 
