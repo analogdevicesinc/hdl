6e194dbcc0967b62579b3afba5b9934a476005f0
Istvan Csomortani <istvan.csomortani@analog.com>
2014-11-14 18:20:31 +0200

    adv7511_ac701: Fix project source files definition

--- projects/adv7511/ac701/system_project.tcl
+++ projects/adv7511/ac701/system_project.tcl
@@ -7,8 +7,7 @@ source $ad_hdl_dir/projects/scripts/adi_project.tcl
 adi_project_create adv7511_ac701
 adi_project_files adv7511_ac701 [list \
   "system_top.v" \
-  "$ad_hdl_dir/projects/common/ac701/ac701_system_constr.xdc" \
-  "$ad_hdl_dir/library/common/ad_interrupts.v"]
+  "$ad_hdl_dir/projects/common/ac701/ac701_system_constr.xdc"]
 
 adi_project_run adv7511_ac701
 
