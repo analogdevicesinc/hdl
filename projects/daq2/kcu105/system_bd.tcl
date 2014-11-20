db004641d3466339de6ef11695f635a5da7565f0
Rejeesh Kutty <rejeesh.kutty@analog.com>
2014-11-20 09:27:17 -0500

    kcu105: dmafifo address width change

--- projects/daq2/kcu105/system_bd.tcl
+++ projects/daq2/kcu105/system_bd.tcl
@@ -2,7 +2,7 @@
 source $ad_hdl_dir/projects/common/kcu105/kcu105_system_bd.tcl
 source $ad_hdl_dir/projects/common/xilinx/sys_dmafifo.tcl
 
-p_sys_dmafifo [current_bd_instance .] axi_ad9680_fifo 128
+p_sys_dmafifo [current_bd_instance .] axi_ad9680_fifo 128 16
 
 source ../common/daq2_bd.tcl
 
