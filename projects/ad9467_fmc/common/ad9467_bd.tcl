114406335d917544f9f0b3b5e1e0c8f3229e6945
Istvan Csomortani <istvan.csomortani@analog.com>
2014-11-24 17:51:33 +0200

    ad9467_fmc: Connect dma controller directly to the HP port

--- projects/ad9467_fmc/common/ad9467_bd.tcl
+++ projects/ad9467_fmc/common/ad9467_bd.tcl
@@ -40,8 +40,7 @@ if {$sys_zynq == 0} {
   set_property -dict [list CONFIG.C_DMA_DATA_WIDTH_DEST {64}] $axi_ad9467_dma
 
 if {$sys_zynq == 1} {
-  set axi_ad9467_dma_interconnect [create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_ad9467_dma_interconnect]
-  set_property -dict [list CONFIG.NUM_MI {1}] $axi_ad9467_dma_interconnect
+  set_property -dict [list CONFIG.C_DMA_AXI_PROTOCOL_DEST {1}] $axi_ad9467_dma
 }
 
   # spi
@@ -177,18 +176,11 @@ if {$sys_zynq == 0} {
   connect_bd_net -net sys_200m_resetn [get_bd_pins axi_mem_interconnect/S08_ARESETN] $sys_200m_resetn_source
   connect_bd_net -net sys_200m_resetn [get_bd_pins axi_ad9467_dma/m_dest_axi_aresetn]
 } else {
-  connect_bd_intf_net -intf_net axi_ad9467_dma_interconnect_s0 [get_bd_intf_pins axi_ad9467_dma_interconnect/S00_AXI] [get_bd_intf_pins axi_ad9467_dma/m_dest_axi]
-  connect_bd_intf_net -intf_net axi_ad9467_dma_interconnect_m00_axi [get_bd_intf_pins axi_ad9467_dma_interconnect/M00_AXI] [get_bd_intf_pins sys_ps7/S_AXI_HP1]
+  connect_bd_intf_net -intf_net axi_ad9467_dma_data [get_bd_intf_pins axi_ad9467_dma/m_dest_axi] [get_bd_intf_pins sys_ps7/S_AXI_HP1]
 
-  connect_bd_net -net sys_200m_clk  [get_bd_pins axi_ad9467_dma_interconnect/S00_ACLK] $sys_200m_clk_source
-  connect_bd_net -net sys_200m_clk  [get_bd_pins axi_ad9467_dma/m_dest_axi_aclk]
-  connect_bd_net -net sys_200m_clk  [get_bd_pins sys_ps7/S_AXI_HP1_ACLK]
-  connect_bd_net -net sys_200m_clk  [get_bd_pins axi_ad9467_dma_interconnect/ACLK] $sys_200m_clk_source
-  connect_bd_net -net sys_200m_clk  [get_bd_pins axi_ad9467_dma_interconnect/M00_ACLK] $sys_200m_clk_source
-  connect_bd_net -net sys_100m_resetn   [get_bd_pins axi_ad9467_dma_interconnect/ARESETN] $sys_100m_resetn_source
-  connect_bd_net -net sys_100m_resetn   [get_bd_pins axi_ad9467_dma_interconnect/M00_ARESETN] $sys_100m_resetn_source
+  connect_bd_net -net sys_200m_clk      [get_bd_pins sys_ps7/S_AXI_HP1_ACLK]
   connect_bd_net -net sys_100m_resetn   [get_bd_pins axi_ad9467_dma/m_dest_axi_aresetn]
-  connect_bd_net -net sys_100m_resetn   [get_bd_pins axi_ad9467_dma_interconnect/S00_ARESETN] $sys_100m_resetn_source
+  connect_bd_net -net sys_200m_clk      [get_bd_pins axi_ad9467_dma/m_dest_axi_aclk]
 }
 
   # ila (with fifo to prevent timing failure)
