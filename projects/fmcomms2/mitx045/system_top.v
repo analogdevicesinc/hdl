2f77daf71d4e72967a164505ec857cf881cdb88f
Adrian Costina <adrian.costina@analog.com>
2014-11-26 11:21:20 +0200

    fmcomms2: Updated mitx045 project. Updated constraints. Updated interrupts

--- projects/fmcomms2/mitx045/system_top.v
+++ projects/fmcomms2/mitx045/system_top.v
@@ -183,6 +183,7 @@ module system_top (
   wire    [48:0]  gpio_o;
   wire    [48:0]  gpio_t;
   wire    [19:0]  gpio_wire;
+  wire    [15:0]  ps_intrs;
 
   // instantiations
 
@@ -237,6 +238,22 @@ module system_top (
     .i2s_sdata_out (i2s_sdata_out),
     .iic_main_scl_io (iic_scl),
     .iic_main_sda_io (iic_sda),
+    .ps_intr_0 (ps_intrs[0]),
+    .ps_intr_1 (ps_intrs[1]),
+    .ps_intr_10 (ps_intrs[10]),
+    .ps_intr_11 (ps_intrs[11]),
+    .ps_intr_12 (ps_intrs[12]),
+    .ps_intr_13 (ps_intrs[13]),
+    .ps_intr_2 (ps_intrs[2]),
+    .ps_intr_3 (ps_intrs[3]),
+    .ps_intr_4 (ps_intrs[4]),
+    .ps_intr_5 (ps_intrs[5]),
+    .ps_intr_6 (ps_intrs[6]),
+    .ps_intr_7 (ps_intrs[7]),
+    .ps_intr_8 (ps_intrs[8]),
+    .ps_intr_9 (ps_intrs[9]),
+    .ad9361_dac_dma_irq (ps_intrs[12]),
+    .ad9361_adc_dma_irq (ps_intrs[13]),
     .rx_clk_in_n (rx_clk_in_n),
     .rx_clk_in_p (rx_clk_in_p),
     .rx_data_in_n (rx_data_in_n),
