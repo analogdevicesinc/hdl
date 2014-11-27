0ccc546aeba24a756dc86528e4bae22e334d69ba
Istvan Csomortani <istvan.csomortani@analog.com>
2014-11-27 14:01:36 +0200

    ad9671_fmc: Fix GT lane number definition

--- projects/ad9671_fmc/common/ad9671_fmc_bd.tcl
+++ projects/ad9671_fmc/common/ad9671_fmc_bd.tcl
@@ -39,7 +39,7 @@ set_property -dict [list CONFIG.C_NODE_IS_TRANSMIT {0}] $axi_ad9671_jesd
 set_property -dict [list CONFIG.C_LANES {2}] $axi_ad9671_jesd
 
 set axi_ad9671_gt [create_bd_cell -type ip -vlnv analog.com:user:axi_jesd_gt:1.0 axi_ad9671_gt]
-set_property -dict [list CONFIG.PCORE_NUM_OF_LANES {2}] [get_bd_cells axi_ad9671_gt]
+set_property -dict [list CONFIG.PCORE_NUM_OF_RX_LANES {2}] [get_bd_cells axi_ad9671_gt]
 set_property -dict [list CONFIG.PCORE_CPLL_FBDIV {4}] $axi_ad9671_gt
 set_property -dict [list CONFIG.PCORE_RX_OUT_DIV {1}] $axi_ad9671_gt
 set_property -dict [list CONFIG.PCORE_TX_OUT_DIV {1}] $axi_ad9671_gt
