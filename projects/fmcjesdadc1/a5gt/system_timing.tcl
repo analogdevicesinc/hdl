6ac774a9dd1d4b1b842ef390e5ca223bf7b50cb7
Adrian Costina <adrian.costina@analog.com>
2014-12-10 17:53:29 +0200

    fmcjesdadc1: Update altera system_timing script

--- projects/fmcjesdadc1/a5gt/system_timing.tcl
+++ projects/fmcjesdadc1/a5gt/system_timing.tcl
@@ -1,3 +1,16 @@
 
-report_timing -detail path_only -npaths 20 -file timing_impl.log
-  
+set worst_path [get_timing_paths -npaths 1 -setup]
+foreach_in_collection path $worst_path {
+  set slack [get_path_info $path -slack]
+}
+
+if {$slack > 0} {
+  set worst_path [get_timing_paths -npaths 1 -hold]
+  foreach_in_collection path $worst_path {
+    set slack [get_path_info $path -slack]
+  }
+}
+
+if {$slack < 0} {
+  use_this_invalid_command_to_crash
+}
