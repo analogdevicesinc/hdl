b595cce6978f91c66ed6f190d6d15d6018795e68
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-01-13 13:39:14 -0500

    kcu105-daq2-2014.4-- intermediate fixes

--- projects/common/kcu105/kcu105_system_mig.tcl
+++ projects/common/kcu105/kcu105_system_mig.tcl
@@ -4,8 +4,7 @@
 set_property -dict [list CONFIG.C0.ControllerType {DDR4_SDRAM}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_TimePeriod {1250}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_InputClockPeriod {3334}] $axi_ddr_cntrl
-set_property -dict [list CONFIG.C0.DDR4_MemoryPart {MT40A256M16HA-083}] $axi_ddr_cntrl
-set_property -dict [list CONFIG.C0.DDR4_AxiSelection {true}] $axi_ddr_cntrl
+set_property -dict [list CONFIG.C0.DDR4_MemoryPart {MT40A256M16HA-093}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_DataWidth {64}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_Mem_Add_Map {ROW_BANK_COLUMN}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_CasWriteLatency {9}] $axi_ddr_cntrl
@@ -135,11 +134,10 @@ set_property -dict [list CONFIG.c0_bg_0                {bank45.byte2.pin10}] $ax
 set_property -dict [list CONFIG.c0_cke_0               {bank45.byte3.pin11}] $axi_ddr_cntrl  
 set_property -dict [list CONFIG.c0_cs_n_0              {bank45.byte1.pin2}]  $axi_ddr_cntrl  
 set_property -dict [list CONFIG.c0_odt_0               {bank45.byte1.pin8}]  $axi_ddr_cntrl  
-set_property -dict [list CONFIG.c0_par                 {bank45.byte3.pin1}]  $axi_ddr_cntrl  
 set_property -dict [list CONFIG.c0_reset_n             {bank45.byte1.pin6}]  $axi_ddr_cntrl  
 set_property -dict [list CONFIG.c0_act_n               {bank45.byte2.pin12}] $axi_ddr_cntrl  
-set_property -dict [list CONFIG.c0_ck_t                {bank45.byte3.pin6}]  $axi_ddr_cntrl  
-set_property -dict [list CONFIG.c0_ck_c                {bank45.byte3.pin7}]  $axi_ddr_cntrl  
+set_property -dict [list CONFIG.c0_ck_t_0              {bank45.byte3.pin6}]  $axi_ddr_cntrl  
+set_property -dict [list CONFIG.c0_ck_c_0              {bank45.byte3.pin7}]  $axi_ddr_cntrl  
 set_property -dict [list CONFIG.c0_sys_clk_p           {bank45.byte1.pin10}] $axi_ddr_cntrl  
 set_property -dict [list CONFIG.c0_sys_clk_n           {bank45.byte1.pin11}] $axi_ddr_cntrl  
 set_property -dict [list CONFIG.sys_rst                {Unassigned}]         $axi_ddr_cntrl  
