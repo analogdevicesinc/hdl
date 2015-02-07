9e2e2ef44e9c0648480b432ee4f272099e805d00
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-02-06 22:07:00 -0500

    xfer-logic: stretch toggles to allow capture

--- library/common/up_xfer_status.v
+++ library/common/up_xfer_status.v
@@ -72,6 +72,9 @@ module up_xfer_status (
 
   // internal registers
 
+  reg             d_xfer_state_m1 = 'd0;
+  reg             d_xfer_state_m2 = 'd0;
+  reg             d_xfer_state = 'd0;
   reg     [ 5:0]  d_xfer_count = 'd0;
   reg             d_xfer_toggle = 'd0;
   reg     [DW:0]  d_xfer_data = 'd0;
@@ -79,27 +82,37 @@ module up_xfer_status (
   reg             up_xfer_toggle_m1 = 'd0;
   reg             up_xfer_toggle_m2 = 'd0;
   reg             up_xfer_toggle_m3 = 'd0;
+  reg             up_xfer_toggle = 'd0;
   reg     [DW:0]  up_data_status = 'd0;
 
   // internal signals
 
+  wire            d_xfer_enable_s;
   wire            up_xfer_toggle_s;
 
   // device status transfer
 
+  assign d_xfer_enable_s = d_xfer_state ^ d_xfer_toggle;
+
   always @(posedge d_clk) begin
     if (d_rst == 1'b1) begin
+      d_xfer_state_m1 <= 'd0;
+      d_xfer_state_m2 <= 'd0;
+      d_xfer_state <= 'd0;
       d_xfer_count <= 'd0;
       d_xfer_toggle <= 'd0;
       d_xfer_data <= 'd0;
       d_acc_data <= 'd0;
     end else begin
+      d_xfer_state_m1 <= up_xfer_toggle;
+      d_xfer_state_m2 <= d_xfer_state_m1;
+      d_xfer_state <= d_xfer_state_m2;
       d_xfer_count <= d_xfer_count + 1'd1;
-      if (d_xfer_count == 6'd1) begin
+      if ((d_xfer_count == 6'd1) && (d_xfer_enable_s == 1'b0)) begin
         d_xfer_toggle <= ~d_xfer_toggle;
         d_xfer_data <= d_acc_data;
       end
-      if (d_xfer_count == 6'd1) begin
+      if ((d_xfer_count == 6'd1) && (d_xfer_enable_s == 1'b0)) begin
         d_acc_data <= d_data_status;
       end else begin
         d_acc_data <= d_acc_data | d_data_status;
@@ -114,11 +127,13 @@ module up_xfer_status (
       up_xfer_toggle_m1 <= 'd0;
       up_xfer_toggle_m2 <= 'd0;
       up_xfer_toggle_m3 <= 'd0;
+      up_xfer_toggle <= 'd0;
       up_data_status <= 'd0;
     end else begin
       up_xfer_toggle_m1 <= d_xfer_toggle;
       up_xfer_toggle_m2 <= up_xfer_toggle_m1;
       up_xfer_toggle_m3 <= up_xfer_toggle_m2;
+      up_xfer_toggle <= up_xfer_toggle_m3;
       if (up_xfer_toggle_s == 1'b1) begin
         up_data_status <= d_xfer_data;
       end
