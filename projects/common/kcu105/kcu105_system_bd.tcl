d7993401e613f2972803129a37f5a88a7835b139
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-03-10 16:13:32 -0400

    kcu105/adv7511: moved hdmi/spdif out of base design

--- projects/common/kcu105/kcu105_system_bd.tcl
+++ projects/common/kcu105/kcu105_system_bd.tcl
@@ -32,24 +32,16 @@ create_bd_port -dir I -from 31 -to 0 gpio1_i
 create_bd_port -dir O -from 31 -to 0 gpio1_o
 create_bd_port -dir O -from 31 -to 0 gpio1_t
 
-create_bd_port -dir O hdmi_out_clk
-create_bd_port -dir O hdmi_hsync
-create_bd_port -dir O hdmi_vsync
-create_bd_port -dir O hdmi_data_e
-create_bd_port -dir O -from 15 -to 0 hdmi_data
-
-# spdif audio
-
-create_bd_port -dir O spdif
-
 # interrupts
 
-create_bd_port -dir I mb_intr_05
-create_bd_port -dir I mb_intr_06
-create_bd_port -dir I mb_intr_12
-create_bd_port -dir I mb_intr_13
-create_bd_port -dir I mb_intr_14
-create_bd_port -dir I mb_intr_15
+create_bd_port -dir I -type intr mb_intr_05
+create_bd_port -dir I -type intr mb_intr_06
+create_bd_port -dir I -type intr mb_intr_07
+create_bd_port -dir I -type intr mb_intr_08
+create_bd_port -dir I -type intr mb_intr_12
+create_bd_port -dir I -type intr mb_intr_13
+create_bd_port -dir I -type intr mb_intr_14
+create_bd_port -dir I -type intr mb_intr_15
 
 # io settings
 
@@ -149,31 +141,6 @@ set_property -dict [list CONFIG.C_HAS_FAST {0}] $axi_intc
 set sys_concat_intc [create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 sys_concat_intc]
 set_property -dict [list CONFIG.NUM_PORTS {16}] $sys_concat_intc
 
-# hdmi peripherals
-
-set axi_hdmi_clkgen [create_bd_cell -type ip -vlnv analog.com:user:axi_clkgen:1.0 axi_hdmi_clkgen]
-set axi_hdmi_core [create_bd_cell -type ip -vlnv analog.com:user:axi_hdmi_tx:1.0 axi_hdmi_core]
-set_property -dict [list CONFIG.PCORE_DEVICE_TYPE {1}] $axi_hdmi_core
-
-set axi_hdmi_dma [create_bd_cell -type ip -vlnv xilinx.com:ip:axi_vdma:6.2 axi_hdmi_dma]
-set_property -dict [list CONFIG.c_m_axis_mm2s_tdata_width {64}] $axi_hdmi_dma
-set_property -dict [list CONFIG.c_use_mm2s_fsync {1}] $axi_hdmi_dma
-set_property -dict [list CONFIG.c_include_s2mm {0}] $axi_hdmi_dma
-
-# audio peripherals
-
-set sys_audio_clkgen [create_bd_cell -type ip -vlnv xilinx.com:ip:clk_wiz:5.1 sys_audio_clkgen]
-set_property -dict [list CONFIG.PRIM_IN_FREQ {200.000}] $sys_audio_clkgen
-set_property -dict [list CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {12.288}] $sys_audio_clkgen
-
-set axi_spdif_tx_core [create_bd_cell -type ip -vlnv analog.com:user:axi_spdif_tx:1.0 axi_spdif_tx_core]
-set_property -dict [list CONFIG.C_DMA_TYPE {0}] $axi_spdif_tx_core
-set_property -dict [list CONFIG.C_S_AXI_ADDR_WIDTH {16}] $axi_spdif_tx_core
-
-set axi_spdif_tx_dma [create_bd_cell -type ip -vlnv xilinx.com:ip:axi_dma:7.1 axi_spdif_tx_dma]
-set_property -dict [list CONFIG.c_include_s2mm {0}] $axi_spdif_tx_dma
-set_property -dict [list CONFIG.c_sg_include_stscntrl_strm {0}] $axi_spdif_tx_dma
-
 # connections
 
 ad_connect  sys_mb_debug/Debug_SYS_Rst sys_rstgen/mb_debug_sys_rst
@@ -225,8 +192,8 @@ ad_connect  sys_concat_intc/In3 axi_ethernet_dma/s2mm_introut
 ad_connect  sys_concat_intc/In4 axi_uart/interrupt
 ad_connect  sys_concat_intc/In5 mb_intr_05
 ad_connect  sys_concat_intc/In6 mb_intr_06
-ad_connect  sys_concat_intc/In7 axi_spdif_tx_dma/mm2s_introut
-ad_connect  sys_concat_intc/In8 axi_hdmi_dma/mm2s_introut
+ad_connect  sys_concat_intc/In7 mb_intr_07
+ad_connect  sys_concat_intc/In8 mb_intr_08
 ad_connect  sys_concat_intc/In9 axi_iic_main/iic2intc_irpt
 ad_connect  sys_concat_intc/In10 axi_spi/ip2intc_irpt
 ad_connect  sys_concat_intc/In11 axi_gpio/ip2intc_irpt
@@ -288,38 +255,6 @@ ad_connect  gpio1_o axi_gpio/gpio2_io_o
 ad_connect  gpio1_t axi_gpio/gpio2_io_t  
 ad_connect  sys_cpu_clk axi_spi/ext_spi_clk 
 
-# hdmi
-
-ad_connect  sys_200m_clk axi_hdmi_clkgen/clk
-ad_connect  sys_cpu_clk axi_hdmi_clkgen/drp_clk
-ad_connect  sys_cpu_clk axi_hdmi_core/m_axis_mm2s_clk
-ad_connect  sys_cpu_clk axi_hdmi_dma/m_axis_mm2s_aclk
-ad_connect  axi_hdmi_core/hdmi_clk axi_hdmi_clkgen/clk_0
-ad_connect  axi_hdmi_core/hdmi_out_clk hdmi_out_clk
-ad_connect  axi_hdmi_core/hdmi_16_hsync hdmi_hsync
-ad_connect  axi_hdmi_core/hdmi_16_vsync hdmi_vsync
-ad_connect  axi_hdmi_core/hdmi_16_data_e hdmi_data_e
-ad_connect  axi_hdmi_core/hdmi_16_data hdmi_data
-ad_connect  axi_hdmi_core/m_axis_mm2s_tvalid axi_hdmi_dma/m_axis_mm2s_tvalid
-ad_connect  axi_hdmi_core/m_axis_mm2s_tdata axi_hdmi_dma/m_axis_mm2s_tdata
-ad_connect  axi_hdmi_core/m_axis_mm2s_tkeep axi_hdmi_dma/m_axis_mm2s_tkeep
-ad_connect  axi_hdmi_core/m_axis_mm2s_tlast axi_hdmi_dma/m_axis_mm2s_tlast
-ad_connect  axi_hdmi_core/m_axis_mm2s_tready axi_hdmi_dma/m_axis_mm2s_tready
-ad_connect  axi_hdmi_core/m_axis_mm2s_fsync axi_hdmi_dma/mm2s_fsync
-ad_connect  axi_hdmi_core/m_axis_mm2s_fsync axi_hdmi_core/m_axis_mm2s_fsync_ret
-
-# spdif audio
-
-ad_connect  axi_spdif_tx_core/S_AXIS_TVALID axi_spdif_tx_dma/m_axis_mm2s_tvalid
-ad_connect  axi_spdif_tx_core/S_AXIS_TDATA axi_spdif_tx_dma/m_axis_mm2s_tdata
-ad_connect  axi_spdif_tx_core/S_AXIS_TLAST axi_spdif_tx_dma/m_axis_mm2s_tlast
-ad_connect  axi_spdif_tx_core/S_AXIS_TREADY axi_spdif_tx_dma/m_axis_mm2s_tready
-ad_connect  sys_200m_clk sys_audio_clkgen/clk_in1
-ad_connect  sys_audio_clkgen/clk_out1 axi_spdif_tx_core/spdif_data_clk
-ad_connect  sys_cpu_clk axi_spdif_tx_core/S_AXIS_ACLK
-ad_connect  sys_cpu_resetn axi_spdif_tx_core/S_AXIS_ARESETN
-ad_connect  spdif axi_spdif_tx_core/spdif_tx_o
-
 # defaults (interconnect - processor)
 
 ad_cpu_interconnect 0x41400000 sys_mb_debug
@@ -331,11 +266,6 @@ ad_cpu_interconnect 0x41200000 axi_intc
 ad_cpu_interconnect 0x41600000 axi_iic_main
 ad_cpu_interconnect 0x40000000 axi_gpio
 ad_cpu_interconnect 0x44A70000 axi_spi
-ad_cpu_interconnect 0x79000000 axi_hdmi_clkgen
-ad_cpu_interconnect 0x70e00000 axi_hdmi_core
-ad_cpu_interconnect 0x43000000 axi_hdmi_dma
-ad_cpu_interconnect 0x75c00000 axi_spdif_tx_core
-ad_cpu_interconnect 0x41E00000 axi_spdif_tx_dma
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_ddr_interconnect
 set_property CONFIG.NUM_MI {1} [get_bd_cells axi_ddr_interconnect]
@@ -354,9 +284,6 @@ ad_mem_hp0_interconnect sys_cpu_clk sys_mb/M_AXI_IC
 ad_mem_hp0_interconnect sys_cpu_clk axi_ethernet_dma/M_AXI_SG
 ad_mem_hp0_interconnect sys_cpu_clk axi_ethernet_dma/M_AXI_MM2S
 ad_mem_hp0_interconnect sys_cpu_clk axi_ethernet_dma/M_AXI_S2MM
-ad_mem_hp0_interconnect sys_cpu_clk axi_hdmi_dma/M_AXI_MM2S
-ad_mem_hp0_interconnect sys_cpu_clk axi_spdif_tx_dma/M_AXI_SG
-ad_mem_hp0_interconnect sys_cpu_clk axi_spdif_tx_dma/M_AXI_MM2S
 
 create_bd_addr_seg -range 0x20000 -offset 0x0 [get_bd_addr_spaces sys_mb/Data] \
   [get_bd_addr_segs sys_dlmb_cntlr/SLMB/Mem] SEG_dlmb_cntlr
