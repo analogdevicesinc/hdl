18891d036bcef0812abde20f6e2469dcc2b67615
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-03-17 14:01:26 -0400

    fmcomms7: 2014.4 updates

--- projects/fmcomms7/zc706/system_project.tcl
+++ projects/fmcomms7/zc706/system_project.tcl
@@ -3,6 +3,7 @@
 
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 adi_project_create fmcomms7_zc706
 adi_project_files fmcomms7_zc706 [list \
