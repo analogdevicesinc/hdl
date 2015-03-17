f9da2f31a6087a1305675753a9142cdf2cb8c719
Adrian Costina <adrian.costina@analog.com>
2015-03-17 17:44:01 +0200

    fmcadc4: Updated project with interrupts in IPI to work correctly in Linux

--- projects/fmcadc4/common/fmcadc4_bd.tcl
+++ projects/fmcadc4/common/fmcadc4_bd.tcl
@@ -56,10 +56,6 @@ if {$sys_zynq == 0} {
   set adc_dsync       [create_bd_port -dir I adc_dsync]
   set adc_ddata       [create_bd_port -dir I -from 255 -to 0 adc_ddata]
 
-  set fmcadc4_dma_intr    [create_bd_port -dir O fmcadc4_dma_intr]
-  set fmcadc4_spi_intr    [create_bd_port -dir O fmcadc4_spi_intr]
-  set fmcadc4_gpio_intr   [create_bd_port -dir O fmcadc4_gpio_intr]
-
   # adc peripherals
 
   set axi_ad9234_core_0 [create_bd_cell -type ip -vlnv analog.com:user:axi_ad9234:1.0 axi_ad9234_core_0]
@@ -239,7 +235,6 @@ if {$sys_zynq == 0} {
   connect_bd_net -net axi_ad9234_dma_ddata             [get_bd_pins axi_ad9234_fifo/dma_wdata]        [get_bd_pins axi_ad9234_dma/s_axis_data]
   connect_bd_net -net axi_ad9234_dma_dready            [get_bd_pins axi_ad9234_fifo/dma_wready]       [get_bd_pins axi_ad9234_dma/s_axis_ready]
   connect_bd_net -net axi_ad9234_dma_xfer_req          [get_bd_pins axi_ad9234_fifo/dma_xfer_req]     [get_bd_pins axi_ad9234_dma/s_axis_xfer_req]
-  connect_bd_net -net fmcadc4_dma_intr                 [get_bd_pins axi_ad9234_dma/irq]               [get_bd_ports fmcadc4_dma_intr]
 
   # dac/adc clocks
 
@@ -287,8 +282,24 @@ if {$sys_zynq == 0} {
   connect_bd_net -net sys_100m_resetn [get_bd_pins axi_fmcadc4_spi/s_axi_aresetn] 
   connect_bd_net -net sys_100m_resetn [get_bd_pins axi_fmcadc4_gpio/s_axi_aresetn] 
 
-  connect_bd_net -net axi_fmcadc4_spi_irq  [get_bd_pins axi_fmcadc4_spi/ip2intc_irpt]   [get_bd_ports fmcadc4_spi_intr]
-  connect_bd_net -net axi_fmcadc4_gpio_irq [get_bd_pins axi_fmcadc4_gpio/ip2intc_irpt]  [get_bd_ports fmcadc4_gpio_intr]
+}
+
+  # interrupts
+
+if {$sys_zynq == 0} {
+
+  delete_bd_objs [get_bd_nets mb_intr_10_s] [get_bd_ports mb_intr_10]
+  delete_bd_objs [get_bd_nets mb_intr_11_s] [get_bd_ports mb_intr_11]
+  delete_bd_objs [get_bd_nets mb_intr_12_s] [get_bd_ports mb_intr_12]
+  connect_bd_net -net axi_fmcadc4_spi_irq  [get_bd_pins axi_fmcadc4_spi/ip2intc_irpt]   [get_bd_pins sys_concat_intc/In10]
+  connect_bd_net -net axi_fmcadc4_gpio_irq [get_bd_pins axi_fmcadc4_gpio/ip2intc_irpt]  [get_bd_pins sys_concat_intc/In11]
+  connect_bd_net -net fmcadc4_dma_intr     [get_bd_pins axi_ad9234_dma/irq]             [get_bd_pins sys_concat_intc/In12]
+
+} else {
+
+  delete_bd_objs [get_bd_nets ps_intr_12_s] [get_bd_ports ps_intr_12]
+  connect_bd_net -net fmcadc4_dma_intr  [get_bd_pins axi_ad9234_dma/irq]  [get_bd_pins sys_concat_intc/In12]
+
 }
 
   # gt uses hp3, and 100MHz clock for both DRP and AXI4
