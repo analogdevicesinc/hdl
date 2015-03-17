ee89862de9de9bc41b92799796007e7b0cca19be
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-03-17 16:52:55 -0400

    ad6676evb: 2014.4 updates

--- projects/ad6676evb/zc706/system_project.tcl
+++ projects/ad6676evb/zc706/system_project.tcl
@@ -3,12 +3,13 @@
 
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 adi_project_create ad6676evb_zc706
 adi_project_files ad6676evb_zc706 [list \
-  "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "system_top.v" \
-  "system_constr.xdc" \
+  "system_constr.xdc"\
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc" ]
 
 set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc]
