9e1e4969ff8c2e196a9d45197f67904614010356
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-03-19 16:32:02 -0400

    fmcadc4: 2014.4 updates

--- projects/fmcadc4/zc706/system_project.tcl
+++ projects/fmcadc4/zc706/system_project.tcl
@@ -3,6 +3,7 @@
 
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 adi_project_create fmcadc4_zc706
 adi_project_files fmcadc4_zc706 [list \
@@ -12,6 +13,9 @@ adi_project_files fmcadc4_zc706 [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/zc706/zc706_system_mig_constr.xdc" \
   "$ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc" ]
+
+set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc]
+set_property PROCESSING_ORDER EARLY [get_files system_constr.xdc]
 
 adi_project_run fmcadc4_zc706
 
