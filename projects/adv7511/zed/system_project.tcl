7c921474bf8df6e4194fb33b919514216c0accdb
Istvan Csomortani <istvan.csomortani@analog.com>
2015-03-23 13:01:51 +0200

    adv7511_zed: Update project to the new framework

--- projects/adv7511/zed/system_project.tcl
+++ projects/adv7511/zed/system_project.tcl
@@ -3,6 +3,7 @@
 
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 adi_project_create adv7511_zed
 adi_project_files adv7511_zed [list \
