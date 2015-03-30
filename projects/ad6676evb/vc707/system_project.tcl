c4a7730c89ec019087863748d059f528819feca4
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-03-30 14:58:49 -0400

    ad6676evb/vc707: 2014.4 updates

--- projects/ad6676evb/vc707/system_project.tcl
+++ projects/ad6676evb/vc707/system_project.tcl
@@ -3,12 +3,17 @@
 
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 adi_project_create ad6676evb_vc707
 adi_project_files ad6676evb_vc707 [list \
   "system_top.v" \
   "system_constr.xdc"\
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/vc707/vc707_system_constr.xdc" ]
+
+set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/vc707/vc707_system_constr.xdc]
+set_property PROCESSING_ORDER EARLY [get_files system_constr.xdc]
 
 adi_project_run ad6676evb_vc707
 
