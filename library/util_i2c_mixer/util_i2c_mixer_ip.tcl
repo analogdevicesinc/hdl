86e6f67d4be1ecaefbed5867ae3fc2a3e45e73e9
Lars-Peter Clausen <lars@metafoo.de>
2015-04-01 14:12:15 +0200

    util_i2c_mixer: Add I2C interface
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/util_i2c_mixer/util_i2c_mixer_ip.tcl
+++ library/util_i2c_mixer/util_i2c_mixer_ip.tcl
@@ -9,5 +9,17 @@ adi_ip_files util_i2c_mixer [list \
 
 adi_ip_properties_lite util_i2c_mixer
 
-ipx::save_core [ipx::current_core]
 
+adi_add_bus "upstream" "slave" \
+	"xilinx.com:interface:iic_rtl:1.0" \
+	"xilinx.com:interface:iic:1.0" \
+	[list \
+		{"upstream_scl_I" "SCL_O"} \
+		{"upstream_scl_O" "SCL_I"} \
+		{"upstream_scl_T" "SCL_T"} \
+		{"upstream_sda_I" "SDA_O"} \
+		{"upstream_sda_O" "SDA_I"} \
+		{"upstream_sda_T" "SDA_T"} \
+	 ]
+
+ipx::save_core [ipx::current_core]
