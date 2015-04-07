0d0c15df98b158392cde760e58f1300bd4ad3a6a
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-04-07 16:40:52 -0400

    axi_adcfifo: fix file names

--- library/axi_adcfifo/axi_adcfifo_ip.tcl
+++ library/axi_adcfifo/axi_adcfifo_ip.tcl
@@ -3,22 +3,22 @@
 source ../scripts/adi_env.tcl
 source $ad_hdl_dir/library/scripts/adi_ip.tcl
 
-adi_ip_create util_adcfifo_axi
-adi_ip_files util_adcfifo_axi [list \
+adi_ip_create axi_adcfifo
+adi_ip_files axi_adcfifo [list \
   "$ad_hdl_dir/library/common/ad_mem.v" \
   "$ad_hdl_dir/library/common/ad_mem_asym.v" \
   "$ad_hdl_dir/library/common/up_xfer_status.v" \
   "$ad_hdl_dir/library/common/ad_axis_inf_rx.v" \
-  "util_adcfifo_axi_adc.v" \
-  "util_adcfifo_axi_dma.v" \
-  "util_adcfifo_axi_wr.v" \
-  "util_adcfifo_axi_rd.v" \
-  "util_adcfifo_axi.v" \
-  "util_adcfifo_axi_constr.xdc" ]
+  "axi_adcfifo_adc.v" \
+  "axi_adcfifo_dma.v" \
+  "axi_adcfifo_wr.v" \
+  "axi_adcfifo_rd.v" \
+  "axi_adcfifo.v" \
+  "axi_adcfifo_constr.xdc" ]
 
-adi_ip_properties_lite util_adcfifo_axi
-adi_ip_constraints util_adcfifo_axi [list \
-  "util_adcfifo_axi_constr.xdc" ]
+adi_ip_properties_lite axi_adcfifo
+adi_ip_constraints axi_adcfifo [list \
+  "axi_adcfifo_constr.xdc" ]
 
 ipx::infer_bus_interfaces {{xilinx.com:interface:aximm:1.0}} [ipx::current_core]
 
