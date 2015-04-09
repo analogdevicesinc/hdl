8fc4b0630ec518190c527339b7b11e632afdfcd0
Lars-Peter Clausen <lars@metafoo.de>
2015-04-09 14:29:41 +0200

    util_axis_resize: Fix typo
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/util_axis_resize/util_axis_resize_ip.tcl
+++ library/util_axis_resize/util_axis_resize_ip.tcl
@@ -2,8 +2,8 @@
 source ../scripts/adi_env.tcl
 source $ad_hdl_dir/library/scripts/adi_ip.tcl
 
-adi_ip_create util_axis_reszie
-adi_ip_files util_axis_reszie [list "util_axis_resize.v"]
+adi_ip_create util_axis_resize
+adi_ip_files util_axis_resize [list "util_axis_resize.v"]
 
 adi_ip_properties_lite util_axis_resize
 
