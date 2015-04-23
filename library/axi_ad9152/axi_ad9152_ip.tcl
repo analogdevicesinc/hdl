09f05cf8e90baa140c2f3ed74adc7bb59ae70d29
Adrian Costina <adrian.costina@analog.com>
2015-04-23 10:21:52 +0300

    axi_ad9152: Added CDC and reset constraints

--- library/axi_ad9152/axi_ad9152_ip.tcl
+++ library/axi_ad9152/axi_ad9152_ip.tcl
@@ -20,9 +20,11 @@ adi_ip_files axi_ad9152 [list \
   "axi_ad9152_channel.v" \
   "axi_ad9152_core.v" \
   "axi_ad9152_if.v" \
+  "axi_ad9152_constr.v" \
   "axi_ad9152.v" ]
 
 adi_ip_properties axi_ad9152
+
 adi_ip_constraints axi_ad9152 [list \
   "axi_ad9152_constr.xdc" ]
 
