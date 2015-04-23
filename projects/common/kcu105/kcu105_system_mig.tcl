7611c2ae17f2796b462fd56164b5665e7392aad0
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-04-23 15:29:34 -0400

    kcu105: ddr mig rbc to rcb

--- projects/common/kcu105/kcu105_system_mig.tcl
+++ projects/common/kcu105/kcu105_system_mig.tcl
@@ -6,7 +6,7 @@ set_property -dict [list CONFIG.C0.DDR4_TimePeriod {833}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_InputClockPeriod {3332}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_MemoryPart {EDY4016AABG-DR-F}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_DataWidth {64}] $axi_ddr_cntrl
-set_property -dict [list CONFIG.C0.DDR4_Mem_Add_Map {ROW_BANK_COLUMN}] $axi_ddr_cntrl
+set_property -dict [list CONFIG.C0.DDR4_Mem_Add_Map {ROW_COLUMN_BANK}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_CasWriteLatency {12}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.Debug_Signal {Enable}] $axi_ddr_cntrl
 set_property -dict [list CONFIG.C0.DDR4_AxiDataWidth {512}] $axi_ddr_cntrl
