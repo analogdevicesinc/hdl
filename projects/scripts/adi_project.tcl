0dc4c9cda945048358d3df08d65dc19cdb82922e
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-05-01 12:21:59 -0400

    adi_project: added partial reconfiguration

--- projects/scripts/adi_project.tcl
+++ projects/scripts/adi_project.tcl
@@ -2,6 +2,9 @@
 variable p_board 
 variable p_device 
 variable sys_zynq
+variable p_prcfg_init
+variable p_prcfg_list
+variable p_prcfg_status
 
 if {![info exists REQUIRED_VIVADO_VERSION]} {
   set REQUIRED_VIVADO_VERSION "2014.4.1"
@@ -13,7 +16,7 @@ if {[info exists ::env(ADI_IGNORE_VERSION_CHECK)]} {
   set IGNORE_VERSION_CHECK 0
 }
 
-proc adi_project_create {project_name} {
+proc adi_project_create {project_name {mode 0}} {
 
   global ad_hdl_dir
   global ad_phdl_dir
@@ -27,11 +30,6 @@ proc adi_project_create {project_name} {
   set p_board "none"
   set sys_zynq 0
 
-  if [regexp "_ml605$" $project_name] {
-    set p_device "xc6vlx240tff1156-1"
-    set p_board "ml605"
-    set sys_zynq 0
-  }
   if [regexp "_ac701$" $project_name] {
     set p_device "xc7a200tfbg676-2"
     set p_board "xilinx.com:artix7:ac701:1.0"
@@ -78,33 +76,18 @@ proc adi_project_create {project_name} {
     set sys_zynq 1
   }
 
-  # planahead - 6 and down
-
-  if {$p_board eq "ml605"} {
-
-    set project_system_dir "./$project_name.srcs/sources_1/edk/$p_board"
-
-    create_project $project_name . -part $p_device  -force
-    set_property board $p_board [current_project]
-
-    import_files -norecurse $ad_hdl_dir/projects/common/ml605/system.xmp
-
-    generate_target {synthesis implementation} [get_files $project_system_dir/system.xmp]
-    make_wrapper -files [get_files $project_system_dir/system.xmp] -top
-    import_files -force -norecurse -fileset sources_1 $project_system_dir/system_stub.v
-
-    return
-  }
-
-  # vivado - 7 and up
-
   if {!$IGNORE_VERSION_CHECK && [string compare [version -short] $REQUIRED_VIVADO_VERSION] != 0} {
     return -code error [format "ERROR: This project requires Vivado %s." $REQUIRED_VIVADO_VERSION]
   }
 
-  set project_system_dir "./$project_name.srcs/sources_1/bd/system"
+  if {$mode == 0} {
+    set project_system_dir "./$project_name.srcs/sources_1/bd/system"
+    create_project $project_name . -part $p_device -force
+  } else {
+    set project_system_dir ".srcs/sources_1/bd/system"
+    create_project -in_memory -part $p_device
+  }
 
-  create_project $project_name . -part $p_device -force
   if {$p_board ne "not-applicable"} {
     set_property board $p_board [current_project]
   }
@@ -132,7 +115,12 @@ proc adi_project_create {project_name} {
 
   generate_target {synthesis implementation} [get_files  $project_system_dir/system.bd]
   make_wrapper -files [get_files $project_system_dir/system.bd] -top
-  import_files -force -norecurse -fileset sources_1 $project_system_dir/hdl/system_wrapper.v
+
+  if {$mode == 0} {
+    import_files -force -norecurse -fileset sources_1 $project_system_dir/hdl/system_wrapper.v
+  } else {
+    write_hwdef -file "$project_name.hwdef"
+  }
 }
 
 proc adi_project_files {project_name project_files} {
@@ -149,38 +137,6 @@ proc adi_project_run {project_name} {
   global ad_hdl_dir
   global ad_phdl_dir
   global p_board
-
-  # planahead - 6 and down
-
-  if {$p_board eq "ml605"} {
-
-    set project_system_dir "./$project_name.srcs/sources_1/edk/$p_board"
-
-    set_property strategy MapTiming [get_runs impl_1]
-    set_property strategy TimingWithIOBPacking [get_runs synth_1]
-
-    launch_runs synth_1
-    wait_on_run synth_1
-    open_run synth_1
-    report_timing -file timing_synth.log
-
-    launch_runs impl_1 -to_step bitgen
-    wait_on_run impl_1
-    open_run impl_1
-    report_timing -file timing_impl.log
-
-    # -- Unable to find an equivalent
-    #if [expr [get_property SLACK [get_timing_paths]] < 0] {
-    #  puts "ERROR: Timing Constraints NOT met."
-    #  use_this_invalid_command_to_crash
-    #}
-
-    export_hardware [get_files $project_system_dir/system.xmp] [get_runs impl_1] -bitstream
-
-    return
-  }
-
-  # vivado - 7 and up
 
   set project_system_dir "./$project_name.srcs/sources_1/bd/system"
 
@@ -200,18 +156,109 @@ proc adi_project_run {project_name} {
   open_run impl_1
   report_timing_summary -file timing_impl.log
 
-  #get_property STATS.THS [get_runs impl_1]
-  #get_property STATS.TNS [get_runs impl_1]
-  #get_property STATS.TPWS [get_runs impl_1]
-
-  #export_hardware [get_files $project_system_dir/system.bd] [get_runs impl_1] -bitstream
-
   file mkdir $project_name.sdk
   file copy -force $project_name.runs/impl_1/system_top.sysdef $project_name.sdk/system_top.hdf
 
   if [expr [get_property SLACK [get_timing_paths]] < 0] {
-    puts "ERROR: Timing Constraints NOT met."
-    use_this_invalid_command_to_crash
+    return -code error [format "ERROR: Timing Constraints NOT met!"]
+  }
+}
+
+proc adi_project_synth {project_name prcfg_name hdl_files {xdc_files ""}} {
+
+  global p_device
+
+  if {$prcfg_name eq ""} {
+
+    read_verilog .srcs/sources_1/bd/system/hdl/system_wrapper.v
+    read_verilog $hdl_files
+    read_xdc $xdc_files
+
+    synth_design -mode default -top system_top -part $p_device > $project_name.synth.rds
+    write_checkpoint -force $project_name.synth.dcp
+    close_project
+
+  } else {
+
+    create_project -in_memory -part $p_device
+    read_verilog $hdl_files
+    synth_design -mode out_of_context -top "prcfg" -part $p_device > $project_name.${prcfg_name}_synth.rds
+    write_checkpoint -force $project_name.${prcfg_name}_synth.dcp
+    close_project
+  }
+}
+
+proc adi_project_impl {project_name prcfg_name {xdc_files ""}} {
+
+  global p_device
+  global p_prcfg_init
+  global p_prcfg_list
+  global p_prcfg_status
+
+  if {$prcfg_name eq "default"} {
+    set p_prcfg_status 0
+    set p_prcfg_init "$project_name.${prcfg_name}_impl.dcp"
+    file mkdir $project_name.sdk
+  }
+
+  if {$prcfg_name eq "default"} {
+
+    open_checkpoint $project_name.synth.dcp -part $p_device
+    read_xdc $xdc_files
+    read_checkpoint -cell i_prcfg $project_name.${prcfg_name}_synth.dcp
+    set_property HD.RECONFIGURABLE 1 [get_cells i_prcfg]
+    opt_design > $project_name.${prcfg_name}_opt.rds
+    write_debug_probes -force $project_name.${prcfg_name}_debug_nets.ltx
+    place_design > $project_name.${prcfg_name}_place.rds
+    route_design > $project_name.${prcfg_name}_route.rds
+
+  } else {
+
+    open_checkpoint $project_name.default_impl_bb.dcp -part $p_device
+    lock_design -level routing
+    read_checkpoint -cell i_prcfg $project_name.${prcfg_name}_synth.dcp
+    opt_design > $project_name.${prcfg_name}_opt.rds
+    place_design > $project_name.${prcfg_name}_place.rds
+    route_design > $project_name.${prcfg_name}_route.rds
+  }
+
+  write_checkpoint -force $project_name.${prcfg_name}_impl.dcp
+  report_utilization -pblocks pb_prcfg -file $project_name.${prcfg_name}_utilization.rpt
+  report_timing_summary -file $project_name.${prcfg_name}_timing_summary.rpt
+
+  if [expr [get_property SLACK [get_timing_paths]] < 0] {
+    set p_prcfg_status 1
+  }
+
+  write_checkpoint -force -cell i_prcfg $project_name.${prcfg_name}_prcfg_impl.dcp
+  update_design -cell i_prcfg -black_box
+  write_checkpoint -force $project_name.${prcfg_name}_impl_bb.dcp
+  open_checkpoint $project_name.${prcfg_name}_impl.dcp -part $p_device
+  write_bitstream -force -bin_file -file $project_name.${prcfg_name}.bit
+  write_sysdef -hwdef $project_name.hwdef -bitfile $project_name.${prcfg_name}.bit -file $project_name.${prcfg_name}.hdf
+  file copy -force $project_name.${prcfg_name}.hdf $project_name.sdk/system_top.${prcfg_name}.hdf
+
+  if {$prcfg_name ne "default"} {
+    lappend p_prcfg_list "$project_name.${prcfg_name}_impl.dcp"
+  }
+
+  if {$prcfg_name eq "default"} {
+    file copy -force $project_name.${prcfg_name}.hdf $project_name.sdk/system_top.hdf
+  }
+}
+
+proc adi_project_verify {project_name} {
+
+  global p_prcfg_init
+  global p_prcfg_list
+  global p_prcfg_status
+
+  pr_verify -full_check -initial $p_prcfg_init \
+    -additional $p_prcfg_list \
+    -file $project_name.prcfg_verify.log
+
+  if {$p_prcfg_status == 1} {
+    return -code error [format "ERROR: Timing Constraints NOT met!"]
   }
 }
 
