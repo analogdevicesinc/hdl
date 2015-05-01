75a81d67d8689c997ea7c0ad0dac30eea13d20c7
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-05-01 12:23:07 -0400

    fmcomms2/zc706: add partial reconfiguration

--- projects/fmcomms2/zc706/system_bd.tcl
+++ projects/fmcomms2/zc706/system_bd.tcl
@@ -2,4 +2,5 @@
 source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
 source $ad_hdl_dir/projects/common/xilinx/sys_wfifo.tcl
 source ../common/fmcomms2_bd.tcl
+source ../common/prcfg_bd.tcl
 
