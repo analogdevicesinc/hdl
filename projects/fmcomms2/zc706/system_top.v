62acd37fee15dd5065467ca6d8c254388f744e44
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-05-04 10:25:07 -0400

    zc706: project mode

--- projects/fmcomms2/zc706/system_top.v
+++ projects/fmcomms2/zc706/system_top.v
@@ -343,32 +343,5 @@ module system_top (
     .up_adc_gpio_out (adc_gpio_input));
 endmodule
 
-// black box definition for pr module
-(* black_box *) module prcfg (
-
-  input                 clk,
-  input       [31:0]    adc_gpio_input,
-  output      [31:0]    adc_gpio_output,
-  input       [31:0]    dac_gpio_input,
-  output      [31:0]    dac_gpio_output,
-  output                dma_dac_en,
-  input                 dma_dac_dunf,
-  input       [63:0]    dma_dac_ddata,
-  input                 dma_dac_dvalid,
-  input                 core_dac_en,
-  output                core_dac_dunf,
-  output      [63:0]    core_dac_ddata,
-  output                core_dac_dvalid,
-  input                 core_adc_dwr,
-  input                 core_adc_dsync,
-  input       [63:0]    core_adc_ddata,
-  output                core_adc_ovf,
-  output                dma_adc_dwr,
-  output                dma_adc_dsync,
-  output      [63:0]    dma_adc_ddata,
-  input                 dma_adc_ovf);
-
-endmodule
-
 // ***************************************************************************
 // ***************************************************************************
