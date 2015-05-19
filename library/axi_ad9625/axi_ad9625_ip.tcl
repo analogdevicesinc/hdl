c8d3c04a05816437914f4642a6922b55ea95ec4f
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-05-19 12:18:37 -0400

    ad9625: delay changes

--- library/axi_ad9625/axi_ad9625_ip.tcl
+++ library/axi_ad9625/axi_ad9625_ip.tcl
@@ -14,7 +14,6 @@ adi_ip_files axi_ad9625 [list \
   "$ad_hdl_dir/library/common/up_xfer_status.v" \
   "$ad_hdl_dir/library/common/up_clock_mon.v" \
   "$ad_hdl_dir/library/common/up_drp_cntrl.v" \
-  "$ad_hdl_dir/library/common/up_delay_cntrl.v" \
   "$ad_hdl_dir/library/common/up_adc_common.v" \
   "$ad_hdl_dir/library/common/up_adc_channel.v" \
   "axi_ad9625_pnmon.v" \
