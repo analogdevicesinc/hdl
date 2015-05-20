d48d3f4aa335556bcfad7d75f98be34bb8a8dcb9
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-05-20 09:11:18 -0400

    scripts/ip-alt- added

---
+++ library/scripts/adi_ip_alt.tcl
@@ -0,0 +1,24 @@
+
+# keep interface-mess out of the way - keeping it pretty is a waste of time
+
+proc ad_alt_intf {type name dir width} {
+
+  if {(($type eq "clock") && ($dir eq "input"))} {
+    add_interface if_${name} clock sink
+    add_interface_port if_${name} ${name} clk ${dir} ${width}
+    return
+  }
+
+  if {(($type eq "clock") && ($dir eq "output"))} {
+    add_interface if_${name} clock source
+    add_interface_port if_${name} ${name} clk ${dir} ${width}
+    return
+  }
+
+  if {$type eq "signal"} {
+    add_interface if_${name} conduit end
+    add_interface_port if_${name} ${name} s_${name} ${dir} ${width}
+    return
+  }
+}
+
