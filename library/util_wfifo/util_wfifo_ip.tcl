cb0324c2b17e5009762803c8099befe504adf657
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-06-05 09:19:05 -0400

    wfifo: multi-channel option

--- library/util_wfifo/util_wfifo_ip.tcl
+++ library/util_wfifo/util_wfifo_ip.tcl
@@ -5,8 +5,7 @@ source $ad_hdl_dir/library/scripts/adi_ip.tcl
 
 adi_ip_create util_wfifo
 adi_ip_files util_wfifo [list \
-  "$ad_hdl_dir/library/common/ad_axis_inf_rx.v" \
-  "util_wfifo_constr.xdc" \
+  "$ad_hdl_dir/library/common/ad_mem.v" \
   "util_wfifo.v" ]
 
 adi_ip_properties_lite util_wfifo
@@ -15,6 +14,25 @@ adi_ip_constraints util_wfifo [list \
   "util_wfifo_constr.xdc" ]
 
 ipx::remove_all_bus_interface [ipx::current_core]
+set_property driver_value 0 [ipx::get_ports *din_enable* -of_objects [ipx::current_core]]
+set_property driver_value 0 [ipx::get_ports *din_valid* -of_objects [ipx::current_core]]
+set_property driver_value 0 [ipx::get_ports *din_data* -of_objects [ipx::current_core]]
+set_property driver_value 0 [ipx::get_ports *dout_ovf* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 1} \
+  [ipx::get_ports *_1* -of_objects [ipx::current_core]] 
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 2} \
+  [ipx::get_ports *_2* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 3} \
+  [ipx::get_ports *_3* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 4} \
+  [ipx::get_ports *_4* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 5} \
+  [ipx::get_ports *_5* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 6} \
+  [ipx::get_ports *_6* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 7} \
+  [ipx::get_ports *_7* -of_objects [ipx::current_core]]
+
 ipx::save_core [ipx::current_core]
 
 
