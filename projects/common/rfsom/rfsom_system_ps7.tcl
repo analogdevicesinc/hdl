a67ae238f82af0fd623550f0da0b24f77789bd0d
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-08-31 15:39:45 -0400

    rfsom-ps7- ddr settings

--- projects/common/rfsom/rfsom_system_ps7.tcl
+++ projects/common/rfsom/rfsom_system_ps7.tcl
@@ -24,20 +24,7 @@ set_property CONFIG.PCW_USB0_RESET_ENABLE                         {1}           
 set_property CONFIG.PCW_USB0_RESET_IO                             {MIO 7}                 [get_bd_cells sys_ps7]
 set_property CONFIG.PCW_QSPI_PERIPHERAL_ENABLE                    {1}                     [get_bd_cells sys_ps7]
 
-## DDR MT41K256M16 RE-125 (32M, 16bit, 8banks)
 ## DDR MT41K256M16 HA-125 (32M, 16bit, 8banks)
-## CONFIG.PCW_UIPARAM_DDR_CLOCK_0_PACKAGE_LENGTH {137} 
-## CONFIG.PCW_UIPARAM_DDR_CLOCK_1_PACKAGE_LENGTH {137} 
-## CONFIG.PCW_UIPARAM_DDR_CLOCK_2_PACKAGE_LENGTH {137} 
-## CONFIG.PCW_UIPARAM_DDR_CLOCK_3_PACKAGE_LENGTH {137}
-## CONFIG.PCW_UIPARAM_DDR_DQS_0_PACKAGE_LENGTH   {98.4} 
-## CONFIG.PCW_UIPARAM_DDR_DQS_1_PACKAGE_LENGTH   {120.5} 
-## CONFIG.PCW_UIPARAM_DDR_DQS_2_PACKAGE_LENGTH   {119.6} 
-## CONFIG.PCW_UIPARAM_DDR_DQS_3_PACKAGE_LENGTH   {141} 
-## CONFIG.PCW_UIPARAM_DDR_DQ_0_PACKAGE_LENGTH    {104.4} 
-## CONFIG.PCW_UIPARAM_DDR_DQ_1_PACKAGE_LENGTH    {120.5} 
-## CONFIG.PCW_UIPARAM_DDR_DQ_2_PACKAGE_LENGTH    {126.4} 
-## CONFIG.PCW_UIPARAM_DDR_DQ_3_PACKAGE_LENGTH    {144} 
 
 set_property CONFIG.PCW_UIPARAM_DDR_PARTNO                        {MT41K256M16 RE-125}    [get_bd_cells sys_ps7]
 set_property CONFIG.PCW_UIPARAM_DDR_BUS_WIDTH                     {32 Bit}                [get_bd_cells sys_ps7]
@@ -53,16 +40,4 @@ set_property CONFIG.PCW_UIPARAM_DDR_BOARD_DELAY0                  {0.264}       
 set_property CONFIG.PCW_UIPARAM_DDR_BOARD_DELAY1                  {0.265}                 [get_bd_cells sys_ps7]
 set_property CONFIG.PCW_UIPARAM_DDR_BOARD_DELAY2                  {0.330}                 [get_bd_cells sys_ps7]
 set_property CONFIG.PCW_UIPARAM_DDR_BOARD_DELAY3                  {0.330}                 [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_CLOCK_0_LENGTH_MM             {34}                    [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_CLOCK_1_LENGTH_MM             {34}                    [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_CLOCK_2_LENGTH_MM             {54}                    [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_CLOCK_3_LENGTH_MM             {54}                    [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_DQS_0_LENGTH_MM               {43.4}                  [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_DQS_1_LENGTH_MM               {43.8}                  [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_DQS_2_LENGTH_MM               {44.2}                  [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_DQS_3_LENGTH_MM               {43.5}                  [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_DQ_0_LENGTH_MM                {43.6}                  [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_DQ_1_LENGTH_MM                {43.75}                 [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_DQ_2_LENGTH_MM                {44.2}                  [get_bd_cells sys_ps7]
-set_property CONFIG.PCW_UIPARAM_DDR_DQ_3_LENGTH_MM                {43.5}                  [get_bd_cells sys_ps7]
 
