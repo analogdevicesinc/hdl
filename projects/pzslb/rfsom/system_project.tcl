49430dc2b07b5da6cc6c7dcc4de5667f2c56acbc
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-08-31 13:28:14 -0400

    pzslb- copy

--- projects/pzslb/rfsom/system_project.tcl
+++ projects/pzslb/rfsom/system_project.tcl
@@ -5,16 +5,18 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project_create pzsdr_rfsom
-adi_project_files pzsdr_rfsom [list \
+adi_project_create pzslb_rfsom
+adi_project_files pzslb_rfsom [list \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/pzsdr/rfsom/system_constr.xdc" \
   "$ad_hdl_dir/projects/common/rfsom/rfsom_system_constr.xdc" ]
 
 set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/rfsom/rfsom_system_constr.xdc]
+set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/pzsdr/rfsom/system_constr.xdc]
 set_property PROCESSING_ORDER EARLY [get_files system_constr.xdc]
 
-adi_project_run pzsdr_rfsom
+adi_project_run pzslb_rfsom
 
 
