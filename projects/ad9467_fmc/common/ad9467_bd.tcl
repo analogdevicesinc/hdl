1c603b830ecd6fbf9c80e32d3581fe60db6825f3
Lars-Peter Clausen <lars@metafoo.de>
2015-09-18 13:16:04 +0200

    ad9467_fmc: Drop explicit axi_dmac clock synchronicity configuration
    The axi_dmac core is now capable of detecting whether its different parts
    run in different clock domains or not. No need to configure it manually any
    more.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- projects/ad9467_fmc/common/ad9467_bd.tcl
+++ projects/ad9467_fmc/common/ad9467_bd.tcl
@@ -19,9 +19,6 @@ set_property -dict [list CONFIG.CYCLIC {0}] $axi_ad9467_dma
 set_property -dict [list CONFIG.SYNC_TRANSFER_START {0}] $axi_ad9467_dma
 set_property -dict [list CONFIG.AXI_SLICE_SRC {0}] $axi_ad9467_dma
 set_property -dict [list CONFIG.AXI_SLICE_DEST {0}] $axi_ad9467_dma
-set_property -dict [list CONFIG.ASYNC_CLK_DEST_REQ {1}] $axi_ad9467_dma
-set_property -dict [list CONFIG.ASYNC_CLK_SRC_DEST {1}] $axi_ad9467_dma
-set_property -dict [list CONFIG.ASYNC_CLK_REQ_SRC {1}] $axi_ad9467_dma
 set_property -dict [list CONFIG.DMA_2D_TRANSFER {0}] $axi_ad9467_dma
 set_property -dict [list CONFIG.DMA_DATA_WIDTH_SRC {16}] $axi_ad9467_dma
 set_property -dict [list CONFIG.DMA_DATA_WIDTH_DEST {64}] $axi_ad9467_dma
