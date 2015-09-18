6d87f537da422f3402e7a2de47e6881f63e6e69c
Lars-Peter Clausen <lars@metafoo.de>
2015-09-18 13:16:16 +0200

    cftl_cip: Drop explicit axi_dmac clock synchronicity configuration
    The axi_dmac core is now capable of detecting whether its different parts
    run in different clock domains or not. No need to configure it manually any
    more.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- projects/cftl_cip/common/cftl_cip_bd.tcl
+++ projects/cftl_cip/common/cftl_cip_bd.tcl
@@ -21,7 +21,6 @@ set_property -dict [list CONFIG.C_DMA_TYPE_DEST {0}] $pmod_spi_dma
 set_property -dict [list CONFIG.ID {0}] $pmod_spi_dma
 set_property -dict [list CONFIG.C_AXI_SLICE_SRC {0}] $pmod_spi_dma
 set_property -dict [list CONFIG.C_AXI_SLICE_DEST {0}] $pmod_spi_dma
-set_property -dict [list CONFIG.ASYNC_CLK_DEST_REQ {1}] $pmod_spi_dma
 set_property -dict [list CONFIG.C_SYNC_TRANSFER_START {0}] $pmod_spi_dma
 set_property -dict [list CONFIG.C_DMA_LENGTH_WIDTH {24}] $pmod_spi_dma
 set_property -dict [list CONFIG.C_DMA_2D_TRANSFER {0}] $pmod_spi_dma
