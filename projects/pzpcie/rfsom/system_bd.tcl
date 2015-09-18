9f8433159f1b79c5891be3e1dc1d87725e0a15b2
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-09-18 16:24:27 -0400

    pzsdr- name changes

--- projects/pzpcie/rfsom/system_bd.tcl
+++
@@ -1,8 +0,0 @@
-
-source $ad_hdl_dir/projects/common/rfsom/rfsom_system_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/sys_wfifo.tcl
-source $ad_hdl_dir/projects/fmcomms2/common/fmcomms2_bd.tcl
-source ../common/pzpcie_bd.tcl
-
-set_property -dict [list CONFIG.DAC_IODELAY_ENABLE {1}] $axi_ad9361
-
--- projects/pzslb/rfsom/system_bd.tcl
+++
@@ -1,8 +0,0 @@
-
-source $ad_hdl_dir/projects/common/rfsom/rfsom_system_bd.tcl -notrace
-source $ad_hdl_dir/projects/common/xilinx/sys_wfifo.tcl -notrace
-source $ad_hdl_dir/projects/fmcomms2/common/fmcomms2_bd.tcl -notrace
-source ../common/pzslb_bd.tcl -notrace
-
-set_property -dict [list CONFIG.DAC_IODELAY_ENABLE {1}] $axi_ad9361
-
