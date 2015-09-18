960c711f3b11e002bc8a71994f261c306f726e39
Lars-Peter Clausen <lars@metafoo.de>
2015-09-18 13:15:53 +0200

    ad9434_fmc: Drop explicit axi_dmac clock synchronicity configuration
    The axi_dmac core is now capable of detecting whether its different parts
    run in different clock domains or not. No need to configure it manually any
    more.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- projects/ad9434_fmc/common/ad9434_bd.tcl
+++ projects/ad9434_fmc/common/ad9434_bd.tcl
@@ -20,9 +20,6 @@ set_property -dict [list CONFIG.CYCLIC {0}] $axi_ad9434_dma
 set_property -dict [list CONFIG.SYNC_TRANSFER_START {0}] $axi_ad9434_dma
 set_property -dict [list CONFIG.AXI_SLICE_SRC {0}] $axi_ad9434_dma
 set_property -dict [list CONFIG.AXI_SLICE_DEST {0}] $axi_ad9434_dma
-set_property -dict [list CONFIG.ASYNC_CLK_DEST_REQ {1}] $axi_ad9434_dma
-set_property -dict [list CONFIG.ASYNC_CLK_SRC_DEST {1}] $axi_ad9434_dma
-set_property -dict [list CONFIG.ASYNC_CLK_REQ_SRC {1}] $axi_ad9434_dma
 set_property -dict [list CONFIG.DMA_2D_TRANSFER {0}] $axi_ad9434_dma
 set_property -dict [list CONFIG.DMA_DATA_WIDTH_SRC {64}]  $axi_ad9434_dma
 
