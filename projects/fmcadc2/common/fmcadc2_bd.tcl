60490c4e2b739f0fab53cfb57de894ac81e466dc
Lars-Peter Clausen <lars@metafoo.de>
2015-09-18 13:16:58 +0200

    fmcadc2: Drop explicit axi_dmac clock synchronicity configuration
    The axi_dmac core is now capable of detecting whether its different parts
    run in different clock domains or not. No need to configure it manually any
    more.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- projects/fmcadc2/common/fmcadc2_bd.tcl
+++ projects/fmcadc2/common/fmcadc2_bd.tcl
@@ -31,7 +31,6 @@ set_property -dict [list CONFIG.DMA_TYPE_DEST {0}] $axi_ad9625_dma
 set_property -dict [list CONFIG.ID {0}] $axi_ad9625_dma
 set_property -dict [list CONFIG.AXI_SLICE_SRC {0}] $axi_ad9625_dma
 set_property -dict [list CONFIG.AXI_SLICE_DEST {0}] $axi_ad9625_dma
-set_property -dict [list CONFIG.ASYNC_CLK_DEST_REQ {1}] $axi_ad9625_dma
 set_property -dict [list CONFIG.SYNC_TRANSFER_START {0}] $axi_ad9625_dma
 set_property -dict [list CONFIG.DMA_LENGTH_WIDTH {24}] $axi_ad9625_dma
 set_property -dict [list CONFIG.DMA_2D_TRANSFER {0}] $axi_ad9625_dma
