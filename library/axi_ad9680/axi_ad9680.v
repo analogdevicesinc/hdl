ebaebc54f7d9566ae228550e93ecb372646e3f17
Istvan Csomortani <istvan.csomortani@analog.com>
2015-09-24 11:14:01 +0300

    axi_ad9680: Clock ratio is indicating a sampling clock ratio
    The software can use this data to calculate the effective sampling clock, independent of the interface type.

--- library/axi_ad9680/axi_ad9680.v
+++ library/axi_ad9680/axi_ad9680.v
@@ -263,7 +263,7 @@ module axi_ad9680 (
     .adc_sync_status (1'd0),
     .adc_status_ovf (adc_dovf),
     .adc_status_unf (adc_dunf),
-    .adc_clk_ratio (32'd40),
+    .adc_clk_ratio (32'd4),
     .adc_start_code (),
     .adc_sync (),
     .up_status_pn_err (up_status_pn_err),
