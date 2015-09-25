046c89dacddef1ea619ed2d8971b1da154f834d5
Istvan Csomortani <istvan.csomortani@analog.com>
2015-09-25 19:10:06 +0300

    fmcomms2/pr: Delete the fmcomms2_pr directory
    The fmcomms2/pr project is moved to fmcomms2/zc706pr

--- projects/fmcomms2_pr/zc706/system_bd.tcl
+++
@@ -1,6 +0,0 @@
-
-source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/sys_wfifo.tcl
-source $ad_hdl_dir/projects/fmcomms2/common/fmcomms2_bd.tcl
-source $ad_hdl_dir/projects/fmcomms2_pr/common/prcfg_setup.tcl
-
