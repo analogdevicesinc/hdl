3b3c645827a04e9f6f2cbcb6168a496d09f1734f
Adrian Costina <adrian.costina@analog.com>
2015-09-28 11:31:08 +0300

    fmcomms6: Updated project to Vivado 2015.2.1. Added cpack

--- projects/fmcomms6/common/fmcomms6_bd.tcl
+++ projects/fmcomms6/common/fmcomms6_bd.tcl
@@ -8,18 +8,6 @@ create_bd_port -dir I adc_or_in_n
 create_bd_port -dir I -from 15 -to 0 adc_data_in_p
 create_bd_port -dir I -from 15 -to 0 adc_data_in_n
 
-# dma interface
-
-create_bd_port -dir O adc_clk
-create_bd_port -dir O adc_valid_0
-create_bd_port -dir O adc_enable_0
-create_bd_port -dir O -from 15 -to 0 adc_data_0
-create_bd_port -dir O adc_valid_1
-create_bd_port -dir O adc_enable_1
-create_bd_port -dir O -from 15 -to 0 adc_data_1
-create_bd_port -dir I adc_dwr
-create_bd_port -dir I -from 31 -to 0 adc_ddata
-
 # adc peripherals
 
 set axi_ad9652 [create_bd_cell -type ip -vlnv analog.com:user:axi_ad9652:1.0 axi_ad9652]
@@ -32,33 +20,56 @@ set_property -dict [list CONFIG.CYCLIC {0}] $axi_ad9652_dma
 set_property -dict [list CONFIG.DMA_DATA_WIDTH_DEST {64}] $axi_ad9652_dma
 set_property -dict [list CONFIG.FIFO_SIZE {8}] $axi_ad9652_dma
 
+set axi_ad9652_adc_fifo [create_bd_cell -type ip -vlnv analog.com:user:util_wfifo:1.0 axi_ad9652_adc_fifo]
+set_property -dict [list CONFIG.NUM_OF_CHANNELS {2}] $axi_ad9652_adc_fifo
+set_property -dict [list CONFIG.DIN_ADDRESS_WIDTH {4}] $axi_ad9652_adc_fifo
+set_property -dict [list CONFIG.DIN_DATA_WIDTH {16}] $axi_ad9652_adc_fifo
+set_property -dict [list CONFIG.DOUT_DATA_WIDTH {32}] $axi_ad9652_adc_fifo
+
+set data_pack [create_bd_cell -type ip -vlnv analog.com:user:util_cpack:1.0 data_pack]
+set_property -dict [list CONFIG.NUM_OF_CHANNELS {2}] $data_pack
+
 # connections (adc)
 
-p_sys_wfifo [current_bd_instance .] sys_wfifo 32 64
-
-ad_connect  adc_clk_in_p axi_ad9652/adc_clk_in_p
-ad_connect  adc_clk_in_n axi_ad9652/adc_clk_in_n
-ad_connect  adc_or_in_p axi_ad9652/adc_or_in_p
-ad_connect  adc_or_in_n axi_ad9652/adc_or_in_n
+ad_connect  adc_clk_in_p  axi_ad9652/adc_clk_in_p
+ad_connect  adc_clk_in_n  axi_ad9652/adc_clk_in_n
+ad_connect  adc_or_in_p   axi_ad9652/adc_or_in_p
+ad_connect  adc_or_in_n   axi_ad9652/adc_or_in_n
 ad_connect  adc_data_in_p axi_ad9652/adc_data_in_p
 ad_connect  adc_data_in_n axi_ad9652/adc_data_in_n
-ad_connect  axi_ad9652/adc_clk adc_clk
-ad_connect  axi_ad9652/adc_clk sys_wfifo/adc_clk
-ad_connect  axi_ad9652/adc_dovf sys_wfifo/adc_wovf
-ad_connect  sys_200m_clk sys_wfifo/dma_clk
+
+ad_connect  axi_ad9652/adc_clk axi_ad9652_adc_fifo/din_clk
+ad_connect  axi_ad9652/adc_rst axi_ad9652_adc_fifo/din_rst
+
 ad_connect  sys_200m_clk axi_ad9652/delay_clk
 ad_connect  sys_200m_clk axi_ad9652_dma/fifo_wr_clk
-ad_connect  adc_valid_0 axi_ad9652/adc_valid_0
-ad_connect  adc_enable_0 axi_ad9652/adc_enable_0
-ad_connect  adc_data_0 axi_ad9652/adc_data_0
-ad_connect  adc_valid_1 axi_ad9652/adc_valid_1
-ad_connect  adc_enable_1 axi_ad9652/adc_enable_1
-ad_connect  adc_data_1 axi_ad9652/adc_data_1
-ad_connect  adc_dwr sys_wfifo/adc_wr
-ad_connect  adc_ddata sys_wfifo/adc_wdata
-ad_connect  sys_wfifo/dma_wr axi_ad9652_dma/fifo_wr_en
-ad_connect  sys_wfifo/dma_wdata axi_ad9652_dma/fifo_wr_din
-ad_connect  sys_wfifo/dma_wovf axi_ad9652_dma/fifo_wr_overflow
+
+ad_connect  sys_200m_clk   data_pack/adc_clk
+ad_connect  sys_cpu_resetn data_pack/adc_rst
+
+ad_connect  axi_ad9652/adc_enable_0 axi_ad9652_adc_fifo/din_enable_0
+ad_connect  axi_ad9652/adc_valid_0  axi_ad9652_adc_fifo/din_valid_0
+ad_connect  axi_ad9652/adc_data_0   axi_ad9652_adc_fifo/din_data_0
+ad_connect  axi_ad9652/adc_enable_1 axi_ad9652_adc_fifo/din_enable_1
+ad_connect  axi_ad9652/adc_valid_1  axi_ad9652_adc_fifo/din_valid_1
+ad_connect  axi_ad9652/adc_data_1   axi_ad9652_adc_fifo/din_data_1
+
+ad_connect  sys_200m_clk    axi_ad9652_adc_fifo/dout_clk
+ad_connect  sys_cpu_resetn  axi_ad9652_adc_fifo/dout_rstn
+
+ad_connect  axi_ad9652_adc_fifo/dout_valid_0   data_pack/adc_valid_0
+ad_connect  axi_ad9652_adc_fifo/dout_enable_0  data_pack/adc_enable_0
+ad_connect  axi_ad9652_adc_fifo/dout_data_0    data_pack/adc_data_0
+ad_connect  axi_ad9652_adc_fifo/dout_valid_1   data_pack/adc_valid_1
+ad_connect  axi_ad9652_adc_fifo/dout_enable_1  data_pack/adc_enable_1
+ad_connect  axi_ad9652_adc_fifo/dout_data_1    data_pack/adc_data_1
+
+ad_connect  axi_ad9652_adc_fifo/din_ovf    axi_ad9652/adc_dovf
+
+ad_connect  data_pack/adc_valid               axi_ad9652_dma/fifo_wr_en
+ad_connect  data_pack/adc_sync                axi_ad9652_dma/fifo_wr_sync
+ad_connect  data_pack/adc_data                axi_ad9652_dma/fifo_wr_din
+ad_connect  axi_ad9652_adc_fifo/dout_ovf      axi_ad9652_dma/fifo_wr_overflow
 
 # interconnect (cpu)
 
