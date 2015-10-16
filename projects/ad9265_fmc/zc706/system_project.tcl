e14e9294c563cb22ef9083690c7c15c4b67a986f
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-10-16 14:13:56 -0400

    project-names -- variables causes scripts to fail- too much parsing

--- projects/ad9265_fmc/zc706/system_project.tcl
+++ projects/ad9265_fmc/zc706/system_project.tcl
@@ -3,14 +3,13 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-set project_name ad9265_fmc_zc706
+adi_project_create ad9265_fmc_zc706
+adi_project_files ad9265_fmc_zc706 [list \
+  "../common/ad9265_spi.v" \
+  "system_top.v" \
+  "system_constr.xdc" \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc"]
 
-adi_project_create $project_name
+adi_project_run ad9265_fmc_zc706
 
-adi_project_files $project_name [list "../common/ad9265_spi.v" \
-                                      "system_top.v" \
-                                      "system_constr.xdc" \
-                                      "$ad_hdl_dir/library/common/ad_iobuf.v" \
-                                      "$ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc"]
-
-adi_project_run $project_name
--- projects/ad9467_fmc/zed/system_project.tcl
+++ projects/ad9467_fmc/zed/system_project.tcl
@@ -3,14 +3,13 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-set project_name ad9467_fmc_zed
+adi_project_create ad9467_fmc_zed
+adi_project_files ad9467_fmc_zed [list \
+  "../common/ad9467_spi.v" \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "system_top.v" \
+  "system_constr.xdc" \
+  "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"]
 
-adi_project_create $project_name
+adi_project_run ad9467_fmc_zed
 
-adi_project_files $project_name [list "../common/ad9467_spi.v" \
-                                      "$ad_hdl_dir/library/common/ad_iobuf.v" \
-                                      "system_top.v" \
-                                      "system_constr.xdc" \
-                                      "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"]
-
-adi_project_run $project_name
