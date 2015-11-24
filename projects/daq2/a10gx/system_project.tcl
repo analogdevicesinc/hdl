a81625e1faca21b191c0baadc7bd4ed84d92d34a
Adrian Costina <adrian.costina@analog.com>
2015-11-24 13:28:53 +0200

    daq2: Updated a10gx project

--- projects/daq2/a10gx/system_project.tcl
+++ projects/daq2/a10gx/system_project.tcl
@@ -4,12 +4,11 @@ load_package flow
 source ../../scripts/adi_env.tcl
 project_new daq2_a10gx -overwrite
 
-source $ad_hdl_dir/projects/common/a10gx/a10gx_system_assign.tcl
-file copy -force $ad_hdl_dir/projects/common/a10gx/a10gx_system_bd.qsys .
-file copy -force $ad_hdl_dir/projects/daq2/common/daq2_bd.qsys .
+source "../../common/a10gx/a10gx_system_assign.tcl"
+set_user_option -name USER_IP_SEARCH_PATHS "../common/;../../common/a10gx/;../../../library/**/*"
 set_global_assignment -name QSYS_FILE system_bd.qsys
 
-set_global_assignment -name VERILOG_FILE $ad_hdl_dir/library/common/ad_iobuf.v
+set_global_assignment -name VERILOG_FILE "../../../library/common/ad_iobuf.v"
 set_global_assignment -name VERILOG_FILE ../common/daq2_spi.v
 set_global_assignment -name VERILOG_FILE system_top.v
 
