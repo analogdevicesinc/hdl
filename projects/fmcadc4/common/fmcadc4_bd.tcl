ea045a3f9af54edb2173a40d978c915a871286f9
Rejeesh Kutty <rejeesh.kutty@analog.com>
2015-12-17 12:34:47 -0500

    fmcadc4: change qpll to receive

--- projects/fmcadc4/common/fmcadc4_bd.tcl
+++ projects/fmcadc4/common/fmcadc4_bd.tcl
@@ -46,6 +46,7 @@ set_property -dict [list CONFIG.TX_NUM_OF_LANES {0}] $axi_fmcadc4_gt
 set_property -dict [list CONFIG.RX_CLKBUF_ENABLE_0 {1}] $axi_fmcadc4_gt
 
 set util_fmcadc4_gt [create_bd_cell -type ip -vlnv analog.com:user:util_jesd_gt:1.0 util_fmcadc4_gt]
+set_property -dict [list CONFIG.QPLL_TX_OR_RX_N {0}] $util_fmcadc4_gt
 set_property -dict [list CONFIG.QPLL0_ENABLE {1}] $util_fmcadc4_gt
 set_property -dict [list CONFIG.QPLL1_ENABLE {1}] $util_fmcadc4_gt
 set_property -dict [list CONFIG.NUM_OF_LANES {8}] $util_fmcadc4_gt
