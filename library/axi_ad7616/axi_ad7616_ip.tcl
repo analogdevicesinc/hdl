0b55325db9422c71e2db4e71453dc121933cc928
Istvan Csomortani <istvan.csomortani@analog.com>
2015-12-21 19:39:14 +0200

    axi_ad7616: Fix IP packaging script

--- library/axi_ad7616/axi_ad7616_ip.tcl
+++ library/axi_ad7616/axi_ad7616_ip.tcl
@@ -5,7 +5,12 @@ source $ad_hdl_dir/library/scripts/adi_ip.tcl
 
 adi_ip_create axi_ad7616
 adi_ip_files axi_ad7616 [list \
-    "axi_ad7616.v" ]
+    "axi_ad7616.v" \
+    "axi_ad7616_control.v" \
+    "$ad_hdl_dir/library/common/ad_edge_detect.v" \
+    "$ad_hdl_dir/library/common/up_axi.v"]
+
+adi_ip_properties axi_ad7616
 
 adi_ip_add_core_dependencies { \
     analog.com:user:spi_engine_execution:1.0 \
@@ -13,4 +18,6 @@ adi_ip_add_core_dependencies { \
     analog.com:user:spi_engine_offload:1.0 \
     analog.com:user:spi_engine_interconnect:1.0 \
 }
+
+ipx::save_core [ipx::current_core]
 
