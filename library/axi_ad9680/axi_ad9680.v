c865dbf3539a4788d4dabe963e3f724daedfb005
István Csomortáni <Csomi@users.noreply.github.com>
2016-01-19 12:49:45 +0200

    axi_ad9680: Fix channel instantiation

--- library/axi_ad9680/axi_ad9680.v
+++ library/axi_ad9680/axi_ad9680.v
@@ -206,7 +206,7 @@ module axi_ad9680 (
 
   // channel
 
-  axi_ad9680_channel #(.Q_OR_I_N(0), .CHANNEL_ID(0)) i_channel_0 (
+  axi_ad9680_channel #(.CHANNEL_ID(0)) i_channel_0 (
     .adc_clk (adc_clk),
     .adc_rst (adc_rst),
     .adc_data (adc_data_a_s),
@@ -229,7 +229,7 @@ module axi_ad9680 (
 
   // channel
 
-  axi_ad9680_channel #(.Q_OR_I_N(1), .CHANNEL_ID(1)) i_channel_1 (
+  axi_ad9680_channel #(.CHANNEL_ID(1)) i_channel_1 (
     .adc_clk (adc_clk),
     .adc_rst (adc_rst),
     .adc_data (adc_data_b_s),
