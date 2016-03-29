255b0ebd40609996d33c552b0211aec2e8df9e3c
Istvan Csomortani <istvan.csomortani@analog.com>
2016-03-29 16:50:00 +0300

    util_dacfifo: Add dac_xfer_out control
    The dac_xfer_out control signal is asserted while the DAC reads back data. Should be connected to upack/dma_xfer_in.

--- projects/common/xilinx/sys_dmafifo.tcl
+++ projects/common/xilinx/sys_dmafifo.tcl
@@ -72,6 +72,7 @@ proc p_sys_dacfifo {p_name m_name data_width addr_width} {
   create_bd_pin -dir I  dac_clk
   create_bd_pin -dir I  dac_valid
   create_bd_pin -dir O  -from [expr ($data_width - 1)] -to 0 dac_data
+  create_bd_pin -dir O dac_xfer_out
 
   set util_dacfifo [create_bd_cell -type ip -vlnv analog.com:user:util_dacfifo:1.0 util_dacfifo]
   set_property -dict [list CONFIG.DATA_WIDTH $data_width] $util_dacfifo
@@ -87,6 +88,7 @@ proc p_sys_dacfifo {p_name m_name data_width addr_width} {
   ad_connect  dma_xfer_last util_dacfifo/dma_xfer_last
   ad_connect  dac_valid util_dacfifo/dac_valid
   ad_connect  dac_data util_dacfifo/dac_data
+  ad_connect  dac_xfer_out util_dacfifo/dac_xfer_out
   ad_connect  dac_fifo_bypass util_dacfifo/dac_fifo_bypass
 
   current_bd_instance $c_instance
