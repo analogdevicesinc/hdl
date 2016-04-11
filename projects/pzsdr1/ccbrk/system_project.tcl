a88ced81362dad9bf3e4cf764df979baa70649c2
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-04-11 16:18:29 -0400

    pzsdr1: lvds/cmos updates

--- projects/pzsdr1/ccbrk/system_project.tcl
+++ projects/pzsdr1/ccbrk/system_project.tcl
@@ -10,10 +10,8 @@ adi_project_files ccbrk_pzsdr1 [list \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "$ad_hdl_dir/projects/common/pzsdr1/pzsdr1_system_constr.xdc" ]
-
-set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/pzsdr1/pzsdr1_system_constr.xdc]
-set_property PROCESSING_ORDER EARLY [get_files system_constr.xdc]
+  "$ad_hdl_dir/projects/common/pzsdr1/pzsdr1_system_constr.xdc" \
+  "$ad_hdl_dir/projects/common/pzsdr1/pzsdr1_lvds_system_constr.xdc" ]
 
 adi_project_run ccbrk_pzsdr1
 
