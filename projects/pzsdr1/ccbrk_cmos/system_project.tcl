7e807d83b16cd58c1e78b78c52de1d8bc2546a22
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-04-11 15:58:29 -0400

    pzsdr1- cmos mode

---
+++ projects/pzsdr1/ccbrk_cmos/system_project.tcl
@@ -0,0 +1,18 @@
+
+
+
+source ../../scripts/adi_env.tcl 
+source $ad_hdl_dir/projects/scripts/adi_project.tcl 
+source $ad_hdl_dir/projects/scripts/adi_board.tcl 
+
+adi_project_create ccbrk_cmos_pzsdr1
+adi_project_files ccbrk_cmos_pzsdr1 [list \
+  "system_top.v" \
+  "../ccbrk/system_constr.xdc"\
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/pzsdr1/pzsdr1_system_constr.xdc" \
+  "$ad_hdl_dir/projects/common/pzsdr1/pzsdr1_cmos_system_constr.xdc" ]
+
+adi_project_run ccbrk_cmos_pzsdr1
+
+
