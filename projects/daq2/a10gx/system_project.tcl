ae1dd1d58e4bdd31ee6c5a7829ba3585bfadaeb2
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-06-07 12:25:56 -0400

    daq2/a10gx- qsys updates

--- projects/daq2/a10gx/system_project.tcl
+++ projects/daq2/a10gx/system_project.tcl
@@ -5,13 +5,11 @@ source ../../scripts/adi_env.tcl
 project_new daq2_a10gx -overwrite
 
 source "../../common/a10gx/a10gx_system_assign.tcl"
-set_user_option -name USER_IP_SEARCH_PATHS "../common/;../../common/a10gx/;../../../library/**/*"
-set_global_assignment -name IP_SEARCH_PATHS "../common/;../../common/a10gx;../../../library/**/*"
-set_global_assignment -name QSYS_FILE system_bd.qsys
 
 set_global_assignment -name VERILOG_FILE "../../../library/common/ad_iobuf.v"
 set_global_assignment -name VERILOG_FILE ../common/daq2_spi.v
 set_global_assignment -name VERILOG_FILE system_top.v
+set_global_assignment -name QSYS_FILE system_bd.qsys
 
 set_global_assignment -name SDC_FILE system_constr.sdc
 set_global_assignment -name TOP_LEVEL_ENTITY system_top
