d5d61ff51898a609084d726397db15bb517c845b
Shrutika Redkar <Shrutika.Redkar@analog.com>
2016-07-28 13:42:30 -0400

    hdl-vivado-2016.2- productivity decimated again!

--- library/util_bsplit/util_bsplit_ip.tcl
+++ library/util_bsplit/util_bsplit_ip.tcl
@@ -13,31 +13,31 @@ adi_ip_constraints util_bsplit [list \
   "util_bsplit_constr.xdc" ]
 
 set_property -dict {driver_value {0} enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 1}} \
-  [ipx::get_port split_data_1 [ipx::current_core]] \
+  [ipx::get_ports *_1* -of_objects [ipx::current_core]] \
 
 
 set_property -dict {driver_value {0} enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 2}} \
-  [ipx::get_port split_data_2 [ipx::current_core]] \
+  [ipx::get_ports *_2* -of_objects [ipx::current_core]] \
 
 
 set_property -dict {driver_value {0} enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 3}} \
-  [ipx::get_port split_data_3 [ipx::current_core]] \
+  [ipx::get_ports *_3* -of_objects [ipx::current_core]] \
 
 
 set_property -dict {driver_value {0} enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 4}} \
-  [ipx::get_port split_data_4 [ipx::current_core]] \
+  [ipx::get_ports *_4* -of_objects [ipx::current_core]] \
 
 
 set_property -dict {driver_value {0} enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 5}} \
-  [ipx::get_port split_data_5 [ipx::current_core]] \
+  [ipx::get_ports *_5* -of_objects [ipx::current_core]] \
 
 
 set_property -dict {driver_value {0} enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 6}} \
-  [ipx::get_port split_data_6 [ipx::current_core]] \
+  [ipx::get_ports *_6* -of_objects [ipx::current_core]] \
 
 
 set_property -dict {driver_value {0} enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 7}} \
-  [ipx::get_port split_data_7 [ipx::current_core]] \
+  [ipx::get_ports *_7* -of_objects [ipx::current_core]] \
 
 
 ipx::save_core [ipx::current_core]
