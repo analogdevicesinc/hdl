6ffe59728b6e4312d1160e7ac47e3c655f3f8d57
Shrutika Redkar <Shrutika.Redkar@analog.com>
2016-07-28 13:39:07 -0400

    hdl-vivado-2016.2- update

--- projects/scripts/adi_project.tcl
+++ projects/scripts/adi_project.tcl
@@ -7,7 +7,7 @@ variable p_prcfg_list
 variable p_prcfg_status
 
 if {![info exists REQUIRED_VIVADO_VERSION]} {
-  set REQUIRED_VIVADO_VERSION "2015.4.2"
+  set REQUIRED_VIVADO_VERSION "2016.2"
 }
 
 if {[info exists ::env(ADI_IGNORE_VERSION_CHECK)]} {
