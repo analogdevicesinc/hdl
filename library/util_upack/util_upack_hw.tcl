ccf1c56b332685b7fb72845da263f61f00b8755b
Istvan Csomortani <istvan.csomortani@analog.com>
2016-08-08 16:39:56 +0300

    util_upack: Patch up the description of Altera IP

--- library/util_upack/util_upack_hw.tcl
+++ library/util_upack/util_upack_hw.tcl
@@ -6,7 +6,7 @@ source ../scripts/adi_ip_alt.tcl
 
 
 set_module_property NAME util_upack
-set_module_property DESCRIPTION "Channel Pack Utility"
+set_module_property DESCRIPTION "Channel Unpack Utility"
 set_module_property VERSION 1.0
 set_module_property GROUP "Analog Devices"
 set_module_property DISPLAY_NAME util_upack
