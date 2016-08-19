85825177127fc0ec2ce2c6cdf74f8456901e5315
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-08-19 15:56:48 -0400

    sdrstk- updates

--- projects/sdrstk/system_project.tcl
+++ projects/sdrstk/system_project.tcl
@@ -1,23 +1,15 @@
 
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
+set p_device "xc7z010clg225-1"
+adi_project_create sdrstk
 
-source ../../scripts/adi_env.tcl 
-source $ad_hdl_dir/projects/scripts/adi_project.tcl 
-source $ad_hdl_dir/projects/scripts/adi_board.tcl 
-
-adi_project_create ccbrk_pzsdr
-adi_project_files ccbrk_pzsdr [list \
+adi_project_files sdrstk [list \
   "system_top.v" \
-  "system_constr.xdc"\
-  "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v" \
-  "$ad_hdl_dir/projects/common/pzsdr/pzsdr_system_constr.xdc" \
-  "$ad_hdl_dir/projects/common/pzsdr/pzsdr_bd_system_constr.xdc" \
-  "$ad_hdl_dir/projects/common/pzsdr/pzsdr_lvds_system_constr.xdc" ]
+  "system_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v"]
 
-set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/pzsdr/pzsdr_system_constr.xdc]
-set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/pzsdr/pzsdr_bd_system_constr.xdc]
-set_property PROCESSING_ORDER EARLY [get_files system_constr.xdc]
-
-adi_project_run ccbrk_pzsdr
-
+adi_project_run sdrstk
 
