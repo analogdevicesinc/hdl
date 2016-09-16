3ca9fe09195c2065f018ab938e7a1f98e057bf8b
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-09-16 14:05:53 -0400

    sdrstk- remove critical warnings from ps7

--- projects/sdrstk/system_project.tcl
+++ projects/sdrstk/system_project.tcl
@@ -11,6 +11,7 @@ adi_project_files sdrstk [list \
   "system_constr.xdc" \
   "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v"]
 
+set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
 adi_project_run sdrstk
 
 
