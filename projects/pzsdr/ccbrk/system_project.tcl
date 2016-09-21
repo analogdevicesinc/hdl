79f34c9de79def90800192c9d9c7de44cb4f1ad4
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-09-21 11:03:21 -0400

    ccbrk- test updates

--- projects/pzsdr/ccbrk/system_project.tcl
+++ projects/pzsdr/ccbrk/system_project.tcl
@@ -1,6 +1,4 @@
 
-
-
 source ../../scripts/adi_env.tcl 
 source $ad_hdl_dir/projects/scripts/adi_project.tcl 
 source $ad_hdl_dir/projects/scripts/adi_board.tcl 
@@ -14,10 +12,7 @@ adi_project_files ccbrk_pzsdr [list \
   "$ad_hdl_dir/projects/common/pzsdr/pzsdr_bd_system_constr.xdc" \
   "$ad_hdl_dir/projects/common/pzsdr/pzsdr_lvds_system_constr.xdc" ]
 
-set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/pzsdr/pzsdr_system_constr.xdc]
-set_property PROCESSING_ORDER EARLY [get_files $ad_hdl_dir/projects/common/pzsdr/pzsdr_bd_system_constr.xdc]
-set_property PROCESSING_ORDER EARLY [get_files system_constr.xdc]
-
+set_property is_enabled false [get_files  *axi_gpreg_constr.xdc]
 adi_project_run ccbrk_pzsdr
 
 
