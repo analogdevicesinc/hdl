273073a584e78a3b0024d55bf33cb5cf460e18da
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-10-10 11:12:34 -0400

    daq2/kcu105- xcvr procedure

--- projects/daq2/kcu105/system_bd.tcl
+++ projects/daq2/kcu105/system_bd.tcl
@@ -8,7 +8,7 @@ p_sys_dacfifo [current_bd_instance .] axi_ad9144_fifo 128 10
 
 source ../common/daq2_bd.tcl
 
-set_property -dict [list CONFIG.GTH_OR_GTX_N {1}] $util_daq2_xcvr
+set_property -dict [list CONFIG.XCVR_TYPE {1}] $util_daq2_xcvr
 set_property -dict [list CONFIG.QPLL_FBDIV {20}] $util_daq2_xcvr
 set_property -dict [list CONFIG.QPLL_REFCLK_DIV {1}] $util_daq2_xcvr
 
