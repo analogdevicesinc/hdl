6607aa707da321546e322a5e7a9d6e8b226a8b41
Adrian Costina <adrian.costina@analog.com>
2016-10-26 11:09:43 +0300

    pzsdr1: Renamed projects to have lvds/cmos sufix

--- projects/pzsdr1/ccbrk_lvds/system_project.tcl
+++ projects/pzsdr1/ccbrk_lvds/system_project.tcl
@@ -1,10 +1,10 @@
 
-source ../../scripts/adi_env.tcl 
-source $ad_hdl_dir/projects/scripts/adi_project.tcl 
-source $ad_hdl_dir/projects/scripts/adi_board.tcl 
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project_create ccbrk_pzsdr1
-adi_project_files ccbrk_pzsdr1 [list \
+adi_project_create ccbrk_lvds_pzsdr1
+adi_project_files ccbrk_lvds_pzsdr1 [list \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v" \
@@ -12,6 +12,6 @@ adi_project_files ccbrk_pzsdr1 [list \
   "$ad_hdl_dir/projects/common/pzsdr1/pzsdr1_lvds_system_constr.xdc" ]
 
 set_property is_enabled false [get_files  *axi_gpreg_constr.xdc]
-adi_project_run ccbrk_pzsdr1
+adi_project_run ccbrk_lvds_pzsdr1
 
 
