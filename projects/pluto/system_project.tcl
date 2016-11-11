f64b44c8acb2e313e14a14cc418270094a23d3ba
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-11-11 13:52:57 -0500

    sdrstk2pluto- contents

--- projects/pluto/system_project.tcl
+++ projects/pluto/system_project.tcl
@@ -4,14 +4,14 @@ source $ad_hdl_dir/projects/scripts/adi_project.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 set p_device "xc7z010clg225-1"
-adi_project_create sdrstk
+adi_project_create pluto
 
-adi_project_files sdrstk [list \
+adi_project_files pluto [list \
   "system_top.v" \
   "system_constr.xdc" \
   "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v"]
 
 set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
-adi_project_run sdrstk
+adi_project_run pluto
 
 
