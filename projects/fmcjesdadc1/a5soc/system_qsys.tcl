e62fe0c086121143eb63772c07098895cc1acf74
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-11-11 10:34:09 -0500

    fmcjesdadc1- a5gt/a5soc- sysclk is different

--- projects/fmcjesdadc1/a5soc/system_qsys.tcl
+++ projects/fmcjesdadc1/a5soc/system_qsys.tcl
@@ -2,5 +2,6 @@
 source $ad_hdl_dir/projects/common/a5soc/a5soc_system_qsys.tcl
 source ../common/fmcjesdadc1_qsys.tcl
 
+set_instance_parameter_value avl_ad9250_xcvr {SYSCLK_FREQUENCY} {50.0}
 save_system "system_bd.qsys"
 
