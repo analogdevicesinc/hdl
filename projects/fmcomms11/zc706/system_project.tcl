11347c49beba0dac7c753aa06f9707090f8fbf79
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-11-16 13:43:07 -0500

    fmcomms11- device set to -3

--- projects/fmcomms11/zc706/system_project.tcl
+++ projects/fmcomms11/zc706/system_project.tcl
@@ -1,11 +1,11 @@
 
-
-
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 adi_project_create fmcomms11_zc706
+
+set_property part xc7z045ffg900-3 [current_project]
 adi_project_files fmcomms11_zc706 [list \
   "../common/fmcomms11_spi.v" \
   "system_top.v" \
@@ -15,5 +15,4 @@ adi_project_files fmcomms11_zc706 [list \
   "$ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc" ]
 
 adi_project_run fmcomms11_zc706
-
 
