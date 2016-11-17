d0166a4c7e0664d87d941e0b169f5f6068afbc03
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-11-17 10:24:11 -0500

    ccbox- updates

--- projects/pzsdr2/ccbox_lvds/system_top.v
+++ projects/pzsdr2/ccbox_lvds/system_top.v
@@ -65,7 +65,44 @@ module system_top (
   inout           iic_scl,
   inout           iic_sda,
 
-  inout   [19:0]  gpio_bd,
+  output          i2s_mclk,
+  output          i2s_bclk,
+  output          i2s_lrclk,
+  output          i2s_sdata_out,
+  input           i2s_sdata_in,
+
+  output          imu_csn,
+  output          imu_clk,
+  output          imu_mosi,
+  input           imu_miso,
+  input           imu_ready,
+  output          imu_rstn,
+  inout           imu_sync,
+
+  output          oled_csn,
+  output          oled_clk,
+  output          oled_mosi,
+  output          oled_rst,
+  output          oled_dc,
+
+  output          switch_led_r,
+  output          switch_led_g,
+  output          switch_led_b,
+
+  output          gps_reset,
+  output          gps_force_on,
+  output          gps_standby,
+  input           gps_pps,
+
+  input   [ 2:0]  pss_valid_n,
+  inout   [ 2:0]  adp5061_io,
+
+  inout           otg_ctrl,
+  inout           adp1614_en,
+  inout           ltc2955_kill_n,
+  inout           ltc2955_int_n,
+  inout           mic_present_n,
+  inout           ts3a227_int_n,
 
   input           rx_clk_in_p,
   input           rx_clk_in_n,
@@ -83,8 +120,13 @@ module system_top (
   output          enable,
   output          txnrx,
   input           clkout_in,
-  output          clkout_out,
 
+  inout           gpio_rf0,
+  inout           gpio_rf1,
+  inout           gpio_rf2,
+  inout           gpio_rf3,
+  inout           gpio_rf4,
+  inout           gpio_rfpwr_enable,
   inout           gpio_clksel,
   inout           gpio_resetb,
   inout           gpio_sync,
@@ -95,59 +137,99 @@ module system_top (
   output          spi_csn,
   output          spi_clk,
   output          spi_mosi,
-  input           spi_miso,
-
-  output  [85:0]  gp_out,
-  input   [85:0]  gp_in,
-
-  input           gt_ref_clk_p,
-  input           gt_ref_clk_n,
-  output  [ 3:0]  gt_tx_p,
-  output  [ 3:0]  gt_tx_n,
-  input   [ 3:0]  gt_rx_p,
-  input   [ 3:0]  gt_rx_n);
+  input           spi_miso);
 
   // internal signals
 
-  wire            gt_ref_clk;
-  wire    [95:0]  gp_out_s;
-  wire    [95:0]  gp_in_s;
   wire    [63:0]  gpio_i;
   wire    [63:0]  gpio_o;
   wire    [63:0]  gpio_t;
 
   // assignments
 
-  assign clkout_out = clkout_in;
-  assign gp_out[85:0] = gp_out_s[85:0];
-  assign gp_in_s[95:86] = gp_out_s[95:86];
-  assign gp_in_s[85: 0] = gp_in[85:0];
+  assign oled_clk = spi_clk;
+  assign oled_mosi = spi_mosi;
 
-  // instantiations
+  // gpio[31:20] controls misc stuff (keep as io)
 
-  IBUFDS_GTE2 i_ibufds_gt_ref_clk (
-    .CEB (1'd0),
-    .I (gt_ref_clk_p),
-    .IB (gt_ref_clk_n),
-    .O (gt_ref_clk),
-    .ODIV2 ());
+  assign gpio_i[31:26] = gpio_o[31:26];
 
-  ad_iobuf #(.DATA_WIDTH(16)) i_iobuf (
-    .dio_t ({gpio_t[51], gpio_t[46:32]}),
-    .dio_i ({gpio_o[51], gpio_o[46:32]}),
-    .dio_o ({gpio_i[51], gpio_i[46:32]}),
-    .dio_p ({ gpio_clksel,        // 51:51
+  ad_iobuf #(.DATA_WIDTH(6)) i_iobuf_misc (
+    .dio_t (gpio_t[25:20]),
+    .dio_i (gpio_o[25:20]),
+    .dio_o (gpio_i[25:20]),
+    .dio_p ({ adp1614_en,
+              otg_ctrl,
+              ltc2955_kill_n,
+              ltc2955_int_n,
+              ts3a227_int_n,
+              mic_present_n}));
+
+  // gpio[19:16] controls adp5061 (keep as io)
+
+  assign gpio_i[19] = gpio_o[19];
+
+  ad_iobuf #(.DATA_WIDTH(3)) i_iobuf_adp5061 (
+    .dio_t (gpio_t[18:16]),
+    .dio_i (gpio_o[18:16]),
+    .dio_o (gpio_i[18:16]),
+    .dio_p (adp5061_io));
+
+  // gpio[15:12] reads power source select valids
+
+  assign gpio_i[15:12] = {gpio_o[15], pss_valid_n};
+
+  // gpio[11:8] controls the imu/oled reset & such.
+
+  assign oled_dc = gpio_o[11];
+  assign oled_rst = gpio_o[10];
+  assign imu_rstn = gpio_o[9];
+  assign gpio_i[11:9] = gpio_o[11:9];
+
+  ad_iobuf #(.DATA_WIDTH(1)) i_iobuf_imu_sync (
+    .dio_t (gpio_t[8]),
+    .dio_i (gpio_o[8]),
+    .dio_o (gpio_i[8]),
+    .dio_p (imu_sync));
+
+  // gpio[7:4] controls the gps
+
+  assign gps_reset = gpio_o[6];
+  assign gps_force_on = gpio_o[5];
+  assign gps_standby = gpio_o[4];
+  assign gpio_i[7:4] = {gps_pps, gpio_o[6:4]};
+
+  // gpio[3:0] controls the power switch led colors
+
+  assign switch_led_r = gpio_o[2];
+  assign switch_led_g = gpio_o[1];
+  assign switch_led_b = gpio_o[0];
+  assign gpio_i[3:0] = gpio_o[3:0];
+
+  // ad9361 gpio - 63-32
+
+  assign gpio_i[63:62] = gpio_o[63:62];
+  assign gpio_i[60:57] = gpio_o[60:57];
+  assign gpio_i[50:47] = gpio_o[50:47];
+
+  ad_iobuf #(.DATA_WIDTH(22)) i_iobuf (
+    .dio_t ({gpio_t[61:61], gpio_t[56:51], gpio_t[46:32]}),
+    .dio_i ({gpio_o[61:61], gpio_o[56:51], gpio_o[46:32]}),
+    .dio_o ({gpio_i[61:61], gpio_i[56:51], gpio_i[46:32]}),
+    .dio_p ({ gpio_rf4,           // 61:61
+              gpio_rf0,           // 56:56
+              gpio_rf1,           // 55:55
+              gpio_rf2,           // 54:54
+              gpio_rf3,           // 53:53
+              gpio_rfpwr_enable,  // 52:52
+              gpio_clksel,        // 51:51
               gpio_resetb,        // 46:46
               gpio_sync,          // 45:45
               gpio_en_agc,        // 44:44
               gpio_ctl,           // 43:40
               gpio_status}));     // 39:32
 
-  ad_iobuf #(.DATA_WIDTH(20)) i_iobuf_bd (
-    .dio_t (gpio_t[19:0]),
-    .dio_i (gpio_o[19:0]),
-    .dio_o (gpio_i[19:0]),
-    .dio_p (gpio_bd));
+  // instantiations
 
   system_wrapper i_system_wrapper (
     .ddr_addr (ddr_addr),
@@ -172,26 +254,18 @@ module system_top (
     .fixed_io_ps_clk (fixed_io_ps_clk),
     .fixed_io_ps_porb (fixed_io_ps_porb),
     .fixed_io_ps_srstb (fixed_io_ps_srstb),
-    .gp_in_0 (gp_in_s[31:0]),
-    .gp_in_1 (gp_in_s[63:32]),
-    .gp_in_2 (gp_in_s[95:64]),
-    .gp_in_3 (32'd0),
-    .gp_out_0 (gp_out_s[31:0]),
-    .gp_out_1 (gp_out_s[63:32]),
-    .gp_out_2 (gp_out_s[95:64]),
-    .gp_out_3 (),
     .gpio_i (gpio_i),
     .gpio_o (gpio_o),
     .gpio_t (gpio_t),
-    .gt_ref_clk (gt_ref_clk),
-    .gt_rx_n (gt_rx_n),
-    .gt_rx_p (gt_rx_p),
-    .gt_tx_n (gt_tx_n),
-    .gt_tx_p (gt_tx_p),
+    .i2s_bclk (i2s_bclk),
+    .i2s_lrclk (i2s_lrclk),
+    .i2s_mclk (i2s_mclk),
+    .i2s_sdata_in (i2s_sdata_in),
+    .i2s_sdata_out (i2s_sdata_out),
     .iic_main_scl_io (iic_scl),
     .iic_main_sda_io (iic_sda),
     .otg_vbusoc (1'b0),
-    .ps_intr_00 (1'b0),
+    .ps_intr_00 (imu_ready),
     .ps_intr_01 (1'b0),
     .ps_intr_02 (1'b0),
     .ps_intr_03 (1'b0),
@@ -213,22 +287,22 @@ module system_top (
     .spi0_clk_i (1'b0),
     .spi0_clk_o (spi_clk),
     .spi0_csn_0_o (spi_csn),
-    .spi0_csn_1_o (),
+    .spi0_csn_1_o (oled_csn),
     .spi0_csn_2_o (),
     .spi0_csn_i (1'b1),
     .spi0_sdi_i (spi_miso),
     .spi0_sdo_i (1'b0),
     .spi0_sdo_o (spi_mosi),
     .spi1_clk_i (1'b0),
-    .spi1_clk_o (),
-    .spi1_csn_0_o (),
+    .spi1_clk_o (imu_clk),
+    .spi1_csn_0_o (imu_csn),
     .spi1_csn_1_o (),
     .spi1_csn_2_o (),
     .spi1_csn_i (1'b1),
-    .spi1_sdi_i (1'b0),
+    .spi1_sdi_i (imu_miso),
     .spi1_sdo_i (1'b0),
-    .spi1_sdo_o (),
-    .tdd_sync_i (1'b0),
+    .spi1_sdo_o (imu_mosi),
+    .tdd_sync_i (gps_pps),
     .tdd_sync_o (),
     .tdd_sync_t (),
     .tx_clk_out_n (tx_clk_out_n),
