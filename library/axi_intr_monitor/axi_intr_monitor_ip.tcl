91ee4394e496b7787fd758cc6aa668df0320bcb6
Adrian Costina <adrian.costina@analog.com>
2016-11-24 15:19:36 +0200

    axi_intr_monitor: Initial commit

---
+++ library/axi_intr_monitor/axi_intr_monitor_ip.tcl
@@ -0,0 +1,17 @@
+# ip
+
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip.tcl
+
+adi_ip_create axi_intr_monitor
+adi_ip_files axi_intr_monitor [list \
+  "$ad_hdl_dir/library/common/up_axi.v" \
+  "axi_intr_monitor.v" ]
+
+adi_ip_properties axi_intr_monitor
+
+ipx::infer_bus_interface irq xilinx.com:signal:interrupt_rtl:1.0 [ipx::current_core]
+
+ipx::save_core [ipx::current_core]
+
+
