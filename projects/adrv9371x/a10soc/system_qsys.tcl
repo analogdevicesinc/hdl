4a783d523dd105cd3143ce7cace0dba07c446f71
Rejeesh Kutty <rejeesh.kutty@analog.com>
2016-12-20 16:27:44 -0500

    projects/altera* - default & common qsys commands

--- projects/adrv9371x/a10soc/system_qsys.tcl
+++ projects/adrv9371x/a10soc/system_qsys.tcl
@@ -3,5 +3,4 @@
 source $ad_hdl_dir/projects/common/a10soc/a10soc_system_qsys.tcl
 source ../common/adrv9371x_qsys.tcl
 
-save_system "system_bd.qsys"
 
--- projects/usdrx1/a5gt/system_qsys.tcl
+++ projects/usdrx1/a5gt/system_qsys.tcl
@@ -2,5 +2,4 @@
 source $ad_hdl_dir/projects/common/a5gt/a5gt_system_qsys.tcl
 source ../common/usdrx1_qsys.tcl
 
-save_system "system_bd.qsys"
 
