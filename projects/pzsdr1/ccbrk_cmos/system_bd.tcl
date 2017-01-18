ecd152c90d35c4299d25b3c403579bf8a6a13c8b
Adrian Costina <adrian.costina@analog.com>
2017-01-18 12:04:04 +0200

    pzsdr1: ccbrk_cmos, fix clkdiv parameters

--- projects/pzsdr1/ccbrk_cmos/system_bd.tcl
+++ projects/pzsdr1/ccbrk_cmos/system_bd.tcl
@@ -2,8 +2,8 @@
 source ../common/pzsdr1_bd.tcl
 source ../common/ccbrk_bd.tcl
 
-set_property -dict [list CONFIG.SEL_0_DIV {BYPASS}] $clkdiv
-set_property -dict [list CONFIG.SEL_1_DIV {BYPASS}] $clkdiv
+set_property -dict [list CONFIG.SEL_0_DIV {2}] $clkdiv
+set_property -dict [list CONFIG.SEL_1_DIV {1}] $clkdiv
 
 cfg_ad9361_interface CMOS
 
