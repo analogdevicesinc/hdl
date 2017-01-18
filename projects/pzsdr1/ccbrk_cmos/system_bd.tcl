165ba76d9d8eafebbe0ed0aaa9bcddb5797a7518
Adrian Costina <adrian.costina@analog.com>
2017-01-18 12:01:24 +0200

    pzsdr1: Added FIFOs for DAC and ADC paths so that they work at l_clk or l_clk/2

--- projects/pzsdr1/ccbrk_cmos/system_bd.tcl
+++ projects/pzsdr1/ccbrk_cmos/system_bd.tcl
@@ -2,5 +2,8 @@
 source ../common/pzsdr1_bd.tcl
 source ../common/ccbrk_bd.tcl
 
+set_property -dict [list CONFIG.SEL_0_DIV {BYPASS}] $clkdiv
+set_property -dict [list CONFIG.SEL_1_DIV {BYPASS}] $clkdiv
+
 cfg_ad9361_interface CMOS
 
