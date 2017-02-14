c6ee76421b257787b8d18496612e161117da3220
Adrian Costina <adrian.costina@analog.com>
2017-02-14 11:48:07 +0200

    axi_usb_fx3: Fixed clock domain association

--- library/axi_usb_fx3/axi_usb_fx3_ip.tcl
+++ library/axi_usb_fx3/axi_usb_fx3_ip.tcl
@@ -12,9 +12,10 @@ adi_ip_files axi_usb_fx3 [list \
   "axi_usb_fx3.v"]
 
 adi_ip_properties axi_usb_fx3
+
 adi_ip_infer_streaming_interfaces axi_usb_fx3
 
-adi_add_bus_clock "s_axi_aclk" "s_axis"
-adi_add_bus_clock "s_axi_aclk" "m_axis"
+ipx::associate_bus_interfaces -busif m_axis -clock s_axi_aclk [ipx::current_core]
+ipx::associate_bus_interfaces -busif s_axis -clock s_axi_aclk [ipx::current_core]
 
 ipx::save_core [ipx::current_core]
