07184b31d291b3388c36f7a9d7a275cf94c83a20
Istvan Csomortani <istvan.csomortani@analog.com>
2017-02-16 12:35:24 +0200

    fmcadc2: Define default clock selection for Xilinx GTs

--- projects/fmcadc2/vc707/system_bd.tcl
+++ projects/fmcadc2/vc707/system_bd.tcl
@@ -6,4 +6,7 @@ p_sys_adcfifo [current_bd_instance .] axi_ad9625_fifo 256 18
 
 source ../common/fmcadc2_bd.tcl
 
+set_property -dict [list CONFIG.LPM_OR_DFE_N {1}] [get_bd_cells axi_ad9625_xcvr]
+set_property -dict [list CONFIG.SYS_CLK_SEL {0}] [get_bd_cells axi_ad9625_xcvr]
+set_property -dict [list CONFIG.OUT_CLK_SEL {2}] [get_bd_cells axi_ad9625_xcvr]
 
