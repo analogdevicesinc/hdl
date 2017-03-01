ec89b1a45f19c528e4f8120b48474498ce96694e
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-03-01 15:52:07 -0500

    altera/adrv9371x- add dacfifo

--- projects/adrv9371x/a10gx/system_qsys.tcl
+++ projects/adrv9371x/a10gx/system_qsys.tcl
@@ -1,6 +1,11 @@
 
+set dac_fifo_name axi_ad9371_tx_fifo
+set dac_fifo_address_width 10
+set dac_data_width 128
+set dac_dma_data_width 128
 
 source $ad_hdl_dir/projects/common/a10gx/a10gx_system_qsys.tcl
+source $ad_hdl_dir/projects/common/altera/dacfifo_qsys.tcl
 source ../common/adrv9371x_qsys.tcl
 
 
