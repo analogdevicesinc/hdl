4a275302a07511a02c123b34b325511b0086ffa0
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-03-22 10:12:34 -0400

    a5soc- add ddr3 location assignments

--- projects/common/a5soc/a5soc_system_assign.tcl
+++ projects/common/a5soc/a5soc_system_assign.tcl
@@ -422,6 +422,91 @@ set_instance_assignment -name PACKAGE_SKEW_COMPENSATION OFF -to ddr3_ras_n
 set_instance_assignment -name PACKAGE_SKEW_COMPENSATION OFF -to ddr3_reset_n
 set_instance_assignment -name PACKAGE_SKEW_COMPENSATION OFF -to ddr3_we_n
 
+set_location_assignment PIN_A11       -to ddr3_ck_p
+set_location_assignment PIN_B10       -to ddr3_ck_n
+set_location_assignment PIN_N9        -to ddr3_a[0]
+set_location_assignment PIN_M9        -to ddr3_a[1]
+set_location_assignment PIN_N10       -to ddr3_a[2]
+set_location_assignment PIN_M10       -to ddr3_a[3]
+set_location_assignment PIN_A8        -to ddr3_a[4]
+set_location_assignment PIN_B7        -to ddr3_a[5]
+set_location_assignment PIN_B9        -to ddr3_a[6]
+set_location_assignment PIN_A9        -to ddr3_a[7]
+set_location_assignment PIN_D9        -to ddr3_a[8]
+set_location_assignment PIN_C10       -to ddr3_a[9]
+set_location_assignment PIN_K7        -to ddr3_a[10]
+set_location_assignment PIN_J7        -to ddr3_a[11]
+set_location_assignment PIN_F9        -to ddr3_a[12]
+set_location_assignment PIN_E9        -to ddr3_a[13]
+set_location_assignment PIN_D11       -to ddr3_a[14]
+set_location_assignment PIN_L7        -to ddr3_ba[0]
+set_location_assignment PIN_C9        -to ddr3_ba[1]
+set_location_assignment PIN_D8        -to ddr3_ba[2]
+set_location_assignment PIN_G9        -to ddr3_cas_n
+set_location_assignment PIN_R8        -to ddr3_cke
+set_location_assignment PIN_H9        -to ddr3_cs_n
+set_location_assignment PIN_H7        -to ddr3_odt
+set_location_assignment PIN_G8        -to ddr3_ras_n
+set_location_assignment PIN_E3        -to ddr3_reset_n
+set_location_assignment PIN_J8        -to ddr3_we_n
+
+set_location_assignment PIN_C6        -to ddr3_dm[0]
+set_location_assignment PIN_E4        -to ddr3_dm[1]
+set_location_assignment PIN_D3        -to ddr3_dm[2]
+set_location_assignment PIN_D1        -to ddr3_dm[3]
+set_location_assignment PIN_T7        -to ddr3_dm[4]
+set_location_assignment PIN_D7        -to ddr3_dq[0]
+set_location_assignment PIN_C7        -to ddr3_dq[1]
+set_location_assignment PIN_R10       -to ddr3_dq[2]
+set_location_assignment PIN_G7        -to ddr3_dq[3]
+set_location_assignment PIN_A6        -to ddr3_dq[4]
+set_location_assignment PIN_A7        -to ddr3_dq[5]
+set_location_assignment PIN_L6        -to ddr3_dq[6]
+set_location_assignment PIN_D6        -to ddr3_dq[7]
+set_location_assignment PIN_H6        -to ddr3_dq[8]
+set_location_assignment PIN_G6        -to ddr3_dq[9]
+set_location_assignment PIN_N8        -to ddr3_dq[10]
+set_location_assignment PIN_G5        -to ddr3_dq[11]
+set_location_assignment PIN_A4        -to ddr3_dq[12]
+set_location_assignment PIN_A5        -to ddr3_dq[13]
+set_location_assignment PIN_R9        -to ddr3_dq[14]
+set_location_assignment PIN_F4        -to ddr3_dq[15]
+set_location_assignment PIN_J5        -to ddr3_dq[16]
+set_location_assignment PIN_K5        -to ddr3_dq[17]
+set_location_assignment PIN_N7        -to ddr3_dq[18]
+set_location_assignment PIN_F3        -to ddr3_dq[19]
+set_location_assignment PIN_H3        -to ddr3_dq[20]
+set_location_assignment PIN_J4        -to ddr3_dq[21]
+set_location_assignment PIN_M5        -to ddr3_dq[22]
+set_location_assignment PIN_C3        -to ddr3_dq[23]
+set_location_assignment PIN_A2        -to ddr3_dq[24]
+set_location_assignment PIN_A3        -to ddr3_dq[25]
+set_location_assignment PIN_P7        -to ddr3_dq[26]
+set_location_assignment PIN_C1        -to ddr3_dq[27]
+set_location_assignment PIN_G2        -to ddr3_dq[28]
+set_location_assignment PIN_F2        -to ddr3_dq[29]
+set_location_assignment PIN_M3        -to ddr3_dq[30]
+set_location_assignment PIN_E1        -to ddr3_dq[31]
+set_location_assignment PIN_G1        -to ddr3_dq[32]
+set_location_assignment PIN_F1        -to ddr3_dq[33]
+set_location_assignment PIN_P6        -to ddr3_dq[34]
+set_location_assignment PIN_L1        -to ddr3_dq[35]
+set_location_assignment PIN_M2        -to ddr3_dq[36]
+set_location_assignment PIN_M1        -to ddr3_dq[37]
+set_location_assignment PIN_N1        -to ddr3_dq[38]
+set_location_assignment PIN_R6        -to ddr3_dq[39]
+set_location_assignment PIN_E7        -to ddr3_dqs_n[0]
+set_location_assignment PIN_E6        -to ddr3_dqs_n[1]
+set_location_assignment PIN_H4        -to ddr3_dqs_n[2]
+set_location_assignment PIN_D2        -to ddr3_dqs_n[3]
+set_location_assignment PIN_H1        -to ddr3_dqs_n[4]
+set_location_assignment PIN_F7        -to ddr3_dqs_p[0]
+set_location_assignment PIN_D5        -to ddr3_dqs_p[1]
+set_location_assignment PIN_G4        -to ddr3_dqs_p[2]
+set_location_assignment PIN_C2        -to ddr3_dqs_p[3]
+set_location_assignment PIN_J1        -to ddr3_dqs_p[4]
+set_location_assignment PIN_K9        -to ddr3_oct_rzqin
+
 set_instance_assignment -name ENABLE_BENEFICIAL_SKEW_OPTIMIZATION_FOR_NON_GLOBAL_CLOCKS ON -to i_system_bd|sys_hps|hps_io|border|hps_sdram_inst
 set_instance_assignment -name PLL_COMPENSATION_MODE DIRECT -to i_system_bd|sys_hps|hps_io|border|hps_sdram_inst|pll0|fbout
 
