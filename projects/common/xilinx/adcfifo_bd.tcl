253516546164dcecbcedde5bbb6faf8f33a9f5b6
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-04-06 13:04:19 -0400

    xilinx- ad-ip-instance & ad-ip-parameter

--- projects/common/xilinx/adcfifo_bd.tcl
+++ projects/common/xilinx/adcfifo_bd.tcl
@@ -1,11 +1,10 @@
-
 # sys bram (use only when dma is not capable of keeping up).
 # generic fifo interface - existence is oblivious to software.
 
-create_bd_cell -type ip -vlnv analog.com:user:util_adcfifo:1.0 $adc_fifo_name
-set_property CONFIG.ADC_DATA_WIDTH $adc_data_width [get_bd_cells $adc_fifo_name]
-set_property CONFIG.DMA_DATA_WIDTH $adc_dma_data_width [get_bd_cells $adc_fifo_name]
-set_property CONFIG.DMA_READY_ENABLE {1} [get_bd_cells $adc_fifo_name]
-set_property CONFIG.DMA_ADDRESS_WIDTH $adc_fifo_address_width [get_bd_cells $adc_fifo_name]
+ad_ip_instance util_adcfifo $adc_fifo_name
+ad_ip_parameter $adc_fifo_name CONFIG.ADC_DATA_WIDTH $adc_data_width
+ad_ip_parameter $adc_fifo_name CONFIG.DMA_DATA_WIDTH $adc_dma_data_width
+ad_ip_parameter $adc_fifo_name CONFIG.DMA_READY_ENABLE 1
+ad_ip_parameter $adc_fifo_name CONFIG.DMA_ADDRESS_WIDTH $adc_fifo_address_width
 
 
--- projects/common/xilinx/dacfifo_bd.tcl
+++ projects/common/xilinx/dacfifo_bd.tcl
@@ -1,4 +1,3 @@
-
 # sys bram (use only when dma is not capable of keeping up).
 # generic fifo interface - existence is oblivious to software.
 
@@ -6,7 +5,7 @@ if {$dac_data_width != $dac_dma_data_width} {
   return -code error [format "ERROR: util_dacfifo dac/dma widths must be the same!"]
 }
 
-create_bd_cell -type ip -vlnv analog.com:user:util_dacfifo:1.0 $dac_fifo_name
-set_property CONFIG.DATA_WIDTH $dac_data_width [get_bd_cells $dac_fifo_name]
-set_property CONFIG.ADDRESS_WIDTH $dac_fifo_address_width [get_bd_cells $dac_fifo_name]
+ad_ip_instance util_dacfifo $dac_fifo_name
+ad_ip_parameter $dac_fifo_name CONFIG.DATA_WIDTH $dac_data_width
+ad_ip_parameter $dac_fifo_name CONFIG.ADDRESS_WIDTH $dac_fifo_address_width
 
