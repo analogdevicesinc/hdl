189e9194c8d8e01eead56dde40d78fa5d62fa03b
Adrian Costina <adrian.costina@analog.com>
2017-04-06 11:32:58 +0300

    m2k: Standalone, enabled power optimization

--- projects/m2k/standalone/system_project.tcl
+++ projects/m2k/standalone/system_project.tcl
@@ -14,10 +14,7 @@ adi_project_files m2k [list \
 
 set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
 
-set_msg_config -suppress -id {Common 17-55} -string {{CRITICAL WARNING: [Common 17-55] 'set_property' expects at least one object. [c:/workspace/hdl/projects/m2k/standalone/system_constr.xdc:74]
-Resolution: If [get_<value>] was used to populate the object, check to make sure this command returns at least one valid object.} }
-set_msg_config -suppress -id {Common 17-55} -string {{CRITICAL WARNING: [Common 17-55] 'set_property' expects at least one object. [c:/workspace/hdl/projects/m2k/standalone/system_constr.xdc:75]
-Resolution: If [get_<value>] was used to populate the object, check to make sure this command returns at least one valid object.} }
+set ADI_POWER_OPTIMIZATION 1
 
 adi_project_run m2k
 
