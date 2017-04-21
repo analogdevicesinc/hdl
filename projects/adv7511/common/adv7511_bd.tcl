4a582bf9adaf07f0094d39023758f5bd8d161911
Lars-Peter Clausen <lars@metafoo.de>
2017-04-21 14:11:08 +0200

    adv7511: audio_clkgen: Disable phase alignment
    There is no need for the audio clock to be phase aligned to its source
    clock. When phase alignment is disabled the MMCM uses an internal feedback
    path without requiring external resources, so disable it.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- projects/adv7511/common/adv7511_bd.tcl
+++ projects/adv7511/common/adv7511_bd.tcl
@@ -35,6 +35,7 @@ ad_ip_instance clk_wiz sys_audio_clkgen
 ad_ip_parameter sys_audio_clkgen CONFIG.CLKOUT1_REQUESTED_OUT_FREQ 12.288
 ad_ip_parameter sys_audio_clkgen CONFIG.USE_LOCKED false
 ad_ip_parameter sys_audio_clkgen CONFIG.USE_RESET false
+ad_ip_parameter sys_audio_clkgen CONFIG.USE_PHASE_ALIGNMENT false
 
 ad_ip_instance axi_spdif_tx axi_spdif_tx_core
 ad_ip_parameter axi_spdif_tx_core CONFIG.DMA_TYPE 0
