4e77acf282d0174f26736ac9702cd080e2b4237a
Istvan Csomortani <istvan.csomortani@analog.com>
2017-04-24 11:48:33 +0300

    adv7511/kcu105: Update tcl command for IP configuration

--- projects/adv7511/kcu105/system_bd.tcl
+++ projects/adv7511/kcu105/system_bd.tcl
@@ -2,5 +2,5 @@
 source $ad_hdl_dir/projects/common/kcu105/kcu105_system_bd.tcl
 source ../common/adv7511_bd.tcl
 
-set_property -dict [list CONFIG.DEVICE_TYPE {1}] $axi_hdmi_core
+ad_ip_parameter axi_hdmi_core CONFIG.DEVICE_TYPE 1
 
