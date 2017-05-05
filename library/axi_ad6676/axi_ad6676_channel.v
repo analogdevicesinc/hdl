a03c7ad18433f89ddc331be737ca64d9c74edd11
Lars-Peter Clausen <lars@metafoo.de>
2017-05-05 19:23:49 +0200

    Remove unused Q_OR_I_N parameter from JESD204 ADC cores
    The cores that handle the JESD204 ADC cores do not feature IQ correction
    logic. The Q_OR_I_N parameter for the channel modules is unused, so remove
    it.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/axi_ad6676/axi_ad6676_channel.v
+++ library/axi_ad6676/axi_ad6676_channel.v
@@ -38,7 +38,6 @@
 
 module axi_ad6676_channel #(
 
-  parameter Q_OR_I_N = 0,
   parameter CHANNEL_ID = 0) (
 
   // adc interface
--- library/axi_ad9250/axi_ad9250.v
+++ library/axi_ad9250/axi_ad9250.v
@@ -169,7 +169,7 @@ module axi_ad9250 #(
 
   // channel
 
-  axi_ad9250_channel #(.Q_OR_I_N(0), .CHANNEL_ID(0)) i_channel_0 (
+  axi_ad9250_channel #(.CHANNEL_ID(0)) i_channel_0 (
     .adc_clk (adc_clk),
     .adc_rst (adc_rst),
     .adc_data (adc_data_a_s),
@@ -192,7 +192,7 @@ module axi_ad9250 #(
 
   // channel
 
-  axi_ad9250_channel #(.Q_OR_I_N(1), .CHANNEL_ID(1)) i_channel_1 (
+  axi_ad9250_channel #(.CHANNEL_ID(1)) i_channel_1 (
     .adc_clk (adc_clk),
     .adc_rst (adc_rst),
     .adc_data (adc_data_b_s),
