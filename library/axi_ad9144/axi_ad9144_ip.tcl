e86c464909e0994402a4fe1ec70ba8aeaab06cec
Lars-Peter Clausen <lars@metafoo.de>
2017-05-05 18:55:22 +0200

    axi_ad9144: Use the generic JESD204 DAC transmitter core
    Replace the axi_ad9144 implementation with the new generic JESD204
    interface DAC core. The replacement is functionally equivalent.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/axi_ad9144/axi_ad9144_ip.tcl
+++ library/axi_ad9144/axi_ad9144_ip.tcl
@@ -5,27 +5,13 @@ source $ad_hdl_dir/library/scripts/adi_ip.tcl
 
 adi_ip_create axi_ad9144
 adi_ip_files axi_ad9144 [list \
-  "$ad_hdl_dir/library/xilinx/common/up_xfer_cntrl_constr.xdc" \
-  "$ad_hdl_dir/library/xilinx/common/ad_rst_constr.xdc" \
-  "$ad_hdl_dir/library/xilinx/common/up_xfer_status_constr.xdc" \
-  "$ad_hdl_dir/library/xilinx/common/up_clock_mon_constr.xdc" \
-  "$ad_hdl_dir/library/xilinx/common/ad_mul.v" \
-  "$ad_hdl_dir/library/common/ad_dds_sine.v" \
-  "$ad_hdl_dir/library/common/ad_dds_1.v" \
-  "$ad_hdl_dir/library/common/ad_dds.v" \
-  "$ad_hdl_dir/library/common/ad_rst.v" \
-  "$ad_hdl_dir/library/common/up_axi.v" \
-  "$ad_hdl_dir/library/common/up_xfer_cntrl.v" \
-  "$ad_hdl_dir/library/common/up_xfer_status.v" \
-  "$ad_hdl_dir/library/common/up_clock_mon.v" \
-  "$ad_hdl_dir/library/common/up_dac_common.v" \
-  "$ad_hdl_dir/library/common/up_dac_channel.v" \
-  "axi_ad9144_channel.v" \
-  "axi_ad9144_core.v" \
-  "axi_ad9144_if.v" \
   "axi_ad9144.v" ]
 
 adi_ip_properties axi_ad9144
+
+adi_ip_add_core_dependencies { \
+  analog.com:user:ad_ip_jesd204_tpl_dac:1.0 \
+}
 
 set_property driver_value 0 [ipx::get_ports *dunf* -of_objects [ipx::current_core]]
 set_property driver_value 0 [ipx::get_ports *tx_ready* -of_objects [ipx::current_core]]
