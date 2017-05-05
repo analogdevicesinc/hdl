8a487816b9d2c6a8fdf8880aef9e710d0b0e3e88
Lars-Peter Clausen <lars@metafoo.de>
2017-05-05 19:31:10 +0200

    axi_ad6676: Use the generic JESD204 ADC receiver core
    Replace the axi_ad6676 implementation with the new generic JESD204
    interface ADC core. The replacement is functionally equivalent, except that
    the converter clock ratio is now correctly reported as 2 rather than 1 as
    before.
    
    Also the adc_rst output port is removed. It is not used in any design. The
    current guidelines for the reset for the JESD204 subsystem is to use an
    external reset generator.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/axi_ad6676/axi_ad6676_ip.tcl
+++ library/axi_ad6676/axi_ad6676_ip.tcl
@@ -5,32 +5,18 @@ source $ad_hdl_dir/library/scripts/adi_ip.tcl
 
 adi_ip_create axi_ad6676
 adi_ip_files axi_ad6676 [list \
-  "$ad_hdl_dir/library/common/ad_rst.v" \
-  "$ad_hdl_dir/library/common/ad_pnmon.v" \
-  "$ad_hdl_dir/library/common/up_axi.v" \
-  "$ad_hdl_dir/library/common/up_xfer_cntrl.v" \
-  "$ad_hdl_dir/library/common/up_xfer_status.v" \
-  "$ad_hdl_dir/library/common/up_clock_mon.v" \
-  "$ad_hdl_dir/library/common/up_adc_common.v" \
-  "$ad_hdl_dir/library/common/up_adc_channel.v" \
-  "$ad_hdl_dir/library/common/ad_xcvr_rx_if.v" \
-  "$ad_hdl_dir/library/xilinx/common/up_xfer_cntrl_constr.xdc" \
-  "$ad_hdl_dir/library/xilinx/common/ad_rst_constr.xdc" \
-  "$ad_hdl_dir/library/xilinx/common/up_xfer_status_constr.xdc" \
-  "$ad_hdl_dir/library/xilinx/common/up_clock_mon_constr.xdc" \
-  "axi_ad6676_pnmon.v" \
-  "axi_ad6676_channel.v" \
-  "axi_ad6676_if.v" \
   "axi_ad6676.v" ]
 
 adi_ip_properties axi_ad6676
+
+adi_ip_add_core_dependencies { \
+  analog.com:user:ad_ip_jesd204_tpl_adc:1.0 \
+}
 
 set_property driver_value 0 [ipx::get_ports *rx_valid* -of_objects [ipx::current_core]]
 set_property driver_value 0 [ipx::get_ports *dovf* -of_objects [ipx::current_core]]
 
 ipx::infer_bus_interface rx_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
 ipx::infer_bus_interface adc_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
-ipx::infer_bus_interface adc_rst xilinx.com:signal:reset_rtl:1.0 [ipx::current_core]
 
 ipx::save_core [ipx::current_core]
-
