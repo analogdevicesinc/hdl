6ea87d094e8c62a1a92e3251ecd9c801bf3213a1
Istvan Csomortani <istvan.csomortani@analog.com>
2017-05-12 11:44:04 +0300

    util_delay: Initial commit
    Generic module to introduce a fix N cycle delay into a datapath.

---
+++ library/util_delay/util_delay_ip.tcl
@@ -0,0 +1,16 @@
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip.tcl
+
+adi_ip_create util_delay
+adi_ip_files util_delay [list \
+	"$ad_hdl_dir/library/common/util_delay.v"]
+
+adi_ip_properties_lite util_delay
+ipx::remove_all_bus_interface [ipx::current_core]
+
+ipx::infer_bus_interface clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+ipx::infer_bus_interface reset xilinx.com:signal:reset_rtl:1.0 [ipx::current_core]
+
+set_property value_validation_range_minimum 1 [ipx::get_user_parameters DELAY_CYCLES -of_objects [ipx::current_core]]
+
+ipx::save_core [ipx::current_core]
