9055774795ff2dc16691e59c2d9986de45d4a476
Istvan Csomortani <istvan.csomortani@analog.com>
2017-05-17 11:44:52 +0300

    all: Update license for all hdl source files
    All the hdl (verilog and vhdl) source files were updated. If a file did not
    have any license, it was added into it. Files, which were generated by
    a tool (like Matlab) or were took over from other source (like opencores.org),
    were unchanged.
    
    New license looks as follows:
    
    Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
    
    Each core or library found in this collection may have its own licensing terms.
    The user should keep this in in mind while exploring these cores.
    
    Redistribution and use in source and binary forms,
    with or without modification of this file, are permitted under the terms of either
     (at the option of the user):
    
      1. The GNU General Public License version 2 as published by the
         Free Software Foundation, which can be found in the top level directory, or at:
    https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
    
    OR
    
      2.  An ADI specific BSD license as noted in the top level directory, or on-line at:
    https://github.com/analogdevicesinc/hdl/blob/dev/LICENSE

--- library/altera/avl_dacfifo/avl_dacfifo_rd.v
+++ library/altera/avl_dacfifo/avl_dacfifo_rd.v
@@ -1,37 +1,23 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright 2016(c) Analog Devices, Inc.
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
 //
-// All rights reserved.
+// Each core or library found in this collection may have its own licensing terms. 
+// The user should keep this in in mind while exploring these cores. 
 //
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//     - Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     - Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in
-//       the documentation and/or other materials provided with the
-//       distribution.
-//     - Neither the name of Analog Devices, Inc. nor the names of its
-//       contributors may be used to endorse or promote products derived
-//       from this software without specific prior written permission.
-//     - The use of this software may or may not infringe the patent rights
-//       of one or more patent holders.  This license does not release you
-//       from the requirement that you obtain separate licenses from these
-//       patent holders to use this software.
-//     - Use of the software either in source or binary form, must be run
-//       on or directly connected to an Analog Devices Inc. component.
+// Redistribution and use in source and binary forms,
+// with or without modification of this file, are permitted under the terms of either
+//  (at the option of the user):
 //
-// THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-// INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
-// PARTICULAR PURPOSE ARE DISCLAIMED.
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory, or at:
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 //
-// IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
-// RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// OR
+//
+//   2.  An ADI specific BSD license as noted in the top level directory, or on-line at:
+// https://github.com/analogdevicesinc/hdl/blob/dev/LICENSE
+//
 // ***************************************************************************
 // ***************************************************************************
 
