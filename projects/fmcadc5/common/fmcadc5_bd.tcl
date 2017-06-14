105b9e7114ef58baa44aa0c8949d8a49669ca5fc
AndreiGrozav <andrei.grozav@analog.com>
2017-06-14 11:33:11 +0300

    fmcadc5: Delete clock and reset duplicate connection

--- projects/fmcadc5/common/fmcadc5_bd.tcl
+++ projects/fmcadc5/common/fmcadc5_bd.tcl
@@ -95,13 +95,8 @@ ad_connect  sys_cpu_clk util_fmcadc5_1_xcvr/up_clk
 ad_xcvrcon  util_fmcadc5_0_xcvr axi_ad9625_0_xcvr axi_ad9625_0_jesd
 ad_xcvrcon  util_fmcadc5_1_xcvr axi_ad9625_1_xcvr axi_ad9625_1_jesd
 
-delete_bd_objs [get_bd_nets -of_objects [get_bd_pins util_fmcadc5_1_xcvr/rx_out_clk_0]]
-delete_bd_objs [get_bd_nets -of_objects [get_bd_pins axi_ad9625_1_jesd_rstgen/peripheral_reset]]
 delete_bd_objs [get_bd_cells axi_ad9625_1_jesd_rstgen]
 
-ad_xcvrpll  util_fmcadc5_0_xcvr/rx_out_clk_0 util_fmcadc5_1_xcvr/rx_clk_*
-ad_connect  util_fmcadc5_0_xcvr/rx_out_clk_0 axi_ad9625_1_jesd/rx_core_clk
-ad_connect  axi_ad9625_0_jesd_rstgen/peripheral_reset axi_ad9625_1_jesd/rx_reset
 ad_connect  util_fmcadc5_0_xcvr/rx_out_clk_0 axi_ad9625_0_core/rx_clk
 ad_connect  axi_ad9625_0_jesd/rx_start_of_frame axi_ad9625_0_core/rx_sof
 ad_connect  axi_ad9625_0_jesd/rx_tdata axi_ad9625_0_core/rx_data
