e33e6a84f476603af78555797525f481cf940b35
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-06-15 11:41:28 -0400

    a5gt/a5soc - removed

--- projects/fmcjesdadc1/a5soc/system_qsys.tcl
+++
@@ -1,5 +0,0 @@
-
-source $ad_hdl_dir/projects/common/a5soc/a5soc_system_qsys.tcl
-source ../common/fmcjesdadc1_qsys.tcl
-
-
--- projects/usdrx1/a5gt/system_qsys.tcl
+++
@@ -1,5 +0,0 @@
-
-source $ad_hdl_dir/projects/common/a5gt/a5gt_system_qsys.tcl
-source ../common/usdrx1_qsys.tcl
-
-
