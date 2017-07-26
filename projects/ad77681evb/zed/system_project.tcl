c15db8b74e542571c173ba744e7ed0eed95f9e76
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-07-26 10:18:26 -0400

    ad77681evb/zed: ad_lvds-ad_data replace

--- projects/ad77681evb/zed/system_project.tcl
+++ projects/ad77681evb/zed/system_project.tcl
@@ -7,7 +7,7 @@ adi_project_xilinx ad77681evb_zed
 
 adi_project_files ad77681evb_zed [list \
     "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v" \
-    "$ad_hdl_dir/library/xilinx/common/ad_cmos_clk.v" \
+    "$ad_hdl_dir/library/xilinx/common/ad_data_clk.v" \
     "system_top.v" \
     "system_constr.xdc" \
     "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"]
