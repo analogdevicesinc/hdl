c1e990b5755c7080436b13e06b74fe51710a3c92
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-07-31 14:48:25 -0400

    ad9361/sw- current sw requires clock edge swap

--- library/axi_ad9361/xilinx/axi_ad9361_cmos_if.v
+++ library/axi_ad9361/xilinx/axi_ad9361_cmos_if.v
@@ -274,7 +274,7 @@ module axi_ad9361_cmos_if #(
   // dac-tx interface
 
   always @(posedge clk) begin
-    tx_clk_p <= {dac_clksel, ~dac_clksel};
+    tx_clk_p <= {~dac_clksel, dac_clksel};
   end
 
   always @(posedge clk) begin
