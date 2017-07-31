207f00a7523eb48837c4a1414770f8d74e903020
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-07-31 09:12:05 -0400

    projects/ remove upack dma_xfer_in

--- projects/daq3/common/daq3_bd.tcl
+++ projects/daq3/common/daq3_bd.tcl
@@ -91,7 +91,6 @@ ad_connect  util_daq3_xcvr/tx_out_clk_0 axi_ad9152_fifo/dac_clk
 ad_connect  axi_ad9152_jesd_rstgen/peripheral_reset axi_ad9152_fifo/dac_rst
 ad_connect  axi_ad9152_upack/dac_valid axi_ad9152_fifo/dac_valid
 ad_connect  axi_ad9152_upack/dac_data axi_ad9152_fifo/dac_data
-ad_connect  axi_ad9152_upack/dma_xfer_in axi_ad9152_fifo/dac_xfer_out
 ad_connect  sys_cpu_clk axi_ad9152_fifo/dma_clk
 ad_connect  sys_cpu_reset axi_ad9152_fifo/dma_rst
 ad_connect  sys_cpu_clk axi_ad9152_dma/m_axis_aclk
