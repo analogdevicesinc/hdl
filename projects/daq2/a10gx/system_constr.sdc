17d3e3c64b13a62018bad5774f2b7b67e62dc635
Lars-Peter Clausen <lars@metafoo.de>
2017-08-17 13:15:40 +0200

    daq2: daq2_qsys.tcl: Convert to ADI JESD204
    Convert the DAQ2 project for Intel/Altera platforms to the ADI JESD204
    framework.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- projects/daq2/a10gx/system_constr.sdc
+++ projects/daq2/a10gx/system_constr.sdc
@@ -1,11 +1,9 @@
 
 create_clock -period "10.000 ns"  -name sys_clk_100mhz      [get_ports {sys_clk}]
+create_clock -period  "3.000 ns"  -name rx_ref_clk          [get_ports {rx_ref_clk}]
+create_clock -period  "3.000 ns"  -name tx_ref_clk          [get_ports {tx_ref_clk}]
 
 derive_pll_clocks
 derive_clock_uncertainty
 
 set_false_path -from [get_registers *altera_reset_synchronizer:alt_rst_sync_uq1|altera_reset_synchronizer_int_chain_out*]
-set_false_path -from [get_clocks {sys_clk_100mhz}] -through [get_nets *altera_jesd204*] -to [get_clocks *outclk0*]
-set_false_path -from [get_clocks *outclk0*] -through [get_nets *altera_jesd204*] -to [get_clocks {sys_clk_100mhz}]
-set_false_path -to [get_registers *altera_jesd204_rx_csr_inst|phy_csr_rx_pcfifo_full_latched*]
-
--- projects/daq2/common/daq2_qsys.tcl
+++ projects/daq2/common/daq2_qsys.tcl
@@ -1,57 +1,32 @@
 
 # ad9144-xcvr
 
-add_instance avl_ad9144_xcvr avl_adxcvr
-set_instance_parameter_value avl_ad9144_xcvr {ID} {0}
-set_instance_parameter_value avl_ad9144_xcvr {TX_OR_RX_N} {1}
-set_instance_parameter_value avl_ad9144_xcvr {PCS_CONFIG} {JESD_PCS_CFG2}
-set_instance_parameter_value avl_ad9144_xcvr {LANE_RATE} {10000.0}
-set_instance_parameter_value avl_ad9144_xcvr {REFCLK_FREQUENCY} {500.0}
-set_instance_parameter_value avl_ad9144_xcvr {NUM_OF_LANES} {4}
-set_instance_parameter_value avl_ad9144_xcvr {NUM_OF_CONVS} {2}
-set_instance_parameter_value avl_ad9144_xcvr {FRM_BCNT} {1}
-set_instance_parameter_value avl_ad9144_xcvr {FRM_SCNT} {1}
-set_instance_parameter_value avl_ad9144_xcvr {MF_FCNT} {32}
-set_instance_parameter_value avl_ad9144_xcvr {HD} {1}
-set_instance_parameter_value avl_ad9144_xcvr {TX_LANE_MAP} {0 3 1 2}
+add_instance ad9144_jesd204 adi_jesd204
+set_instance_parameter_value ad9144_jesd204 {ID} {0}
+set_instance_parameter_value ad9144_jesd204 {TX_OR_RX_N} {1}
+set_instance_parameter_value ad9144_jesd204 {LANE_RATE} {10000}
+set_instance_parameter_value ad9144_jesd204 {REFCLK_FREQUENCY} {333.333333}
+set_instance_parameter_value ad9144_jesd204 {LANE_MAP} {0 3 1 2}
+set_instance_parameter_value ad9144_jesd204 {SOFT_PCS} {true}
 
-add_connection sys_clk.clk avl_ad9144_xcvr.sys_clk
-add_connection sys_clk.clk_reset avl_ad9144_xcvr.sys_resetn
+add_connection sys_clk.clk ad9144_jesd204.sys_clk
+add_connection sys_clk.clk_reset ad9144_jesd204.sys_resetn
 add_interface tx_ref_clk clock sink
-set_interface_property tx_ref_clk EXPORT_OF avl_ad9144_xcvr.ref_clk
-add_interface tx_data_0 conduit end
-set_interface_property tx_data_0 EXPORT_OF avl_ad9144_xcvr.tx_data_0
-add_interface tx_data_1 conduit end
-set_interface_property tx_data_1 EXPORT_OF avl_ad9144_xcvr.tx_data_1
-add_interface tx_data_2 conduit end
-set_interface_property tx_data_2 EXPORT_OF avl_ad9144_xcvr.tx_data_2
-add_interface tx_data_3 conduit end
-set_interface_property tx_data_3 EXPORT_OF avl_ad9144_xcvr.tx_data_3
+set_interface_property tx_ref_clk EXPORT_OF ad9144_jesd204.ref_clk
+add_interface tx_serial_data conduit end
+set_interface_property tx_serial_data EXPORT_OF ad9144_jesd204.serial_data
 add_interface tx_sysref conduit end
-set_interface_property tx_sysref EXPORT_OF avl_ad9144_xcvr.sysref
+set_interface_property tx_sysref EXPORT_OF ad9144_jesd204.sysref
 add_interface tx_sync conduit end
-set_interface_property tx_sync EXPORT_OF avl_ad9144_xcvr.sync
-
-# ad9144-xcvr
-
-add_instance axi_ad9144_xcvr axi_adxcvr
-set_instance_parameter_value axi_ad9144_xcvr {ID} {0}
-set_instance_parameter_value axi_ad9144_xcvr {TX_OR_RX_N} {1}
-set_instance_parameter_value axi_ad9144_xcvr {NUM_OF_LANES} {4}
-
-add_connection sys_clk.clk axi_ad9144_xcvr.s_axi_clock
-add_connection sys_clk.clk_reset axi_ad9144_xcvr.s_axi_reset
-add_connection axi_ad9144_xcvr.if_up_rst avl_ad9144_xcvr.rst
-add_connection avl_ad9144_xcvr.ready axi_ad9144_xcvr.ready
-add_connection axi_ad9144_xcvr.core_pll_locked avl_ad9144_xcvr.core_pll_locked
+set_interface_property tx_sync EXPORT_OF ad9144_jesd204.sync
 
 # ad9144-core
 
 add_instance axi_ad9144_core axi_ad9144
 set_instance_parameter_value axi_ad9144_core {QUAD_OR_DUAL_N} {0}
 
-add_connection avl_ad9144_xcvr.core_clk axi_ad9144_core.if_tx_clk
-add_connection axi_ad9144_core.if_tx_data avl_ad9144_xcvr.ip_data
+add_connection ad9144_jesd204.link_clk axi_ad9144_core.if_tx_clk
+add_connection axi_ad9144_core.if_tx_data ad9144_jesd204.link_data
 add_connection sys_clk.clk_reset axi_ad9144_core.s_axi_reset
 add_connection sys_clk.clk axi_ad9144_core.s_axi_clock
 
@@ -61,7 +36,7 @@ add_instance util_ad9144_upack util_upack
 set_instance_parameter_value util_ad9144_upack {CHANNEL_DATA_WIDTH} {64}
 set_instance_parameter_value util_ad9144_upack {NUM_OF_CHANNELS} {2}
 
-add_connection avl_ad9144_xcvr.core_clk util_ad9144_upack.if_dac_clk
+add_connection ad9144_jesd204.link_clk util_ad9144_upack.if_dac_clk
 add_connection axi_ad9144_core.dac_ch_0 util_ad9144_upack.dac_ch_0
 add_connection axi_ad9144_core.dac_ch_1 util_ad9144_upack.dac_ch_1
 
@@ -74,7 +49,7 @@ set_instance_parameter_value axi_ad9144_dma {DMA_2D_TRANSFER} {0}
 set_instance_parameter_value axi_ad9144_dma {DMA_TYPE_DEST} {2}
 set_instance_parameter_value axi_ad9144_dma {DMA_TYPE_SRC} {0}
 
-add_connection avl_ad9144_xcvr.core_clk axi_ad9144_dma.if_fifo_rd_clk
+add_connection ad9144_jesd204.link_clk axi_ad9144_dma.if_fifo_rd_clk
 add_connection util_ad9144_upack.if_dac_valid axi_ad9144_dma.if_fifo_rd_en
 add_connection util_ad9144_upack.if_dac_data axi_ad9144_dma.if_fifo_rd_dout
 add_connection axi_ad9144_dma.if_fifo_rd_underflow axi_ad9144_core.if_dac_dunf
@@ -85,56 +60,32 @@ add_connection sys_dma_clk.clk axi_ad9144_dma.m_src_axi_clock
 
 # ad9680-xcvr
 
-add_instance avl_ad9680_xcvr avl_adxcvr
-set_instance_parameter_value avl_ad9680_xcvr {ID} {1}
-set_instance_parameter_value avl_ad9680_xcvr {TX_OR_RX_N} {0}
-set_instance_parameter_value avl_ad9680_xcvr {PCS_CONFIG} {JESD_PCS_CFG2}
-set_instance_parameter_value avl_ad9680_xcvr {LANE_RATE} {10000.0}
-set_instance_parameter_value avl_ad9680_xcvr {REFCLK_FREQUENCY} {500.0}
-set_instance_parameter_value avl_ad9680_xcvr {NUM_OF_LANES} {4}
-set_instance_parameter_value avl_ad9680_xcvr {NUM_OF_CONVS} {2}
-set_instance_parameter_value avl_ad9680_xcvr {FRM_BCNT} {1}
-set_instance_parameter_value avl_ad9680_xcvr {FRM_SCNT} {1}
-set_instance_parameter_value avl_ad9680_xcvr {MF_FCNT} {32}
-set_instance_parameter_value avl_ad9680_xcvr {HD} {1}
+add_instance ad9680_jesd204 adi_jesd204
+set_instance_parameter_value ad9680_jesd204 {ID} {1}
+set_instance_parameter_value ad9680_jesd204 {TX_OR_RX_N} {0}
+set_instance_parameter_value ad9680_jesd204 {LANE_RATE} {10000.0}
+set_instance_parameter_value ad9680_jesd204 {REFCLK_FREQUENCY} {333.333333}
+set_instance_parameter_value ad9680_jesd204 {NUM_OF_LANES} {4}
+set_instance_parameter_value ad9680_jesd204 {SOFT_PCS} {true}
 
-add_connection sys_clk.clk avl_ad9680_xcvr.sys_clk
-add_connection sys_clk.clk_reset avl_ad9680_xcvr.sys_resetn
+add_connection sys_clk.clk ad9680_jesd204.sys_clk
+add_connection sys_clk.clk_reset ad9680_jesd204.sys_resetn
 add_interface rx_ref_clk clock sink
-set_interface_property rx_ref_clk EXPORT_OF avl_ad9680_xcvr.ref_clk
-add_interface rx_data_0 conduit end
-set_interface_property rx_data_0 EXPORT_OF avl_ad9680_xcvr.rx_data_0
-add_interface rx_data_1 conduit end
-set_interface_property rx_data_1 EXPORT_OF avl_ad9680_xcvr.rx_data_1
-add_interface rx_data_2 conduit end
-set_interface_property rx_data_2 EXPORT_OF avl_ad9680_xcvr.rx_data_2
-add_interface rx_data_3 conduit end
-set_interface_property rx_data_3 EXPORT_OF avl_ad9680_xcvr.rx_data_3
+set_interface_property rx_ref_clk EXPORT_OF ad9680_jesd204.ref_clk
+add_interface rx_serial_data conduit end
+set_interface_property rx_serial_data EXPORT_OF ad9680_jesd204.serial_data
 add_interface rx_sysref conduit end
-set_interface_property rx_sysref EXPORT_OF avl_ad9680_xcvr.sysref
+set_interface_property rx_sysref EXPORT_OF ad9680_jesd204.sysref
 add_interface rx_sync conduit end
-set_interface_property rx_sync EXPORT_OF avl_ad9680_xcvr.sync
-
-# ad9680-xcvr
-
-add_instance axi_ad9680_xcvr axi_adxcvr
-set_instance_parameter_value axi_ad9680_xcvr {ID} {1}
-set_instance_parameter_value axi_ad9680_xcvr {TX_OR_RX_N} {0}
-set_instance_parameter_value axi_ad9680_xcvr {NUM_OF_LANES} {4}
-
-add_connection sys_clk.clk axi_ad9680_xcvr.s_axi_clock
-add_connection sys_clk.clk_reset axi_ad9680_xcvr.s_axi_reset
-add_connection axi_ad9680_xcvr.if_up_rst avl_ad9680_xcvr.rst
-add_connection avl_ad9680_xcvr.ready axi_ad9680_xcvr.ready
-add_connection axi_ad9680_xcvr.core_pll_locked avl_ad9680_xcvr.core_pll_locked
+set_interface_property rx_sync EXPORT_OF ad9680_jesd204.sync
 
 # ad9680
 
 add_instance axi_ad9680_core axi_ad9680
 
-add_connection avl_ad9680_xcvr.core_clk axi_ad9680_core.if_rx_clk
-add_connection avl_ad9680_xcvr.ip_sof axi_ad9680_core.if_rx_sof
-add_connection avl_ad9680_xcvr.ip_data axi_ad9680_core.if_rx_data
+add_connection ad9680_jesd204.link_clk axi_ad9680_core.if_rx_clk
+add_connection ad9680_jesd204.link_sof axi_ad9680_core.if_rx_sof
+add_connection ad9680_jesd204.link_data axi_ad9680_core.if_rx_data
 add_connection sys_clk.clk_reset axi_ad9680_core.s_axi_reset
 add_connection sys_clk.clk axi_ad9680_core.s_axi_clock
 
@@ -145,8 +96,7 @@ set_instance_parameter_value util_ad9680_cpack {CHANNEL_DATA_WIDTH} {64}
 set_instance_parameter_value util_ad9680_cpack {NUM_OF_CHANNELS} {2}
 
 add_connection sys_clk.clk_reset util_ad9680_cpack.if_adc_rst
-add_connection sys_dma_clk.clk_reset  util_ad9680_cpack.if_adc_rst
-add_connection avl_ad9680_xcvr.core_clk util_ad9680_cpack.if_adc_clk
+add_connection ad9680_jesd204.link_clk util_ad9680_cpack.if_adc_clk
 add_connection axi_ad9680_core.adc_ch_0 util_ad9680_cpack.adc_ch_0
 add_connection axi_ad9680_core.adc_ch_1 util_ad9680_cpack.adc_ch_1
 
@@ -158,7 +108,7 @@ set_instance_parameter_value ad9680_adcfifo {DMA_DATA_WIDTH} {128}
 set_instance_parameter_value ad9680_adcfifo {DMA_ADDRESS_WIDTH} {16}
 
 add_connection sys_clk.clk_reset ad9680_adcfifo.if_adc_rst
-add_connection avl_ad9680_xcvr.core_clk ad9680_adcfifo.if_adc_clk
+add_connection ad9680_jesd204.link_clk ad9680_adcfifo.if_adc_clk
 add_connection util_ad9680_cpack.if_adc_valid ad9680_adcfifo.if_adc_wr
 add_connection util_ad9680_cpack.if_adc_data ad9680_adcfifo.if_adc_wdata
 add_connection sys_dma_clk.clk ad9680_adcfifo.if_dma_clk
@@ -189,36 +139,20 @@ add_connection sys_dma_clk.clk axi_ad9680_dma.m_dest_axi_clock
 
 # reconfig sharing
 
-add_instance avl_adxcfg_0 avl_adxcfg
-add_connection sys_clk.clk avl_adxcfg_0.rcfg_clk
-add_connection sys_clk.clk_reset avl_adxcfg_0.rcfg_reset_n
-add_connection avl_adxcfg_0.rcfg_m0 avl_ad9144_xcvr.phy_reconfig_0
-add_connection avl_adxcfg_0.rcfg_m1 avl_ad9680_xcvr.phy_reconfig_0
-
-add_instance avl_adxcfg_1 avl_adxcfg
-add_connection sys_clk.clk avl_adxcfg_1.rcfg_clk
-add_connection sys_clk.clk_reset avl_adxcfg_1.rcfg_reset_n
-add_connection avl_adxcfg_1.rcfg_m0 avl_ad9144_xcvr.phy_reconfig_1
-add_connection avl_adxcfg_1.rcfg_m1 avl_ad9680_xcvr.phy_reconfig_1
-
-add_instance avl_adxcfg_2 avl_adxcfg
-add_connection sys_clk.clk avl_adxcfg_2.rcfg_clk
-add_connection sys_clk.clk_reset avl_adxcfg_2.rcfg_reset_n
-add_connection avl_adxcfg_2.rcfg_m0 avl_ad9144_xcvr.phy_reconfig_2
-add_connection avl_adxcfg_2.rcfg_m1 avl_ad9680_xcvr.phy_reconfig_2
-
-add_instance avl_adxcfg_3 avl_adxcfg
-add_connection sys_clk.clk avl_adxcfg_3.rcfg_clk
-add_connection sys_clk.clk_reset avl_adxcfg_3.rcfg_reset_n
-add_connection avl_adxcfg_3.rcfg_m0 avl_ad9144_xcvr.phy_reconfig_3
-add_connection avl_adxcfg_3.rcfg_m1 avl_ad9680_xcvr.phy_reconfig_3
+for {set i 0} {$i < 4} {incr i} {
+  add_instance avl_adxcfg_${i} avl_adxcfg
+  add_connection sys_clk.clk avl_adxcfg_${i}.rcfg_clk
+  add_connection sys_clk.clk_reset avl_adxcfg_${i}.rcfg_reset_n
+  add_connection avl_adxcfg_${i}.rcfg_m0 ad9144_jesd204.phy_reconfig_${i}
+  add_connection avl_adxcfg_${i}.rcfg_m1 ad9680_jesd204.phy_reconfig_${i}
+}
 
 # addresses
 
-ad_cpu_interconnect 0x00020000 avl_ad9144_xcvr.ip_reconfig
-ad_cpu_interconnect 0x00024000 axi_ad9144_xcvr.s_axi
-ad_cpu_interconnect 0x00025000 avl_ad9144_xcvr.core_pll_reconfig
-ad_cpu_interconnect 0x00026000 avl_ad9144_xcvr.lane_pll_reconfig
+ad_cpu_interconnect 0x00020000 ad9144_jesd204.link_reconfig
+ad_cpu_interconnect 0x00024000 ad9144_jesd204.link_management
+ad_cpu_interconnect 0x00025000 ad9144_jesd204.link_pll_reconfig
+ad_cpu_interconnect 0x00026000 ad9144_jesd204.lane_pll_reconfig
 ad_cpu_interconnect 0x00028000 avl_adxcfg_0.rcfg_s0
 ad_cpu_interconnect 0x00029000 avl_adxcfg_1.rcfg_s0
 ad_cpu_interconnect 0x0002a000 avl_adxcfg_2.rcfg_s0
@@ -226,9 +160,9 @@ ad_cpu_interconnect 0x0002b000 avl_adxcfg_3.rcfg_s0
 ad_cpu_interconnect 0x0002c000 axi_ad9144_dma.s_axi
 ad_cpu_interconnect 0x00030000 axi_ad9144_core.s_axi
 
-ad_cpu_interconnect 0x00040000 avl_ad9680_xcvr.ip_reconfig
-ad_cpu_interconnect 0x00044000 axi_ad9680_xcvr.s_axi
-ad_cpu_interconnect 0x00045000 avl_ad9680_xcvr.core_pll_reconfig
+ad_cpu_interconnect 0x00040000 ad9680_jesd204.link_reconfig
+ad_cpu_interconnect 0x00044000 ad9680_jesd204.link_management
+ad_cpu_interconnect 0x00045000 ad9680_jesd204.link_pll_reconfig
 ad_cpu_interconnect 0x00048000 avl_adxcfg_0.rcfg_s1
 ad_cpu_interconnect 0x00049000 avl_adxcfg_1.rcfg_s1
 ad_cpu_interconnect 0x0004a000 avl_adxcfg_2.rcfg_s1
@@ -243,5 +177,7 @@ ad_dma_interconnect axi_ad9680_dma.m_dest_axi
 
 # interrupts
 
+ad_cpu_interrupt 8 ad9680_jesd204.interrupt
+ad_cpu_interrupt 9 ad9144_jesd204.interrupt
 ad_cpu_interrupt 10 axi_ad9680_dma.interrupt_sender
 ad_cpu_interrupt 11 axi_ad9144_dma.interrupt_sender
