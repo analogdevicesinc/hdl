d690a614c1879ed54a9edbd9b4902b5dfe1c1a64
Adrian Costina <adrian.costina@analog.com>
2017-10-04 16:16:00 +0100

    a10gx: Force all used tiles to high speed, in order to improve timing

--- projects/common/a10gx/a10gx_system_assign.tcl
+++ projects/common/a10gx/a10gx_system_assign.tcl
@@ -6,6 +6,7 @@ set_location_assignment PIN_AR37  -to "sys_clk(n)"
 set_location_assignment PIN_BD27  -to sys_resetn
 set_instance_assignment -name IO_STANDARD LVDS -to sys_clk
 set_instance_assignment -name IO_STANDARD "1.8 V" -to sys_resetn
+set_global_assignment -name PROGRAMMABLE_POWER_TECHNOLOGY_SETTING "FORCE ALL USED TILES TO HIGH SPEED"
 
 # ddr3
 
