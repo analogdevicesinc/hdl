20326d5635d1df22350a0460803165081542b766
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-10-25 15:09:13 -0400

    de10- updates

--- projects/de10/system_project.tcl
+++ projects/de10/system_project.tcl
@@ -1,6 +1,6 @@
 
-source ../../scripts/adi_env.tcl
-source ../../scripts/adi_project_alt.tcl
+source ../scripts/adi_env.tcl
+source ../scripts/adi_project_alt.tcl
 
 adi_project_altera generic_de10
 
