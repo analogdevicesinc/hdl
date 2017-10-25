23119e78bf8f69cad5c54a14720527c7a28e14e9
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-10-25 13:44:40 -0400

    de10- updates

--- projects/de10/system_project.tcl
+++ projects/de10/system_project.tcl
@@ -2,9 +2,9 @@
 source ../../scripts/adi_env.tcl
 source ../../scripts/adi_project_alt.tcl
 
-adi_project_altera arradio_c5soc
+adi_project_altera generic_de10
 
-source $ad_hdl_dir/projects/common/c5soc/c5soc_system_assign.tcl
+source $ad_hdl_dir/projects/common/de10/de10_system_assign.tcl
 
 # ad9361 interface
 
