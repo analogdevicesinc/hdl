33cfe5fc4d2172f7394efcc4b5839fe6f5835ba5
Istvan Csomortani <istvan.csomortani@analog.com>
2017-11-29 13:50:47 +0000

    axi_logic_analyzer: Delete remove_bus_interface calls

--- library/axi_logic_analyzer/axi_logic_analyzer_ip.tcl
+++ library/axi_logic_analyzer/axi_logic_analyzer_ip.tcl
@@ -16,8 +16,5 @@ adi_ip_files axi_logic_analyzer [list \
 
 adi_ip_properties axi_logic_analyzer
 
-ipx::remove_bus_interface {clk} [ipx::current_core]
-ipx::associate_bus_interfaces -busif s_axi -clock s_axi_aclk [ipx::current_core]
-
 ipx::save_core [ipx::current_core]
 
