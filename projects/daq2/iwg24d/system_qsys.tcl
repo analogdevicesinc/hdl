ea61a01e215957ebf06089af54fdbced09b51ed5
Istvan Csomortani <istvan.csomortani@analog.com>
2018-02-09 08:59:36 +0000

    daq2/iwg24d: Add support for offload FIFO implemented in FPGA side DDR4

--- projects/daq2/iwg24d/system_qsys.tcl
+++ projects/daq2/iwg24d/system_qsys.tcl
@@ -5,6 +5,6 @@ set dac_data_width 128
 set dac_dma_data_width 128
 
 source $ad_hdl_dir/projects/common/iwg24d/iwg24d_system_qsys.tcl
-source $ad_hdl_dir/projects/common/altera/dacfifo_qsys.tcl
+source $ad_hdl_dir/projects/common/iwg24d/iwg24d_fpga_ddr4_qsys.tcl
 source ../common/daq2_qsys.tcl
 
