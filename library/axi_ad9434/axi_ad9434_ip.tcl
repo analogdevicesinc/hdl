16a049e67dfd79eecf024d3c0fd8d492801ee9bf
Adrian Costina <adrian.costina@analog.com>
2018-02-15 08:41:14 +0000

    axi_ad9434: Add clock interface

--- library/axi_ad9434/axi_ad9434_ip.tcl
+++ library/axi_ad9434/axi_ad9434_ip.tcl
@@ -32,5 +32,8 @@ adi_ip_properties axi_ad9434
 
 set_property driver_value 0 [ipx::get_ports *dovf* -of_objects [ipx::current_core]]
 
+ipx::infer_bus_interface delay_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+ipx::infer_bus_interface adc_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+
 ipx::save_core [ipx::current_core]
 
