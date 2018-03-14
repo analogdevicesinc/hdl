425e8033640ad1f05189cadd1181efd9d39996ec
Istvan Csomortani <istvan.csomortani@analog.com>
2018-03-14 16:45:47 +0200

    license: Fix a spelling mistake

--- library/axi_ad9361/altera/axi_ad9361_lvds_if_10.v
+++ library/axi_ad9361/altera/axi_ad9361_lvds_if_10.v
@@ -8,7 +8,7 @@
 // terms.
 //
 // The user should read each of these license terms, and understand the
-// freedoms and responsabilities that he or she has by using this source/core.
+// freedoms and responsibilities that he or she has by using this source/core.
 //
 // This core is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
