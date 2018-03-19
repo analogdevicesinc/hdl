937889631f9dc862d3777e7b601fb4c41c6d37ce
Lars-Peter Clausen <lars@metafoo.de>
2018-03-19 15:51:12 +0100

    altera: adi_jesd204: Add support for more than 6 transmit lanes
    On Arria10 there are 6 transceivers in a single bank. If more than 6
    transceivers are used these will end up in multiple banks.
    
    The ATX PLL can directly connect to the transceivers in the same bank
    through the 1x clock network. To connect to transceivers in another bank it
    has to go through a master clock generation block (MCGB) and the xN clock
    network.
    
    Add support for instantiating the MCGB if more than 6 lanes are used. In
    this case the first 6 transceivers will still have a direct connection to
    the PLL while all other transceivers will be clocked by the MCGB.
    
    Note that this requires that the first 6 transceivers are all in the same
    bank.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

---
+++ library/altera/adi_jesd204/adi_jesd204_glue.v
@@ -0,0 +1,45 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+module adi_jesd204_glue (
+  input in_pll_powerdown,
+  output out_pll_powerdown,
+  output out_mcgb_rst
+);
+
+assign out_pll_powerdown = in_pll_powerdown;
+assign out_mcgb_rst = in_pll_powerdown;
+
+endmodule
