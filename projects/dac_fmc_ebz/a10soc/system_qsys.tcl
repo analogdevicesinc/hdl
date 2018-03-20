c6d8c015a3bc921ea1c3c82f5414ab2921927e0e
Lars-Peter Clausen <lars@metafoo.de>
2018-03-20 14:43:36 +0100

    dac_fmc_ebz: Add initial Arria10 SoC support
    Add support for the Arria 10 SoC development kit to the dac_fmc_ebz
    project.
    
    This allows to use the following FMC boards on the Arria 10 SoC development
    Kit carrier:
      * AD9135-FMC-EBZ
      * AD9136-FMC-EBZ
      * AD9144-FMC-EBZ
      * AD9152-FMC-EBZ
      * AD9154-FMC-EBZ
      * AD9171-FMC-EBZ
      * AD9172-FMC-EBZ
      * AD9173-FMC-EBZ
    
    Note that the board in its default configuration is not fully compatible with the
    mentioned FMC boards and some slight re-work moving some 0 Ohm resistors is
    required. The rework concerns the LA01 and LA05 pins, which by default are
    not connected to the FPGA. The changes required are:
    
      LA01_P_CC
        R612: R0 -> DNI
        R610: DNI -> R0
      LA01_N_CC
        R613: R0 -> DNI
        R611: DNI -> R0
      LA05_P
        R621: R0 -> DNI
        R620: DNI -> R0
      LA05_N
        R633: R0 -> DNI
        R632: DNI -> R0
    
    The main differences between AD9144-FMC-EBZ and AD9172-FMC-EBZ are:
      * The DAC txen signals are connected to different pins
      * The polarity of the spi_en signal is active low instead of active high
      * The maximum lane rate is up to 15.4 Gpbs
    
    To accommodate this all 4 possible txen signals as well as the spi_en
    signal are connected to GPIOs. Software can decide how to use them
    depending on which FMC board is connected.
    
    Note that each carrier has a maximum supported lane rate. Modes of the
    AD9172 (and similar) that exceed the carrier specific limit can not be used
    on that carrier. The limits are as following:
      * A10SoC: 14.2 Gbps

---
+++ projects/dac_fmc_ebz/a10soc/system_qsys.tcl
@@ -0,0 +1,37 @@
+#
+# Copyright 2018 (c) Analog Devices, Inc. All rights reserved.
+#
+# In this HDL repository, there are many different and unique modules, consisting
+# of various HDL (Verilog or VHDL) components. The individual modules are
+# developed independently, and may be accompanied by separate and unique license
+# terms.
+#
+# The user should read each of these license terms, and understand the
+# freedoms and responsibilities that he or she has by using this source/core.
+#
+# This core is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.
+#
+# Redistribution and use of source or resulting binaries, with or without modification
+# of this file, are permitted under one of the following two license terms:
+#
+#   1. The GNU General Public License version 2 as published by the
+#      Free Software Foundation, which can be found in the top level directory
+#      of this repository (LICENSE_GPL2), and also online at:
+#      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+#
+# OR
+#
+#   2. An ADI specific BSD license, which can be found in the top level directory
+#      of this repository (LICENSE_ADIBSD), and also on-line at:
+#      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+#      This will allow to generate bit files and not release the source code,
+#      as long as it attaches to an ADI device.
+#
+
+set dac_fifo_address_width 13
+
+source $ad_hdl_dir/projects/common/a10soc/a10soc_system_qsys.tcl
+source $ad_hdl_dir/projects/common/a10soc/a10soc_plddr4_dacfifo_qsys.tcl
+source ../common/dac_fmc_ebz_qsys.tcl
