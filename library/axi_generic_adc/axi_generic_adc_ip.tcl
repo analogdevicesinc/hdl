b9b9952753260292031cfc7f69d71d4983a69fa6
Lars-Peter Clausen <lars@metafoo.de>
2018-03-27 10:51:31 +0200

    library: Remove unreferenced files from IP file lists
    Some IP core have files in their file list for common modules that are not
    used by the IP itself. Remove those.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/axi_generic_adc/axi_generic_adc_ip.tcl
+++ library/axi_generic_adc/axi_generic_adc_ip.tcl
@@ -7,7 +7,6 @@ adi_ip_create axi_generic_adc
 adi_ip_files axi_generic_adc [list \
   "$ad_hdl_dir/library/common/ad_rst.v" \
   "$ad_hdl_dir/library/common/up_axi.v" \
-  "$ad_hdl_dir/library/common/up_delay_cntrl.v" \
   "$ad_hdl_dir/library/common/up_xfer_cntrl.v" \
   "$ad_hdl_dir/library/common/up_xfer_status.v" \
   "$ad_hdl_dir/library/common/up_clock_mon.v" \
