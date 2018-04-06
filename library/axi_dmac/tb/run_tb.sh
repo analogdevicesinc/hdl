a58dea545c140fd79758eddf66bb5cf32d99edc9
Laszlo Nagy <laszlo.nagy@analog.com>
2018-04-06 10:15:37 +0100

    axi_dmac: fix indentation of run_tb.sh

--- library/axi_dmac/tb/run_tb.sh
+++ library/axi_dmac/tb/run_tb.sh
@@ -2,20 +2,20 @@ NAME=`basename $0`
 
 if [[ ${SIMULATOR} != "modelsim" ]]; then
 
-#Icarus flow
-mkdir -p run
-mkdir -p vcd
-iverilog -o run/run_${NAME} -I.. ${SOURCE} $1 || exit 1
+  #Icarus flow
+  mkdir -p run
+  mkdir -p vcd
+  iverilog -o run/run_${NAME} -I.. ${SOURCE} $1 || exit 1
 
-cd vcd
-../run/run_${NAME}
+  cd vcd
+  ../run/run_${NAME}
 
 else
 
-# ModelSim flow
-vlib work
+  # ModelSim flow
+  vlib work
 
-vlog ${SOURCE} || exit 1
-vsim "dmac_"${NAME} -do "add log /* -r; run -a" -gui || exit 1
+  vlog ${SOURCE} || exit 1
+  vsim "dmac_"${NAME} -do "add log /* -r; run -a" -gui || exit 1
 
 fi
