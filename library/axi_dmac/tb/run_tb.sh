5c2e10eca5411fddc858e7fc37707b0a7067cbe2
Laszlo Nagy <laszlo.nagy@analog.com>
2018-04-06 09:59:35 +0100

    axi_dmac: added ModelSim support to run_tb.sh
    Modified run_tb.sh to work with Modelsim if the SIMULATOR variable is set to
    "modelsim"

--- library/axi_dmac/tb/run_tb.sh
+++ library/axi_dmac/tb/run_tb.sh
@@ -1,8 +1,21 @@
 NAME=`basename $0`
 
-mkdir -p run
-mkdir -p vcd
-iverilog -I.. ${SOURCE} -o run/run_${NAME} $1 || exit 1
+if [[ ${SIMULATOR} != "modelsim" ]]; then
 
-cd vcd
-../run/run_${NAME}
+  #Icarus flow
+  mkdir -p run
+  mkdir -p vcd
+  iverilog -o run/run_${NAME} -I.. ${SOURCE} $1 || exit 1
+
+  cd vcd
+  ../run/run_${NAME}
+
+else
+
+  # ModelSim flow
+  vlib work
+
+  vlog ${SOURCE} || exit 1
+  vsim "dmac_"${NAME} -do "add log /* -r; run -a" -gui || exit 1
+
+fi
