e4c4559b6d4a0a4cb007b090edc4dfdbc9684264
Adrian Costina <adrian.costina@analog.com>
2018-04-10 15:06:32 +0300

    adrv9379:ZCU102: Initial commit

---
+++ projects/adrv9379/zcu102/system_bd.tcl
@@ -0,0 +1,29 @@
+
+set dac_fifo_name axi_ad9379_dacfifo
+set dac_fifo_address_width 10
+set dac_data_width 128
+set dac_dma_data_width 128
+
+source $ad_hdl_dir/projects/common/zcu102/zcu102_system_bd.tcl
+source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
+
+ad_ip_parameter sys_ps8 CONFIG.PSU__FPGA_PL2_ENABLE 1
+ad_ip_parameter sys_ps8 CONFIG.PSU__CRL_APB__PL2_REF_CTRL__SRCSEL {IOPLL}
+ad_ip_parameter sys_ps8 CONFIG.PSU__CRL_APB__PL2_REF_CTRL__FREQMHZ 200
+
+source ../common/adrv9379_bd.tcl
+
+ad_connect sys_dma_clk sys_ps8/pl_clk2
+ad_connect sys_dma_rstgen/ext_reset_in sys_rstgen/peripheral_reset
+
+ad_ip_parameter axi_ad9379_tx_xcvr CONFIG.XCVR_TYPE 2
+ad_ip_parameter axi_ad9379_rx_xcvr CONFIG.XCVR_TYPE 2
+ad_ip_parameter axi_ad9379_rx_os_xcvr CONFIG.XCVR_TYPE 2
+
+ad_ip_parameter util_ad9379_xcvr CONFIG.XCVR_TYPE 2
+ad_ip_parameter util_ad9379_xcvr CONFIG.QPLL_FBDIV 80
+ad_ip_parameter util_ad9379_xcvr CONFIG.QPLL_REFCLK_DIV 1
+
+ad_ip_parameter axi_ad9379_rx_clkgen CONFIG.DEVICE_TYPE 2
+ad_ip_parameter axi_ad9379_tx_clkgen CONFIG.DEVICE_TYPE 2
+
