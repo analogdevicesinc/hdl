a277d55c3575124cc100c57d5f032b6060d37e64
Istvan Csomortani <istvan.csomortani@analog.com>
2018-04-13 12:35:25 +0100

    adv7511:kc705: Delete deprecated project

--- projects/adv7511/kc705/system_bd.tcl
+++
@@ -1,6 +0,0 @@
-
-source $ad_hdl_dir/projects/common/kc705/kc705_system_bd.tcl
-source $ad_hdl_dir/projects/adv7511/common/adv7511_bd.tcl
-
-ad_ip_parameter axi_hdmi_dma CONFIG.c_m_axi_mm2s_data_width 512
-ad_ip_parameter axi_hdmi_dma CONFIG.c_m_axis_mm2s_tdata_width 64
