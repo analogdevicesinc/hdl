47ece080fb0192981d8476b817a7baf8a84bf521
Istvan Csomortani <istvan.csomortani@analog.com>
2018-04-13 17:09:57 +0100

    fmcomms11: Initial commit

---
+++ projects/fmcomms11/common/fmcomms11_bd.tcl
@@ -0,0 +1,147 @@
+
+source $ad_hdl_dir/library/jesd204/scripts/jesd204.tcl
+
+# dac peripherals
+
+ad_ip_instance axi_adxcvr axi_ad9162_xcvr
+ad_ip_parameter axi_ad9162_xcvr CONFIG.NUM_OF_LANES 8
+ad_ip_parameter axi_ad9162_xcvr CONFIG.QPLL_ENABLE 1
+ad_ip_parameter axi_ad9162_xcvr CONFIG.TX_OR_RX_N 1
+
+ad_ip_instance axi_ad9162 axi_ad9162_core
+
+adi_axi_jesd204_tx_create axi_ad9162_jesd 8
+
+ad_ip_instance axi_dmac axi_ad9162_dma
+ad_ip_parameter axi_ad9162_dma CONFIG.DMA_TYPE_SRC 0
+ad_ip_parameter axi_ad9162_dma CONFIG.DMA_TYPE_DEST 1
+ad_ip_parameter axi_ad9162_dma CONFIG.ID 1
+ad_ip_parameter axi_ad9162_dma CONFIG.AXI_SLICE_SRC 0
+ad_ip_parameter axi_ad9162_dma CONFIG.AXI_SLICE_DEST 0
+ad_ip_parameter axi_ad9162_dma CONFIG.DMA_LENGTH_WIDTH 24
+ad_ip_parameter axi_ad9162_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_ad9162_dma CONFIG.CYCLIC 0
+ad_ip_parameter axi_ad9162_dma CONFIG.DMA_DATA_WIDTH_SRC 256
+ad_ip_parameter axi_ad9162_dma CONFIG.DMA_DATA_WIDTH_DEST 256
+
+# adc peripherals
+
+ad_ip_instance axi_adxcvr axi_ad9625_xcvr
+ad_ip_parameter axi_ad9625_xcvr CONFIG.NUM_OF_LANES 8
+ad_ip_parameter axi_ad9625_xcvr CONFIG.QPLL_ENABLE 0
+ad_ip_parameter axi_ad9625_xcvr CONFIG.TX_OR_RX_N 0
+
+ad_ip_instance axi_ad9625 axi_ad9625_core
+
+adi_axi_jesd204_rx_create axi_ad9625_jesd 8
+
+ad_ip_instance axi_dmac axi_ad9625_dma
+ad_ip_parameter axi_ad9625_dma CONFIG.DMA_TYPE_SRC 1
+ad_ip_parameter axi_ad9625_dma CONFIG.DMA_TYPE_DEST 0
+ad_ip_parameter axi_ad9625_dma CONFIG.ID 0
+ad_ip_parameter axi_ad9625_dma CONFIG.AXI_SLICE_SRC 0
+ad_ip_parameter axi_ad9625_dma CONFIG.AXI_SLICE_DEST 0
+ad_ip_parameter axi_ad9625_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter axi_ad9625_dma CONFIG.DMA_LENGTH_WIDTH 24
+ad_ip_parameter axi_ad9625_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_ad9625_dma CONFIG.CYCLIC 0
+ad_ip_parameter axi_ad9625_dma CONFIG.DMA_DATA_WIDTH_SRC 64
+ad_ip_parameter axi_ad9625_dma CONFIG.DMA_DATA_WIDTH_DEST 64
+
+# shared transceiver core
+
+ad_ip_instance util_adxcvr util_fmcomms11_xcvr
+ad_ip_parameter util_fmcomms11_xcvr CONFIG.QPLL_FBDIV 0x120
+ad_ip_parameter util_fmcomms11_xcvr CONFIG.CPLL_FBDIV 4
+ad_ip_parameter util_fmcomms11_xcvr CONFIG.TX_NUM_OF_LANES 8
+ad_ip_parameter util_fmcomms11_xcvr CONFIG.TX_CLK25_DIV 7
+ad_ip_parameter util_fmcomms11_xcvr CONFIG.RX_NUM_OF_LANES 8
+ad_ip_parameter util_fmcomms11_xcvr CONFIG.RX_CLK25_DIV 7
+ad_ip_parameter util_fmcomms11_xcvr CONFIG.RX_DFE_LPM_CFG 0x0904
+ad_ip_parameter util_fmcomms11_xcvr CONFIG.RX_CDR_CFG 0x03000023ff10400020
+
+# reference clocks & resets
+
+create_bd_port -dir I tx_ref_clk_0
+create_bd_port -dir I rx_ref_clk_0
+
+ad_xcvrpll  tx_ref_clk_0 util_fmcomms11_xcvr/qpll_ref_clk_*
+ad_xcvrpll  rx_ref_clk_0 util_fmcomms11_xcvr/cpll_ref_clk_*
+ad_xcvrpll  axi_ad9162_xcvr/up_pll_rst util_fmcomms11_xcvr/up_qpll_rst_*
+ad_xcvrpll  axi_ad9625_xcvr/up_pll_rst util_fmcomms11_xcvr/up_cpll_rst_*
+ad_connect  sys_cpu_resetn util_fmcomms11_xcvr/up_rstn
+ad_connect  sys_cpu_clk util_fmcomms11_xcvr/up_clk
+
+# connections (dac)
+
+ad_xcvrcon  util_fmcomms11_xcvr axi_ad9162_xcvr axi_ad9162_jesd
+ad_connect  util_fmcomms11_xcvr/tx_out_clk_0 axi_ad9162_core/tx_clk
+ad_connect  axi_ad9162_jesd/tx_data_tdata axi_ad9162_core/tx_data
+ad_connect  util_fmcomms11_xcvr/tx_out_clk_0 axi_ad9162_fifo/dac_clk
+ad_connect  axi_ad9162_core/dac_valid axi_ad9162_fifo/dac_valid
+ad_connect  axi_ad9162_core/dac_ddata axi_ad9162_fifo/dac_data
+ad_connect  axi_ad9162_core/dac_dunf axi_ad9162_fifo/dac_dunf
+ad_connect  axi_ad9162_jesd_rstgen/peripheral_reset axi_ad9162_fifo/dac_rst
+ad_connect  sys_cpu_clk axi_ad9162_fifo/dma_clk
+ad_connect  sys_cpu_reset axi_ad9162_fifo/dma_rst
+ad_connect  sys_cpu_clk axi_ad9162_dma/m_axis_aclk
+ad_connect  sys_cpu_resetn axi_ad9162_dma/m_src_axi_aresetn
+ad_connect  axi_ad9162_fifo/dma_xfer_req axi_ad9162_dma/m_axis_xfer_req
+ad_connect  axi_ad9162_fifo/dma_ready axi_ad9162_dma/m_axis_ready
+ad_connect  axi_ad9162_fifo/dma_data axi_ad9162_dma/m_axis_data
+ad_connect  axi_ad9162_fifo/dma_valid axi_ad9162_dma/m_axis_valid
+ad_connect  axi_ad9162_fifo/dma_xfer_last axi_ad9162_dma/m_axis_last
+
+# connections (adc)
+
+ad_xcvrcon  util_fmcomms11_xcvr axi_ad9625_xcvr axi_ad9625_jesd
+ad_connect  util_fmcomms11_xcvr/rx_out_clk_0 axi_ad9625_core/rx_clk
+ad_connect  axi_ad9625_jesd/rx_sof axi_ad9625_core/rx_sof
+ad_connect  axi_ad9625_jesd/rx_data_tdata axi_ad9625_core/rx_data
+ad_connect  util_fmcomms11_xcvr/rx_out_clk_0 axi_ad9625_fifo/adc_clk
+ad_connect  axi_ad9625_jesd_rstgen/peripheral_reset axi_ad9625_fifo/adc_rst
+ad_connect  axi_ad9625_core/adc_valid axi_ad9625_fifo/adc_wr
+ad_connect  axi_ad9625_core/adc_data axi_ad9625_fifo/adc_wdata
+ad_connect  sys_cpu_clk axi_ad9625_fifo/dma_clk
+ad_connect  sys_cpu_clk axi_ad9625_dma/s_axis_aclk
+ad_connect  sys_cpu_resetn axi_ad9625_dma/m_dest_axi_aresetn
+ad_connect  axi_ad9625_fifo/dma_wr axi_ad9625_dma/s_axis_valid
+ad_connect  axi_ad9625_fifo/dma_wdata axi_ad9625_dma/s_axis_data
+ad_connect  axi_ad9625_fifo/dma_wready axi_ad9625_dma/s_axis_ready
+ad_connect  axi_ad9625_fifo/dma_xfer_req axi_ad9625_dma/s_axis_xfer_req
+ad_connect  axi_ad9625_core/adc_dovf axi_ad9625_fifo/adc_wovf
+
+# interconnect (cpu)
+
+ad_cpu_interconnect 0x44A60000 axi_ad9162_xcvr
+ad_cpu_interconnect 0x44A00000 axi_ad9162_core
+ad_cpu_interconnect 0x44A90000 axi_ad9162_jesd
+ad_cpu_interconnect 0x7c420000 axi_ad9162_dma
+ad_cpu_interconnect 0x44A50000 axi_ad9625_xcvr
+ad_cpu_interconnect 0x44A10000 axi_ad9625_core
+ad_cpu_interconnect 0x44AA0000 axi_ad9625_jesd
+ad_cpu_interconnect 0x7c400000 axi_ad9625_dma
+
+# gt uses hp3, and 100MHz clock for both DRP and AXI4
+
+ad_mem_hp3_interconnect sys_cpu_clk sys_ps7/S_AXI_HP3
+ad_mem_hp3_interconnect sys_cpu_clk axi_ad9625_xcvr/m_axi
+
+# interconnect (mem/dac)
+
+ad_mem_hp1_interconnect sys_cpu_clk sys_ps7/S_AXI_HP1
+ad_mem_hp1_interconnect sys_cpu_clk axi_ad9162_dma/m_src_axi
+ad_mem_hp2_interconnect sys_cpu_clk sys_ps7/S_AXI_HP2
+ad_mem_hp2_interconnect sys_cpu_clk axi_ad9625_dma/m_dest_axi
+
+# interrupts
+
+ad_cpu_interrupt ps-10 mb-15 axi_ad9162_jesd/irq
+ad_cpu_interrupt ps-11 mb-14 axi_ad9625_jesd/irq
+ad_cpu_interrupt ps-12 mb-12 axi_ad9162_dma/irq
+ad_cpu_interrupt ps-13 mb-13 axi_ad9625_dma/irq
+
+# unused
+
+ad_connect  axi_ad9162_fifo/bypass GND
+
---
+++ projects/fmcomms11/common/fmcomms11_spi.v
@@ -0,0 +1,120 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module fmcomms11_spi (
+
+  // 4 wire
+
+  input   [ 2:0]  spi_csn,
+  input           spi_clk,
+  input           spi_mosi,
+  output          spi_miso,
+
+  // 3 wire
+
+  output          spi_csn_ad9625,
+  output          spi_csn_ad9162,
+  output          spi_csn_ad9508,
+  output          spi_csn_adl5240,
+  output          spi_csn_adf4355,
+  output          spi_csn_hmc1119,
+  inout           spi_sdio,
+  output          spi_dir);
+
+  // internal registers
+
+  reg     [ 5:0]  spi_count = 'd0;
+  reg             spi_rd_wr_n = 'd0;
+  reg             spi_enable = 'd0;
+
+  // internal signals
+
+  wire    [ 5:0]  spi_csn6_s;
+  wire            spi_csn_s;
+  wire            spi_enable_s;
+
+  // check on rising edge and change on falling edge
+
+  assign spi_csn_ad9625 = spi_csn6_s[0];
+  assign spi_csn_ad9162 = spi_csn6_s[1];
+  assign spi_csn_ad9508 = spi_csn6_s[2];
+  assign spi_csn_adl5240 = spi_csn6_s[3];
+  assign spi_csn_adf4355 = spi_csn6_s[4];
+  assign spi_csn_hmc1119 = spi_csn6_s[5];
+
+  assign spi_csn6_s[0] = (spi_csn == 3'b001) ? 1'b0 : 1'b1;
+  assign spi_csn6_s[1] = (spi_csn == 3'b010) ? 1'b0 : 1'b1;
+  assign spi_csn6_s[2] = (spi_csn == 3'b011) ? 1'b0 : 1'b1;
+  assign spi_csn6_s[3] = (spi_csn == 3'b100) ? 1'b0 : 1'b1;
+  assign spi_csn6_s[4] = (spi_csn == 3'b101) ? 1'b0 : 1'b1;
+  assign spi_csn6_s[5] = (spi_csn == 3'b110) ? 1'b0 : 1'b1;
+
+  assign spi_csn_s = & spi_csn6_s;
+  assign spi_dir = ~spi_enable_s;
+  assign spi_enable_s = spi_enable & ~spi_csn_s;
+
+  always @(posedge spi_clk or posedge spi_csn_s) begin
+    if (spi_csn_s == 1'b1) begin
+      spi_count <= 6'd0;
+      spi_rd_wr_n <= 1'd0;
+    end else begin
+      spi_count <= (spi_count < 6'h3f) ? spi_count + 1'b1 : spi_count;
+      if (spi_count == 6'd0) begin
+        spi_rd_wr_n <= spi_mosi;
+      end
+    end
+  end
+
+  always @(negedge spi_clk or posedge spi_csn_s) begin
+    if (spi_csn_s == 1'b1) begin
+      spi_enable <= 1'b0;
+    end else begin
+      if (spi_count == 6'd16) begin
+        spi_enable <= spi_rd_wr_n;
+      end
+    end
+  end
+
+  // io buffer
+
+  assign spi_miso = spi_sdio;
+  assign spi_sdio = (spi_enable_s == 1'b1) ? 1'bz : spi_mosi;
+
+endmodule
+
+// ***************************************************************************
+// ***************************************************************************
