18bdf91254f987675035ce576f694b866f876af9
Istvan Csomortani <istvan.csomortani@analog.com>
2018-04-13 12:48:23 +0100

    daq1: Move project to a feature branch

--- projects/daq1/zed/system_bd.tcl
+++
@@ -1,9 +0,0 @@
-
-set adc_fifo_name axi_ad9684_fifo
-set adc_fifo_address_width 10
-set adc_data_width 64
-set adc_dma_data_width 64
-
-source $ad_hdl_dir/projects/common/xilinx/adcfifo_bd.tcl
-source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
-source ../common/daq1_bd.tcl
