8e90d5db20591136a17a797b77a14155555167a5
Lars-Peter Clausen <lars@metafoo.de>
2018-04-17 13:49:50 +0200

    axi_ad9162: Infer clock signal for tx_clk port
    Fixes the following warning:
      [BD 41-1731] Type mismatch between connected pins: /util_fmcomms11_xcvr/tx_out_clk_0(clk) and /axi_ad9162_core/tx_clk(undef)
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/axi_ad9162/axi_ad9162_ip.tcl
+++ library/axi_ad9162/axi_ad9162_ip.tcl
@@ -31,6 +31,6 @@ set_property driver_value 0 [ipx::get_ports *dovf* -of_objects [ipx::current_cor
 set_property driver_value 0 [ipx::get_ports *dunf* -of_objects [ipx::current_core]]
 set_property driver_value 0 [ipx::get_ports *tx_ready* -of_objects [ipx::current_core]]
 
+ipx::infer_bus_interface tx_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+
 ipx::save_core [ipx::current_core]
-
-
