da546771013d0e5c7664f121d782a0bc3f32d164
Istvan Csomortani <istvan.csomortani@analog.com>
2018-04-18 09:00:00 +0100

    usrpe31x: Initial commit
    This project was moved from master into 'usrpe31x' feature branch.
    
    To see the old commits, checkout the dev_prj_2018_r1 tag.

---
+++ projects/usrpe31x/system_project.tcl
@@ -0,0 +1,16 @@
+
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+set p_device "xc7z020clg484-1"
+adi_project_xilinx usrpe31x
+
+adi_project_files usrpe31x [list \
+  "system_top.v" \
+  "system_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v"]
+
+adi_project_run usrpe31x
+source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
+
