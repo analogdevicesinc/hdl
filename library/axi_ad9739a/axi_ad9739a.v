92dbd75414ca43eac505fdc142decf2f535e73fe
AndreiGrozav <andrei.grozav@analog.com>
2018-06-06 12:28:16 +0300

    axi_ad9739a: Updates for ad_dds phase acc wrapper

--- library/axi_ad9739a/axi_ad9739a.v
+++ library/axi_ad9739a/axi_ad9739a.v
@@ -43,6 +43,7 @@ module axi_ad9739a #(
   parameter   MMCM_OR_BUFIO_N = 1,
   parameter   DAC_DDS_TYPE = 1,
   parameter   DAC_DDS_CORDIC_DW = 16,
+  parameter   DAC_DDS_CORDIC_PHASE_DW = 16,
   parameter   DAC_DATAPATH_DISABLE = 0,
   parameter   IO_DELAY_GROUP = "dev_if_delay_group") (
 
@@ -165,8 +166,9 @@ module axi_ad9739a #(
 
   axi_ad9739a_core #(
     .ID(ID),
-    .DDS_TYPE (DAC_DDS_TYPE),
-    .DDS_CORDIC_DW (DAC_DDS_CORDIC_DW),
+    .DAC_DDS_TYPE (DAC_DDS_TYPE),
+    .DAC_DDS_CORDIC_DW (DAC_DDS_CORDIC_DW),
+    .DAC_DDS_CORDIC_PHASE_DW (DAC_DDS_CORDIC_PHASE_DW),
     .DATAPATH_DISABLE(DAC_DATAPATH_DISABLE))
   i_core (
     .dac_div_clk (dac_div_clk),
--- library/axi_ad9739a/axi_ad9739a_core.v
+++ library/axi_ad9739a/axi_ad9739a_core.v
@@ -38,8 +38,9 @@
 module axi_ad9739a_core #(
 
   parameter   ID = 0,
-  parameter   DDS_TYPE = 1,
-  parameter   DDS_CORDIC_DW = 16,
+  parameter   DAC_DDS_TYPE = 1,
+  parameter   DAC_DDS_CORDIC_DW = 16,
+  parameter   DAC_DDS_CORDIC_PHASE_DW = 16,
   parameter   DATAPATH_DISABLE = 0) (
 
   // dac interface
@@ -120,8 +121,9 @@ module axi_ad9739a_core #(
 
   axi_ad9739a_channel #(
     .CHANNEL_ID(0),
-    .DDS_TYPE (DDS_TYPE),
-    .DDS_CORDIC_DW (DDS_CORDIC_DW),
+    .DAC_DDS_TYPE (DAC_DDS_TYPE),
+    .DAC_DDS_CORDIC_DW (DAC_DDS_CORDIC_DW),
+    .DAC_DDS_CORDIC_PHASE_DW (DAC_DDS_CORDIC_PHASE_DW),
     .DATAPATH_DISABLE(DATAPATH_DISABLE))
   i_channel_0 (
     .dac_div_clk (dac_div_clk),
