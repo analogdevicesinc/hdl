ad6e1653a633541d3e979bbe9e7903743d805598
Adrian Costina <adrian.costina@analog.com>
2018-06-13 10:47:18 +0300

    adrv9009: Throughput improvements
    - increased DMACs MAX_BYTES_PER_BURST to 256
    - increased DMACs FIFO_SIZE to 32
    - increased dma_clk frequency to ~333 MHz
    - reduced dac_fifo_address_width to 14, as it's not large enough for actual data, so decreasing it improves timing

--- projects/adrv9009/zcu102/system_bd.tcl
+++ projects/adrv9009/zcu102/system_bd.tcl
@@ -1,15 +1,11 @@
 
 set dac_fifo_name axi_adrv9009_dacfifo
-set dac_fifo_address_width 17
+set dac_fifo_address_width 14
 set dac_data_width 128
 set dac_dma_data_width 128
 
 source $ad_hdl_dir/projects/common/zcu102/zcu102_system_bd.tcl
 source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
-
-ad_ip_parameter sys_ps8 CONFIG.PSU__FPGA_PL2_ENABLE 1
-ad_ip_parameter sys_ps8 CONFIG.PSU__CRL_APB__PL2_REF_CTRL__SRCSEL {IOPLL}
-ad_ip_parameter sys_ps8 CONFIG.PSU__CRL_APB__PL2_REF_CTRL__FREQMHZ 300
 
 ad_mem_hp0_interconnect sys_cpu_clk sys_ps8/S_AXI_HP0
 
@@ -18,10 +14,21 @@ source ../common/adrv9009_bd.tcl
 ad_ip_parameter axi_adrv9009_tx_dma CONFIG.DMA_DATA_WIDTH_SRC 128
 ad_ip_parameter axi_adrv9009_rx_dma CONFIG.DMA_DATA_WIDTH_DEST 128
 ad_ip_parameter axi_adrv9009_rx_os_dma CONFIG.DMA_DATA_WIDTH_DEST 128
-ad_ip_parameter axi_adrv9009_rx_dma CONFIG.FIFO_SIZE {16}
-ad_ip_parameter axi_adrv9009_rx_os_dma CONFIG.FIFO_SIZE {16}
+ad_ip_parameter axi_adrv9009_rx_dma CONFIG.FIFO_SIZE 32
+ad_ip_parameter axi_adrv9009_rx_os_dma CONFIG.FIFO_SIZE 32
+ad_ip_parameter axi_adrv9009_tx_dma CONFIG.FIFO_SIZE 32
 
-ad_connect sys_dma_clk sys_ps8/pl_clk2
+ad_ip_instance clk_wiz dma_clk_wiz
+ad_ip_parameter dma_clk_wiz CONFIG.PRIMITIVE MMCM
+ad_ip_parameter dma_clk_wiz CONFIG.RESET_TYPE ACTIVE_LOW
+ad_ip_parameter dma_clk_wiz CONFIG.USE_LOCKED false
+ad_ip_parameter dma_clk_wiz CONFIG.CLKOUT1_REQUESTED_OUT_FREQ 332.9
+ad_ip_parameter dma_clk_wiz CONFIG.PRIM_SOURCE No_buffer
+
+ad_connect sys_cpu_clk dma_clk_wiz/clk_in1
+ad_connect sys_cpu_resetn dma_clk_wiz/resetn
+
+ad_connect sys_dma_clk dma_clk_wiz/clk_out1
 ad_connect sys_dma_rstgen/ext_reset_in sys_rstgen/peripheral_reset
 
 ad_ip_parameter axi_adrv9009_tx_xcvr CONFIG.XCVR_TYPE 2
