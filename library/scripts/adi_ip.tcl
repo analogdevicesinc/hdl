9441f00f5fe703143de955f928e6bf083df2db0a
Istvan Csomortani <istvan.csomortani@analog.com>
2018-06-22 16:22:44 +0300

    adi_ip: Define the default driver value to 0 for unused ports

--- library/scripts/adi_ip.tcl
+++ library/scripts/adi_ip.tcl
@@ -71,7 +71,7 @@ proc adi_set_ports_dependency {port_prefix dependency {driver_value {}}} {
 
 proc adi_set_bus_dependency {bus prefix dependency} {
   set_property ENABLEMENT_DEPENDENCY $dependency [ipx::get_bus_interfaces $bus -of_objects [ipx::current_core]]
-  adi_set_ports_dependency $prefix $dependency
+  adi_set_ports_dependency $prefix $dependency 0
 }
 
 proc adi_add_port_map {bus phys logic} {
