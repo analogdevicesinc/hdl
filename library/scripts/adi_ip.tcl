b8243068db5bb3ecf527e0d87312ba7e117da6e8
Istvan Csomortani <istvan.csomortani@analog.com>
2018-06-25 11:29:22 +0300

    adi_board/adi_ip: Update Vivado version to 2018.2

--- library/scripts/adi_ip.tcl
+++ library/scripts/adi_ip.tcl
@@ -3,7 +3,7 @@
 ## check tool version
 
 if {![info exists REQUIRED_VIVADO_VERSION]} {
-  set REQUIRED_VIVADO_VERSION "2017.4.1"
+  set REQUIRED_VIVADO_VERSION "2018.2"
 }
 
 if {[info exists ::env(ADI_IGNORE_VERSION_CHECK)]} {
