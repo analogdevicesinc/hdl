10297250de3c21df547afe1ea079dabcef5f2119
AndreiGrozav <andrei.grozav@analog.com>
2018-07-10 10:57:23 +0100

    fmcomms2_fir_filters: Remove redundant comments

--- projects/fmcomms2_fir_filters/zc706/system_project.tcl
+++ projects/fmcomms2_fir_filters/zc706/system_project.tcl
@@ -3,9 +3,6 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-# set p_device "xc7z045ffg900-2"
-# set p_board "xilinx.com:zc706:part0:1.2"
-
 adi_project_create fmcomms2_zc706_fir_filters_zc706
 adi_project_files fmcomms2_zc706_fir_filters_zc706 [list \
   "$ad_hdl_dir/projects/fmcomms2/zc706/system_top.v" \
