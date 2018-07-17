89692b40b53247a7bcca2e14ec5836df39fb0c77
AndreiGrozav <andrei.grozav@analog.com>
2018-07-17 10:24:21 +0100

    axi_ad9144: Increase dds quality

--- library/axi_ad9144/axi_ad9144.v
+++ library/axi_ad9144/axi_ad9144.v
@@ -40,8 +40,8 @@ module axi_ad9144 #(
   parameter   ID = 0,
   parameter   QUAD_OR_DUAL_N = 1,
   parameter   DAC_DDS_TYPE = 1,
-  parameter   DAC_DDS_CORDIC_DW = 16,
-  parameter   DAC_DDS_CORDIC_PHASE_DW = 16,
+  parameter   DAC_DDS_CORDIC_DW = 20,
+  parameter   DAC_DDS_CORDIC_PHASE_DW = 18,
   parameter   DAC_DATAPATH_DISABLE = 0) (
 
   // jesd interface
