235636a337b4d19ee979598df1b816d508ec88d2
AndreiGrozav <andrei.grozav@analog.com>
2018-07-18 12:19:56 +0100

    fmcomms5_zc702: Enable AXI_SLICE for the DMA
    This will increase the timing margin for the design

--- projects/fmcomms5/zc702/system_bd.tcl
+++ projects/fmcomms5/zc702/system_bd.tcl
@@ -8,4 +8,6 @@ source ../common/fmcomms5_bd.tcl
 
 ad_ip_parameter axi_ad9361_0 CONFIG.ADC_INIT_DELAY 24
 ad_ip_parameter axi_ad9361_1 CONFIG.ADC_INIT_DELAY 24
+ad_ip_parameter axi_ad9361_adc_dma CONFIG.AXI_SLICE_DEST 1
+ad_ip_parameter axi_ad9361_dac_dma CONFIG.AXI_SLICE_SRC 1
 
