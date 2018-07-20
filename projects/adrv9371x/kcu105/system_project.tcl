2e007cd93f1d9fbc97b818293b9c939823a0da05
Istvan Csomortani <istvan.csomortani@analog.com>
2018-07-20 14:44:55 +0100

    adrv9371x:kcu105: Performance_Retiming results the highest WNS
    In default strategy we having a few path with small negative slack inside of
    the MIG, due to the high UI clock (300MHz).
    
    This new strategy solves this issue.

--- projects/adrv9371x/kcu105/system_project.tcl
+++ projects/adrv9371x/kcu105/system_project.tcl
@@ -11,4 +11,7 @@ adi_project_files adrv9371x_kcu105 [list \
   "$ad_hdl_dir/projects/common/kcu105/kcu105_system_constr.xdc" \
   "$ad_hdl_dir/projects/common/kcu105/kcu105_system_lutram_constr.xdc" ]
 
+## To improve timing in DDR4 MIG
+set_property strategy Performance_Retiming [get_runs impl_1]
+
 adi_project_run adrv9371x_kcu105
