f4cc89a7f311528d222cc7d5568645c13e9b4505
Adrian Costina <adrian.costina@analog.com>
2018-08-31 11:33:50 +0300

    fmcomms2: cleanup system_top for kc705,kcu105, vc707 and zcu102

--- projects/fmcomms2/zcu102/system_top.v
+++ projects/fmcomms2/zcu102/system_top.v
@@ -69,8 +69,8 @@ module system_top (
 
   // internal signals
 
-  wire    [95:0]  gpio_i;
-  wire    [95:0]  gpio_o;
+  wire    [94:0]  gpio_i;
+  wire    [94:0]  gpio_o;
   wire    [ 2:0]  spi0_csn;
 
   // defaults
@@ -81,7 +81,7 @@ module system_top (
   assign gpio_ctl = gpio_o[43:40];
   assign gpio_bd_o = gpio_o[20:13];
 
-  assign gpio_i[95:40] = 'h0;
+  assign gpio_i[94:40] = 'h0;
   assign gpio_i[39:32] = gpio_status;
   assign gpio_i[31:13] = 'h0;
   assign gpio_i[12: 0] = gpio_bd_i;
