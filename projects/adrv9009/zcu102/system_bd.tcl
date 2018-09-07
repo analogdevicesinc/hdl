2fb6f807496af5f9e14e785380c0595d3d6dbd7b
Adrian Costina <adrian.costina@analog.com>
2018-09-07 11:58:05 +0100

    axi_adrv9009: zcu102: set util_adxcvr parameters

--- projects/adrv9009/zcu102/system_bd.tcl
+++ projects/adrv9009/zcu102/system_bd.tcl
@@ -42,3 +42,27 @@ ad_ip_parameter util_adrv9009_xcvr CONFIG.QPLL_REFCLK_DIV 1
 ad_ip_parameter axi_adrv9009_rx_clkgen CONFIG.DEVICE_TYPE 2
 ad_ip_parameter axi_adrv9009_tx_clkgen CONFIG.DEVICE_TYPE 2
 
+ad_ip_parameter util_adrv9009_xcvr CONFIG.CH_HSPMUX  0x2424        ;#16'b0010010000100100
+ad_ip_parameter util_adrv9009_xcvr CONFIG.CPLL_CFG0 0x03fe         ;#16'b0000001111111110
+ad_ip_parameter util_adrv9009_xcvr CONFIG.CPLL_CFG1 0x021          ;#16'b0000000000100001
+ad_ip_parameter util_adrv9009_xcvr CONFIG.CPLL_CFG2 0x0203         ;#16'b0000001000000011
+ad_ip_parameter util_adrv9009_xcvr CONFIG.PREIQ_FREQ_BST 0
+ad_ip_parameter util_adrv9009_xcvr CONFIG.PCIE_BUFG_DIV_CTRL 0x3500;#16'b0011010100000000
+ad_ip_parameter util_adrv9009_xcvr CONFIG.PCIE_PLL_SEL_MODE_GEN12 2
+ad_ip_parameter util_adrv9009_xcvr CONFIG.RXCDR_CFG3 0x0012        ;#16'b0000000000010010
+ad_ip_parameter util_adrv9009_xcvr CONFIG.RXCDR_CFG3_GEN2 0x12     ;#6'b010010
+ad_ip_parameter util_adrv9009_xcvr CONFIG.RXCDR_CFG3_GEN3 0x0012   ;#16'b0000000000010010
+ad_ip_parameter util_adrv9009_xcvr CONFIG.RXPI_CFG0 0x0102         ;#16'b0000000100000010
+ad_ip_parameter util_adrv9009_xcvr CONFIG.RXPI_CFG1 0x0015         ;#16'b0000000000010101
+ad_ip_parameter util_adrv9009_xcvr CONFIG.RX_WIDEMODE_CDR 0
+ad_ip_parameter util_adrv9009_xcvr CONFIG.TXPH_CFG 0x0323          ;#16'b0000001100100011
+ad_ip_parameter util_adrv9009_xcvr CONFIG.TXPI_CFG 0x0054          ;#16'b0000000001010100
+ad_ip_parameter util_adrv9009_xcvr CONFIG.TXPI_CFG3 0
+ad_ip_parameter util_adrv9009_xcvr CONFIG.TXPI_CFG4 1
+ad_ip_parameter util_adrv9009_xcvr CONFIG.TX_PI_BIASSET 1
+
+ad_ip_parameter util_adrv9009_xcvr CONFIG.PPF0_CFG 0x0600      ;#16'b0000011000000000
+ad_ip_parameter util_adrv9009_xcvr CONFIG.QPLL0_CFG2 0x0fc1    ;#16'b0000111111000001
+ad_ip_parameter util_adrv9009_xcvr CONFIG.QPLL0_CFG2_G3 0x0fc1 ;#16'b0000111111000001
+ad_ip_parameter util_adrv9009_xcvr CONFIG.QPLL0_CFG4 0x0003    ;#16'b0000000000000011
+ad_ip_parameter util_adrv9009_xcvr CONFIG.QPLL0_LPF 0x37f      ;#10'b1101111111
