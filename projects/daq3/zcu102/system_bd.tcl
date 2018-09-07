c7ee685bc6ee47e0cfaa66a3571076bcc19bd30d
Adrian Costina <adrian.costina@analog.com>
2018-09-07 11:59:33 +0100

    daq3: zcu102: set util_adxcvr parameters

--- projects/daq3/zcu102/system_bd.tcl
+++ projects/daq3/zcu102/system_bd.tcl
@@ -20,6 +20,31 @@ ad_ip_parameter util_daq3_xcvr CONFIG.XCVR_TYPE 2
 ad_ip_parameter util_daq3_xcvr CONFIG.QPLL_FBDIV 20
 ad_ip_parameter util_daq3_xcvr CONFIG.QPLL_REFCLK_DIV 1
 
+ad_ip_parameter util_daq3_xcvr CONFIG.CH_HSPMUX  0x4444        ;#16'b0100010001000100
+ad_ip_parameter util_daq3_xcvr CONFIG.CPLL_CFG0  0x03fe        ;#16'b0000001111111110
+ad_ip_parameter util_daq3_xcvr CONFIG.CPLL_CFG1  0x0021        ;#16'b0000000000100001
+ad_ip_parameter util_daq3_xcvr CONFIG.CPLL_CFG2  0x0203        ;#16'b0000001000000011
+ad_ip_parameter util_daq3_xcvr CONFIG.PREIQ_FREQ_BST 1
+ad_ip_parameter util_daq3_xcvr CONFIG.PCIE_BUFG_DIV_CTRL 0x3500;#16'b0011010100000000
+ad_ip_parameter util_daq3_xcvr CONFIG.PCIE_PLL_SEL_MODE_GEN12 2
+ad_ip_parameter util_daq3_xcvr CONFIG.RXCDR_CFG3 0x001a        ;#16'b0000000000011010
+ad_ip_parameter util_daq3_xcvr CONFIG.RXCDR_CFG3_GEN2 0x1a     ;#6'b011010
+ad_ip_parameter util_daq3_xcvr CONFIG.RXCDR_CFG3_GEN3 0x001a   ;#16'b0000000000011010
+ad_ip_parameter util_daq3_xcvr CONFIG.RXPI_CFG0 0x2004         ;#16'b0010000000000100
+ad_ip_parameter util_daq3_xcvr CONFIG.RXPI_CFG1 0x0000         ;#16'b0000000000000000
+ad_ip_parameter util_daq3_xcvr CONFIG.RX_WIDEMODE_CDR 1
+ad_ip_parameter util_daq3_xcvr CONFIG.TXPH_CFG 0x0323          ;#16'b0000001100100011
+ad_ip_parameter util_daq3_xcvr CONFIG.TXPI_CFG 0x0000          ;#16'b0000000000000000
+ad_ip_parameter util_daq3_xcvr CONFIG.TXPI_CFG3 1
+ad_ip_parameter util_daq3_xcvr CONFIG.TXPI_CFG4 0
+ad_ip_parameter util_daq3_xcvr CONFIG.TX_PI_BIASSET 2
+
+ad_ip_parameter util_daq3_xcvr CONFIG.PPF0_CFG 0x0800      ;#16'b0000100000000000
+ad_ip_parameter util_daq3_xcvr CONFIG.QPLL0_CFG2 0x0fc1    ;#16'b0000111111000001
+ad_ip_parameter util_daq3_xcvr CONFIG.QPLL0_CFG2_G3 0x0fc1 ;#16'b0000111111000001
+ad_ip_parameter util_daq3_xcvr CONFIG.QPLL0_CFG4 0x0004    ;#16'b0000000000000100
+ad_ip_parameter util_daq3_xcvr CONFIG.QPLL0_LPF 0x37f      ;#10'b1101111111
+
 ad_ip_parameter axi_ad9152_dma CONFIG.FIFO_SIZE 32
 ad_ip_parameter axi_ad9152_dma CONFIG.AXI_SLICE_SRC 1
 ad_ip_parameter axi_ad9152_dma CONFIG.AXI_SLICE_DEST 1
