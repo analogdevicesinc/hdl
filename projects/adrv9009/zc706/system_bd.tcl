929d05dfc6d248399e29db49f227c35bbd53562e
Laszlo Nagy <laszlo.nagy@analog.com>
2018-10-11 09:21:10 +0100

    adrv9009: use generic TPL
    Make the block design parametrizable.
    Limitations:
      F = 1,2,4

--- projects/adrv9009/zc706/system_bd.tcl
+++ projects/adrv9009/zc706/system_bd.tcl
@@ -1,7 +1,7 @@
 
 set dac_fifo_name axi_adrv9009_dacfifo
 set dac_fifo_address_width 10
-set dac_data_width 128
+set dac_data_width 128     ; # should be 32*L (number of TX lanes)
 set dac_dma_data_width 128
 
 source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
