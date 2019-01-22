bed5ce516ca68af7dd506d26c0d592a3f68183ab
Laszlo Nagy <laszlo.nagy@analog.com>
2019-01-22 13:29:29 +0000

    adcfifo/dacfifo: fix alignments

--- projects/common/altera/dacfifo_qsys.tcl
+++ projects/common/altera/dacfifo_qsys.tcl
@@ -1,13 +1,13 @@
 
 proc ad_dacfifo_create {dac_fifo_name dac_data_width dac_dma_data_width dac_fifo_address_width} {
 
-if {$dac_data_width != $dac_dma_data_width} {
-  return -code error [format "ERROR: util_dacfifo dac/dma widths must be the same!"]
-}
+  if {$dac_data_width != $dac_dma_data_width} {
+    return -code error [format "ERROR: util_dacfifo dac/dma widths must be the same!"]
+  }
 
-add_instance $dac_fifo_name util_dacfifo
-set_instance_parameter_value $dac_fifo_name {ADDRESS_WIDTH} $dac_fifo_address_width
-set_instance_parameter_value $dac_fifo_name {DATA_WIDTH} $dac_data_width
+  add_instance $dac_fifo_name util_dacfifo
+  set_instance_parameter_value $dac_fifo_name {ADDRESS_WIDTH} $dac_fifo_address_width
+  set_instance_parameter_value $dac_fifo_name {DATA_WIDTH} $dac_data_width
 
 }
 
--- projects/common/xilinx/dacfifo_bd.tcl
+++ projects/common/xilinx/dacfifo_bd.tcl
@@ -2,12 +2,12 @@
 # generic fifo interface - existence is oblivious to software.
 proc ad_dacfifo_create {dac_fifo_name dac_data_width dac_dma_data_width dac_fifo_address_width} {
 
-if {$dac_data_width != $dac_dma_data_width} {
-  return -code error [format "ERROR: util_dacfifo dac/dma widths must be the same!"]
-}
+  if {$dac_data_width != $dac_dma_data_width} {
+    return -code error [format "ERROR: util_dacfifo dac/dma widths must be the same!"]
+  }
 
-ad_ip_instance util_dacfifo $dac_fifo_name
-ad_ip_parameter $dac_fifo_name CONFIG.DATA_WIDTH $dac_data_width
-ad_ip_parameter $dac_fifo_name CONFIG.ADDRESS_WIDTH $dac_fifo_address_width
+  ad_ip_instance util_dacfifo $dac_fifo_name
+  ad_ip_parameter $dac_fifo_name CONFIG.DATA_WIDTH $dac_data_width
+  ad_ip_parameter $dac_fifo_name CONFIG.ADDRESS_WIDTH $dac_fifo_address_width
 
 }
