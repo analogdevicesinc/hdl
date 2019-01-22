942e623f7b8cea6c01404b611fcdd56910ccd98e
Laszlo Nagy <laszlo.nagy@analog.com>
2019-01-22 13:19:09 +0000

    adrv9371: update adcfifo/dacfifo

--- projects/adrv9371x/zc706/system_bd.tcl
+++ projects/adrv9371x/zc706/system_bd.tcl
@@ -1,8 +1,5 @@
 
-set dac_fifo_name axi_ad9371_dacfifo
 set dac_fifo_address_width 10
-set dac_data_width 128
-set dac_dma_data_width 128
 
 source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
 source $ad_hdl_dir/projects/common/zc706/zc706_plddr3_dacfifo_bd.tcl
