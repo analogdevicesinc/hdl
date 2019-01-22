a3766b464bc4af434a82e958672bd2423caa7401
Laszlo Nagy <laszlo.nagy@analog.com>
2019-01-22 13:17:02 +0000

    adcfifo/dacfifo: Use proc to create infrastructure
    Create the dacfifo/adcfifo infrastructure with procedures.
    This will allow moving the parameters of the dac/adcfifo inside
    the block design so it can be calculated based on other parameters.

--- projects/common/xilinx/adcfifo_bd.tcl
+++ projects/common/xilinx/adcfifo_bd.tcl
@@ -1,10 +1,13 @@
 # sys bram (use only when dma is not capable of keeping up).
 # generic fifo interface - existence is oblivious to software.
 
+proc ad_adcfifo_create {adc_fifo_name adc_data_width adc_dma_data_width adc_fifo_address_width} {
+
 ad_ip_instance util_adcfifo $adc_fifo_name
 ad_ip_parameter $adc_fifo_name CONFIG.ADC_DATA_WIDTH $adc_data_width
 ad_ip_parameter $adc_fifo_name CONFIG.DMA_DATA_WIDTH $adc_dma_data_width
 ad_ip_parameter $adc_fifo_name CONFIG.DMA_READY_ENABLE 1
 ad_ip_parameter $adc_fifo_name CONFIG.DMA_ADDRESS_WIDTH $adc_fifo_address_width
 
+}
 
