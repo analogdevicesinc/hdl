6c12a5661a3eb4c8fa9052f81f8a8ee9d501ba4e
Laszlo Nagy <laszlo.nagy@analog.com>
2019-01-22 13:19:37 +0000

    daq2: update adcfifo/dacfifo

--- projects/daq2/a10gx/system_qsys.tcl
+++ projects/daq2/a10gx/system_qsys.tcl
@@ -1,8 +1,5 @@
 
-set dac_fifo_name avl_ad9144_fifo
 set dac_fifo_address_width 10
-set dac_data_width 128
-set dac_dma_data_width 128
 
 source $ad_hdl_dir/projects/common/a10gx/a10gx_system_qsys.tcl
 source $ad_hdl_dir/projects/common/altera/dacfifo_qsys.tcl
--- projects/daq2/kc705/system_bd.tcl
+++ projects/daq2/kc705/system_bd.tcl
@@ -1,15 +1,9 @@
 
 ## FIFO depth is 4Mb - 250k samples
-set adc_fifo_name axi_ad9680_fifo
 set adc_fifo_address_width 16
-set adc_data_width 128
-set adc_dma_data_width 64
 
 ## FIFO depth is 4Mb - 250k samples
-set dac_fifo_name axi_ad9144_fifo
 set dac_fifo_address_width 15
-set dac_data_width 128
-set dac_dma_data_width 128
 
 ## NOTE: With this configuration the #36Kb BRAM utilization is at ~80%
 
