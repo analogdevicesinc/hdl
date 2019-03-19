4f6c59f0d5160fa8367769dc18db4ef2fc74d22b
Istvan Csomortani <istvan.csomortani@analog.com>
2019-03-19 12:05:14 +0000

    axi_laser_driver: Initial commit
    The laser driver contains the axi_pulse_gen's IP and an additional
    register map which controls/monitor the laser driver enable control line
    and the over temperature warning line (OTW).
    
    It also contains an interrupt logic, which allows to generate an
    interrupt in function of the generated pulse or incoming OTW signal.
    
    The IPs register maps looks as follow:
    
    0x00 - axi_pulse_gen register map
    0x80 - axi_laser_driver register map
      0x80 - DRIVER_ENABLE
      0x84 - DRIVER_OTW
      0x88 - EXT_CLK_COUNTER
      0xA0 - IRQ_MASK
      0xA4 - IRQ_SOURCE
      0xA8 - IRQ_PENDING
      0xAC - SEQUENCER_CONTROL
             0 - SEQUENCER_ENABLE
             1 - AUTO_SEQUENCER_ENABLED
      0xB0 - SEQUENCER_SYNC_OFFSET
      0xB4 - AUTO_SEQUENCE
             [ 1: 0] - CHANNEL_SEL_0
             [ 5: 4] - CHANNEL_SEL_1
             [ 9: 8] - CHANNEL_SEL_2
             [13:12] - CHANNEL_SEL_3
      0xB8 - MANUAL_SEQUENCE
             [ 1: 0] - MANUAL_CHANNEL_SEL
    
    Current interrupt sources scheme is:
        - bit 0 : pulse (triggered by the level of the pulse)
        - bit 1 : OTW_N enter (triggered by positive edge of the OTW_N)
        - bit 2 : OTW_N exit (triggered by the level of the pulse)
    
    Generate a reset signal before the pulse which can be used to reset
    various IP's of the data path (eg. pack/cpack). This can help to clear out the
    internal buffers and registers of these IP, starting clean at the moment when
    the actual pulse arrives.
    
    The sequencer has an auto and a manual mode, and can be set to custom
    sequences of the TIA channel selection lines sate.
    
    The sequencer in auto mode is synchronized to the pulse, it will change
    its state before a generated pulse which will drive the lasers. The
    offset between the sequencer beat and the laser driver pulse can be
    modified through an AXI register.

---
+++ library/axi_laser_driver/axi_laser_driver_constr.xdc
@@ -0,0 +1,20 @@
+
+set_property ASYNC_REG TRUE \
+  [get_cells -hier {*cdc_sync_stage1_reg*}] \
+  [get_cells -hier {*cdc_sync_stage2_reg*}]
+
+set_false_path \
+  -to [get_pins -hierarchical * -filter {NAME=~*i_driver_otw_sync/cdc_sync_stage1_reg[0]/D}]
+
+set_false_path \
+  -to [get_pins -hierarchical * -filter {NAME=~*i_pulse_sync/cdc_sync_stage1_reg[0]/D}]
+
+set_false_path \
+  -to [get_pins -hierarchical * -filter {NAME=~*i_sequencer_sync/cdc_sync_stage1_reg[*]/D}]
+
+set_false_path \
+  -to [get_pins -hierarchical * -filter {NAME=~*i_sequence_offset_sync/cdc_sync_stage1_reg[*]/D}]
+
+set_false_path \
+  -to [get_pins -hierarchical * -filter {NAME=~*i_sequence_control_sync/cdc_sync_stage1_reg[*]/D}]
+
---
+++ library/axi_laser_driver/axi_laser_driver_ip.tcl
@@ -0,0 +1,96 @@
+# ip
+
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip.tcl
+
+adi_ip_create axi_laser_driver
+adi_ip_files axi_laser_driver [list \
+  "$ad_hdl_dir/library/common/up_clock_mon.v" \
+  "axi_laser_driver_constr.xdc" \
+  "axi_laser_driver_regmap.v" \
+  "axi_laser_driver.v"]
+
+adi_ip_properties axi_laser_driver
+adi_ip_ttcl axi_laser_driver "$ad_hdl_dir/library/axi_pulse_gen/axi_pulse_gen_constr.ttcl"
+
+adi_ip_add_core_dependencies { \
+	analog.com:user:util_cdc:1.0 \
+	analog.com:user:axi_pulse_gen:1.0 \
+}
+
+set cc [ipx::current_core]
+
+set_property display_name "ADI AXI Laser Driver" $cc
+set_property description  "ADI AXI Laser Driver" $cc
+
+## define ext_clk port as a clock interface
+adi_add_bus ext_clk slave \
+  "xilinx.com:signal:clock_rtl:1.0" \
+  "xilinx.com:signal:clock:1.0" \
+  [list {"ext_clk" "CLK"} ]
+
+adi_set_ports_dependency "ext_clk" \
+  "(spirit:decode(id('MODELPARAM_VALUE.ASYNC_CLK_EN')) = 1)" 0
+
+## Parameter validation
+
+set_property -dict [list \
+  "value_format" "bool" \
+  "value" "true" \
+ ] \
+[ipx::get_user_parameters ASYNC_CLK_EN -of_objects $cc]
+
+set_property -dict [list \
+  "value_format" "bool" \
+  "value" "true" \
+ ] \
+[ipx::get_hdl_parameters ASYNC_CLK_EN -of_objects $cc]
+
+set_property -dict [list \
+  "value_validation_type" "range_long" \
+  "value_validation_range_minimum" "0" \
+  "value_validation_range_maximum" "2147483647" \
+ ] \
+[ipx::get_user_parameters PULSE_WIDTH -of_objects $cc]
+
+set_property -dict [list \
+  "value_validation_type" "range_long" \
+  "value_validation_range_minimum" "0" \
+  "value_validation_range_maximum" "2147483647" \
+ ] \
+[ipx::get_user_parameters PULSE_PERIOD -of_objects $cc]
+
+## Customize XGUI layout
+
+## Remove the automatically generated GUI page
+ipgui::remove_page -component $cc [ipgui::get_pagespec -name "Page 0" -component $cc]
+ipx::save_core $cc
+
+## Create a new GUI page
+
+ipgui::add_page -name {AXI Pulse Generator} -component $cc -display_name {AXI Pulse Generator}
+set page0 [ipgui::get_pagespec -name "AXI Pulse Generator" -component $cc]
+
+ipgui::add_param -name "ASYNC_CLK_EN" -component $cc -parent $page0
+set_property -dict [list \
+  "display_name" "ASYNC_CLK_EN" \
+  "tooltip" "External clock for the counter" \
+  "widget" "checkBox" \
+] [ipgui::get_guiparamspec -name "ASYNC_CLK_EN" -component $cc]
+
+ipgui::add_param -name "PULSE_WIDTH" -component $cc -parent $page0
+set_property -dict [list \
+  "display_name" "Pulse width" \
+  "tooltip" "Pulse width of the generated signal. The unit interval is the system or external clock period." \
+] [ipgui::get_guiparamspec -name "PULSE_WIDTH" -component $cc]
+
+ipgui::add_param -name "PULSE_PERIOD" -component $cc -parent $page0
+set_property -dict [list \
+  "display_name" "Pulse period" \
+  "tooltip" "Period of the generated signal. The unit interval is the system or external clock period." \
+] [ipgui::get_guiparamspec -name "PULSE_PERIOD" -component $cc]
+
+## Save the modifications
+
+ipx::create_xgui_files $cc
+ipx::save_core $cc
