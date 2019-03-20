c7098a9d49568661def90ba8f890fa263e6ad46f
Sergiu Arpadi <sergiu.arpadi@analog.com>
2019-03-20 16:13:21 +0000

    axi_fan_control: Initial commit

---
+++ library/axi_fan_control/axi_fan_control_ip.tcl
@@ -0,0 +1,15 @@
+# ip
+
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip.tcl
+
+adi_ip_create axi_fan_control
+adi_ip_files axi_fan_control [list \
+  "$ad_hdl_dir/library/common/up_axi.v" \
+  "$ad_hdl_dir/library/common/util_pulse_gen.v" \
+  "axi_fan_control.v"]
+
+adi_ip_properties axi_fan_control
+set cc [ipx::current_core]
+
+ipx::save_core $cc
