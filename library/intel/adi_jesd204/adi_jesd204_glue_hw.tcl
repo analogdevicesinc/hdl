54bd1f6264bdcb11bcfb49ac05321f2d5d03d16d
Istvan Csomortani <istvan.csomortani@analog.com>
2019-04-16 08:18:28 +0100

    jesd204_framework: Add Stratix10 support
    This patch contains an initial effort to support the Stratix 10
    architecture in our JESD204 framework.
    
    Several instances were updated, doing simple context switching using the
    DEVICE_FAMILY system parameter:
    
      - xcvr_reset_control
      - lane PLL (ATX PLL)
      - link PLL (fPLL)
      - native XCVR instance
    
    Apart from the slightly different parameters of the instances above,
    there were small differences at the reconfiguration Avalon_MM interface.
    
    The link_pll_reset_control is required just for Arria10, so in case of
    Stratix10 it isn't instantiated.
    
    In Stratix 10 architecture there are several additional ports of the
    xcvr_reset_control module that must be connected to the native XCVR
    instance or tied to GND.
    
    The following xcvr_reset_control ports were defined and connected to the
    XCVR:
    
      - rx|tx_analogreset_stat
      - rx|tx_digitalreset_stat
      - pll_select

--- library/intel/adi_jesd204/adi_jesd204_glue_hw.tcl
+++ library/intel/adi_jesd204/adi_jesd204_glue_hw.tcl
@@ -58,11 +58,18 @@ ad_ip_files adi_jesd204_glue [list \
 
 # parameters
 
+ad_ip_parameter IN_PLL_POWERDOWN_EN BOOLEAN 1 false
+
 proc jesd204_phy_glue_elab {} {
   add_interface in_pll_powerdown conduit end
   add_interface_port in_pll_powerdown in_pll_powerdown pll_powerdown Input 1
+  set_interface_property in_pll_powerdown ENABLED [get_parameter IN_PLL_POWERDOWN_EN]
+
   add_interface out_pll_powerdown conduit end
   add_interface_port out_pll_powerdown out_pll_powerdown pll_powerdown Output 1
   add_interface out_mcgb_rst conduit end
   add_interface_port out_mcgb_rst out_mcgb_rst mcgb_rst Output 1
+
+  add_interface out_pll_select_gnd conduit end
+  add_interface_port out_pll_select_gnd out_pll_select_gnd pll_select Output 1
 }
