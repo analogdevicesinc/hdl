525c0689932c430899e02c3512a868575ee6a467
Laszlo Nagy <laszlo.nagy@analog.com>
2019-04-17 11:33:17 +0100

    scripts: Support for incremental compilation
    The scripts are looking for a previous run result, a routed design
    checkpoint to use it as a reference during the incremental build flow.
    Before clearing the project files, the scrips will save the reference dcp
    file in the project folder.
    If the reference dcp does not exists the build continues normally.
    
    Proposed workflow:
      1. Build your project normally with 'make' or place manually a
      reference.dcp file in the Vivado project folder.
      2. Do some minor modifications
      3. Run the make with the following option:
        make MODE=incr
      4. Repeat steps 2-3

--- projects/scripts/project-xilinx.mk
+++ projects/scripts/project-xilinx.mk
@@ -33,13 +33,15 @@ M_DEPS += $(wildcard system_constr.xdc) # Not all projects have this file
 M_DEPS += $(HDL_PROJECT_PATH)scripts/adi_project.tcl
 M_DEPS += $(HDL_PROJECT_PATH)scripts/adi_env.tcl
 M_DEPS += $(HDL_PROJECT_PATH)scripts/adi_board.tcl
+M_DEPS += prepare_incremental_compile
 
 M_DEPS += $(foreach dep,$(LIB_DEPS),$(HDL_LIBRARY_PATH)$(dep)/component.xml)
 
-.PHONY: all lib clean clean-all
+.PHONY: all lib clean clean-all prepare_incremental_compile 
 all: lib $(PROJECT_NAME).sdk/system_top.hdf
 
 clean:
+	-rm -f reference.dcp
 	$(call clean, \
 		$(CLEAN_TARGET), \
 		$(HL)$(PROJECT_NAME)$(NC) project)
@@ -48,6 +50,19 @@ clean-all: clean
 	@for lib in $(LIB_DEPS); do \
 		$(MAKE) -C $(HDL_LIBRARY_PATH)$${lib} clean; \
 	done
+
+MODE ?= "default"
+
+prepare_incremental_compile:
+	@if [ $(MODE) = incr ]; then \
+		if [ -f */impl_1/system_top_routed.dcp ]; then \
+			echo Found previous run result at `ls */impl_1/system_top_routed.dcp`; \
+			cp -u */impl_1/system_top_routed.dcp ./reference.dcp ; \
+		fi; \
+		if [ -f ./reference.dcp ]; then \
+			echo Using reference checkpoint for incremental compilation; \
+		fi; \
+	fi; 
 
 $(PROJECT_NAME).sdk/system_top.hdf: $(M_DEPS)
 	-rm -rf $(CLEAN_TARGET)
