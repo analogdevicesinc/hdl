wueesmic/ad-ipfn-hdl

util_dacfifo: set OOC default clock constraints
Out of Context constraints are needed for timing driven synthesis as for
avoiding critical warnings due clock queries.
The memory from the FIFO is inferred in different ways for high clock
speeds. Assume the highest frequency for all projects.

library/util_dacfifo/util_dacfifo_ip.tcl
@@ -11,9 +11,13 @@ adi_ip_files util_dacfifo [list \
   "$ad_hdl_dir/library/common/ad_g2b.v" \
   "util_dacfifo.v" \
   "util_dacfifo_bypass.v" \
+  "util_dacfifo_ooc.ttcl" \
   "util_dacfifo_constr.xdc"]
 
+
 adi_ip_properties_lite util_dacfifo
+
+adi_ip_ttcl axi_dmac "util_dacfifo_ooc.ttcl"
 
 ipx::infer_bus_interface dma_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
 ipx::infer_bus_interface dma_rst xilinx.com:signal:reset_rtl:1.0 [ipx::current_core]

analogdevicesinc/hdl

library/util_dacfifo/util_dacfifo_ip.tcl
@@ -11,9 +11,13 @@ adi_ip_files util_dacfifo [list \
   "$ad_hdl_dir/library/common/ad_g2b.v" \
   "util_dacfifo.v" \
   "util_dacfifo_bypass.v" \
+  "util_dacfifo_ooc.ttcl" \
   "util_dacfifo_constr.xdc"]
 
+
 adi_ip_properties_lite util_dacfifo
+
+adi_ip_ttcl util_dacfifo "util_dacfifo_ooc.ttcl"
 
 ipx::infer_bus_interface dma_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
 ipx::infer_bus_interface dma_rst xilinx.com:signal:reset_rtl:1.0 [ipx::current_core]