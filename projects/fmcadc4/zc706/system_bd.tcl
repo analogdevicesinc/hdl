1c8e71ec4e229c7184eb75fbd7d3936fb9ad49d2
Adrian Costina <adrian.costina@analog.com>
2019-05-16 15:10:04 +0100

    fmcadc4: Remove project

--- projects/fmcadc4/zc706/system_bd.tcl
+++
@@ -1,7 +0,0 @@
-
-set adc_fifo_address_width 18
-
-source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
-source $ad_hdl_dir/projects/common/zc706/zc706_plddr3_adcfifo_bd.tcl
-source ../common/fmcadc4_bd.tcl
-
