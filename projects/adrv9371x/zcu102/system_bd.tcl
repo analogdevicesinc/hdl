ea98aceae1bd3e3633d6606aeecb996fe936e201
Istvan Csomortani <istvan.csomortani@analog.com>
2019-05-29 07:17:00 +0100

    adrv9371x: Clean out system_db.tcl

--- projects/adrv9371x/zcu102/system_bd.tcl
+++ projects/adrv9371x/zcu102/system_bd.tcl
@@ -7,14 +7,7 @@ set dac_fifo_address_width 17
 source $ad_hdl_dir/projects/common/zcu102/zcu102_system_bd.tcl
 source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
 
-ad_ip_parameter sys_ps8 CONFIG.PSU__FPGA_PL2_ENABLE 1
-ad_ip_parameter sys_ps8 CONFIG.PSU__CRL_APB__PL2_REF_CTRL__SRCSEL {IOPLL}
-ad_ip_parameter sys_ps8 CONFIG.PSU__CRL_APB__PL2_REF_CTRL__FREQMHZ 200
-
 source ../common/adrv9371x_bd.tcl
-
-ad_connect sys_dma_clk sys_ps8/pl_clk2
-ad_connect sys_dma_rstgen/ext_reset_in sys_rstgen/peripheral_reset
 
 ad_ip_parameter axi_ad9371_tx_xcvr CONFIG.TX_DIFFCTRL 6
 
