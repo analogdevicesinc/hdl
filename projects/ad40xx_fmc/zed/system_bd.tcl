ddb80b776b5eef1814cb2df3a729b87c02903d56
Sergiu Arpadi <sergiu.arpadi@analog.com>
2019-05-29 09:18:52 +0100

    ad40xx/zed: fixed system_bd
    spi_engine_execution: fixed sdo default

--- projects/ad40xx_fmc/zed/system_bd.tcl
+++ projects/ad40xx_fmc/zed/system_bd.tcl
@@ -2,7 +2,7 @@
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 
 # specify the spi reference clock frequency in MHz
-set spi_clk_ref_frequency 160
+set spi_clk_ref_frequency 166
 
 # specify ADC resolution -- supported resolutions 16/18/20 bits
 set adc_resolution 20
