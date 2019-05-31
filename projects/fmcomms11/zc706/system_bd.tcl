58d55f61dbaef0de7180dd67ea7ff30e8f4c3684
Istvan Csomortani <istvan.csomortani@analog.com>
2019-05-31 10:27:14 +0300

    fmcomms11: Add desciption how to swap memory resource for the FIFOs

--- projects/fmcomms11/zc706/system_bd.tcl
+++ projects/fmcomms11/zc706/system_bd.tcl
@@ -1,13 +1,19 @@
 
+# instantiate the base design
 source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
+
+# load all the FIFO related proccesses
 source $ad_hdl_dir/projects/common/zc706/zc706_plddr3_adcfifo_bd.tcl
 source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
+# NOTE: to swap the resources comment the two lines above, and uncomment to two line below
+#source $ad_hdl_dir/projects/common/zc706/zc706_plddr3_dacfifo_bd.tcl
+#source $ad_hdl_dir/projects/common/xilinx/adcfifo_bd.tcl
 
 # the DAC FIFO has a 500KSMP depth - 1 Mbyte
 set dac_fifo_address_width 15
 
 # by default PLDDR is used (1 Gbyte), this varible should be ignored
-set adc_fifo_address_width 18
+set adc_fifo_address_width 15
 
 source ../common/fmcomms11_bd.tcl
 
