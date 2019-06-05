8694611dd651e4e2b49e7ee9086cfc40da27da99
Istvan Csomortani <istvan.csomortani@analog.com>
2019-06-05 15:25:57 +0300

    fmcomms2:fmcomms5: ZCU102 uses 500MHz IO delay clock

--- projects/fmcomms2/zcu102/system_bd.tcl
+++ projects/fmcomms2/zcu102/system_bd.tcl
@@ -5,4 +5,5 @@ source ../common/fmcomms2_bd.tcl
 ad_ip_parameter util_ad9361_divclk CONFIG.SIM_DEVICE ULTRASCALE
 
 ad_ip_parameter axi_ad9361 CONFIG.ADC_INIT_DELAY 11
+ad_ip_parameter axi_ad9361 CONFIG.DELAY_REFCLK_FREQUENCY 500
 
--- projects/fmcomms5/zcu102/system_bd.tcl
+++ projects/fmcomms5/zcu102/system_bd.tcl
@@ -4,6 +4,8 @@ source $ad_hdl_dir/projects/common/zcu102/zcu102_system_bd.tcl
 source ../common/fmcomms5_bd.tcl
 
 ad_ip_parameter axi_ad9361_0 CONFIG.ADC_INIT_DELAY 8
+ad_ip_parameter axi_ad9361_0 CONFIG.DELAY_REFCLK_FREQUENCY 500
 ad_ip_parameter axi_ad9361_1 CONFIG.ADC_INIT_DELAY 8
+ad_ip_parameter axi_ad9361_1 CONFIG.DELAY_REFCLK_FREQUENCY 500
 
 ad_ip_parameter util_ad9361_divclk CONFIG.SIM_DEVICE ULTRASCALE
