6a721c0bf0111fc492533429ad974d9088e148be
Istvan Csomortani <istvan.csomortani@analog.com>
2019-07-12 10:24:48 +0300

    adi_env: Update system level environment variable definition
    Our internal repository was changed from phdl to ghdl. Update the
    adi_env.tcl scripts and other scripts, which depends on the $ad_ghdl_dir
    variable. This way the tools will see all the internal IPs too.

--- library/scripts/adi_env.tcl
+++ library/scripts/adi_env.tcl
@@ -2,14 +2,16 @@
 # environment related stuff
 
 set ad_hdl_dir [file normalize [file join [file dirname [info script]] "../.."]]
-set ad_phdl_dir $ad_hdl_dir
+
+# assuming that ghdl is cloned next to hdl repository
+set ad_ghdl_dir [file normalize [file join [file dirname [info script]] "../../../ghdl"]]
 
 
 if [info exists ::env(ADI_HDL_DIR)] {
   set ad_hdl_dir [file normalize $::env(ADI_HDL_DIR)]
 }
 
-if [info exists ::env(ADI_PHDL_DIR)] {
-  set ad_phdl_dir [file normalize $::env(ADI_PHDL_DIR)]
+if [info exists ::env(ADI_GHDL_DIR)] {
+  set ad_ghdl_dir [file normalize $::env(ADI_GHDL_DIR)]
 }
 
