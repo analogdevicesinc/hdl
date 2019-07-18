71584cdd673ab3e6052ee722c505252eaadad758
Laszlo Nagy <laszlo.nagy@analog.com>
2019-07-18 07:26:24 +0100

    up_delay_cntrl:ad_serdes_in: Make delay value width parametrizable
    US/US+ devices have IDELAY/ODELAY with 512 taps. This requires wider
    control value for delay selection. 9 bits contrary to 5 on 7series.

--- library/common/up_delay_cntrl.v
+++ library/common/up_delay_cntrl.v
@@ -42,6 +42,7 @@ module up_delay_cntrl #(
   parameter   DISABLE = 0,
   parameter   INIT_DELAY = 0,
   parameter   DATA_WIDTH = 8,
+  parameter   DRP_WIDTH = 5,
   parameter   BASE_ADDRESS = 6'h02) (
 
   // delay interface
@@ -53,8 +54,8 @@ module up_delay_cntrl #(
   // io interface
 
   output  [(DATA_WIDTH-1):0]      up_dld,
-  output  [((DATA_WIDTH*5)-1):0]  up_dwdata,
-  input   [((DATA_WIDTH*5)-1):0]  up_drdata,
+  output  [((DATA_WIDTH*DRP_WIDTH)-1):0]  up_dwdata,
+  input   [((DATA_WIDTH*DRP_WIDTH)-1):0]  up_drdata,
 
   // processor interface
 
@@ -92,22 +93,22 @@ module up_delay_cntrl #(
   reg                             up_dlocked_m3 = 'd0;
   reg                             up_dlocked = 'd0;
   reg     [(DATA_WIDTH-1):0]      up_dld_int = 'd0;
-  reg     [((DATA_WIDTH*5)-1):0]  up_dwdata_int = 'd0;
+  reg     [((DATA_WIDTH*DRP_WIDTH)-1):0]  up_dwdata_int = 'd0;
 
   // internal signals
 
   wire                            up_wreq_s;
   wire                            up_rreq_s;
-  wire    [ 4:0]                  up_rdata_s;
+  wire    [ DRP_WIDTH-1:0]        up_rdata_s;
   wire    [(DATA_WIDTH-1):0]      up_drdata4_s;
   wire    [(DATA_WIDTH-1):0]      up_drdata3_s;
   wire    [(DATA_WIDTH-1):0]      up_drdata2_s;
   wire    [(DATA_WIDTH-1):0]      up_drdata1_s;
   wire    [(DATA_WIDTH-1):0]      up_drdata0_s;
   wire    [(DATA_WIDTH-1):0]      up_dld_s;
-  wire    [((DATA_WIDTH*5)-1):0]  up_dwdata_s;
+  wire    [((DATA_WIDTH*DRP_WIDTH)-1):0]  up_dwdata_s;
   wire    [(DATA_WIDTH-1):0]      up_dinit_s;
-  wire    [((DATA_WIDTH*5)-1):0]  up_dinitdata_s;
+  wire    [((DATA_WIDTH*DRP_WIDTH)-1):0]  up_dinitdata_s;
   wire                            delay_rst_s;
 
   // variables
@@ -118,19 +119,8 @@ module up_delay_cntrl #(
 
   assign up_wreq_s = (up_waddr[13:8] == BASE_ADDRESS) ? up_wreq : 1'b0;
   assign up_rreq_s = (up_raddr[13:8] == BASE_ADDRESS) ? up_rreq : 1'b0;
-  assign up_rdata_s[4] = | up_drdata4_s;
-  assign up_rdata_s[3] = | up_drdata3_s;
-  assign up_rdata_s[2] = | up_drdata2_s;
-  assign up_rdata_s[1] = | up_drdata1_s;
-  assign up_rdata_s[0] = | up_drdata0_s;
 
-  for (n = 0; n < DATA_WIDTH; n = n + 1) begin: g_drd
-  assign up_drdata4_s[n] = (up_raddr[7:0] == n) ? up_drdata[((n*5)+4)] : 1'd0;
-  assign up_drdata3_s[n] = (up_raddr[7:0] == n) ? up_drdata[((n*5)+3)] : 1'd0;
-  assign up_drdata2_s[n] = (up_raddr[7:0] == n) ? up_drdata[((n*5)+2)] : 1'd0;
-  assign up_drdata1_s[n] = (up_raddr[7:0] == n) ? up_drdata[((n*5)+1)] : 1'd0;
-  assign up_drdata0_s[n] = (up_raddr[7:0] == n) ? up_drdata[((n*5)+0)] : 1'd0;
-  end
+  assign up_rdata_s = up_drdata >> (DRP_WIDTH*up_raddr[7:0]);
 
   // processor interface
 
@@ -156,7 +146,7 @@ module up_delay_cntrl #(
         if (up_dlocked == 1'b0) begin
           up_rdata_int <= 32'hffffffff;
         end else begin
-          up_rdata_int <= {27'd0, up_rdata_s};
+          up_rdata_int <= {{32-DRP_WIDTH{1'b0}}, up_rdata_s};
         end
       end else begin
         up_rdata_int <= 32'd0;
@@ -172,15 +162,15 @@ module up_delay_cntrl #(
 
   for (n = 0; n < DATA_WIDTH; n = n + 1) begin: g_dinit
   assign up_dinit_s[n] = up_dlocked_m2 & ~up_dlocked_m3;
-  assign up_dinitdata_s[((n*5)+4):(n*5)] = INIT_DELAY;
+  assign up_dinitdata_s[(n*DRP_WIDTH) +: DRP_WIDTH] = INIT_DELAY;
   end
 
   // write does not hold- read back what goes into effect.
 
   for (n = 0; n < DATA_WIDTH; n = n + 1) begin: g_dwr
   assign up_dld_s[n] = (up_waddr[7:0] == n) ? up_wreq_s : 1'b0;
-  assign up_dwdata_s[((n*5)+4):(n*5)] = (up_waddr[7:0] == n) ?
-    up_wdata[4:0] : up_dwdata_int[((n*5)+4):(n*5)];
+  assign up_dwdata_s[(n*DRP_WIDTH) +: DRP_WIDTH] = (up_waddr[7:0] == n) ?
+    up_wdata[DRP_WIDTH-1:0] : up_dwdata_int[(n*DRP_WIDTH) +: DRP_WIDTH];
   end
 
   assign up_dld = up_dld_int;
