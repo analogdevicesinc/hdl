ca1031933dd57fe7597308fc024f452db84dc835
Istvan Csomortani <istvan.csomortani@analog.com>
2019-08-06 14:25:58 +0100

    adrv9009/s10soc: Rename altera to intel

--- projects/adrv9009/s10soc/system_qsys.tcl
+++ projects/adrv9009/s10soc/system_qsys.tcl
@@ -3,5 +3,5 @@ set dac_fifo_address_width 10
 set xcvr_reconfig_addr_width 11
 
 source $ad_hdl_dir/projects/common/s10soc/s10soc_system_qsys.tcl
-source $ad_hdl_dir/projects/common/altera/dacfifo_qsys.tcl
+source $ad_hdl_dir/projects/common/intel/dacfifo_qsys.tcl
 source ../common/adrv9009_qsys.tcl
