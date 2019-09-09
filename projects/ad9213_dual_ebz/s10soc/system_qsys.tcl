3ecb6e2a15405b9ee8dc9da99832667ba0e227ae
Istvan Csomortani <istvan.csomortani@analog.com>
2019-09-09 15:06:06 +0100

    ad9213_dual_ebz: Initial commit

---
+++ projects/ad9213_dual_ebz/s10soc/system_qsys.tcl
@@ -0,0 +1,7 @@
+
+set adc_fifo_address_width 10
+set xcvr_reconfig_addr_width 11
+
+source $ad_hdl_dir/projects/common/s10soc/s10soc_system_qsys.tcl
+source $ad_hdl_dir/projects/common/intel/adcfifo_qsys.tcl
+source ../common/ad9213_dual_qsys.tcl
