9770fd86d9bba7ce4eb9da19ef413bdb287632d7
AndreiGrozav <andrei.grozav@analog.com>
2019-09-24 18:43:15 +0300

    cn0506_mii: Common design initial commit

---
+++ projects/cn0506_mii/common/cn0506_bd.tcl
@@ -0,0 +1,11 @@
+create_bd_port -dir O reset
+
+ad_connect reset sys_rstgen/peripheral_reset
+
+#system ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+set sys_cstring "sys rom custom string placeholder"
+sysid_gen_sys_init_file $sys_cstring
+
