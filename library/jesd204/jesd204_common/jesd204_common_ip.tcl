5d7d6299e61f96072ea9bc6571bf0ba99e30a16c
Laszlo Nagy <laszlo.nagy@analog.com>
2019-10-02 14:25:40 +0100

    jesd204_common: Added JESD204C components

--- library/jesd204/jesd204_common/jesd204_common_ip.tcl
+++ library/jesd204/jesd204_common/jesd204_common_ip.tcl
@@ -50,14 +50,16 @@ adi_ip_create jesd204_common
 add_files -fileset [get_filesets sources_1] [list \
   "jesd204_lmfc.v" \
   "jesd204_scrambler.v" \
+  "jesd204_scrambler_64b.v" \
+  "jesd204_crc12.v" \
   "jesd204_eof_generator.v" \
   "pipeline_stage.v" \
 ]
 
 adi_ip_properties_lite jesd204_common
 
-set_property display_name "ADI JESD204B Common Library" [ipx::current_core]
-set_property description "ADI JESD204B Common Library" [ipx::current_core]
+set_property display_name "ADI JESD204C Common Library" [ipx::current_core]
+set_property description "ADI JESD204C Common Library" [ipx::current_core]
 set_property hide_in_gui {1} [ipx::current_core]
 
 ipx::save_core [ipx::current_core]
