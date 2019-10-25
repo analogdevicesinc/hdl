d7c9c19df7e96a888af5575d4a126d4a7e1f60bb
Istvan Csomortani <istvan.csomortani@analog.com>
2019-10-25 10:16:47 +0300

    axi_laser_driver: Fix ip.tcl file
      - Add a missing contraint file
      - Fix the path of the ttclk file

--- library/axi_laser_driver/axi_laser_driver_ip.tcl
+++ library/axi_laser_driver/axi_laser_driver_ip.tcl
@@ -6,6 +6,7 @@ source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
 adi_ip_create axi_laser_driver
 adi_ip_files axi_laser_driver [list \
   "$ad_hdl_dir/library/common/up_clock_mon.v" \
+  "$ad_hdl_dir/library/xilinx/common/up_clock_mon_constr.xdc" \
   "axi_laser_driver_constr.xdc" \
   "axi_laser_driver_regmap.v" \
   "axi_laser_driver.v"]
