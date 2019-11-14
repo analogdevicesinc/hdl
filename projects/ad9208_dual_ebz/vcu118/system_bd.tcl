10a808b504d2a4f4e7475e173bd0302eaf0ead93
Laszlo Nagy <laszlo.nagy@analog.com>
2019-11-14 13:23:21 +0000

    ad9208_dual_ebz/vcu118: remove GTY prefix from parameters

--- projects/ad9208_dual_ebz/vcu118/system_bd.tcl
+++ projects/ad9208_dual_ebz/vcu118/system_bd.tcl
@@ -20,16 +20,16 @@ foreach i {0 1} {
   ad_ip_parameter util_adc_${i}_xcvr CONFIG.CPLL_CFG1 0x2b
   ad_ip_parameter util_adc_${i}_xcvr CONFIG.CPLL_CFG2 0x2
   ad_ip_parameter util_adc_${i}_xcvr CONFIG.CPLL_FBDIV 2
-  ad_ip_parameter util_adc_${i}_xcvr CONFIG.GTY4_CH_HSPMUX 0x4040
-  ad_ip_parameter util_adc_${i}_xcvr CONFIG.GTY4_PREIQ_FREQ_BST 1
-  ad_ip_parameter util_adc_${i}_xcvr CONFIG.GTY4_RTX_BUF_CML_CTRL 0x5
-  ad_ip_parameter util_adc_${i}_xcvr CONFIG.GTY4_RXPI_CFG0 0x3002
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.CH_HSPMUX 0x4040
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.PREIQ_FREQ_BST 1
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.RTX_BUF_CML_CTRL 0x5
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.RXPI_CFG0 0x3002
 
   ad_ip_parameter util_adc_${i}_xcvr CONFIG.QPLL_REFCLK_DIV 1
   ad_ip_parameter util_adc_${i}_xcvr CONFIG.QPLL_CFG0 0x333c
   ad_ip_parameter util_adc_${i}_xcvr CONFIG.QPLL_CFG4 0x2
   ad_ip_parameter util_adc_${i}_xcvr CONFIG.QPLL_FBDIV 20
-  ad_ip_parameter util_adc_${i}_xcvr CONFIG.GTY4_PPF0_CFG 0xB00
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.PPF0_CFG 0xB00
 
 }
 
