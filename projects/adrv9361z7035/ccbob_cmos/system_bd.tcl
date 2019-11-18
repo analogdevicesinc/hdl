570dae7df6a0c672f52c80719e14cac7204a1025
Sergiu Arpadi <sergiu.arpadi@analog.com>
2019-11-18 11:02:06 +0000

    adrv9361: Added sysid to all projects

--- projects/adrv9361z7035/ccbob_cmos/system_bd.tcl
+++ projects/adrv9361z7035/ccbob_cmos/system_bd.tcl
@@ -9,3 +9,9 @@ cfg_ad9361_interface CMOS
 
 ad_ip_parameter axi_ad9361 CONFIG.ADC_INIT_DELAY 29
 
+#system ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+set sys_cstring "sys rom custom string placeholder"
+sysid_gen_sys_init_file $sys_cstring
