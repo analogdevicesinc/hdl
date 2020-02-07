1f7671cb3649f41a864013bf528f0a94d3229e52
Laszlo Nagy <laszlo.nagy@analog.com>
2020-02-07 10:48:47 +0000

    scripts/adi_env.tcl: helper function for environment variables
    Ease the access of the environment variables.

--- projects/scripts/adi_env.tcl
+++ projects/scripts/adi_env.tcl
@@ -15,3 +15,17 @@ if [info exists ::env(ADI_GHDL_DIR)] {
   set ad_ghdl_dir [file normalize $::env(ADI_GHDL_DIR)]
 }
 
+
+# This helper pocedure retrieves the value of varible from environment if exists,
+# other case returns the provided default value
+#  name - name of the environment variable
+#  default_value - returned vale in case environment variable does not exists
+proc get_env_param {name default_value} {
+  if [info exists ::env($name)] {
+    return $::env($name)
+  } else {
+    return $default_value
+  }
+}
+
+
