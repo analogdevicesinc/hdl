3deb18cecae6e02d8303255575acdd9fab92fd23
Istvan Csomortani <istvan.csomortani@analog.com>
2020-03-31 10:18:59 +0100

    adrv9009/s10soc: Add support for Stratix10 SOC

---
+++ projects/adrv9009/s10soc/system_qsys.tcl
@@ -0,0 +1,7 @@
+
+set dac_fifo_address_width 10
+set xcvr_reconfig_addr_width 11
+
+source $ad_hdl_dir/projects/common/s10soc/s10soc_system_qsys.tcl
+source $ad_hdl_dir/projects/common/intel/dacfifo_qsys.tcl
+source ../common/adrv9009_qsys.tcl
