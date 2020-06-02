b27f3ac18f8f97cb9e5672ca5d5e8f4bf0b93263
Laszlo Nagy <laszlo.nagy@analog.com>
2020-06-02 17:56:17 +0100

    adrv9001:zcu102: Initial version
    Generic project that supports CMOS or LVDS interface for the ADRV9001 transceiver.

---
+++ projects/adrv9001/common/adrv9001_bd.tcl
@@ -0,0 +1,262 @@
+create_bd_port -dir I ref_clk
+
+create_bd_port -dir I tx_output_enable
+
+create_bd_port -dir I mssi_sync
+
+# adrv9001 interface
+create_bd_port -dir I rx1_dclk_in_n
+create_bd_port -dir I rx1_dclk_in_p
+create_bd_port -dir I rx1_idata_in_n
+create_bd_port -dir I rx1_idata_in_p
+create_bd_port -dir I rx1_qdata_in_n
+create_bd_port -dir I rx1_qdata_in_p
+create_bd_port -dir I rx1_strobe_in_n
+create_bd_port -dir I rx1_strobe_in_p
+
+create_bd_port -dir I rx2_dclk_in_n
+create_bd_port -dir I rx2_dclk_in_p
+create_bd_port -dir I rx2_idata_in_n
+create_bd_port -dir I rx2_idata_in_p
+create_bd_port -dir I rx2_qdata_in_n
+create_bd_port -dir I rx2_qdata_in_p
+create_bd_port -dir I rx2_strobe_in_n
+create_bd_port -dir I rx2_strobe_in_p
+
+create_bd_port -dir O tx1_dclk_out_n
+create_bd_port -dir O tx1_dclk_out_p
+create_bd_port -dir I tx1_dclk_in_n
+create_bd_port -dir I tx1_dclk_in_p
+create_bd_port -dir O tx1_idata_out_n
+create_bd_port -dir O tx1_idata_out_p
+create_bd_port -dir O tx1_qdata_out_n
+create_bd_port -dir O tx1_qdata_out_p
+create_bd_port -dir O tx1_strobe_out_n
+create_bd_port -dir O tx1_strobe_out_p
+
+create_bd_port -dir O tx2_dclk_out_n
+create_bd_port -dir O tx2_dclk_out_p
+create_bd_port -dir I tx2_dclk_in_n
+create_bd_port -dir I tx2_dclk_in_p
+create_bd_port -dir O tx2_idata_out_n
+create_bd_port -dir O tx2_idata_out_p
+create_bd_port -dir O tx2_qdata_out_n
+create_bd_port -dir O tx2_qdata_out_p
+create_bd_port -dir O tx2_strobe_out_n
+create_bd_port -dir O tx2_strobe_out_p
+
+# adrv9001
+
+ad_ip_instance axi_adrv9001 axi_adrv9001
+ad_ip_parameter axi_adrv9001 CONFIG.CMOS_LVDS_N  $ad_project_params(CMOS_LVDS_N)
+
+# dma for rx1
+
+ad_ip_instance axi_dmac axi_adrv9001_rx1_dma
+ad_ip_parameter axi_adrv9001_rx1_dma CONFIG.DMA_TYPE_SRC 2
+ad_ip_parameter axi_adrv9001_rx1_dma CONFIG.DMA_TYPE_DEST 0
+ad_ip_parameter axi_adrv9001_rx1_dma CONFIG.CYCLIC 0
+ad_ip_parameter axi_adrv9001_rx1_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter axi_adrv9001_rx1_dma CONFIG.AXI_SLICE_SRC 0
+ad_ip_parameter axi_adrv9001_rx1_dma CONFIG.AXI_SLICE_DEST 0
+ad_ip_parameter axi_adrv9001_rx1_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_adrv9001_rx1_dma CONFIG.DMA_DATA_WIDTH_SRC 64
+
+ad_ip_instance util_cpack2 util_adc_1_pack { \
+  NUM_OF_CHANNELS 4 \
+  SAMPLE_DATA_WIDTH 16 \
+}
+
+# dma for rx2
+
+ad_ip_instance axi_dmac axi_adrv9001_rx2_dma
+ad_ip_parameter axi_adrv9001_rx2_dma CONFIG.DMA_TYPE_SRC 2
+ad_ip_parameter axi_adrv9001_rx2_dma CONFIG.DMA_TYPE_DEST 0
+ad_ip_parameter axi_adrv9001_rx2_dma CONFIG.CYCLIC 0
+ad_ip_parameter axi_adrv9001_rx2_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter axi_adrv9001_rx2_dma CONFIG.AXI_SLICE_SRC 0
+ad_ip_parameter axi_adrv9001_rx2_dma CONFIG.AXI_SLICE_DEST 0
+ad_ip_parameter axi_adrv9001_rx2_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_adrv9001_rx2_dma CONFIG.DMA_DATA_WIDTH_SRC 32
+
+ad_ip_instance util_cpack2 util_adc_2_pack { \
+  NUM_OF_CHANNELS 2 \
+  SAMPLE_DATA_WIDTH 16 \
+}
+
+# dma for tx1
+
+ad_ip_instance axi_dmac axi_adrv9001_tx1_dma
+ad_ip_parameter axi_adrv9001_tx1_dma CONFIG.DMA_TYPE_SRC 0
+ad_ip_parameter axi_adrv9001_tx1_dma CONFIG.DMA_TYPE_DEST 1
+ad_ip_parameter axi_adrv9001_tx1_dma CONFIG.CYCLIC 1
+ad_ip_parameter axi_adrv9001_tx1_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter axi_adrv9001_tx1_dma CONFIG.AXI_SLICE_SRC 0
+ad_ip_parameter axi_adrv9001_tx1_dma CONFIG.AXI_SLICE_DEST 0
+ad_ip_parameter axi_adrv9001_tx1_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_adrv9001_tx1_dma CONFIG.DMA_DATA_WIDTH_DEST 64
+
+ad_ip_instance util_upack2 util_dac_1_upack { \
+  NUM_OF_CHANNELS 4 \
+  SAMPLE_DATA_WIDTH 16 \
+}
+
+# dma for tx1
+
+ad_ip_instance axi_dmac axi_adrv9001_tx2_dma
+ad_ip_parameter axi_adrv9001_tx2_dma CONFIG.DMA_TYPE_SRC 0
+ad_ip_parameter axi_adrv9001_tx2_dma CONFIG.DMA_TYPE_DEST 1
+ad_ip_parameter axi_adrv9001_tx2_dma CONFIG.CYCLIC 1
+ad_ip_parameter axi_adrv9001_tx2_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter axi_adrv9001_tx2_dma CONFIG.AXI_SLICE_SRC 0
+ad_ip_parameter axi_adrv9001_tx2_dma CONFIG.AXI_SLICE_DEST 0
+ad_ip_parameter axi_adrv9001_tx2_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_adrv9001_tx2_dma CONFIG.DMA_DATA_WIDTH_DEST 32
+
+ad_ip_instance util_upack2 util_dac_2_upack { \
+  NUM_OF_CHANNELS 2 \
+  SAMPLE_DATA_WIDTH 16 \
+}
+
+# ad9001 connections
+
+ad_connect  $sys_iodelay_clk       axi_adrv9001/delay_clk
+ad_connect  axi_adrv9001/adc_1_clk axi_adrv9001_rx1_dma/fifo_wr_clk
+ad_connect  axi_adrv9001/adc_1_clk util_adc_1_pack/clk
+
+ad_connect  axi_adrv9001/adc_2_clk axi_adrv9001_rx2_dma/fifo_wr_clk
+ad_connect  axi_adrv9001/adc_2_clk util_adc_2_pack/clk
+
+ad_connect  axi_adrv9001/dac_1_clk axi_adrv9001_tx1_dma/m_axis_aclk
+ad_connect  axi_adrv9001/dac_1_clk util_dac_1_upack/clk
+
+ad_connect  axi_adrv9001/dac_2_clk axi_adrv9001_tx2_dma/m_axis_aclk
+ad_connect  axi_adrv9001/dac_2_clk util_dac_2_upack/clk
+
+ad_connect ref_clk           axi_adrv9001/ref_clk
+
+ad_connect tx_output_enable  axi_adrv9001/tx_output_enable
+
+ad_connect mssi_sync         axi_adrv9001/mssi_sync
+
+ad_connect rx1_dclk_in_n     axi_adrv9001/rx1_dclk_in_n_NC
+ad_connect rx1_dclk_in_p     axi_adrv9001/rx1_dclk_in_p_dclk_in
+ad_connect rx1_idata_in_n    axi_adrv9001/rx1_idata_in_n_idata0
+ad_connect rx1_idata_in_p    axi_adrv9001/rx1_idata_in_p_idata1
+ad_connect rx1_qdata_in_n    axi_adrv9001/rx1_qdata_in_n_qdata2
+ad_connect rx1_qdata_in_p    axi_adrv9001/rx1_qdata_in_p_qdata3
+ad_connect rx1_strobe_in_n   axi_adrv9001/rx1_strobe_in_n_NC
+ad_connect rx1_strobe_in_p   axi_adrv9001/rx1_strobe_in_p_strobe_in
+
+ad_connect rx2_dclk_in_n     axi_adrv9001/rx2_dclk_in_n_NC
+ad_connect rx2_dclk_in_p     axi_adrv9001/rx2_dclk_in_p_dclk_in
+ad_connect rx2_idata_in_n    axi_adrv9001/rx2_idata_in_n_idata0
+ad_connect rx2_idata_in_p    axi_adrv9001/rx2_idata_in_p_idata1
+ad_connect rx2_qdata_in_n    axi_adrv9001/rx2_qdata_in_n_qdata2
+ad_connect rx2_qdata_in_p    axi_adrv9001/rx2_qdata_in_p_qdata3
+ad_connect rx2_strobe_in_n   axi_adrv9001/rx2_strobe_in_n_NC
+ad_connect rx2_strobe_in_p   axi_adrv9001/rx2_strobe_in_p_strobe_in
+
+ad_connect tx1_dclk_out_n    axi_adrv9001/tx1_dclk_out_n_NC
+ad_connect tx1_dclk_out_p    axi_adrv9001/tx1_dclk_out_p_dclk_out
+ad_connect tx1_dclk_in_n     axi_adrv9001/tx1_dclk_in_n_NC
+ad_connect tx1_dclk_in_p     axi_adrv9001/tx1_dclk_in_p_dclk_in
+ad_connect tx1_idata_out_n   axi_adrv9001/tx1_idata_out_n_idata0
+ad_connect tx1_idata_out_p   axi_adrv9001/tx1_idata_out_p_idata1
+ad_connect tx1_qdata_out_n   axi_adrv9001/tx1_qdata_out_n_qdata2
+ad_connect tx1_qdata_out_p   axi_adrv9001/tx1_qdata_out_p_qdata3
+ad_connect tx1_strobe_out_n  axi_adrv9001/tx1_strobe_out_n_NC
+ad_connect tx1_strobe_out_p  axi_adrv9001/tx1_strobe_out_p_strobe_out
+
+ad_connect tx2_dclk_out_n    axi_adrv9001/tx2_dclk_out_n_NC
+ad_connect tx2_dclk_out_p    axi_adrv9001/tx2_dclk_out_p_dclk_out
+ad_connect tx2_dclk_in_n     axi_adrv9001/tx2_dclk_in_n_NC
+ad_connect tx2_dclk_in_p     axi_adrv9001/tx2_dclk_in_p_dclk_in
+ad_connect tx2_idata_out_n   axi_adrv9001/tx2_idata_out_n_idata0
+ad_connect tx2_idata_out_p   axi_adrv9001/tx2_idata_out_p_idata1
+ad_connect tx2_qdata_out_n   axi_adrv9001/tx2_qdata_out_n_qdata2
+ad_connect tx2_qdata_out_p   axi_adrv9001/tx2_qdata_out_p_qdata3
+ad_connect tx2_strobe_out_n  axi_adrv9001/tx2_strobe_out_n_NC
+ad_connect tx2_strobe_out_p  axi_adrv9001/tx2_strobe_out_p_strobe_out
+
+# RX1_RX2 - CPACK - RX_DMA1
+ad_connect  axi_adrv9001/adc_1_rst       util_adc_1_pack/reset
+ad_connect  axi_adrv9001/adc_1_valid_i0  util_adc_1_pack/fifo_wr_en
+ad_connect  axi_adrv9001/adc_1_enable_i0 util_adc_1_pack/enable_0
+ad_connect  axi_adrv9001/adc_1_data_i0   util_adc_1_pack/fifo_wr_data_0
+ad_connect  axi_adrv9001/adc_1_enable_q0 util_adc_1_pack/enable_1
+ad_connect  axi_adrv9001/adc_1_data_q0   util_adc_1_pack/fifo_wr_data_1
+ad_connect  axi_adrv9001/adc_1_enable_i1 util_adc_1_pack/enable_2
+ad_connect  axi_adrv9001/adc_1_data_i1   util_adc_1_pack/fifo_wr_data_2
+ad_connect  axi_adrv9001/adc_1_enable_q1 util_adc_1_pack/enable_3
+ad_connect  axi_adrv9001/adc_1_data_q1   util_adc_1_pack/fifo_wr_data_3
+
+ad_connect  axi_adrv9001/adc_1_dovf      util_adc_1_pack/fifo_wr_overflow
+
+ad_connect util_adc_1_pack/packed_fifo_wr axi_adrv9001_rx1_dma/fifo_wr
+
+# RX2 - CPACK - RX_DMA2
+ad_connect  axi_adrv9001/adc_2_rst       util_adc_2_pack/reset
+ad_connect  axi_adrv9001/adc_2_valid_i0  util_adc_2_pack/fifo_wr_en
+ad_connect  axi_adrv9001/adc_2_enable_i0 util_adc_2_pack/enable_0
+ad_connect  axi_adrv9001/adc_2_data_i0   util_adc_2_pack/fifo_wr_data_0
+ad_connect  axi_adrv9001/adc_2_enable_q0 util_adc_2_pack/enable_1
+ad_connect  axi_adrv9001/adc_2_data_q0   util_adc_2_pack/fifo_wr_data_1
+
+ad_connect  axi_adrv9001/adc_2_dovf       util_adc_2_pack/fifo_wr_overflow
+
+ad_connect util_adc_2_pack/packed_fifo_wr axi_adrv9001_rx2_dma/fifo_wr
+
+# TX_DMA1 - UPACK - TX1
+ad_connect  axi_adrv9001/dac_1_rst        util_dac_1_upack/reset
+ad_connect  axi_adrv9001/dac_1_valid_i0   util_dac_1_upack/fifo_rd_en
+ad_connect  axi_adrv9001/dac_1_enable_i0  util_dac_1_upack/enable_0
+ad_connect  axi_adrv9001/dac_1_data_i0    util_dac_1_upack/fifo_rd_data_0
+ad_connect  axi_adrv9001/dac_1_enable_q0  util_dac_1_upack/enable_1
+ad_connect  axi_adrv9001/dac_1_data_q0    util_dac_1_upack/fifo_rd_data_1
+ad_connect  axi_adrv9001/dac_1_enable_i1  util_dac_1_upack/enable_2
+ad_connect  axi_adrv9001/dac_1_data_i1    util_dac_1_upack/fifo_rd_data_2
+ad_connect  axi_adrv9001/dac_1_enable_q1  util_dac_1_upack/enable_3
+ad_connect  axi_adrv9001/dac_1_data_q1    util_dac_1_upack/fifo_rd_data_3
+
+ad_connect  axi_adrv9001_tx1_dma/m_axis   util_dac_1_upack/s_axis
+ad_connect  axi_adrv9001/dac_1_dunf       util_dac_1_upack/fifo_rd_underflow
+
+# TX_DMA2 - UPACK - TX2
+ad_connect  axi_adrv9001/dac_2_rst        util_dac_2_upack/reset
+ad_connect  axi_adrv9001/dac_2_valid_i0   util_dac_2_upack/fifo_rd_en
+ad_connect  axi_adrv9001/dac_2_enable_i0  util_dac_2_upack/enable_0
+ad_connect  axi_adrv9001/dac_2_data_i0    util_dac_2_upack/fifo_rd_data_0
+ad_connect  axi_adrv9001/dac_2_enable_q0  util_dac_2_upack/enable_1
+ad_connect  axi_adrv9001/dac_2_data_q0    util_dac_2_upack/fifo_rd_data_1
+
+ad_connect  axi_adrv9001_tx2_dma/m_axis   util_dac_2_upack/s_axis
+ad_connect  axi_adrv9001/dac_2_dunf       util_dac_2_upack/fifo_rd_underflow
+
+# interconnect
+
+ad_cpu_interconnect 0x44A00000  axi_adrv9001
+ad_cpu_interconnect 0x44A30000  axi_adrv9001_rx1_dma
+ad_cpu_interconnect 0x44A40000  axi_adrv9001_rx2_dma
+ad_cpu_interconnect 0x44A50000  axi_adrv9001_tx1_dma
+ad_cpu_interconnect 0x44A60000  axi_adrv9001_tx2_dma
+
+# memory inteconnect
+
+ad_mem_hp1_interconnect $sys_dma_clk sys_ps7/S_AXI_HP1
+ad_mem_hp1_interconnect $sys_dma_clk axi_adrv9001_rx1_dma/m_dest_axi
+ad_mem_hp1_interconnect $sys_dma_clk axi_adrv9001_rx2_dma/m_dest_axi
+ad_mem_hp1_interconnect $sys_dma_clk axi_adrv9001_tx1_dma/m_src_axi
+ad_mem_hp1_interconnect $sys_dma_clk axi_adrv9001_tx2_dma/m_src_axi
+
+ad_connect $sys_dma_resetn axi_adrv9001_rx1_dma/m_dest_axi_aresetn
+ad_connect $sys_dma_resetn axi_adrv9001_rx2_dma/m_dest_axi_aresetn
+ad_connect $sys_dma_resetn axi_adrv9001_tx1_dma/m_src_axi_aresetn
+ad_connect $sys_dma_resetn axi_adrv9001_tx2_dma/m_src_axi_aresetn
+# interrupts
+
+ad_cpu_interrupt ps-13 mb-12 axi_adrv9001_rx1_dma/irq
+ad_cpu_interrupt ps-12 mb-11 axi_adrv9001_rx2_dma/irq
+ad_cpu_interrupt ps-11 mb-6 axi_adrv9001_tx1_dma/irq
+ad_cpu_interrupt ps-10 mb-5 axi_adrv9001_tx2_dma/irq
+
---
+++ projects/adrv9001/zcu102/system_top.v
@@ -0,0 +1,265 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module system_top (
+
+  input       [12:0]      gpio_bd_i,
+  output      [ 7:0]      gpio_bd_o,
+
+  inout                   iic_scl,
+  inout                   iic_sda,
+
+  output                  spi_clk,
+  output                  spi_dio,
+  input                   spi_do,
+  output                  spi_en,
+
+  // Device clock
+  input                   fpga_ref_clk_n,
+  input                   fpga_ref_clk_p,
+  // Device clock passed through 9001
+  input                   dev_clk_out,
+
+  input                   fpga_mcs_in_n,
+  input                   fpga_mcs_in_p,
+  output                  dev_mcs_fpga_in_n,
+  output                  dev_mcs_fpga_in_p,
+
+  inout                   dgpio_0,
+  inout                   dgpio_1,
+  inout                   dgpio_2,
+  inout                   dgpio_3,
+  inout                   dgpio_4,
+  inout                   dgpio_5,
+  inout                   dgpio_6,
+  inout                   dgpio_7,
+  inout                   dgpio_8,
+  inout                   dgpio_9,
+  inout                   dgpio_10,
+  inout                   dgpio_11,
+
+  inout                   gp_int,
+  inout                   mode,
+  inout                   reset_trx,
+
+  input                   rx1_dclk_out_n,
+  input                   rx1_dclk_out_p,
+  inout                   rx1_enable,
+  input                   rx1_idata_out_n,
+  input                   rx1_idata_out_p,
+  input                   rx1_qdata_out_n,
+  input                   rx1_qdata_out_p,
+  input                   rx1_strobe_out_n,
+  input                   rx1_strobe_out_p,
+
+  input                   rx2_dclk_out_n,
+  input                   rx2_dclk_out_p,
+  inout                   rx2_enable,
+  input                   rx2_idata_out_n,
+  input                   rx2_idata_out_p,
+  input                   rx2_qdata_out_n,
+  input                   rx2_qdata_out_p,
+  input                   rx2_strobe_out_n,
+  input                   rx2_strobe_out_p,
+
+  output                  tx1_dclk_in_n,
+  output                  tx1_dclk_in_p,
+  input                   tx1_dclk_out_n,
+  input                   tx1_dclk_out_p,
+  inout                   tx1_enable,
+  output                  tx1_idata_in_n,
+  output                  tx1_idata_in_p,
+  output                  tx1_qdata_in_n,
+  output                  tx1_qdata_in_p,
+  output                  tx1_strobe_in_n,
+  output                  tx1_strobe_in_p,
+
+  output                  tx2_dclk_in_n,
+  output                  tx2_dclk_in_p,
+  input                   tx2_dclk_out_n,
+  input                   tx2_dclk_out_p,
+  inout                   tx2_enable,
+  output                  tx2_idata_in_n,
+  output                  tx2_idata_in_p,
+  output                  tx2_qdata_in_n,
+  output                  tx2_qdata_in_p,
+  output                  tx2_strobe_in_n,
+  output                  tx2_strobe_in_p,
+
+  inout                   sm_fan_tach,
+  output                  vadj_test_1,
+  output                  vadj_test_2
+);
+  // internal registers
+  reg         [  2:0] mcs_sync_m = 'd0;
+  reg                 dev_mcs_fpga_in = 1'b0;
+
+  // internal signals
+  wire        [94:0]      gpio_i;
+  wire        [94:0]      gpio_o;
+  wire        [94:0]      gpio_t;
+  wire        [ 2:0]      spi_csn;
+
+  wire fpga_ref_clk;
+  wire fpga_mcs_in;
+
+  // instantiations
+
+  IBUFDS i_ibufgs_fpga_ref_clk (
+    .I (fpga_ref_clk_p),
+    .IB (fpga_ref_clk_n),
+    .O (fpga_ref_clk));
+
+  IBUFDS i_ibufgs_fpga_mcs_in (
+    .I (fpga_mcs_in_p),
+    .IB (fpga_mcs_in_n),
+    .O (fpga_mcs_in));
+
+  OBUFDS i_obufds_dev_mcs_fpga_in (
+    .I (dev_mcs_fpga_in),
+    .O (dev_mcs_fpga_in_p),
+    .OB (dev_mcs_fpga_in_n));
+
+  // multi-chip synchronization
+  //
+  always @(posedge fpga_ref_clk) begin
+    mcs_sync_m <= {mcs_sync_m[1:0], gpio_o[53]};
+    dev_mcs_fpga_in <= mcs_sync_m[2] & ~mcs_sync_m[1];
+  end
+
+  // multi-ssi synchronization
+  //
+  assign mssi_sync = gpio_o[54];
+
+  assign {vadj_test_2,vadj_test_1} = 2'b11;
+
+  ad_iobuf #(.DATA_WIDTH(20)) i_iobuf (
+    .dio_t ({gpio_t[51:32]}),
+    .dio_i ({gpio_o[51:32]}),
+    .dio_o ({gpio_i[51:32]}),
+    .dio_p ({tx2_enable,
+             tx1_enable,
+             rx2_enable,
+             rx1_enable,
+             sm_fan_tach,
+             reset_trx,
+             mode,
+             gp_int,
+             dgpio_11,
+             dgpio_10,
+             dgpio_9,
+             dgpio_8,
+             dgpio_7,
+             dgpio_6,
+             dgpio_5,
+             dgpio_4,
+             dgpio_3,
+             dgpio_2,
+             dgpio_1,
+             dgpio_0 }));  // 32
+
+  assign gpio_i[ 7: 0] = gpio_o[ 7: 0];
+  assign gpio_i[20: 8] = gpio_bd_i;
+  assign gpio_bd_o = gpio_o[ 7: 0];
+
+  assign gpio_i[94:52] = gpio_o[94:52];
+  assign gpio_i[31:21] = gpio_o[31:21];
+
+  assign spi_en = spi_csn[0];
+
+  system_wrapper i_system_wrapper (
+    .ref_clk (fpga_ref_clk),
+    .mssi_sync (mssi_sync),
+
+    .tx_output_enable (1'b1),
+
+    .rx1_dclk_in_n (rx1_dclk_out_n),
+    .rx1_dclk_in_p (rx1_dclk_out_p),
+    .rx1_idata_in_n (rx1_idata_out_n),
+    .rx1_idata_in_p (rx1_idata_out_p),
+    .rx1_qdata_in_n (rx1_qdata_out_n),
+    .rx1_qdata_in_p (rx1_qdata_out_p),
+    .rx1_strobe_in_n (rx1_strobe_out_n),
+    .rx1_strobe_in_p (rx1_strobe_out_p),
+
+    .rx2_dclk_in_n (rx2_dclk_out_n),
+    .rx2_dclk_in_p (rx2_dclk_out_p),
+    .rx2_idata_in_n (rx2_idata_out_n),
+    .rx2_idata_in_p (rx2_idata_out_p),
+    .rx2_qdata_in_n (rx2_qdata_out_n),
+    .rx2_qdata_in_p (rx2_qdata_out_p),
+    .rx2_strobe_in_n (rx2_strobe_out_n),
+    .rx2_strobe_in_p (rx2_strobe_out_p),
+
+    .tx1_dclk_out_n (tx1_dclk_in_n),
+    .tx1_dclk_out_p (tx1_dclk_in_p),
+    .tx1_dclk_in_n (tx1_dclk_out_n),
+    .tx1_dclk_in_p (tx1_dclk_out_p),
+    .tx1_idata_out_n (tx1_idata_in_n),
+    .tx1_idata_out_p (tx1_idata_in_p),
+    .tx1_qdata_out_n (tx1_qdata_in_n),
+    .tx1_qdata_out_p (tx1_qdata_in_p),
+    .tx1_strobe_out_n (tx1_strobe_in_n),
+    .tx1_strobe_out_p (tx1_strobe_in_p),
+
+    .tx2_dclk_out_n (tx2_dclk_in_n),
+    .tx2_dclk_out_p (tx2_dclk_in_p),
+    .tx2_dclk_in_n (tx2_dclk_out_n),
+    .tx2_dclk_in_p (tx2_dclk_out_p),
+    .tx2_idata_out_n (tx2_idata_in_n),
+    .tx2_idata_out_p (tx2_idata_in_p),
+    .tx2_qdata_out_n (tx2_qdata_in_n),
+    .tx2_qdata_out_p (tx2_qdata_in_p),
+    .tx2_strobe_out_n (tx2_strobe_in_n),
+    .tx2_strobe_out_p (tx2_strobe_in_p),
+
+    .gpio_i (gpio_i),
+    .gpio_o (gpio_o),
+    .gpio_t (gpio_t),
+    .spi0_sclk (spi_clk),
+    .spi0_csn (spi_csn),
+    .spi0_miso (spi_do),
+    .spi0_mosi (spi_dio),
+    .spi1_sclk (),
+    .spi1_csn (),
+    .spi1_miso (1'b0),
+    .spi1_mosi ()
+  );
+endmodule
+
+// ***************************************************************************
+// ***************************************************************************
