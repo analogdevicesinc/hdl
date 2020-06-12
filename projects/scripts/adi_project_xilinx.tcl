1070bc57e15ff246963fb0768d957939cbe5aefc
andrea <andrea.reale@enea.it>
2020-06-12 14:19:10 +0200

    change for vivado 2020.1

--- projects/scripts/adi_project_xilinx.tcl
+++ projects/scripts/adi_project_xilinx.tcl
@@ -1,7 +1,7 @@
 
 ## Define the supported tool version
 if {![info exists REQUIRED_VIVADO_VERSION]} {
-  set REQUIRED_VIVADO_VERSION "2019.2"
+  set REQUIRED_VIVADO_VERSION "2020.1"
 }
 
 ## Define the ADI_IGNORE_VERSION_CHECK environment variable to skip version check
