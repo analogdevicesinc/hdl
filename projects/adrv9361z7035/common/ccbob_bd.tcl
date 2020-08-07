33697b1cf03522fa77856ef96eff2b682e3721c2
sergiu arpadi <sergiu.arpadi@analog.com>
2020-08-07 22:47:47 +0300

    adrv936x: Added clock monitor IP

--- projects/adrv9361z7035/common/ccbob_bd.tcl
+++ projects/adrv9361z7035/common/ccbob_bd.tcl
@@ -37,13 +37,102 @@ create_bd_port -dir O -from 31 -to 0 gp_out_1
 create_bd_port -dir O -from 31 -to 0 gp_out_2
 create_bd_port -dir O -from 31 -to 0 gp_out_3
 
-ad_connect  gp_in_0 axi_gpreg/up_gp_in_0
-ad_connect  gp_in_1 axi_gpreg/up_gp_in_1
-ad_connect  gp_in_2 axi_gpreg/up_gp_in_2
-ad_connect  gp_in_3 axi_gpreg/up_gp_in_3
-ad_connect  gp_out_0 axi_gpreg/up_gp_out_0
-ad_connect  gp_out_1 axi_gpreg/up_gp_out_1
-ad_connect  gp_out_2 axi_gpreg/up_gp_out_2
-ad_connect  gp_out_3 axi_gpreg/up_gp_out_3
-ad_cpu_interconnect 0x41200000 axi_gpreg
+create_bd_port -dir I -from 23 -to 0 gpio_p4_p5_c1_i
+create_bd_port -dir O -from 23 -to 0 gpio_p4_p5_c1_o
+create_bd_port -dir O -from 23 -to 0 gpio_p4_p5_c1_t
+create_bd_port -dir I -from 23 -to 0 gpio_p4_p5_c2_i
+create_bd_port -dir O -from 23 -to 0 gpio_p4_p5_c2_o
+create_bd_port -dir O -from 23 -to 0 gpio_p4_p5_c2_t
+
+create_bd_port -dir I -from 15 -to 0 gpio_p6_p7_c1_i
+create_bd_port -dir O -from 15 -to 0 gpio_p6_p7_c1_o
+create_bd_port -dir O -from 15 -to 0 gpio_p6_p7_c1_t
+create_bd_port -dir I -from 17 -to 0 gpio_p6_p7_c2_i
+create_bd_port -dir O -from 17 -to 0 gpio_p6_p7_c2_o
+create_bd_port -dir O -from 17 -to 0 gpio_p6_p7_c2_t
+
+create_bd_port -dir I clkout_in_s
+
+ad_ip_instance axi_gpio gpio_p2
+ad_ip_parameter gpio_p2 CONFIG.C_IS_DUAL 1
+ad_ip_parameter gpio_p2 CONFIG.C_GPIO_WIDTH 22
+ad_ip_parameter gpio_p2 CONFIG.C_GPIO2_WIDTH 22
+
+ad_ip_instance axi_gpio gpio_p13
+ad_ip_parameter gpio_p13 CONFIG.C_IS_DUAL 1
+ad_ip_parameter gpio_p13 CONFIG.C_GPIO_WIDTH 22
+ad_ip_parameter gpio_p13 CONFIG.C_GPIO2_WIDTH 22
+
+ad_ip_instance axi_gpio gpio_p4_p5
+ad_ip_parameter gpio_p4_p5 CONFIG.C_IS_DUAL 1
+ad_ip_parameter gpio_p4_p5 CONFIG.C_GPIO_WIDTH 24
+ad_ip_parameter gpio_p4_p5 CONFIG.C_GPIO2_WIDTH 24
+
+ad_ip_instance axi_gpio gpio_p6_p7
+ad_ip_parameter gpio_p6_p7 CONFIG.C_IS_DUAL 1
+ad_ip_parameter gpio_p6_p7 CONFIG.C_GPIO_WIDTH 16
+ad_ip_parameter gpio_p6_p7 CONFIG.C_GPIO2_WIDTH 18
+
+ad_ip_instance axi_clock_monitor clk_monitor_0
+ad_ip_parameter clk_monitor_0 CONFIG.NUM_OF_CLOCKS 2
+ad_connect clkout_in_s clk_monitor_0/clock_0
+ad_connect sys_cpu_clk clk_monitor_0/clock_1
+
+
+ad_connect gpio_p2_c1_i gpio_p2/gpio_io_i
+ad_connect gpio_p2_c1_o gpio_p2/gpio_io_o
+ad_connect gpio_p2_c1_t gpio_p2/gpio_io_t
+ad_connect gpio_p2_c2_i gpio_p2/gpio2_io_i
+ad_connect gpio_p2_c2_o gpio_p2/gpio2_io_o
+ad_connect gpio_p2_c2_t gpio_p2/gpio2_io_t
+
+ad_connect gpio_p13_c1_i gpio_p13/gpio_io_i
+ad_connect gpio_p13_c1_o gpio_p13/gpio_io_o
+ad_connect gpio_p13_c1_t gpio_p13/gpio_io_t
+ad_connect gpio_p13_c2_i gpio_p13/gpio2_io_i
+ad_connect gpio_p13_c2_o gpio_p13/gpio2_io_o
+ad_connect gpio_p13_c2_t gpio_p13/gpio2_io_t
+
+ad_connect gpio_p4_p5_c1_i gpio_p4_p5/gpio_io_i
+ad_connect gpio_p4_p5_c1_o gpio_p4_p5/gpio_io_o
+ad_connect gpio_p4_p5_c1_t gpio_p4_p5/gpio_io_t
+ad_connect gpio_p4_p5_c2_i gpio_p4_p5/gpio2_io_i
+ad_connect gpio_p4_p5_c2_o gpio_p4_p5/gpio2_io_o
+ad_connect gpio_p4_p5_c2_t gpio_p4_p5/gpio2_io_t
+
+ad_connect gpio_p6_p7_c1_i gpio_p6_p7/gpio_io_i
+ad_connect gpio_p6_p7_c1_o gpio_p6_p7/gpio_io_o
+ad_connect gpio_p6_p7_c1_t gpio_p6_p7/gpio_io_t
+ad_connect gpio_p6_p7_c2_i gpio_p6_p7/gpio2_io_i
+ad_connect gpio_p6_p7_c2_o gpio_p6_p7/gpio2_io_o
+ad_connect gpio_p6_p7_c2_t gpio_p6_p7/gpio2_io_t
+
+ad_cpu_interconnect 0x412A0000 gpio_p2
+ad_cpu_interconnect 0x412B0000 gpio_p13
+ad_cpu_interconnect 0x412C0000 gpio_p4_p5
+ad_cpu_interconnect 0x412D0000 gpio_p6_p7
+ad_cpu_interconnect 0x41620000 clk_monitor_0
+
+#ad_ip_instance axi_gpreg axi_gpreg
+#ad_ip_parameter axi_gpreg CONFIG.NUM_OF_CLK_MONS 0
+#ad_ip_parameter axi_gpreg CONFIG.NUM_OF_IO 4
+
+#create_bd_port -dir I -from 31 -to 0 gp_in_0
+#create_bd_port -dir I -from 31 -to 0 gp_in_1
+#create_bd_port -dir I -from 31 -to 0 gp_in_2
+#create_bd_port -dir I -from 31 -to 0 gp_in_3
+#create_bd_port -dir O -from 31 -to 0 gp_out_0
+#create_bd_port -dir O -from 31 -to 0 gp_out_1
+#create_bd_port -dir O -from 31 -to 0 gp_out_2
+#create_bd_port -dir O -from 31 -to 0 gp_out_3
+
+#ad_connect  gp_in_0 axi_gpreg/up_gp_in_0
+#ad_connect  gp_in_1 axi_gpreg/up_gp_in_1
+#ad_connect  gp_in_2 axi_gpreg/up_gp_in_2
+#ad_connect  gp_in_3 axi_gpreg/up_gp_in_3
+#ad_connect  gp_out_0 axi_gpreg/up_gp_out_0
+#ad_connect  gp_out_1 axi_gpreg/up_gp_out_1
+#ad_connect  gp_out_2 axi_gpreg/up_gp_out_2
+#ad_connect  gp_out_3 axi_gpreg/up_gp_out_3
+#ad_cpu_interconnect 0x41200000 axi_gpreg
 
--- projects/adrv9364z7020/common/ccbob_bd.tcl
+++ projects/adrv9364z7020/common/ccbob_bd.tcl
@@ -10,10 +10,25 @@ ad_ip_instance axi_gpreg axi_gpreg
 ad_ip_parameter axi_gpreg CONFIG.NUM_OF_CLK_MONS 0
 ad_ip_parameter axi_gpreg CONFIG.NUM_OF_IO 1
 
-create_bd_port -dir I -from 31 -to 0 gp_in_0
-create_bd_port -dir O -from 31 -to 0 gp_out_0
+ad_ip_instance axi_clock_monitor clk_monitor_0
+ad_ip_parameter clk_monitor_0 CONFIG.NUM_OF_CLOCKS 2
 
-ad_connect  gp_in_0 axi_gpreg/up_gp_in_0
-ad_connect  gp_out_0 axi_gpreg/up_gp_out_0
-ad_cpu_interconnect 0x41200000 axi_gpreg
+create_bd_port -dir I -from 25 -to 0 gpio_0_c1_i
+create_bd_port -dir O -from 25 -to 0 gpio_0_c1_o
+create_bd_port -dir O -from 25 -to 0 gpio_0_c1_t
+create_bd_port -dir I -from 27 -to 0 gpio_0_c2_i
+create_bd_port -dir O -from 27 -to 0 gpio_0_c2_o
+create_bd_port -dir O -from 27 -to 0 gpio_0_c2_t
+create_bd_port -dir I clkout_in_s
 
+ad_connect clkout_in_s clk_monitor_0/clock_0
+ad_connect sys_cpu_clk clk_monitor_0/clock_1
+ad_connect gpio_0_c1_i gpio_0/gpio_io_i
+ad_connect gpio_0_c1_o gpio_0/gpio_io_o
+ad_connect gpio_0_c1_t gpio_0/gpio_io_t
+ad_connect gpio_0_c2_i gpio_0/gpio2_io_i
+ad_connect gpio_0_c2_o gpio_0/gpio2_io_o
+ad_connect gpio_0_c2_t gpio_0/gpio2_io_t
+
+ad_cpu_interconnect 0x41200000 gpio_0
+ad_cpu_interconnect 0x41620000 clk_monitor_0
