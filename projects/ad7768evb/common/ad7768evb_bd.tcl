4397aee8476034c70dfcad7507537f34f4ff2454
Stanca Pop <stanca.pop@analog.com>
2020-08-18 10:58:23 +0300

    ad7768evb: Change num_of_channels dynamically

--- projects/ad7768evb/common/ad7768evb_bd.tcl
+++ projects/ad7768evb/common/ad7768evb_bd.tcl
@@ -3,20 +3,34 @@
 
 create_bd_port -dir I adc_clk
 create_bd_port -dir I adc_valid
-create_bd_port -dir I -from 31 -to 0 adc_data_0
-create_bd_port -dir I -from 31 -to 0 adc_data_1
-create_bd_port -dir I -from 31 -to 0 adc_data_2
-create_bd_port -dir I -from 31 -to 0 adc_data_3
-create_bd_port -dir I -from 31 -to 0 adc_data_4
-create_bd_port -dir I -from 31 -to 0 adc_data_5
-create_bd_port -dir I -from 31 -to 0 adc_data_6
-create_bd_port -dir I -from 31 -to 0 adc_data_7
+# create_bd_port -dir I -from 31 -to 0 adc_data_0
+# create_bd_port -dir I -from 31 -to 0 adc_data_1
+# create_bd_port -dir I -from 31 -to 0 adc_data_2
+# create_bd_port -dir I -from 31 -to 0 adc_data_3
+# create_bd_port -dir I -from 31 -to 0 adc_data_4
+# create_bd_port -dir I -from 31 -to 0 adc_data_5
+# create_bd_port -dir I -from 31 -to 0 adc_data_6
+# create_bd_port -dir I -from 31 -to 0 adc_data_7
 create_bd_port -dir I -from 31 -to 0 adc_gpio_0_i
 create_bd_port -dir O -from 31 -to 0 adc_gpio_0_o
 create_bd_port -dir O -from 31 -to 0 adc_gpio_0_t
 create_bd_port -dir I -from 31 -to 0 adc_gpio_1_i
 create_bd_port -dir O -from 31 -to 0 adc_gpio_1_o
 create_bd_port -dir O -from 31 -to 0 adc_gpio_1_t
+
+for {set i 0} {$i < $num_of_channels} {incr i} {
+  create_bd_port -dir I -from 31 -to 0 adc_data_$i
+};
+
+if { $num_of_channels == 1 } {
+  set dma_width_src 32
+} elseif { $num_of_channels == 2 } {
+  set dma_width_src 64
+} elseif {( $num_of_channels == 3) || ( $num_of_channels == 4 )} {
+  set dma_width_src 128
+} else {
+  set dma_width_src 256
+};
 
 # instances
 
@@ -28,7 +42,7 @@ ad_ip_parameter ad7768_dma CONFIG.SYNC_TRANSFER_START 1
 ad_ip_parameter ad7768_dma CONFIG.AXI_SLICE_SRC 0
 ad_ip_parameter ad7768_dma CONFIG.AXI_SLICE_DEST 0
 ad_ip_parameter ad7768_dma CONFIG.DMA_2D_TRANSFER 0
-ad_ip_parameter ad7768_dma CONFIG.DMA_DATA_WIDTH_SRC 256
+ad_ip_parameter ad7768_dma CONFIG.DMA_DATA_WIDTH_SRC $dma_width_src
 
 # ps7-hp1
 
@@ -45,23 +59,23 @@ ad_ip_parameter ad7768_gpio CONFIG.C_INTERRUPT_PRESENT 1
 # adc-path channel pack
 
 ad_ip_instance util_cpack2 util_ad7768_adc_pack
-ad_ip_parameter util_ad7768_adc_pack CONFIG.NUM_OF_CHANNELS 8
+ad_ip_parameter util_ad7768_adc_pack CONFIG.NUM_OF_CHANNELS $num_of_channels
 ad_ip_parameter util_ad7768_adc_pack CONFIG.SAMPLE_DATA_WIDTH 32
 
 ad_connect adc_clk util_ad7768_adc_pack/clk
 ad_connect sys_rstgen/peripheral_reset util_ad7768_adc_pack/reset
 ad_connect adc_valid util_ad7768_adc_pack/fifo_wr_en
 
-for {set i 0} {$i < 8} {incr i} {
+for {set i 0} {$i < $num_of_channels} {incr i} {
   ad_connect adc_data_$i util_ad7768_adc_pack/fifo_wr_data_$i
 }
 
 # axi_generic_adc
 
 ad_ip_instance axi_generic_adc axi_ad7768_adc
-ad_ip_parameter axi_ad7768_adc CONFIG.NUM_OF_CHANNELS 8
+ad_ip_parameter axi_ad7768_adc CONFIG.NUM_OF_CHANNELS $num_of_channels
 
-for {set i 0} {$i < 8} {incr i} {
+for {set i 0} {$i < $num_of_channels} {incr i} {
   ad_ip_instance xlslice xlslice_$i
   set_property -dict [list CONFIG.DIN_FROM {$i} CONFIG.DIN_WIDTH {8} CONFIG.DOUT_WIDTH {1} CONFIG.DIN_TO {$i}] [get_bd_cells xlslice_$i]
   ad_connect axi_ad7768_adc/adc_enable xlslice_$i/Din
--- projects/ad7768evb/zed/system_bd.tcl
+++ projects/ad7768evb/zed/system_bd.tcl
@@ -1,12 +1,15 @@
 
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 
-#system ID
+# system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 set sys_cstring "sys rom custom string placeholder"
 sysid_gen_sys_init_file $sys_cstring
+
+# specify the number of channels -- between 1 and 8 active channels
+set num_of_channels 8
 
 source ../common/ad7768evb_bd.tcl
 
