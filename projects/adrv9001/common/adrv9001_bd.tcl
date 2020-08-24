bae7e48c50c47997813bcda5156f760a166dca2d
Laszlo Nagy <laszlo.nagy@analog.com>
2020-08-24 15:25:52 +0100

    adrv9001/common: Run DMAs @ 100MHz

--- projects/adrv9001/common/adrv9001_bd.tcl
+++ projects/adrv9001/common/adrv9001_bd.tcl
@@ -269,16 +269,16 @@ ad_cpu_interconnect 0x44A60000  axi_adrv9001_tx2_dma
 
 # memory inteconnect
 
-ad_mem_hp1_interconnect $sys_dma_clk sys_ps7/S_AXI_HP1
-ad_mem_hp1_interconnect $sys_dma_clk axi_adrv9001_rx1_dma/m_dest_axi
-ad_mem_hp1_interconnect $sys_dma_clk axi_adrv9001_rx2_dma/m_dest_axi
-ad_mem_hp1_interconnect $sys_dma_clk axi_adrv9001_tx1_dma/m_src_axi
-ad_mem_hp1_interconnect $sys_dma_clk axi_adrv9001_tx2_dma/m_src_axi
+ad_mem_hp1_interconnect $sys_cpu_clk sys_ps7/S_AXI_HP1
+ad_mem_hp1_interconnect $sys_cpu_clk axi_adrv9001_rx1_dma/m_dest_axi
+ad_mem_hp1_interconnect $sys_cpu_clk axi_adrv9001_rx2_dma/m_dest_axi
+ad_mem_hp1_interconnect $sys_cpu_clk axi_adrv9001_tx1_dma/m_src_axi
+ad_mem_hp1_interconnect $sys_cpu_clk axi_adrv9001_tx2_dma/m_src_axi
 
-ad_connect $sys_dma_resetn axi_adrv9001_rx1_dma/m_dest_axi_aresetn
-ad_connect $sys_dma_resetn axi_adrv9001_rx2_dma/m_dest_axi_aresetn
-ad_connect $sys_dma_resetn axi_adrv9001_tx1_dma/m_src_axi_aresetn
-ad_connect $sys_dma_resetn axi_adrv9001_tx2_dma/m_src_axi_aresetn
+ad_connect $sys_cpu_resetn axi_adrv9001_rx1_dma/m_dest_axi_aresetn
+ad_connect $sys_cpu_resetn axi_adrv9001_rx2_dma/m_dest_axi_aresetn
+ad_connect $sys_cpu_resetn axi_adrv9001_tx1_dma/m_src_axi_aresetn
+ad_connect $sys_cpu_resetn axi_adrv9001_tx2_dma/m_src_axi_aresetn
 # interrupts
 
 ad_cpu_interrupt ps-13 mb-12 axi_adrv9001_rx1_dma/irq
