8970c5a96288dfe9c73c6ea1ca1510e67f3ac32a
AndreiGrozav <andrei.grozav@analog.com>
2020-09-10 09:47:22 +0300

    ad_csc: Fix warning for axi_hdmi_tx
    Converting from RGB to YCbCr takes one less stage than converting
    from YCbCr to RGB color space.
    Moving extra delay stage(5), of the sync signals to a particular
    YCbCr to RGB color space conversion case.

--- library/common/ad_csc.v
+++ library/common/ad_csc.v
@@ -79,7 +79,6 @@ module ad_csc #(
   reg         [DELAY_DW-1:0]  sync_2_m;
   reg         [DELAY_DW-1:0]  sync_3_m;
   reg         [DELAY_DW-1:0]  sync_4_m;
-  reg         [DELAY_DW-1:0]  sync_5_m;
   reg         [         7:0]  csc_data_d;
 
 
@@ -96,7 +95,6 @@ module ad_csc #(
     sync_2_m <= sync_1_m;
     sync_3_m <= sync_2_m;
     sync_4_m <= sync_3_m;
-    sync_5_m <= sync_4_m;
   end
 
   assign color1 = {1'd0,    data[23:16]};
@@ -120,6 +118,7 @@ module ad_csc #(
   generate
     // in RGB to YCbCr there are no overflows or underflows
     if (YCbCr_2_RGB) begin
+      reg  [DELAY_DW-1:0]  sync_5_m;
       // output registers, output is unsigned (0 if sum is < 0) and saturated.
       // the inputs are expected to be 1.4.20 format (output is 8bits).
 
@@ -131,6 +130,7 @@ module ad_csc #(
         end else begin
           csc_data_d <= s_data_3[22:15];
         end
+        sync_5_m <= sync_4_m;
       end
       assign csc_data = csc_data_d;
       assign csc_sync = sync_5_m;
