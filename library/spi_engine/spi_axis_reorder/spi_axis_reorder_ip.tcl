971877b0fb72bfa74c74dfedec05845f6a18a020
Istvan Csomortani <istvan.csomortani@analog.com>
2020-09-28 19:24:46 +0100

    spi_engine/data_reorder: Initial commit
    In case of multiple SDI (MISO) lanes, the samples arrives in a parallel
    fashion. For example in case of 4 MISO line, at the first latching clock
    edge 4 bits of a sample will be saved, one bit into each shift register.
    
    The data reorder module reconstruct the incoming samples from the AXI
    stream of the offload module.

---
+++ library/spi_engine/spi_axis_reorder/spi_axis_reorder_ip.tcl
@@ -0,0 +1,33 @@
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
+
+adi_ip_create spi_axis_reorder
+adi_ip_files spi_axis_reorder [list \
+	"spi_axis_reorder.v" \
+]
+
+adi_ip_properties_lite spi_axis_reorder
+# Remove all inferred interfaces
+ipx::remove_all_bus_interface [ipx::current_core]
+
+# Interface definitions
+
+adi_add_bus "s_axis" "slave" \
+	"xilinx.com:interface:axis_rtl:1.0" \
+	"xilinx.com:interface:axis:1.0" \
+	[list {"s_axis_ready" "TREADY"} \
+	      {"s_axis_valid" "TVALID"} \
+	      {"s_axis_data" "TDATA"}]
+
+adi_add_bus "m_axis" "master" \
+	"xilinx.com:interface:axis_rtl:1.0" \
+	"xilinx.com:interface:axis:1.0" \
+	[list {"m_axis_ready" "TREADY"} \
+	      {"m_axis_valid" "TVALID"} \
+	      {"m_axis_data" "TDATA"}]
+
+adi_add_bus_clock "axis_aclk" "s_axis:m_axis" "axis_aresetn"
+
+ipx::save_core [ipx::current_core]
+
