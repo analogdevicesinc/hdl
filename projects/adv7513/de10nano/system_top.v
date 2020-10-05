b7fef66772513105f3225d8514144dc06781bcbf
AndreiGrozav <andrei.grozav@analog.com>
2020-10-05 11:31:04 +0300

    adv7513_de10nano: Fix gpio_bd assignments

--- projects/adv7513/de10nano/system_top.v
+++ projects/adv7513/de10nano/system_top.v
@@ -131,9 +131,8 @@ module system_top (
 
   // instantiations
 
-  assign gpio_i[63:32] = gpio_o[63:32];
-  assign gpio_i[31:12] = gpio_o[31:12];
-  assign gpio_i[11:4] = gpio_bd_i[5:0];
+  assign gpio_i[63:14] = gpio_o[63:14];
+  assign gpio_i[13:8] = gpio_bd_i[5:0];
 
   assign gpio_bd_o[7:0] = gpio_o[7:0];
   assign ltc2308_cs = gpio_o[41];
