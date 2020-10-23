b4c45383de4daf4dd31774466abaeb044a55e1e5
Laszlo Nagy <laszlo.nagy@analog.com>
2020-10-23 11:11:23 +0100

    testbenches/jesd_loopback: Added test configuration cfg2 with M=16 L=8

---
+++ testbenches/jesd_loopback/cfgs/cfg2.tcl
@@ -0,0 +1,6 @@
+set ad_project_params(JESD_M) 16
+set ad_project_params(JESD_L) 8
+set ad_project_params(JESD_F) 4
+set ad_project_params(JESD_S) 1
+set ad_project_params(JESD_K) 32
+set ad_project_params(JESD_NP) 16
