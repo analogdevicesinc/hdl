f7b8a2dfb5fc991db2efb07fa3e29dedb752d8f2
Istvan Csomortani <istvan.csomortani@analog.com>
2020-11-18 16:15:45 +0000

    axi_dmac: Update IP with the new util_axis_fifo
    Update instantiation, false path definitions and make file.

--- library/axi_dmac/request_arb.v
+++ library/axi_dmac/request_arb.v
@@ -431,7 +431,7 @@ util_axis_fifo #(
   .s_axis_aresetn(src_resetn),
   .s_axis_valid(src_bl_valid),
   .s_axis_ready(src_bl_ready),
-  .s_axis_empty(),
+  .s_axis_full(),
   .s_axis_data(src_burst_length),
   .s_axis_room(),
 
@@ -440,7 +440,8 @@ util_axis_fifo #(
   .m_axis_valid(dest_bl_valid),
   .m_axis_ready(dest_bl_ready),
   .m_axis_data(dest_src_burst_length),
-  .m_axis_level()
+  .m_axis_level(),
+  .m_axis_empty()
 );
 
 // Adapt burst length from source width to destination width by either
@@ -767,7 +768,7 @@ util_axis_fifo #(
   .s_axis_aresetn(src_resetn),
   .s_axis_valid(rewind_req_valid),
   .s_axis_ready(rewind_req_ready),
-  .s_axis_empty(),
+  .s_axis_full(),
   .s_axis_data(rewind_req_data),
   .s_axis_room(),
 
@@ -776,7 +777,8 @@ util_axis_fifo #(
   .m_axis_valid(req_rewind_req_valid),
   .m_axis_ready(req_rewind_req_ready),
   .m_axis_data(req_rewind_req_data),
-  .m_axis_level()
+  .m_axis_level(),
+  .m_axis_empty()
 );
 
 end else begin
@@ -1028,7 +1030,7 @@ util_axis_fifo #(
   .s_axis_aresetn(src_resetn),
   .s_axis_valid(src_dest_valid_hs_masked),
   .s_axis_ready(src_dest_ready_hs),
-  .s_axis_empty(),
+  .s_axis_full(),
   .s_axis_data({
     src_req_dest_address_cur,
     src_req_xlast_cur
@@ -1043,7 +1045,8 @@ util_axis_fifo #(
     dest_req_dest_address,
     dest_req_xlast
   }),
-  .m_axis_level()
+  .m_axis_level(),
+  .m_axis_empty()
 );
 
 util_axis_fifo #(
@@ -1055,7 +1058,7 @@ util_axis_fifo #(
   .s_axis_aresetn(req_resetn),
   .s_axis_valid(req_src_valid),
   .s_axis_ready(req_src_ready),
-  .s_axis_empty(),
+  .s_axis_full(),
   .s_axis_data({
     req_dest_address,
     req_src_address,
@@ -1077,7 +1080,8 @@ util_axis_fifo #(
     src_req_sync_transfer_start,
     src_req_xlast
   }),
-  .m_axis_level()
+  .m_axis_level(),
+  .m_axis_empty()
 );
 
 // Save the descriptor in the source clock domain since the submission to
