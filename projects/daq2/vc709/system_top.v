bd7a46221306dd3dd97798f90823398c6496c6db
Dan Hotoleanu <dan.hotoleanu@analog.com>
2020-11-26 16:11:19 +0000

    daq2:vc709: Modify system_top.v file
    Modified the gpio assignments in the system_top.v file.
    
    Signed-off-by: Dan Hotoleanu <dan.hotoleanu@analog.com>

--- projects/daq2/vc709/system_top.v
+++ projects/daq2/vc709/system_top.v
@@ -193,16 +193,18 @@ module system_top (
   assign gpio_i[12: 0] = gpio_bd_i;
   assign gpio_i[31:13] = gpio_o[31:13];
 
-  assign adc_pd = gpio_o[35];
-  assign dac_txen = gpio_o[34];
-  assign dac_reset = gpio_o[33];
-  assign clkd_sync = gpio_o[32];
-  assign gpio_i[37:37] = trig;
+  assign adc_pd = gpio_o[42];
+  assign dac_txen = gpio_o[41];
+  assign dac_reset = gpio_o[40];
+  assign clkd_sync = gpio_o[38];
+  assign gpio_i[43:43] = trig;
   assign gpio_i[36:36] = adc_fdb;
   assign gpio_i[35:35] = adc_fda;
   assign gpio_i[34:34] = dac_irq;
   assign gpio_i[33:32] = clkd_status;
-  assign gpio_i[63:38] = gpio_o[63:38];
+  assign gpio_i[63:44] = gpio_o[63:44];
+  assign gpio_i[39:39] = gpio_o[39:39];
+  assign gpio_i[37:37] = gpio_o[37:37];
 
   system_wrapper i_system_wrapper (
     .ddr3_addr (ddr3_addr),
