d9639db991aaa38c111dd0262554a28d83ecd15b
Istvan Csomortani <istvan.csomortani@analog.com>
2021-01-14 18:22:40 +0200

    m2k: Use adi_project_create instead of adi_project

--- projects/m2k/standalone/system_project.tcl
+++ projects/m2k/standalone/system_project.tcl
@@ -3,8 +3,7 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-set p_device "xc7z010clg225-1"
-adi_project m2k
+adi_project_create m2k 0 {} "xc7z010clg225-1"
 
 adi_project_files m2k [list \
   "../common/m2k_spi.v" \
