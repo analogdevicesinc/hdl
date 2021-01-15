1e34816f5940f593f4943f4052b70559bd5f96f4
Istvan Csomortani <istvan.csomortani@analog.com>
2021-01-15 10:06:35 +0000

    sidekiqz2: Use adi_project_create instead of adi_project

--- projects/sidekiqz2/system_project.tcl
+++ projects/sidekiqz2/system_project.tcl
@@ -3,8 +3,7 @@ source ../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-set p_device "xc7z010clg225-1"
-adi_project sidekiqz2
+adi_project_create sidekiqz2 0 {} "xc7z010clg225-1"
 
 adi_project_files sidekiqz2 [list \
   "system_top.v" \
