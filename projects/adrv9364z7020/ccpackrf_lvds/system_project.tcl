f68393ecb9165e836998dbd0689b6561648be299
Istvan Csomortani <istvan.csomortani@analog.com>
2021-01-15 10:06:04 +0000

    adrv936x: Use adi_project_create instead of adi_project

--- projects/adrv9364z7020/ccpackrf_lvds/system_project.tcl
+++ projects/adrv9364z7020/ccpackrf_lvds/system_project.tcl
@@ -3,8 +3,7 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-set p_device "xc7z020clg400-1"
-adi_project adrv9364z7020_ccpackrf_lvds
+adi_project_create adrv9364z7020_ccpackrf_lvds 0 {} "xc7z020clg400-1"
 adi_project_files adrv9364z7020_ccpackrf_lvds [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/library/common/ad_adl5904_rst.v" \
