b160ddb8b3caad540656c5712e93ee13d8ea4a05
Laszlo Nagy <laszlo.nagy@analog.com>
2021-01-20 15:58:39 +0000

    ad9081_fmca_ebz: a10soc: Initial version
    Parametrizable project with default profile of:
    
      M=8 L=4 SampleRate=250 MSPS
      LaneRate=10 Gbps

---
+++ projects/ad9081_fmca_ebz/a10soc/system_qsys.tcl
@@ -0,0 +1,20 @@
+
+## ADC FIFO depth in samples per converter
+set adc_fifo_samples_per_converter [expr $ad_project_params(RX_KS_PER_CHANNEL)*1024]
+## DAC FIFO depth in samples per converter
+set dac_fifo_samples_per_converter [expr $ad_project_params(TX_KS_PER_CHANNEL)*1024]
+
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+source $ad_hdl_dir/projects/common/a10soc/a10soc_system_qsys.tcl
+source $ad_hdl_dir/projects/common/intel/dacfifo_qsys.tcl
+source $ad_hdl_dir/projects/common/intel/adcfifo_qsys.tcl
+source ../common/ad9081_fmca_ebz_qsys.tcl
+
+#system ID
+set_instance_parameter_value axi_sysid_0 {ROM_ADDR_BITS} {9}
+set_instance_parameter_value rom_sys_0 {ROM_ADDR_BITS} {9}
+
+set_instance_parameter_value rom_sys_0 {PATH_TO_FILE} "[pwd]/mem_init_sys.txt"
+
+sysid_gen_sys_init_file;
+
