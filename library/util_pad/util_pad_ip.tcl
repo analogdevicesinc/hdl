36d0a8b3e8b58ad57b9ffdc497f3687afe0269ea
Laszlo Nagy <laszlo.nagy@analog.com>
2021-03-03 11:51:16 +0000

    library:util_pad: Initial version
    Data to DMA/system memory must be presented in widths of multiple of 8 bits,
    however this padding is not optimal if is done in the transport layer
    since this will affect the DAC/ADC FIFO or offload storage.
    
    This utility block adds or removes padding from sample stream in case the
    sample with is not multiple of 8 bits, and can be placed between the DMA
    and FIFO/Offload blocks.

---
+++ library/util_pad/util_pad_ip.tcl
@@ -0,0 +1,12 @@
+# ip
+
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
+
+adi_ip_create util_pad
+adi_ip_files util_pad [list \
+  "util_pad.v" ]
+
+adi_ip_properties_lite util_pad
+
+ipx::save_core [ipx::current_core]
