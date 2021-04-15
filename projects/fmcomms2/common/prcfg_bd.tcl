cecb17ba621d9128fbcff8daed9b0397cd56a27a
Dan Hotoleanu <dan.hotoleanu@analog.com>
2021-04-15 12:12:56 +0100

    partial_reconfig: Correct PR clk connection and PR block size
    Corrected the clk connection from the fmcomms2 block design to the PR
    module.
    Corrected the PR block size to encompass the qpsk architecture for the
    reconfigurable module (the design needed more DSPs in the PR area).
    
    Signed-off-by: Dan Hotoleanu <dan.hotoleanu@analog.com>

--- projects/fmcomms2/common/prcfg_bd.tcl
+++ projects/fmcomms2/common/prcfg_bd.tcl
@@ -64,7 +64,8 @@ create_bd_port -dir O  -from 31 -to 0 up_adc_gpio_out
 
 # re-wiring, split between ad9361 core & upack/cpack modules
 
-ad_connect  axi_ad9361_clk clk
+#ad_connect  axi_ad9361_clk clk
+ad_connect  util_ad9361_divclk/clk_out clk
 
 ad_reconct  util_ad9361_dac_upack/enable_0 dma_dac_i0_enable
 ad_reconct  util_ad9361_dac_upack/enable_1 dma_dac_q0_enable
