8c3db30a330e9f0c3aa68d8979e3a8995af0726a
Dan Hotoleanu <dan.hotoleanu@analog.com>
2021-04-15 12:12:56 +0100

    partial_reconfig: Correct PR clk connection and PR block size
    Corrected the clk connection from the fmcomms2 block design to the PR
    module.
    Corrected the PR block size to encompass the qpsk architecture for the
    reconfigurable module (the design needed more DSPs in the PR area).
    
    Signed-off-by: Dan Hotoleanu <dan.hotoleanu@analog.com>

--- projects/fmcomms2/zc706pr/system_project.tcl
+++ projects/fmcomms2/zc706pr/system_project.tcl
@@ -4,7 +4,7 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-set mode 0
+set mode 1
 if {$::argc > 0} {
   set mode [lindex $argv 0]
 }
