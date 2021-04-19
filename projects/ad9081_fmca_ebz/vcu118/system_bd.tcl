bc957dcfe7980aced9fcf04b18f247cebbf6a714
Laszlo Nagy <laszlo.nagy@analog.com>
2021-04-19 14:57:19 +0100

    ad9081_fmca_ebz/vcu118: Set XCVR params for 204C link
    Set XCVR parameter for 204C 24.75 Gbps with a  dynamic range of 10Gbps..24.75Gpbs
    
    Organize XCVR params based on lane rate

--- projects/ad9081_fmca_ebz/vcu118/system_bd.tcl
+++ projects/ad9081_fmca_ebz/vcu118/system_bd.tcl
@@ -20,7 +20,6 @@ ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
 sysid_gen_sys_init_file
 
-if {$ad_project_params(JESD_MODE) == "8B10B"} {
 # Parameters for 15.5Gpbs lane rate
 
 ad_ip_parameter util_mxfe_xcvr CONFIG.RX_CLK25_DIV 31
@@ -47,6 +46,7 @@ ad_ip_parameter util_mxfe_xcvr CONFIG.TXFE_CFG3 0xAA00
 ad_ip_parameter util_mxfe_xcvr CONFIG.TXPI_CFG0 0x3100
 ad_ip_parameter util_mxfe_xcvr CONFIG.TXPI_CFG1 0x0
 ad_ip_parameter util_mxfe_xcvr CONFIG.TX_PI_BIASSET 1
+ad_ip_parameter util_mxfe_xcvr CONFIG.RXPI_CFG1 0x54
 
 ad_ip_parameter util_mxfe_xcvr CONFIG.QPLL_REFCLK_DIV 1
 ad_ip_parameter util_mxfe_xcvr CONFIG.QPLL_CFG0 0x333c
@@ -54,9 +54,53 @@ ad_ip_parameter util_mxfe_xcvr CONFIG.QPLL_CFG4 0x2
 ad_ip_parameter util_mxfe_xcvr CONFIG.QPLL_FBDIV 20
 ad_ip_parameter util_mxfe_xcvr CONFIG.PPF0_CFG 0xB00
 ad_ip_parameter util_mxfe_xcvr CONFIG.QPLL_LPF 0x2ff
-} else {
-  set_property -dict [list CONFIG.ADDN_UI_CLKOUT4_FREQ_HZ {50}] [get_bd_cells axi_ddr_cntrl]
-  ad_connect  /axi_ddr_cntrl/addn_ui_clkout4 jesd204_phy_121/drpclk
-  ad_connect  /axi_ddr_cntrl/addn_ui_clkout4 jesd204_phy_126/drpclk  
-}
 
+# 204C params 16.5Gbps..24.75Gpbs
+if {$ad_project_params(JESD_MODE) == "64B66B"} {
+  # Lane rate indepentent parameters
+  ad_ip_parameter util_mxfe_xcvr CONFIG.RXCDR_CFG3_GEN2 0x12
+  ad_ip_parameter util_mxfe_xcvr CONFIG.RXCDR_CFG3_GEN3 0x12
+  ad_ip_parameter util_mxfe_xcvr CONFIG.RXCDR_CFG3_GEN4 0x12
+  ad_ip_parameter util_mxfe_xcvr CONFIG.RXPI_CFG1 0x0
+  ad_ip_parameter util_mxfe_xcvr CONFIG.RX_WIDEMODE_CDR 0x2
+  ad_ip_parameter util_mxfe_xcvr CONFIG.CH_HSPMUX 0x6060
+  ad_ip_parameter util_mxfe_xcvr CONFIG.PREIQ_FREQ_BST 2
+  ad_ip_parameter util_mxfe_xcvr CONFIG.TX_PI_BIASSET 2
+  ad_ip_parameter util_mxfe_xcvr CONFIG.RXDFE_KH_CFG2 0x281C
+  ad_ip_parameter util_mxfe_xcvr CONFIG.RXDFE_KH_CFG3 0x4120
+
+  # set dividers for 24.75Gbps, are overwritten by software
+  ad_ip_parameter util_mxfe_xcvr CONFIG.RX_CLK25_DIV 10
+  ad_ip_parameter util_mxfe_xcvr CONFIG.TX_CLK25_DIV 10
+  ad_ip_parameter util_mxfe_xcvr CONFIG.QPLL_FBDIV 66
+  ad_ip_parameter util_mxfe_xcvr CONFIG.QPLL_REFCLK_DIV 2
+
+  if {$ad_project_params(RX_LANE_RATE) < 20} {
+    ad_ip_parameter util_mxfe_xcvr CONFIG.RTX_BUF_CML_CTRL 0x5
+    ad_ip_parameter util_mxfe_xcvr CONFIG.RXPI_CFG0 0x0104
+  } else {
+    ad_ip_parameter util_mxfe_xcvr CONFIG.RTX_BUF_CML_CTRL 0x6
+    ad_ip_parameter util_mxfe_xcvr CONFIG.RXPI_CFG0 0x3004
+  }
+
+  if {$ad_project_params(TX_LANE_RATE) < 20} {
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXDRV_FREQBAND   1
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXFE_CFG0 0x3C2
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXFE_CFG1 0xAA00
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXFE_CFG2 0xAA00
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXFE_CFG3 0xAA00
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXPI_CFG0 0x0100
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXPI_CFG1 0x1000
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXSWBST_EN 0
+  } else {
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXDRV_FREQBAND   3
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXFE_CFG0 0x3C6
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXFE_CFG1 0xF800
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXFE_CFG2 0xF800
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXFE_CFG3 0xF800
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXPI_CFG0 0x3000
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXPI_CFG1 0x0
+    ad_ip_parameter util_mxfe_xcvr CONFIG.TXSWBST_EN 1
+  }
+
+}
