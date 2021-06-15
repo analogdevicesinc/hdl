5a82206271ead7ca70b1316d3d85e93800e0c030
stefan.raus <stefan.raus@analog.com>
2021-06-15 15:49:22 +0100

    adrv9001:a10soc:system_qsys.tcl: set clock polarity to 0
    For fixing "Failed to reset the device and set SPI Config"
    error, both clockPolarity and clockPhase should be disabled
    or both enabled. By default both are unset.
    
    Signed-off-by: Stefan Raus <stefan.raus@analog.com>

--- projects/adrv9001/a10soc/system_qsys.tcl
+++ projects/adrv9001/a10soc/system_qsys.tcl
@@ -2,7 +2,7 @@ source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 source $ad_hdl_dir/projects/common/a10soc/a10soc_system_qsys.tcl
 source ../common/adrv9001_qsys.tcl
 
-set_instance_parameter_value sys_spi {clockPolarity} {1}
+set_instance_parameter_value sys_spi {clockPolarity} {0}
 
 #system ID
 set_instance_parameter_value axi_sysid_0 {ROM_ADDR_BITS} {9}
