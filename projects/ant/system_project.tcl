ff5e2a9722b925bad5bc27d6ba0914937b6862b5
black-pigeon <1530604142@qq.com>
2021-06-19 15:29:43 +0800

    Create a new branch antsdr, add support for ANTSDR

---
+++ projects/ant/system_project.tcl
@@ -0,0 +1,17 @@
+
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+set p_device "xc7z020clg400-2"
+adi_project ant
+
+adi_project_files ant [list \
+  "system_top.v" \
+  "system_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v"]
+
+set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
+adi_project_run ant
+source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
+
