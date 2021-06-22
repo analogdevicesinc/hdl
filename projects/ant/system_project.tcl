048736153563d0efa7710c6cde2de9b790e495be
black-pigeon <1530604142@qq.com>
2021-06-22 18:49:57 +0800

    Edit the ANTSDR hdl project makefile

--- projects/ant/system_project.tcl
+++ projects/ant/system_project.tcl
@@ -9,7 +9,7 @@ adi_project ant
 adi_project_files ant [list \
   "system_top.v" \
   "system_constr.xdc" \
-  "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v"]
+  "$ad_hdl_dir/library/common/ad_iobuf.v"]
 
 set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
 adi_project_run ant
