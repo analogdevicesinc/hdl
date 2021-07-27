b5f33bb2ac30d2ee15954cf07b195aeed7256a14
Stanca Pop <stanca.pop@analog.com>
2021-07-27 16:06:13 +0300

    pulsar_adc_pmdz: Initial commit

---
+++ projects/pulsar_adc_pmdz/coraz7s/system_project.tcl
@@ -0,0 +1,15 @@
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project pulsar_adc_pmdz_coraz7s
+
+adi_project_files pulsar_adc_pmdz_coraz7s [list \
+    "$ad_hdl_dir/library/common/ad_iobuf.v" \
+    "system_top.v" \
+    "system_constr.xdc" \
+    "$ad_hdl_dir/projects/common/coraz7s/coraz7s_system_constr.xdc"]
+
+adi_project_run pulsar_adc_pmdz_coraz7s
+
