cfe0c0ced53b0664b903a93bc0bdce9fc12b3ab0
stefan.raus <stefan.raus@analog.com>
2021-09-21 16:09:26 +0100

    adi_project_xilinx.tcl, adi_ip_xilinx.tcl: update version to 2021.1
    Update vivado version from 2020.2 to 2021.1 in projects and library scripts.

--- library/scripts/adi_ip_xilinx.tcl
+++ library/scripts/adi_ip_xilinx.tcl
@@ -4,7 +4,7 @@ source $ad_hdl_dir/library/scripts/adi_xilinx_device_info_enc.tcl
 # check tool version
 
 if {![info exists REQUIRED_VIVADO_VERSION]} {
-  set REQUIRED_VIVADO_VERSION "2020.2"
+  set REQUIRED_VIVADO_VERSION "2021.1"
 }
 
 if {[info exists ::env(ADI_IGNORE_VERSION_CHECK)]} {
