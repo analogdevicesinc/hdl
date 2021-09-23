3640c2b5849b0a46ef853edc19bd373b7f53b9fa
Mihaita Nagy <mihaita.nagy@analog.com>
2021-09-23 12:30:37 +0100

    daq2/kcu105: Fix the ad9144 data offload to use internal BRAM

--- projects/daq2/kcu105/system_bd.tcl
+++ projects/daq2/kcu105/system_bd.tcl
@@ -1,15 +1,16 @@
+## Offload attributes
+set adc_offload_type 0
+set adc_offload_size [expr 512 * 1024]
 
-## FIFO depth is 4Mb - 250k samples
-set adc_fifo_address_width 16
+set dac_offload_type 0
+set dac_offload_size [expr 1 * 1024 * 1024]
 
-## FIFO depth is 4Mb - 250k samples
-set dac_fifo_address_width 15
+set plddr_offload_axi_data_width 0
+set plddr_offload_axi_addr_width 0
 
-## NOTE: With this configuration the #36Kb BRAM utilization is at ~70%
+## NOTE: With this configuration the #36Kb BRAM utilization is at ~78%
 
 source $ad_hdl_dir/projects/common/kcu105/kcu105_system_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/adcfifo_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
 source ../common/daq2_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 
@@ -18,7 +19,8 @@ ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
-sysid_gen_sys_init_file
+set sys_cstring "ADC_OFFLOAD_TYPE=$adc_offload_type\nDAC_OFFLOAD_TYPE=$dac_offload_type"
+sysid_gen_sys_init_file $sys_cstring
 
 ad_ip_parameter util_daq2_xcvr CONFIG.QPLL_FBDIV 20
 ad_ip_parameter util_daq2_xcvr CONFIG.QPLL_REFCLK_DIV 1
