8d547f31e167e4ad2e87fce2a6d32907357c3c6a
Laszlo Nagy <laszlo.nagy@analog.com>
2021-09-30 13:46:07 +0100

    ad9081_fmca_ebz/vck190: Initial version

---
+++ projects/ad9081_fmca_ebz/vck190/system_bd.tcl
@@ -0,0 +1,23 @@
+
+## ADC FIFO depth in samples per converter
+set adc_fifo_samples_per_converter [expr $ad_project_params(RX_KS_PER_CHANNEL)*1024]
+## DAC FIFO depth in samples per converter
+set dac_fifo_samples_per_converter [expr $ad_project_params(TX_KS_PER_CHANNEL)*1024]
+
+source $ad_hdl_dir/projects/common/vck190/vck190_system_bd.tcl
+source $ad_hdl_dir/projects/common/xilinx/adcfifo_bd.tcl
+source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
+
+# use versal transceiver wizard
+set ADI_PHY_SEL 0
+
+source $ad_hdl_dir/projects/ad9081_fmca_ebz/common/ad9081_fmca_ebz_bd.tcl
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+#system ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+
+sysid_gen_sys_init_file
+
