1d951cfbaef755fdf2048edcbb344d35df86c5b2
Laszlo Nagy <laszlo.nagy@analog.com>
2021-10-15 20:06:18 +0100

    ad9081_fmca_ebz/vck190: Change default profile to 2 lanes

--- projects/ad9081_fmca_ebz/vck190/system_project.tcl
+++ projects/ad9081_fmca_ebz/vck190/system_project.tcl
@@ -31,15 +31,15 @@ source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 adi_project ad9081_fmca_ebz_vck190 0 [list \
   JESD_MODE    [get_env_param JESD_MODE    64B66B ]\
-  RX_LANE_RATE [get_env_param RX_RATE      24.75 ] \
-  TX_LANE_RATE [get_env_param TX_RATE      24.75 ] \
-  RX_JESD_M    [get_env_param RX_JESD_M    4 ] \
-  RX_JESD_L    [get_env_param RX_JESD_L    4 ] \
-  RX_JESD_S    [get_env_param RX_JESD_S    2 ] \
+  RX_LANE_RATE [get_env_param RX_RATE      11.88 ] \
+  TX_LANE_RATE [get_env_param TX_RATE      11.88 ] \
+  RX_JESD_M    [get_env_param RX_JESD_M    2 ] \
+  RX_JESD_L    [get_env_param RX_JESD_L    2 ] \
+  RX_JESD_S    [get_env_param RX_JESD_S    4 ] \
   RX_JESD_NP   [get_env_param RX_JESD_NP   12] \
   RX_NUM_LINKS [get_env_param RX_NUM_LINKS 1 ] \
-  TX_JESD_M    [get_env_param TX_JESD_M    4 ] \
-  TX_JESD_L    [get_env_param TX_JESD_L    4 ] \
+  TX_JESD_M    [get_env_param TX_JESD_M    2 ] \
+  TX_JESD_L    [get_env_param TX_JESD_L    2 ] \
   TX_JESD_S    [get_env_param TX_JESD_S    2 ] \
   TX_JESD_NP   [get_env_param TX_JESD_NP   12] \
   TX_NUM_LINKS [get_env_param TX_NUM_LINKS 1 ] \
