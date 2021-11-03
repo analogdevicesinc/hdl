7ac0f5f97727edde7f6b275db22c7ee98575213f
Dan Hotoleanu <dan.hotoleanu@analog.com>
2021-11-03 13:16:09 +0000

    fmcjesdadc1: Fix ad9250 core parameters settings
    Fix CONVERTER_RESOLUTION parameter setting for ad9250. Also deleted the
    setting of BITS_PER_SAMPLE and DMA_BITS_PER_SAMPLE for ad9250 since they
    are set by default to the desired values.
    
    Signed-off-by: Dan Hotoleanu <dan.hotoleanu@analog.com>

--- projects/fmcjesdadc1/common/fmcjesdadc1_bd.tcl
+++ projects/fmcjesdadc1/common/fmcjesdadc1_bd.tcl
@@ -25,9 +25,7 @@ adi_tpl_jesd204_rx_create axi_ad9250_core $RX_NUM_OF_LANES \
                                             $RX_NUM_OF_CONVERTERS \
                                             $RX_SAMPLES_PER_FRAME \
                                             $RX_SAMPLE_WIDTH
-ad_ip_parameter axi_ad9250_core CONFIG.CONVERTER_RESOLUTION 14
-ad_ip_parameter axi_ad9250_core CONFIG.BITS_PER_SAMPLE 16
-ad_ip_parameter axi_ad9250_core CONFIG.DMA_BITS_PER_SAMPLE 16
+ad_ip_parameter axi_ad9250_core/adc_tpl_core CONFIG.CONVERTER_RESOLUTION 14
 
 ad_ip_instance util_cpack2 axi_ad9250_cpack [list \
   NUM_OF_CHANNELS $RX_NUM_OF_CONVERTERS \
