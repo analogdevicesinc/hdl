76a48f2e136e1ec71ddc3d33fb6d5387d0c405e8
Laszlo Nagy <laszlo.nagy@analog.com>
2021-11-05 17:16:56 +0200

    adrv9001/zed: Use global clock buffers for better fit the design
    Occasionally with zed, the implementation failed at the placement stage where
    the tool could not fit the logic cells inside a single clock region,
    constraint required by the usage of regional clock buffers.
    
    This commit allows the usage of the global clock buffers which help the tool
    in such cases and allow a larger application logic to be implemented in fabric.

--- projects/adrv9001/zed/system_bd.tcl
+++ projects/adrv9001/zed/system_bd.tcl
@@ -3,6 +3,9 @@ source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 source ../common/adrv9001_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 
+ad_ip_parameter axi_adrv9001 CONFIG.RX_USE_BUFG 1
+ad_ip_parameter axi_adrv9001 CONFIG.TX_USE_BUFG 1
+
 #system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
