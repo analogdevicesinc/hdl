11a554c9263f71a9ec68bca4e1c155a2b3ebbf4a
Mihaita Nagy <mihaita.nagy@analog.com>
2021-11-26 10:03:40 +0000

    library/adaq8092: Add support

---
+++ library/axi_adaq8092/axi_adaq8092_if.v
@@ -0,0 +1,127 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+// This is the LVDS/DDR interface, note that overrange is independent of data path,
+// software will not be able to relate overrange to a specific sample!
+
+`timescale 1ns/100ps
+
+module axi_adaq8092_if #(
+
+  parameter   FPGA_TECHNOLOGY = 0,
+  parameter   IO_DELAY_GROUP = "adc_if_delay_group",
+  parameter   DELAY_REFCLK_FREQUENCY = 200) (
+
+  // adc interface (clk, data, over-range)
+  // nominal clock 125 MHz, up to 300 MHz
+
+  input                   adc_clk_in_p,
+  input                   adc_clk_in_n,
+  input       [13:0]      adc_data_in_p,
+  input       [13:0]      adc_data_in_n,
+
+  // interface outputs
+
+  output                  adc_clk,
+  output  reg [27:0]      adc_data,
+  output  reg             adc_status,
+
+  // delay control signals
+
+  input                   up_clk,
+  input       [ 8:0]      up_dld,
+  input       [44:0]      up_dwdata,
+  output      [44:0]      up_drdata,
+  input                   delay_clk,
+  input                   delay_rst,
+  output                  delay_locked);
+
+
+  // internal registers
+
+  reg     [13:0]  adc_data_p = 'd0;
+  reg     [13:0]  adc_data_n = 'd0;
+
+  // internal signals
+
+  wire    [13:0]  adc_data_p_s;
+  wire    [13:0]  adc_data_n_s;
+
+  genvar          l_inst;
+
+  always @(posedge adc_clk)
+  begin
+    adc_status <= 1'b1;
+    adc_data <= { adc_data_p_s[13], adc_data_n_s[13], adc_data_p_s[12], adc_data_n_s[12], adc_data_p_s[11], adc_data_n_s[11], adc_data_p_s[10], adc_data_n_s[10], adc_data_p_s[9], adc_data_n_s[9], adc_data_p_s[8], adc_data_n_s[8], adc_data_p_s[7], adc_data_n_s[7], adc_data_p_s[6], adc_data_n_s[6], adc_data_p_s[5], adc_data_n_s[5], adc_data_p_s[4], adc_data_n_s[4], adc_data_p_s[3], adc_data_n_s[3], adc_data_p_s[2], adc_data_n_s[2], adc_data_p_s[1], adc_data_n_s[1], adc_data_p_s[0], adc_data_n_s[0]};
+  end
+
+  // data interface
+
+  generate
+  for (l_inst = 0; l_inst <= 13; l_inst = l_inst + 1) begin : g_adc_if
+  ad_data_in #(
+    .FPGA_TECHNOLOGY (FPGA_TECHNOLOGY),
+    .IODELAY_CTRL (0),
+    .IODELAY_GROUP (IO_DELAY_GROUP),
+    .REFCLK_FREQUENCY (DELAY_REFCLK_FREQUENCY))
+  i_adc_data (
+    .rx_clk (adc_clk),
+    .rx_data_in_p (adc_data_in_p[l_inst]),
+    .rx_data_in_n (adc_data_in_n[l_inst]),
+    .rx_data_p (adc_data_p_s[l_inst]),
+    .rx_data_n (adc_data_n_s[l_inst]),
+    .up_clk (up_clk),
+    .up_dld (up_dld[l_inst]),
+    .up_dwdata (up_dwdata[((l_inst*5)+4):(l_inst*5)]),
+    .up_drdata (up_drdata[((l_inst*5)+4):(l_inst*5)]),
+    .delay_clk (delay_clk),
+    .delay_rst (delay_rst),
+    .delay_locked ());
+  end
+  endgenerate
+
+  // clock
+
+  ad_data_clk
+  i_adc_clk (
+    .rst (1'b0),
+    .locked (),
+    .clk_in_p (adc_clk_in_p),
+    .clk_in_n (adc_clk_in_n),
+    .clk (adc_clk));
+
+endmodule
+
+// ***************************************************************************
+// ***************************************************************************
---
+++ library/axi_adaq8092/axi_adaq8092_ip.tcl
@@ -0,0 +1,48 @@
+# ip
+
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
+
+adi_ip_create axi_adaq8092
+adi_ip_files axi_adaq8092 [list \
+  "$ad_hdl_dir/library/common/ad_rst.v" \
+  "$ad_hdl_dir/library/xilinx/common/ad_data_clk.v" \
+  "$ad_hdl_dir/library/xilinx/common/ad_data_in.v" \
+  "$ad_hdl_dir/library/xilinx/common/ad_dcfilter.v" \
+  "$ad_hdl_dir/library/common/ad_datafmt.v" \
+  "$ad_hdl_dir/library/common/ad_pnmon.v" \
+  "$ad_hdl_dir/library/common/up_xfer_status.v" \
+  "$ad_hdl_dir/library/common/up_xfer_cntrl.v" \
+  "$ad_hdl_dir/library/common/up_clock_mon.v" \
+  "$ad_hdl_dir/library/common/up_delay_cntrl.v" \
+  "$ad_hdl_dir/library/common/up_adc_common.v" \
+  "$ad_hdl_dir/library/common/up_adc_channel.v" \
+  "$ad_hdl_dir/library/common/up_axi.v" \
+  "$ad_hdl_dir/library/xilinx/common/up_xfer_cntrl_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/ad_rst_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/up_xfer_status_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/up_clock_mon_constr.xdc" \
+  "axi_adaq8092_pnmon.v" \
+  "axi_adaq8092_if.v" \
+  "axi_adaq8092_channel.v" \
+  "axi_adaq8092.v" ]
+
+adi_ip_properties axi_adaq8092
+
+adi_init_bd_tcl
+adi_ip_bd axi_adaq8092 "bd/bd.tcl"
+
+set_property company_url {https://wiki.analog.com/resources/fpga/docs/axi_adaq8092} [ipx::current_core]
+
+set_property driver_value 0 [ipx::get_ports *dovf* -of_objects [ipx::current_core]]
+
+ipx::infer_bus_interface adc_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+ipx::infer_bus_interface delay_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+ipx::infer_bus_interface adc_clk_in_P xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+ipx::infer_bus_interface adc_clk_in_n xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+ipx::infer_bus_interface adc_rst xilinx.com:signal:reset_rtl:1.0 [ipx::current_core]
+
+adi_add_auto_fpga_spec_params
+ipx::create_xgui_files [ipx::current_core]
+
+ipx::save_core [ipx::current_core]
