276063201943f4003231e72b51bc28866e3dfb48
AndreiGrozav <andrei.grozav@analog.com>
2021-12-14 15:02:09 +0200

    daq1_zc706: Update to latest master

--- projects/daq1/zc706/system_bd.tcl
+++ projects/daq1/zc706/system_bd.tcl
@@ -1,10 +1,18 @@
 
-set adc_fifo_name axi_ad9684_fifo
 set adc_fifo_address_width 18
 set adc_data_width 64
 set adc_dma_data_width 64
 
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
 source $ad_hdl_dir/projects/common/zc706/zc706_plddr3_adcfifo_bd.tcl
 source ../common/daq1_bd.tcl
 
+# System ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+
+#set sys_cstring "ADC_OFFLOAD_TYPE=$adc_offload_type\nDAC_OFFLOAD_TYPE=$dac_offload_type"
+#sysid_gen_sys_init_file $sys_cstring
+sysid_gen_sys_init_file
