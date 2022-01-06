77bbd7b13f8c23dbd0c8b69dfa44772f0b8022f2
Laszlo Nagy <laszlo.nagy@analog.com>
2022-01-06 17:31:47 +0200

    adrv9001: REVERT THIS  RX2/TX2 SSI DISABLED

--- projects/adrv9001/zc706/system_bd.tcl
+++ projects/adrv9001/zc706/system_bd.tcl
@@ -3,6 +3,9 @@ source $ad_hdl_dir/projects/common/zc706/zc706_system_bd.tcl
 source ../common/adrv9001_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 
+ad_ip_parameter axi_adrv9001 CONFIG.DISABLE_RX2_SSI 1
+ad_ip_parameter axi_adrv9001 CONFIG.DISABLE_TX2_SSI 1
+
 #system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
