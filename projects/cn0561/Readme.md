e71dbbd6f97d276648f01c0c9ba78f561d2ad386
Stanca Pop <stanca.pop@analog.com>
2022-01-21 20:07:37 +0200

    cn0561_zed: Initial commit

---
+++ projects/cn0561/Readme.md
@@ -0,0 +1,8 @@
+# CN0561 HDL Project
+
+Here are some pointers to help you:
+  * [Board Product Page](https://www.analog.com/CN0561)
+  * Parts: [24-Bit, 4-Channel Simultaneous Sampling 1.5 MSPS Precision Alias Free ADC](https://www.analog.com/ad4134)
+  * Project Doc: https://wiki.analog.com/resources/eval/user-guides/circuits-from-the-lab/cn0561
+  * HDL Doc: https://wiki.analog.com/resources/eval/user-guides/cn0561/hdl
+  * Linux Drivers: https://wiki.analog.com/resources/tools-software/linux-drivers-all
---
+++ projects/cn0561/zed/system_bd.tcl
@@ -0,0 +1,25 @@
+
+source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+#system ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+
+adi_project_files cn0561_fmc_zed [list \
+  "$ad_hdl_dir/library/common/ad_edge_detect.v" \
+  "$ad_hdl_dir/library/util_cdc/sync_bits.v" \
+]
+
+sysid_gen_sys_init_file
+
+# specify ADC resolution -- the design supports 16/24/32 bit resolutions
+
+set adc_resolution 24
+
+# ADC number of channels
+
+set adc_num_of_channels 4
+
+source ../common/cn0561_bd.tcl
