3ff2887485f19050348ecb9c07fde59b89138e61
Filip Gherman <Filip.Gherman@analog.com>
2022-01-21 15:22:07 +0200

    dac_fmc_ebz_vcu118: Initial commit

---
+++ projects/dac_fmc_ebz/vcu118/system_bd.tcl
@@ -0,0 +1,18 @@
+
+set dac_fifo_address_width 14
+
+source $ad_hdl_dir/projects/common/vcu118/vcu118_system_bd.tcl
+source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
+source ../common/dac_fmc_ebz_bd.tcl
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+#system ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+set ADI_DAC_DEVICE $::env(ADI_DAC_DEVICE)
+set ADI_DAC_MODE $::env(ADI_DAC_MODE)
+set sys_cstring "$ADI_DAC_DEVICE - $ADI_DAC_MODE"
+sysid_gen_sys_init_file $sys_cstring
+
+ad_ip_parameter dac_jesd204_link/tx CONFIG.SYSREF_IOB false
