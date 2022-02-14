e22a597752d97789fe65949ea54ff977ac984aac
Stanca Pop <stanca.pop@analog.com>
2022-02-14 13:39:42 +0200

    adrv2crr_fmcxmwbr1: Initial commit

---
+++ projects/adrv9009zu11eg/adrv2crr_fmcxmwbr1/system_project.tcl
@@ -0,0 +1,29 @@
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project_create adrv9009zu11eg_fmcxmwbr1 0 [list \
+  JESD_RX_M 8 \
+  JESD_RX_L 4 \
+  JESD_RX_S 1 \
+  JESD_TX_M 8 \
+  JESD_TX_L 8 \
+  JESD_TX_S 1 \
+  JESD_OBS_M 4 \
+  JESD_OBS_L 4 \
+  JESD_OBS_S 1 \
+] "xczu11eg-ffvf1517-2-i"
+
+adi_project_files adrv9009zu11eg_fmcxmwbr1 [list \
+  "system_top.v" \
+  "system_constr.xdc"\
+  "../common/adrv9009zu11eg_spi.v" \
+  "../common/adrv9009zu11eg_constr.xdc" \
+  "../common/adrv2crr_fmc_constr.xdc" \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" ]
+
+## To improve timing in DDR4 MIG
+set_property strategy Performance_ExploreWithRemap [get_runs impl_1]
+
+adi_project_run adrv9009zu11eg_fmcxmwbr1
