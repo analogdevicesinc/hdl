7b6463ffa3d797616ba2d69be340b4c5d9c53351
Liviu Adace <liviu.adace@analog.com>
2022-03-25 11:36:41 +0200

    cn0501: Removed DMA0 and connected the remaining DMA to SDRAM 1         Changed SPI CLK to 1MHz and CLK polarity to 0

--- projects/cn0501/common/cn0501_qsys.tcl
+++ projects/cn0501/common/cn0501_qsys.tcl
@@ -6,18 +6,8 @@ set_instance_parameter_value axi_dmac_0 {DMA_TYPE_SRC} {2}
 set_instance_parameter_value axi_dmac_0 {DMA_TYPE_DEST} {0}
 set_instance_parameter_value axi_dmac_0 {CYCLIC} {0}
 set_instance_parameter_value axi_dmac_0 {SYNC_TRANSFER_START} {1}
-set_instance_parameter_value axi_dmac_0 {DMA_DATA_WIDTH_SRC} {32}
-set_instance_parameter_value axi_dmac_0 {DMA_DATA_WIDTH_DEST} {64}
-
-# dma 1
-
-add_instance axi_dmac_1 axi_dmac
-set_instance_parameter_value axi_dmac_1 {DMA_TYPE_SRC} {2}
-set_instance_parameter_value axi_dmac_1 {DMA_TYPE_DEST} {0}
-set_instance_parameter_value axi_dmac_1 {CYCLIC} {0}
-set_instance_parameter_value axi_dmac_1 {SYNC_TRANSFER_START} {1}
-set_instance_parameter_value axi_dmac_1 {DMA_DATA_WIDTH_SRC} {256}
-set_instance_parameter_value axi_dmac_1 {DMA_DATA_WIDTH_DEST} {64}
+set_instance_parameter_value axi_dmac_0 {DMA_DATA_WIDTH_SRC} {256}
+set_instance_parameter_value axi_dmac_0 {DMA_DATA_WIDTH_DEST} {128}
 
 # cpack
 
@@ -53,23 +43,19 @@ set_interface_property if_data_in_bd               EXPORT_OF axi_ad7768_0.if_dat
 add_connection sys_clk.clk axi_dmac_0.s_axi_clock
 add_connection sys_dma_clk.clk axi_dmac_0.m_dest_axi_clock
 
-add_connection sys_clk.clk axi_dmac_1.s_axi_clock
-add_connection sys_dma_clk.clk axi_dmac_1.m_dest_axi_clock
-
 add_connection sys_clk.clk axi_ad7768_0.s_axi_clock
 
 add_connection axi_ad7768_0.if_adc_clk util_ad7768_adc_pack.clk
 add_connection axi_ad7768_0.if_adc_clk axi_dmac_0.if_fifo_wr_clk
-add_connection axi_ad7768_0.if_adc_clk axi_dmac_1.if_fifo_wr_clk
 
 add_connection axi_dmac_0.if_fifo_wr_en axi_ad7768_0.if_adc_valid
 add_connection axi_dmac_0.if_fifo_wr_din axi_ad7768_0.if_adc_data_p
 add_connection axi_dmac_0.if_fifo_wr_sync axi_ad7768_0.if_adc_sync
 
-add_connection util_ad7768_adc_pack.if_packed_fifo_wr_en  axi_dmac_1.if_fifo_wr_en
-add_connection util_ad7768_adc_pack.if_packed_fifo_wr_sync axi_dmac_1.if_fifo_wr_sync
-add_connection util_ad7768_adc_pack.if_packed_fifo_wr_data axi_dmac_1.if_fifo_wr_din
-add_connection util_ad7768_adc_pack.if_packed_fifo_wr_overflow axi_dmac_1.if_fifo_wr_overflow
+add_connection util_ad7768_adc_pack.if_packed_fifo_wr_en  axi_dmac_0.if_fifo_wr_en
+add_connection util_ad7768_adc_pack.if_packed_fifo_wr_sync axi_dmac_0.if_fifo_wr_sync
+add_connection util_ad7768_adc_pack.if_packed_fifo_wr_data axi_dmac_0.if_fifo_wr_din
+add_connection util_ad7768_adc_pack.if_packed_fifo_wr_overflow axi_dmac_0.if_fifo_wr_overflow
 
 add_connection axi_ad7768_0.if_adc_dovf util_ad7768_adc_pack.if_fifo_wr_overflow
 add_connection axi_ad7768_0.adc_ch_0 util_ad7768_adc_pack.adc_ch_0
@@ -87,22 +73,17 @@ add_connection sys_clk.clk_reset axi_ad7768_0.s_axi_reset
 add_connection sys_clk.clk_reset util_ad7768_adc_pack.reset
 add_connection sys_clk.clk_reset axi_dmac_0.s_axi_reset
 add_connection sys_dma_clk.clk_reset axi_dmac_0.m_dest_axi_reset
-add_connection sys_clk.clk_reset axi_dmac_1.s_axi_reset
-add_connection sys_dma_clk.clk_reset axi_dmac_1.m_dest_axi_reset
 
 # interfaces
 
-add_connection axi_dmac_0.m_dest_axi sys_hps.f2h_sdram0_data
-add_connection axi_dmac_1.m_dest_axi sys_hps.f2h_sdram0_data
+add_connection axi_dmac_0.m_dest_axi sys_hps.f2h_sdram1_data
 
 # cpu interconnects
 
-ad_cpu_interconnect 0x00020000 axi_dmac_0.s_axi
-ad_cpu_interconnect 0x00028000 axi_dmac_1.s_axi
+ad_cpu_interconnect 0x00028000 axi_dmac_0.s_axi
 ad_cpu_interconnect 0x00030000 axi_ad7768_0.s_axi
 
 #interrupts
 
-ad_cpu_interrupt 6 axi_dmac_0.interrupt_sender
-ad_cpu_interrupt 5 axi_dmac_1.interrupt_sender
+ad_cpu_interrupt 5 axi_dmac_0.interrupt_sender
 
