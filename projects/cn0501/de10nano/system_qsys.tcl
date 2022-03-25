1889ffcded9386abe5663fa5ed8a33543d348e52
Liviu Adace <liviu.adace@analog.com>
2022-03-25 11:36:41 +0200

    cn0501: Removed DMA0 and connected the remaining DMA to SDRAM 1         Changed SPI CLK to 1MHz and CLK polarity to 0

--- projects/cn0501/de10nano/system_qsys.tcl
+++ projects/cn0501/de10nano/system_qsys.tcl
@@ -4,3 +4,5 @@ set dac_fifo_address_width 10
 source $ad_hdl_dir/projects/common/de10nano/de10nano_system_qsys.tcl
 source ../common/cn0501_qsys.tcl
 
+set_instance_parameter_value sys_spi {clockPolarity} {0}
+set_instance_parameter_value sys_spi {targetClockRate} {1000000.0}
