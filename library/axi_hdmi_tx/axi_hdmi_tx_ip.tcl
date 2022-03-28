f259db41dec79ee616cc58dd8fd5a0962d891966
PopPaul2021 <Paul.Pop@analog.com>
2022-03-28 18:02:11 +0300

    axi_hdmi_tx update for: ZedBoard, ZC706, ZC702, de10nano, ADRV9361-Z7035

--- library/axi_hdmi_tx/axi_hdmi_tx_ip.tcl
+++ library/axi_hdmi_tx/axi_hdmi_tx_ip.tcl
@@ -37,6 +37,9 @@ set_property driver_value 0 [ipx::get_ports *hsync* -of_objects [ipx::current_co
 set_property driver_value 0 [ipx::get_ports *vsync* -of_objects [ipx::current_core]]
 set_property driver_value 0 [ipx::get_ports *data* -of_objects [ipx::current_core]]
 set_property driver_value 0 [ipx::get_ports *es_data* -of_objects [ipx::current_core]]
+set_property driver_value 0 [ipx::get_ports *red* -of_objects [ipx::current_core]]
+set_property driver_value 0 [ipx::get_ports *green* -of_objects [ipx::current_core]]
+set_property driver_value 0 [ipx::get_ports *blue* -of_objects [ipx::current_core]]
 
 set_property driver_value 0 [ipx::get_ports *vdma_end_of_frame* -of_objects [ipx::current_core]]
 set_property driver_value 0 [ipx::get_ports *vdma_valid* -of_objects [ipx::current_core]]
@@ -45,8 +48,11 @@ set_property driver_value 0 [ipx::get_ports *vdma_ready* -of_objects [ipx::curre
 
 set_property value_format string [ipx::get_user_parameters INTERFACE -of_objects [ipx::current_core]]
 set_property value_format string [ipx::get_hdl_parameters INTERFACE -of_objects [ipx::current_core]]
-set_property value_validation_list {16_BIT 24_BIT 36_BIT 16_BIT_EMBEDDED_SYNC} \
+set_property value_validation_list {16_BIT 24_BIT 36_BIT 16_BIT_EMBEDDED_SYNC VGA_INTERFACE} \
   [ipx::get_user_parameters INTERFACE -of_objects [ipx::current_core]]
+
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.INTERFACE')) == "16_BIT" || spirit:decode(id('MODELPARAM_VALUE.INTERFACE')) == "24_BIT" || spirit:decode(id('MODELPARAM_VALUE.INTERFACE')) == "36_BIT" || spirit:decode(id('MODELPARAM_VALUE.INTERFACE')) == "16_BIT_EMBEDDED_SYNC" } \
+  [ipx::get_ports *hdmi_out_clk* -of_objects [ipx::current_core]]
 
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.INTERFACE')) == "16_BIT"} \
   [ipx::get_ports *hdmi_16* -of_objects [ipx::current_core]]
@@ -56,6 +62,8 @@ set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.INTERFACE
   [ipx::get_ports *hdmi_36* -of_objects [ipx::current_core]]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.INTERFACE')) == "16_BIT_EMBEDDED_SYNC"} \
   [ipx::get_ports *hdmi_16_es_data* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.INTERFACE')) == "VGA_INTERFACE"} \
+  [ipx::get_ports *vga* -of_objects [ipx::current_core]]
 
 adi_add_bus "s_axis" "slave" \
          "xilinx.com:interface:axis_rtl:1.0" \
@@ -65,8 +73,9 @@ adi_add_bus "s_axis" "slave" \
            {"vdma_data" "TDATA"} \
            {"vdma_end_of_frame" "TLAST"} ]
 
-ipx::infer_bus_interface hdmi_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+ipx::infer_bus_interface reference_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
 ipx::infer_bus_interface hdmi_out_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+ipx::infer_bus_interface vga_out_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
 ipx::infer_bus_interface vdma_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
 
 ipx::associate_bus_interfaces -busif s_axis -clock vdma_clk [ipx::current_core]
