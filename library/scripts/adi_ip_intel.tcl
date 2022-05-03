7faefab1bea9f9aef2dccb648823dd257b1b3749
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2022-05-03 09:29:43 +0100

    library/scripts: Add SV support for Intel boards
    Signed-off-by: Ionut Podgoreanu <ionut.podgoreanu@analog.com>

--- library/scripts/adi_ip_intel.tcl
+++ library/scripts/adi_ip_intel.tcl
@@ -361,8 +361,16 @@ proc ad_ip_addfile {pname pfile} {
     add_fileset_file $pmodule VERILOG PATH $pfile
     return
   }
+  if {$ptype eq ".sv"} {
+    add_fileset_file $pmodule SYSTEM_VERILOG PATH $pfile
+    return
+  }
   if {$ptype eq ".vh"} {
     add_fileset_file $pmodule VERILOG_INCLUDE PATH $pfile
+    return
+  }
+  if {$ptype eq ".svh"} {
+    add_fileset_file $pmodule SYSTEM_VERILOG_INCLUDE PATH $pfile
     return
   }
   if {$ptype eq ".sdc"} {
