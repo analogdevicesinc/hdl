6a5174edaa9a8951d36c68e39dec4205dbe248bc
AndreiGrozav <andrei.grozav@analog.com>
2022-05-17 10:56:01 +0300

    ad9083_vna: Init commit
    Compatible with RevB

---
+++ projects/ad9083_vna/Readme.md
@@ -0,0 +1,15 @@
+# AD9083-VNA HDL Project
+
+Here are some pointers to help you:
+  * [Board Product Page](https://www.analog.com/eval-ad9083-vna)
+  * Part : [AD9083 - 16-Channel, 125 MHz Bandwidth, JESD204B Analog-to-Digital Converter](https://www.analog.com/ad9083)
+  * Part : [ADMV8818 - 2 GHz to 18 GHz, Digitally Tunable, High-Pass and Low-Pass Filter](https://www.analog.com/admv8818)
+  * Part : [ADRF5021 - 9 kHz to 30 GHz, Silicon SPDT Switch](https://www.analog.com/adrf5021)
+  * Part : [ADRF5045 - 9 kHz to 30 GHz, Silicon, SP4T Switch](https://www.analog.com/adrf5045)
+  * Part : [ADF4371S - Microwave Wideband Synthesizer with Integrated VCO](https://www.analog.com/adf4371)
+  * Part : [ADL5960 - 10 MHz to 20 GHz, Integrated Vector Network Analyzer Front-End](https://www.analog.com/adl5960)
+  * Part : [HMC8411 - Low Noise Amplifier, 0.01 GHz to 10 GHz](https://www.analog.com/hmc8411)
+  * Part : [HMC994 - 0.5 Watt Power Amplifier, DC - 30 GHz](https://www.analog.com/hmc994)
+  * Project Doc: https://wiki.analog.com/resources/eval/ad9083-vna
+  * HDL Doc: https://wiki.analog.com/resources/eval/user-guides/ad9083_vna/ad9083_vna_reference_hdl
+  * Linux Drivers: https://wiki.analog.com/resources/tools-software/linux-drivers/iio-adc/ad9083
---
+++ projects/ad9083_vna/common/ad9083_vna_bd.tcl
@@ -0,0 +1,92 @@
+
+# RX parameters
+set RX_NUM_OF_LANES 1        ; # L
+set RX_NUM_OF_CONVERTERS 32  ; # M
+set RX_SAMPLES_PER_FRAME 1   ; # S
+set RX_SAMPLE_WIDTH 16       ; # N/NP
+
+source ../../ad9083_evb/common/ad9083_evb_bd.tcl
+
+# add spi interfaces
+
+create_bd_port -dir O -from 1 -to 0 spi_bus1_csn_o
+create_bd_port -dir I -from 1 -to 0 spi_bus1_csn_i
+create_bd_port -dir I spi_bus1_clk_i
+create_bd_port -dir O spi_bus1_clk_o
+create_bd_port -dir I spi_bus1_sdo_i
+create_bd_port -dir O spi_bus1_sdo_o
+create_bd_port -dir I spi_bus1_sdi_i
+
+create_bd_port -dir O -from 3 -to 0 spi_adl5960_1_csn_o
+create_bd_port -dir I -from 3 -to 0 spi_adl5960_1_csn_i
+create_bd_port -dir I spi_adl5960_1_clk_i
+create_bd_port -dir O spi_adl5960_1_clk_o
+create_bd_port -dir I spi_adl5960_1_sdo_i
+create_bd_port -dir O spi_adl5960_1_sdo_o
+create_bd_port -dir I spi_adl5960_1_sdi_i
+
+create_bd_port -dir O -from 3 -to 0 spi_adl5960_2_csn_o
+create_bd_port -dir I -from 3 -to 0 spi_adl5960_2_csn_i
+create_bd_port -dir I spi_adl5960_2_clk_i
+create_bd_port -dir O spi_adl5960_2_clk_o
+create_bd_port -dir I spi_adl5960_2_sdo_i
+create_bd_port -dir O spi_adl5960_2_sdo_o
+create_bd_port -dir I spi_adl5960_2_sdi_i
+
+# spi instances
+
+ad_ip_instance axi_quad_spi axi_spi_bus1
+ad_ip_parameter axi_spi_bus1 CONFIG.C_USE_STARTUP 0
+ad_ip_parameter axi_spi_bus1 CONFIG.C_NUM_SS_BITS 2
+ad_ip_parameter axi_spi_bus1 CONFIG.C_SCK_RATIO 8
+
+ad_ip_instance axi_quad_spi axi_spi_adl5960_1
+ad_ip_parameter axi_spi_adl5960_1 CONFIG.C_USE_STARTUP 0
+ad_ip_parameter axi_spi_adl5960_1 CONFIG.C_NUM_SS_BITS 4
+ad_ip_parameter axi_spi_adl5960_1 CONFIG.C_SCK_RATIO 16
+ad_ip_parameter axi_spi_adl5960_1 CONFIG.Multiples16 8
+
+ad_ip_instance axi_quad_spi axi_spi_adl5960_2
+ad_ip_parameter axi_spi_adl5960_2 CONFIG.C_USE_STARTUP 0
+ad_ip_parameter axi_spi_adl5960_2 CONFIG.C_NUM_SS_BITS 4
+ad_ip_parameter axi_spi_adl5960_2 CONFIG.C_SCK_RATIO 16
+ad_ip_parameter axi_spi_adl5960_2 CONFIG.Multiples16 8
+
+# spi connections
+
+ad_connect  sys_cpu_clk  axi_spi_bus1/ext_spi_clk
+ad_connect  spi_bus1_csn_i  axi_spi_bus1/ss_i
+ad_connect  spi_bus1_csn_o  axi_spi_bus1/ss_o
+ad_connect  spi_bus1_clk_i  axi_spi_bus1/sck_i
+ad_connect  spi_bus1_clk_o  axi_spi_bus1/sck_o
+ad_connect  spi_bus1_sdo_i  axi_spi_bus1/io0_i
+ad_connect  spi_bus1_sdo_o  axi_spi_bus1/io0_o
+ad_connect  spi_bus1_sdi_i  axi_spi_bus1/io1_i
+
+ad_connect  sys_cpu_clk  axi_spi_adl5960_1/ext_spi_clk
+ad_connect  spi_adl5960_1_csn_i  axi_spi_adl5960_1/ss_i
+ad_connect  spi_adl5960_1_csn_o  axi_spi_adl5960_1/ss_o
+ad_connect  spi_adl5960_1_clk_i  axi_spi_adl5960_1/sck_i
+ad_connect  spi_adl5960_1_clk_o  axi_spi_adl5960_1/sck_o
+ad_connect  spi_adl5960_1_sdo_i  axi_spi_adl5960_1/io0_i
+ad_connect  spi_adl5960_1_sdo_o  axi_spi_adl5960_1/io0_o
+ad_connect  spi_adl5960_1_sdi_i  axi_spi_adl5960_1/io1_i
+
+ad_connect  sys_cpu_clk  axi_spi_adl5960_2/ext_spi_clk
+ad_connect  spi_adl5960_2_csn_i  axi_spi_adl5960_2/ss_i
+ad_connect  spi_adl5960_2_csn_o  axi_spi_adl5960_2/ss_o
+ad_connect  spi_adl5960_2_clk_i  axi_spi_adl5960_2/sck_i
+ad_connect  spi_adl5960_2_clk_o  axi_spi_adl5960_2/sck_o
+ad_connect  spi_adl5960_2_sdo_i  axi_spi_adl5960_2/io0_i
+ad_connect  spi_adl5960_2_sdo_o  axi_spi_adl5960_2/io0_o
+ad_connect  spi_adl5960_2_sdi_i  axi_spi_adl5960_2/io1_i
+
+ad_cpu_interconnect 0x48000000 axi_spi_bus1
+ad_cpu_interconnect 0x48100000 axi_spi_adl5960_1
+ad_cpu_interconnect 0x48200000 axi_spi_adl5960_2
+
+# interrupts
+
+ad_cpu_interrupt ps-9 mb-8   axi_spi_bus1/ip2intc_irpt
+ad_cpu_interrupt ps-10 mb-15 axi_spi_adl5960_1/ip2intc_irpt
+ad_cpu_interrupt ps-11 mb-14 axi_spi_adl5960_2/ip2intc_irpt
