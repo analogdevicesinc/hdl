7fd756a4fd63263b879c90b525faaa2fcf830841
Sergiu Arpadi <sergiu.arpadi@analog.com>
2022-06-02 22:55:45 +0300

    quad_iso_adc_fmc: Cleaned up

--- projects/quad_iso_adc_fmc/zed/system_project.tcl
+++ projects/quad_iso_adc_fmc/zed/system_project.tcl
@@ -7,8 +7,8 @@ adi_project quad_iso_adc_fmc_zed
 
 adi_project_files quad_iso_adc_fmc_zed [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "system_top_ad40xx.v" \
-  "system_constr_ad40xx.xdc" \
+  "system_top.v" \
+  "system_constr.xdc" \
   "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"]
 
 adi_project_run quad_iso_adc_fmc_zed
