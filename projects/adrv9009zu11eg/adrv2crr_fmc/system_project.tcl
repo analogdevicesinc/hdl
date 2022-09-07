6f4d0604724b85b277e84a3283b15da840a1d3ea
LIacob106 <liviu.iacob@analog.com>
2022-09-07 12:49:49 +0100

    projects/adrv9009zu11eg: expose jesd params to make and add FMCOMMS8 parameter
    Expose JESD parameters to make.
    Add FMCOMMS8 parameter.
    Changed the name of the observation path to match the rest of the repo.
    Replace old dac_data_width formula with a more generic one.

--- projects/adrv9009zu11eg/adrv2crr_fmc/system_project.tcl
+++ projects/adrv9009zu11eg/adrv2crr_fmc/system_project.tcl
@@ -4,15 +4,15 @@ source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 adi_project_create adrv9009zu11eg 0 [list \
-  JESD_RX_M 8 \
-  JESD_RX_L 4 \
-  JESD_RX_S 1 \
-  JESD_TX_M 8 \
-  JESD_TX_L 8 \
-  JESD_TX_S 1 \
-  JESD_OBS_M 4 \
-  JESD_OBS_L 4 \
-  JESD_OBS_S 1 \
+  RX_JESD_M       [get_env_param RX_JESD_M     8] \
+  RX_JESD_L       [get_env_param RX_JESD_L     4] \
+  RX_JESD_S       [get_env_param RX_JESD_S     1] \
+  TX_JESD_M       [get_env_param TX_JESD_M     8] \
+  TX_JESD_L       [get_env_param TX_JESD_L     8] \
+  TX_JESD_S       [get_env_param TX_JESD_S     1] \
+  RX_OS_JESD_M    [get_env_param RX_OS_JESD_M  4] \
+  RX_OS_JESD_L    [get_env_param RX_OS_JESD_L  4] \
+  RX_OS_JESD_S    [get_env_param RX_OS_JESD_S  1] \
 ] "xczu11eg-ffvf1517-2-i"
 
 adi_project_files adrv9009zu11eg [list \
--- projects/adrv9009zu11eg/adrv2crr_fmcomms8/system_bd.tcl
+++ projects/adrv9009zu11eg/adrv2crr_fmcomms8/system_bd.tcl
@@ -1,3 +1,4 @@
+set FMCOMMS8 1
 
 source ../common/adrv9009zu11eg_bd.tcl
 source ../common/adrv2crr_fmc_bd.tcl
