805318f69a9c0818f6b962c4cdeee8caf778bffc
AndreiGrozav <andrei.grozav@analog.com>
2022-10-11 09:41:36 +0100

    ad9083 based projects: Expose JESD parameters

--- projects/ad9083_evb/zcu102/system_bd.tcl
+++ projects/ad9083_evb/zcu102/system_bd.tcl
@@ -7,5 +7,8 @@ source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
-sysid_gen_sys_init_file
+set sys_cstring "RX_NUM_OF_LANES=$RX_NUM_OF_LANES \
+RX_NUM_OF_CONVERTERS=$RX_NUM_OF_CONVERTERS \
+RX_SAMPLES_PER_FRAME=$RX_SAMPLES_PER_FRAME"
+sysid_gen_sys_init_file $sys_cstring
 
