0070cd6a09cf13c6d123edfc4887c0b6d1ee4ede
alin724 <Alin-Tudor.Sferle@analog.com>
2022-10-27 21:33:34 +0300

    axi_ad7606x: Initial commit

---
+++ library/axi_ad7606x/axi_ad7606x.v
@@ -0,0 +1,463 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2022 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module axi_ad7606x #(
+
+  parameter       ID = 0,
+  parameter       DEV_CONFIG = 0,
+  parameter       ADC_READ_MODE = 0,
+  parameter       EXTERNAL_CLK = 0
+) (
+
+  // physical data interface
+
+  output                  rx_cs_n,
+  output      [15:0]      rx_db_o,
+  input       [15:0]      rx_db_i,
+  output                  rx_db_t,
+  output                  rx_rd_n,
+  output                  rx_wr_n,
+  input                   external_clk,
+
+  // physical control interface
+
+  input                   rx_busy,
+  input                   first_data,
+
+  // AXI Slave Memory Map
+
+  input                   s_axi_aclk,
+  input                   s_axi_aresetn,
+  input                   s_axi_awvalid,
+  input       [15:0]      s_axi_awaddr,
+  input       [ 2:0]      s_axi_awprot,
+  output                  s_axi_awready,
+  input                   s_axi_wvalid,
+  input       [31:0]      s_axi_wdata,
+  input       [ 3:0]      s_axi_wstrb,
+  output                  s_axi_wready,
+  output                  s_axi_bvalid,
+  output      [ 1:0]      s_axi_bresp,
+  input                   s_axi_bready,
+  input                   s_axi_arvalid,
+  input       [15:0]      s_axi_araddr,
+  input       [ 2:0]      s_axi_arprot,
+  output                  s_axi_arready,
+  output                  s_axi_rvalid,
+  output      [ 1:0]      s_axi_rresp,
+  output      [31:0]      s_axi_rdata,
+  input                   s_axi_rready,
+
+  input                   adc_dovf,
+  output                  adc_clk,
+
+  output                  adc_valid,
+  output      [31:0]      adc_data_0,
+  output      [31:0]      adc_data_1,
+  output      [31:0]      adc_data_2,
+  output      [31:0]      adc_data_3,
+  output      [31:0]      adc_data_4,
+  output      [31:0]      adc_data_5,
+  output      [31:0]      adc_data_6,
+  output      [31:0]      adc_data_7,
+  output                  adc_enable_0,
+  output                  adc_enable_1,
+  output                  adc_enable_2,
+  output                  adc_enable_3,
+  output                  adc_enable_4,
+  output                  adc_enable_5,
+  output                  adc_enable_6,
+  output                  adc_enable_7,
+  output                  adc_reset
+);
+
+  localparam                        RD_RAW_CAP = 32'h2000;
+
+  // internal registers
+
+  reg                               up_wack = 1'b0;
+  reg                               up_rack = 1'b0;
+  reg     [31:0]                    up_rdata = 32'b0;
+  reg     [31:0]                    up_rdata_r;
+  reg                               up_rack_r;
+  reg                               up_wack_r;
+
+  // internal signals
+
+  wire    [ 7:0]                    adc_status_header[0:7];
+  wire                              adc_status;
+  wire    [15:0]                    adc_crc;
+  wire    [15:0]                    adc_crc_res;
+  wire                              adc_crc_err;
+
+  wire    [ 7:0]                    adc_enable;
+  wire                              adc_reset_s;
+
+  wire                              up_clk;
+  wire                              up_rstn;
+  wire                              up_rreq_s;
+  wire    [13:0]                    up_raddr_s;
+  wire                              up_wreq_s;
+  wire    [13:0]                    up_waddr_s;
+  wire    [31:0]                    up_wdata_s;
+  wire    [31:0]                    up_rdata_s[0:8];
+  wire    [8:0]                     up_rack_s;
+  wire    [8:0]                     up_wack_s;
+
+  wire                              up_wack_cntrl_s;
+  wire                              up_rack_cntrl_s;
+  wire    [31:0]                    up_rdata_cntrl_s;
+
+  wire                              adc_clk_s;
+
+  wire                              wr_req_s;
+  wire    [31:0]                    wr_data_s;
+  wire                              rd_req_s;
+  wire    [15:0]                    rd_data_s;
+  wire                              rd_valid_s;
+  wire                              m_axis_ready_s;
+  wire                              m_axis_valid_s;
+  wire    [15:0]                    m_axis_data_s;
+  wire                              m_axis_xfer_req_s;
+
+  // defaults
+
+  assign up_clk = s_axi_aclk;
+  assign up_rstn = s_axi_aresetn;
+  assign adc_reset = adc_reset_s;
+
+  assign adc_enable_0 = adc_enable[0];
+  assign adc_enable_1 = adc_enable[1];
+  assign adc_enable_2 = adc_enable[2];
+  assign adc_enable_3 = adc_enable[3];
+  assign adc_enable_4 = adc_enable[4];
+  assign adc_enable_5 = adc_enable[5];
+  assign adc_enable_6 = adc_enable[6];
+  assign adc_enable_7 = adc_enable[7];
+
+  // processor read interface
+
+  integer j;
+
+  always @(*) begin
+    up_rdata_r = 'h00;
+    up_rack_r = 'h00;
+    up_wack_r = 'h00;
+    for (j = 0; j <= 8; j=j+1) begin
+      up_rack_r = up_rack_r | up_rack_s[j];
+      up_wack_r = up_wack_r | up_wack_s[j];
+      up_rdata_r = up_rdata_r | up_rdata_s[j];
+    end
+  end
+
+  always @(negedge up_rstn or posedge up_clk) begin
+    if (up_rstn == 0) begin
+      up_rdata <= 'd0;
+      up_rack <= 'd0;
+      up_wack <= 'd0;
+    end else begin
+      up_rdata <= up_rdata_r;
+      up_rack <= up_rack_r;
+      up_wack <= up_wack_r;
+    end
+  end
+
+  generate
+    if (EXTERNAL_CLK == 1'b1) begin
+      assign adc_clk_s = external_clk;
+    end else begin
+      assign adc_clk_s = up_clk;
+    end
+  endgenerate
+
+  assign adc_clk = adc_clk_s;
+
+  if (DEV_CONFIG == 0 || DEV_CONFIG == 1) begin
+    assign adc_data_0[15:0] = 16'b0;
+    assign adc_data_1[15:0] = 16'b0;
+    assign adc_data_2[15:0] = 16'b0;
+    assign adc_data_3[15:0] = 16'b0;
+    assign adc_data_4[15:0] = 16'b0;
+    assign adc_data_5[15:0] = 16'b0;
+    assign adc_data_6[15:0] = 16'b0;
+    assign adc_data_7[15:0] = 16'b0;
+  end else begin
+    assign adc_data_0[13:0] = 14'b0;
+    assign adc_data_1[13:0] = 14'b0;
+    assign adc_data_2[13:0] = 14'b0;
+    assign adc_data_3[13:0] = 14'b0;
+    assign adc_data_4[13:0] = 14'b0;
+    assign adc_data_5[13:0] = 14'b0;
+    assign adc_data_6[13:0] = 14'b0;
+    assign adc_data_7[13:0] = 14'b0;
+  end
+
+  generate
+    genvar i;
+    for (i = 0; i < 8; i = i + 1) begin
+      up_adc_channel #(
+        .CHANNEL_ID(i)
+      ) i_up_adc_channel (
+        .adc_clk (adc_clk_s),
+        .adc_rst (adc_reset_s),
+        .adc_enable (adc_enable[i]),
+        .adc_iqcor_enb (),
+        .adc_dcfilt_enb (),
+        .adc_dfmt_se (),
+        .adc_dfmt_type (),
+        .adc_dfmt_enable (),
+        .adc_dcfilt_offset (),
+        .adc_dcfilt_coeff (),
+        .adc_iqcor_coeff_1 (),
+        .adc_iqcor_coeff_2 (),
+        .adc_pnseq_sel (),
+        .adc_data_sel (),
+        .adc_pn_err (1'b0),
+        .adc_pn_oos (1'b0),
+        .adc_or (1'b0),
+        .adc_read_data (rd_data_s),
+        .adc_status_header(adc_status_header[i]),
+        .adc_crc_err(adc_crc_err),
+        .up_adc_pn_err (),
+        .up_adc_pn_oos (),
+        .up_adc_or (),
+        .up_usr_datatype_be (),
+        .up_usr_datatype_signed (),
+        .up_usr_datatype_shift (),
+        .up_usr_datatype_total_bits (),
+        .up_usr_datatype_bits (),
+        .up_usr_decimation_m (),
+        .up_usr_decimation_n (),
+        .adc_usr_datatype_be (1'b0),
+        .adc_usr_datatype_signed (1'b1),
+        .adc_usr_datatype_shift (8'd0),
+        .adc_usr_datatype_total_bits (8'd16),
+        .adc_usr_datatype_bits (8'd16),
+        .adc_usr_decimation_m (16'd1),
+        .adc_usr_decimation_n (16'd1),
+        .up_rstn (up_rstn),
+        .up_clk (up_clk),
+        .up_wreq (up_wreq_s),
+        .up_waddr (up_waddr_s),
+        .up_wdata (up_wdata_s),
+        .up_wack (up_wack_s[i]),
+        .up_rreq (up_rreq_s),
+        .up_raddr (up_raddr_s),
+        .up_rdata (up_rdata_s[i]),
+        .up_rack (up_rack_s[i]));
+    end
+  endgenerate
+
+  generate
+    if ((DEV_CONFIG == 0) || (DEV_CONFIG == 1)) begin
+      axi_ad7606x_16b_pif #(
+        .ADC_READ_MODE (ADC_READ_MODE)
+      ) i_ad7606_parallel_interface (
+        .cs_n (rx_cs_n),
+        .db_o (rx_db_o),
+        .db_i (rx_db_i),
+        .db_t (rx_db_t),
+        .rd_n (rx_rd_n),
+        .wr_n (rx_wr_n),
+        .busy (rx_busy),
+        .first_data (first_data),
+        .adc_data_0 (adc_data_0[31:16]),
+        .adc_status_0 (adc_status_header[0]),
+        .adc_data_1 (adc_data_1[31:16]),
+        .adc_status_1 (adc_status_header[1]),
+        .adc_data_2 (adc_data_2[31:16]),
+        .adc_status_2 (adc_status_header[2]),
+        .adc_data_3 (adc_data_3[31:16]),
+        .adc_status_3 (adc_status_header[3]),
+        .adc_data_4 (adc_data_4[31:16]),
+        .adc_status_4 (adc_status_header[4]),
+        .adc_data_5 (adc_data_5[31:16]),
+        .adc_status_5 (adc_status_header[5]),
+        .adc_data_6 (adc_data_6[31:16]),
+        .adc_status_6 (adc_status_header[6]),
+        .adc_data_7 (adc_data_7[31:16]),
+        .adc_status_7 (adc_status_header[7]),
+        .adc_status (adc_status),
+        .adc_crc (adc_crc),
+        .adc_crc_res (adc_crc_res),
+        .adc_crc_err (adc_crc_err),
+        .adc_valid (adc_valid),
+        .clk (adc_clk_s),
+        .rstn (up_rstn),
+        .rd_req (rd_req_s),
+        .wr_req (wr_req_s),
+        .wr_data (wr_data_s[15:0]),
+        .rd_data (rd_data_s),
+        .rd_valid (rd_valid_s));
+    end else begin
+      axi_ad7606x_18b_pif #(
+        .ADC_READ_MODE (ADC_READ_MODE)
+      ) i_ad7606_parallel_interface (
+        .cs_n (rx_cs_n),
+        .db_o (rx_db_o),
+        .db_i (rx_db_i),
+        .db_t (rx_db_t),
+        .rd_n (rx_rd_n),
+        .wr_n (rx_wr_n),
+        .busy (rx_busy),
+        .first_data (first_data),
+        .adc_data_0 (adc_data_0[31:14]),
+        .adc_status_0 (adc_status_header[0]),
+        .adc_data_1 (adc_data_1[31:14]),
+        .adc_status_1 (adc_status_header[1]),
+        .adc_data_2 (adc_data_2[31:14]),
+        .adc_status_2 (adc_status_header[2]),
+        .adc_data_3 (adc_data_3[31:14]),
+        .adc_status_3 (adc_status_header[3]),
+        .adc_data_4 (adc_data_4[31:14]),
+        .adc_status_4 (adc_status_header[4]),
+        .adc_data_5 (adc_data_5[31:14]),
+        .adc_status_5 (adc_status_header[5]),
+        .adc_data_6 (adc_data_6[31:14]),
+        .adc_status_6 (adc_status_header[6]),
+        .adc_data_7 (adc_data_7[31:14]),
+        .adc_status_7 (adc_status_header[7]),
+        .adc_status (adc_status),
+        .adc_crc (adc_crc),
+        .adc_crc_res (adc_crc_res),
+        .adc_crc_err (adc_crc_err),
+        .adc_valid (adc_valid),
+        .clk (adc_clk_s),
+        .rstn (up_rstn),
+        .rd_req (rd_req_s),
+        .wr_req (wr_req_s),
+        .wr_data (wr_data_s[15:0]),
+        .rd_data (rd_data_s),
+        .rd_valid (rd_valid_s));
+    end
+  endgenerate
+
+  up_adc_common #(
+    .ID (ID),
+    .CONFIG (RD_RAW_CAP)
+  ) i_up_adc_common (
+    .mmcm_rst (),
+    .adc_clk (adc_clk_s),
+    .adc_rst (adc_reset_s),
+    .adc_r1_mode (),
+    .adc_ddr_edgesel (),
+    .adc_pin_mode (),
+    .adc_status (adc_status),
+    .adc_sync_status (1'b1),
+    .adc_status_ovf (adc_dovf),
+    .adc_clk_ratio (),
+    .adc_start_code (),
+    .adc_sref_sync (),
+    .adc_sync (),
+    .adc_ext_sync_arm (),
+    .adc_ext_sync_disarm (),
+    .adc_ext_sync_manual_req (),
+    .adc_num_lanes (),
+    .adc_custom_control (),
+    .adc_crc_enable (),
+    .adc_sdr_ddr_n (),
+    .adc_symb_op (),
+    .adc_symb_8_16b (),
+    .up_pps_rcounter (),
+    .up_pps_status (),
+    .up_pps_irq_mask (),
+    .up_adc_r1_mode (),
+    .up_status_pn_err (),
+    .up_status_pn_oos (),
+    .up_status_or (),
+    .up_drp_sel (),
+    .up_drp_wr (),
+    .up_drp_addr (),
+    .up_drp_wdata (),
+    .up_drp_rdata (),
+    .up_drp_ready (),
+    .up_drp_locked (),
+    .adc_custom_wr (wr_data_s),
+    .adc_write_req (wr_req_s),
+    .adc_custom_rd ({16'd0, rd_data_s}),
+    .adc_read_valid (rd_valid_s),
+    .adc_read_req (rd_req_s),
+    .up_usr_chanmax_out (),
+    .up_usr_chanmax_in (),
+    .up_adc_gpio_in (),
+    .up_adc_gpio_out (),
+    .up_adc_ce (),
+    .up_rstn (up_rstn),
+    .up_clk (up_clk),
+    .up_wreq (up_wreq_s),
+    .up_waddr (up_waddr_s),
+    .up_wdata (up_wdata_s),
+    .up_wack (up_wack_s[8]),
+    .up_rreq (up_rreq_s),
+    .up_raddr (up_raddr_s),
+    .up_rdata (up_rdata_s[8]),
+    .up_rack (up_rack_s[8]));
+
+  // up bus interface
+
+  up_axi #(
+    .AXI_ADDRESS_WIDTH (16)
+  ) i_up_axi (
+    .up_rstn (up_rstn),
+    .up_clk (up_clk),
+    .up_axi_awvalid (s_axi_awvalid),
+    .up_axi_awaddr (s_axi_awaddr),
+    .up_axi_awready (s_axi_awready),
+    .up_axi_wvalid (s_axi_wvalid),
+    .up_axi_wdata (s_axi_wdata),
+    .up_axi_wstrb (s_axi_wstrb),
+    .up_axi_wready (s_axi_wready),
+    .up_axi_bvalid (s_axi_bvalid),
+    .up_axi_bresp (s_axi_bresp),
+    .up_axi_bready (s_axi_bready),
+    .up_axi_arvalid (s_axi_arvalid),
+    .up_axi_araddr (s_axi_araddr),
+    .up_axi_arready (s_axi_arready),
+    .up_axi_rvalid (s_axi_rvalid),
+    .up_axi_rresp (s_axi_rresp),
+    .up_axi_rdata (s_axi_rdata),
+    .up_axi_rready (s_axi_rready),
+    .up_wreq (up_wreq_s),
+    .up_waddr (up_waddr_s),
+    .up_wdata (up_wdata_s),
+    .up_wack (up_wack),
+    .up_rreq (up_rreq_s),
+    .up_raddr (up_raddr_s),
+    .up_rdata (up_rdata),
+    .up_rack (up_rack));
+
+endmodule
---
+++ library/axi_ad7606x/axi_ad7606x_ip.tcl
@@ -0,0 +1,117 @@
+# ip
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
+
+global VIVADO_IP_LIBRARY
+
+adi_ip_create axi_ad7606x
+
+adi_ip_files axi_ad7606 [list \
+    "$ad_hdl_dir/library/common/ad_edge_detect.v" \
+    "$ad_hdl_dir/library/xilinx/common/ad_rst_constr.xdc" \
+    "$ad_hdl_dir/library/common/ad_rst.v" \
+    "$ad_hdl_dir/library/common/up_axi.v" \
+    "$ad_hdl_dir/library/xilinx/common/ad_dcfilter.v" \
+    "$ad_hdl_dir/library/common/ad_datafmt.v" \
+    "$ad_hdl_dir/library/common/up_xfer_cntrl.v" \
+    "$ad_hdl_dir/library/common/up_xfer_status.v" \
+    "$ad_hdl_dir/library/common/up_clock_mon.v" \
+    "$ad_hdl_dir/library/common/up_delay_cntrl.v" \
+    "$ad_hdl_dir/library/common/up_adc_channel.v" \
+    "$ad_hdl_dir/library/common/up_adc_common.v" \
+    "$ad_hdl_dir/library/xilinx/common/up_xfer_cntrl_constr.xdc" \
+    "$ad_hdl_dir/library/xilinx/common/ad_rst_constr.xdc" \
+    "$ad_hdl_dir/library/xilinx/common/up_xfer_status_constr.xdc" \
+    "$ad_hdl_dir/library/xilinx/common/up_clock_mon_constr.xdc" \
+    "axi_ad7606x_16b_pif.v" \
+    "axi_ad7606x_18b_pif.v" \
+    "axi_ad7606x.v" ]
+
+adi_ip_properties axi_ad7606x
+
+set_property company_url {https://wiki.analog.com/resources/fpga/docs/axi_ad7606x} [ipx::current_core]
+
+ipx::infer_bus_interface adc_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+set reset_intf [ipx::infer_bus_interface adc_reset xilinx.com:signal:reset_rtl:1.0 [ipx::current_core]]
+set reset_polarity [ipx::add_bus_parameter "POLARITY" $reset_intf]
+set_property value "ACTIVE_HIGH" $reset_polarity
+
+set cc [ipx::current_core]
+
+set_property display_name "AXI AD7606X" $cc
+set_property description "AXI AD7606X HDL interface" $cc
+
+## define external_clk port as clock interface
+adi_add_bus external_clk slave \
+  "xilinx.com:signal:clock_rtl:1.0" \
+  "xilinx.com:signal:clock:1.0" \
+  [list {"external_clk" "CLK"} ]
+
+adi_set_ports_dependency "external_clk" \
+  "(spirit:decode(id('MODELPARAM_VALUE.EXTERNAL_CLK')) = 1)" 0
+
+## parameter validation
+
+set_property -dict [list \
+  "value_format" "bool" \
+  "value" "true" \
+  ] \
+[ipx::get_hdl_parameters EXTERNAL_CLK -of_objects $cc]
+
+## customize XGUI layout
+
+## remove the automatically generated GUI page
+
+ipgui::remove_page -component $cc [ipgui::get_pagespec -name "Page 0" -component $cc]
+ipx::save_core $cc
+
+## create a new GUI page
+
+ipgui::add_page -name {AXI AD7606X} -component $cc -display_name {AXI AD7606X}
+set page0 [ipgui::get_pagespec -name "AXI AD7606X" -component $cc]
+
+ipgui::add_param -name "EXTERNAL_CLK" -component $cc -parent $page0
+set_property -dict [list \
+  "display_name" "EXTERNAL_CLK" \
+  "tooltip" "External clock for the ADC" \
+  "widget" "checkBox" \
+] [ipgui::get_guiparamspec -name "EXTERNAL_CLK" -component $cc]
+
+ipgui::add_param -name "DEV_CONFIG" -component $cc -parent $page0
+set_property -dict [list \
+  "widget" "comboBox" \
+  "display_name" "Device Type Selection" \
+] [ipgui::get_guiparamspec -name "DEV_CONFIG" -component $cc]
+
+set_property -dict [list \
+  "value_validation_type" "pairs" \
+  "value_validation_pairs" { \
+    "AD7606B" "0" \
+    "AD7606C-16" "1" \
+    "AD7606C-18" "2" \
+  } \
+] [ipx::get_user_parameters DEV_CONFIG -of_objects $cc]
+
+ipgui::add_param -name "ADC_READ_MODE" -component $cc
+set_property -dict [list \
+  "widget" "comboBox" \
+  "display_name" "ADC Read Mode Selection" \
+] [ipgui::get_guiparamspec -name "ADC_READ_MODE" -component $cc]
+
+set_property -dict [list \
+  "value_validation_type" "pairs" \
+  "value_validation_pairs" { \
+    "SIMPLE" "0" \
+    "STATUS_HEADER" "1" \
+    "CRC_ENABLED" "2" \
+    "CRC_STATUS" "3" \
+  } \
+] [ipx::get_user_parameters ADC_READ_MODE -of_objects $cc]
+
+adi_add_auto_fpga_spec_params
+
+## save the modifications
+
+ipx::create_xgui_files  $cc
+ipx::save_core $cc
