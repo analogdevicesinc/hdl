381818724b813f251726e09993471486eb6af6e5
Adrian Costina <adrian.costina@analog.com>
2022-11-08 09:27:03 +0000

    axi_ad3552r: Initial commit

---
+++ library/axi_ad3552r/axi_ad3552r_if.v
@@ -0,0 +1,269 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2022 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module axi_ad3552r_if (
+
+  input                   clk_in,  // 120MHz
+  input                   reset_in,
+  input       [31:0]      dac_data,
+  input                   dac_data_valid,
+  output reg              dac_data_ready,
+
+  input       [ 7:0]      address,
+  output      [23:0]      data_read,
+  input       [23:0]      data_write,
+  input                   ddr_sdr_n,
+  input                   reg_16b_8bn,
+  input                   transfer_data,
+  input                   stream,
+
+  // DAC control signals
+  output                  sclk,
+  output reg              csn,
+  output                   sdio_0,
+  output                   sdio_1,
+  output                   sdio_2,
+  output                   sdio_3
+);
+
+  wire transfer_data_s;
+
+  reg [55:0]  transfer_reg = 56'h0;
+
+  reg [31:0] dac_data_int = 32'h0;
+  reg [15:0] counter = 16'h0;
+  reg [2:0] transfer_state = 0;
+  reg [2:0] transfer_state_next = 0;
+
+  reg cycle_done = 1'b0;
+  reg transfer_step = 1'b0;
+  reg sclk_ddr = 1'b0;
+  reg full_speed = 1'b0;
+  reg transfer_data_d = 1'b0;
+  reg transfer_data_dd = 1'b0;
+
+  localparam  [ 2:0]    IDLE = 3'h0,
+                        CS_LOW = 3'h1,
+                        WRITE_ADDRESS = 3'h2,
+                        TRANSFER_REGISTER = 3'h3,
+                        READ_REGISTER = 3'h4,
+                        STREAM = 3'h5,
+                        CS_HIGH = 3'h6;
+
+ localparam ADDRESS_COUNTS = 16;
+ localparam ADDRESS_COUNTS_FAST = ADDRESS_COUNTS >> 2;
+ localparam TRANSFER_8B_SDR = 16;
+ localparam TRANSFER_8B_DDR = 16;
+ localparam TRANSFER_16B_SDR = 16;
+ localparam TRANSFER_16B_DDR = 16;
+ localparam TRANSFER_24B_SDR = 16;
+ localparam TRANSFER_24B_DDR = 16;
+
+// transform the transfer data rising edge into a pulse
+
+ assign transfer_data_s = transfer_data_d & ~transfer_data_dd;
+
+ always @(posedge clk_in) begin
+  if(reset_in == 1'b1) begin
+    transfer_data_d  <= 'd0;
+    transfer_data_dd <= 'd0;
+  end else begin
+    transfer_data_d  <= transfer_data;
+    transfer_data_dd <= transfer_data_d;
+  end
+ end
+
+
+// dac_data_int is a register allows capturing asinchronously with the
+// transmission
+  always @(posedge clk_in) begin
+    if (dac_data_valid) begin
+      dac_data_int <= dac_data;
+    end end
+  always @(posedge clk_in) begin
+    if (reset_in == 1'b1) begin
+      transfer_state <= IDLE;
+    end else begin
+      transfer_state <= transfer_state_next;
+    end
+  end
+
+  // FSM next state logic
+  always @(*) begin
+    case (transfer_state)
+      IDLE : begin
+        transfer_state_next = transfer_data_s ? CS_LOW : IDLE;
+        csn = 1'b1;
+        transfer_step = 0;
+        cycle_done = 0;
+      end
+      CS_LOW : begin
+        // brings CS down
+        // loads all configuration
+        // puts data on the SDIO pins
+        // needs 5 ns before risedge of the clock
+        transfer_state_next = WRITE_ADDRESS;
+        csn = 1'b0;
+        transfer_step = 0;
+        cycle_done = 0;
+      end
+      WRITE_ADDRESS : begin
+        // writes the address
+        // it works either at full speed (60 MHz) when streaming or normal
+        // speed (15 MHz)
+        // full speed - 2 clock cycles
+        // half speed 8 clock cycles
+        cycle_done = full_speed ? (counter == 16'h3) : (counter == 16'hf);
+        transfer_state_next = cycle_done ? (stream ? STREAM : TRANSFER_REGISTER) : WRITE_ADDRESS ;
+        csn = 1'b0;
+        transfer_step = full_speed ? (counter[0]== 1'h1) : ((counter[4:0] == 5'h5)); // in streaming, change data on falledge. On regular transfer, change data on negedgE. A single step should be done for 8 bit addressing
+      end
+      TRANSFER_REGISTER : begin
+        // always works at 15 MHz
+        // can be DDR or SDR
+        //
+        cycle_done = ddr_sdr_n ? (reg_16b_8bn ? (counter == 16'h11) : (counter == 16'h09)): (reg_16b_8bn ? (counter == 16'h20) : (counter == 16'h10));
+        transfer_state_next = cycle_done ? CS_HIGH : TRANSFER_REGISTER;
+        csn = 1'b0;
+        transfer_step = ddr_sdr_n ? (counter[1:0] == 2'h0) : (counter[2:0] == 3'h0); // in DDR mode, change data on falledge
+      end
+      STREAM : begin
+        // can be DDR or SDR
+        // in DDR mode needs to be make sure the clock and data is shifted by
+        // 2 ns
+        cycle_done = stream ? (ddr_sdr_n ? (counter == 16'h7) : (counter == 16'h0f )): (ddr_sdr_n ? (counter == 16'h7) : (counter == 16'h10 ));
+        transfer_state_next = stream ? STREAM: (cycle_done ?  CS_HIGH :STREAM);
+        csn = 1'b0;
+        transfer_step = ddr_sdr_n ? 1'b1 : counter[0] ;
+      end
+      CS_HIGH : begin
+        cycle_done = 1'b1;
+        transfer_state_next = cycle_done ? IDLE : CS_HIGH;
+        csn = 1'b1;
+        transfer_step = 0;
+      end
+      default : begin
+        cycle_done = 0;
+        transfer_state_next = IDLE;
+        csn = 1'b1;
+        transfer_step = 0;
+      end
+    endcase
+  end
+
+  // counter is used to time all states
+  // depends on number of clock cycles per phase
+  //
+  always@(posedge clk_in) begin
+    if (transfer_state == IDLE) begin
+      counter <= 1'b0;
+    end else if (transfer_state == WRITE_ADDRESS | transfer_state == TRANSFER_REGISTER | transfer_state == STREAM) begin
+      if (cycle_done) begin
+        counter <= 0;
+      end else  begin
+        counter <= counter + 1;
+      end
+    end
+  end
+  // always@(posedge clk_in) begin
+  //   if (transfer_state == IDLE) begin
+  //     counter <= 0;
+  //   end else if (transfer_state == CS_LOW | transfer_state == WRITE_ADDRESS | transfer_state == TRANSFER_REGISTER) begin
+  //     if (transfer_state_next == STREAM) begin
+  //       counter <= 0;
+  //     end else begin
+  //       counter <= counter + 1;
+  //     end
+  //   end else if (transfer_state == STREAM) begin
+  //     if (cycle_done == 1'b1) begin
+  //       counter <= 0;
+  //     end else begin
+  //       counter <= counter + 1;
+  //     end
+  //   end
+  // end
+
+// selection between 60 MHz and 15 MHz
+
+  always @(negedge clk_in) begin
+    if (transfer_state == STREAM | transfer_state == WRITE_ADDRESS ) begin
+      sclk_ddr <= !sclk_ddr;
+    end else begin
+     sclk_ddr <= 0;
+   end
+ end
+
+  assign sclk = full_speed ? (ddr_sdr_n ? sclk_ddr : counter[0]) : counter[2];
+
+  always@(posedge clk_in) begin
+    if (transfer_state == CS_LOW & stream) begin
+        dac_data_ready <= 1'b1;
+    end else if (transfer_state == STREAM) begin
+      if (cycle_done == 1'b1) begin
+        dac_data_ready <= stream;
+      end else begin
+        dac_data_ready <= 1'b0;
+      end
+    end else begin
+        dac_data_ready <= 1'b0;
+    end
+    if (transfer_state == CS_LOW) begin
+        full_speed = stream;
+      if(stream) begin
+        transfer_reg <= {address,dac_data_int, 16'h0};
+      end else begin
+        transfer_reg <= {address,data_write, 24'h0};
+      end
+    end else if (transfer_state == STREAM & cycle_done) begin
+        transfer_reg <= {dac_data_int, 24'h0};
+    end else if (transfer_step) begin
+      transfer_reg <= {transfer_reg[51:0], sdio_3,sdio_2,sdio_1,sdio_0};
+    end
+  end
+
+  // address[7] is r_wn : depends also on the state machine, input only when
+  // in TRANSFER register mode
+//  assign sdio_0 = address[7] ? 1'bz : transfer_reg[52];
+//  assign sdio_1 = address[7] ? 1'bz : transfer_reg[53];
+//  assign sdio_2 = address[7] ? 1'bz : transfer_reg[54];
+//  assign sdio_3 = address[7] ? 1'bz : transfer_reg[55];
+  assign sdio_0 = transfer_reg[52];
+  assign sdio_1 = transfer_reg[53];
+  assign sdio_2 = transfer_reg[54];
+  assign sdio_3 = transfer_reg[55];
+
+endmodule
---
+++ library/axi_ad3552r/axi_ad3552r_if_tb.v
@@ -0,0 +1,247 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module axi_ad3552r_if_tb;
+  parameter VCD_FILE = {`__FILE__,"cd"};
+
+  `define TIMEOUT 1000000
+  `include "tb_base.v"
+
+  reg    [  7:0]   address;
+  wire    [23:0]    data_read;
+  wire              dac_valid;
+  wire              dac_sclk;
+  wire              dac_csn;
+  wire              dac_data_ready;
+
+  reg clk_a = 1'b0;
+  reg clk_b = 1'b0;
+  reg clk_c = 1'b0;
+  reg reset_in = 1'b1;
+  reg transfer_data = 1'b0;
+
+  reg [5:0] resetn_shift = 'h0;
+
+  reg [10:0] counter = 'h00;
+  reg [10:0] counter_n = 'h00;
+
+  reg [15:0] ad3552r_8bit_reg = 'h00;
+  reg [15:0] ad3552r_16bit_reg = 'h00;
+  reg [15:0] dac_a = 'h00;
+  reg [15:0] dac_b = 'h00;
+  reg [31:0] shift_register = 'h00;
+  reg [31:0] shift_register_n = 'h00;
+  reg [31:0] dac_data = 32'ha5a5f0f0;
+
+  reg ctrl_enable = 1'b0;
+  reg ctrl_pause = 1'b0;
+  reg address_read = 1'b0;
+  reg ddr_sdr_n = 1'b0;
+  reg reg_16b_8bn = 1'b0;
+  reg stream = 1'b0;
+  reg [23:0]    data_write = 24'h0;
+
+  always #4 clk_a <= ~clk_a;
+
+  initial begin
+   #100 reset_in = 1'b0;
+   data_write = 24'hf1a500;
+   ddr_sdr_n = 1'b0;
+   reg_16b_8bn = 1'b0;
+   stream = 1'b0;
+   #500 transfer_data = 1'b1;
+   #100 transfer_data = 1'b0;
+   #500 data_write = 24'ha5f000;
+   ddr_sdr_n = 1'b0;
+   reg_16b_8bn = 1'b1;
+   stream = 1'b0;
+   #500 transfer_data = 1'b1;
+   #100 transfer_data = 1'b0;
+   #500 data_write = 24'h123400;
+   ddr_sdr_n = 1'b1;
+   reg_16b_8bn = 1'b0;
+   stream = 1'b0;
+   #500 transfer_data = 1'b1;
+   #100 transfer_data = 1'b0;
+   #500 data_write = 24'hf5a500;
+   ddr_sdr_n = 1'b1;
+   reg_16b_8bn = 1'b1;
+   stream = 1'b0;
+   #500 transfer_data = 1'b1;
+   #100 transfer_data = 1'b0;
+   #500 data_write = 24'h000000;
+   ddr_sdr_n = 1'b0;
+   reg_16b_8bn = 1'b0;
+   stream = 1'b1;
+   #500 transfer_data = 1'b1;
+   #100 transfer_data = 1'b0;
+   #5000 stream = 1'b0;
+   #2000 ddr_sdr_n = 1'b1;
+   reg_16b_8bn = 1'b0;
+   stream = 1'b1;
+   #500 transfer_data = 1'b1;
+   #100 transfer_data = 1'b0;
+   #5000 stream = 1'b0;
+
+ end
+
+  always@(posedge dac_sclk or posedge dac_csn) begin
+    if(dac_csn == 1'b1) begin
+      counter <= 0;
+      shift_register <= 0;
+      address_read <= 1'b0;
+    end else begin
+      if (counter == 2 && address_read == 1'b0) begin
+        counter <= 0;
+        address_read <= 1'b1;
+      end else begin
+        if (stream) begin
+          if (ddr_sdr_n == 1'b1 & counter == 3) begin
+            counter <= 0;
+          end else if (ddr_sdr_n == 1'b0 & counter == 7) begin
+            counter <= 0;
+          end else begin
+            counter<= counter + 1;
+          end
+        end else begin
+          if (counter <= 7) begin
+            counter<= counter + 1;
+          end else begin
+            counter <= 1;
+          end
+        end
+      end
+      shift_register <= {shift_register[27:0],dac_sdio_3,dac_sdio_2,dac_sdio_1,dac_sdio_0};
+    end
+  end
+
+  always@(negedge dac_sclk or posedge dac_csn) begin
+    if(dac_csn == 1'b1 ) begin
+      counter_n <= 0;
+      shift_register_n <= 0;
+    end else begin
+        if ((counter == 2 && address_read == 1'b0)) begin
+          counter_n <= 1;
+        end else begin
+          if (ddr_sdr_n == 1'b1 & counter_n == 5) begin
+            counter_n <= 2;
+          end else if (ddr_sdr_n == 1'b0 & counter_n  == 7) begin
+            counter_n <= 1;
+          end else begin
+            counter_n <= counter_n + 1;
+          end
+        shift_register_n <= {shift_register_n[27:0],dac_sdio_3,dac_sdio_2,dac_sdio_1,dac_sdio_0};
+        end
+    end
+  end
+
+  always @(*) begin
+    if (counter == 2) begin
+      address <= shift_register[7:0];
+    end
+
+    if (stream) begin 
+      if(ddr_sdr_n == 1'b0) begin
+        if (counter == 3) begin
+          dac_a = shift_register[15:0];
+        end
+        if (counter == 7) begin
+          dac_b = shift_register[15:0];
+        end
+      end else begin
+        if (counter_n == 3 && counter == 1) begin
+          dac_a = {shift_register[7:4],shift_register_n[7:4],shift_register[3:0],shift_register_n[3:0]};
+        end
+        if (counter_n == 5 && counter == 3) begin
+          dac_b = {shift_register[7:4],shift_register_n[7:4],shift_register[3:0],shift_register_n[3:0]};
+        end
+      end
+
+    end else begin
+      if(ddr_sdr_n == 1'b0) begin
+        if (counter_n == 2) begin
+          ad3552r_8bit_reg = shift_register[7:0];
+        end
+        if (counter_n == 4) begin
+          ad3552r_16bit_reg = shift_register[15:0];
+          dac_a = shift_register[15:0];
+        end
+        if (counter_n == 10) begin
+          dac_b = shift_register[15:0];
+        end
+      end else begin
+        if (counter_n == 2 && address_read) begin
+          ad3552r_8bit_reg = {shift_register[3:0],shift_register_n[3:0]};
+        end
+        if (counter_n== 3) begin
+          ad3552r_16bit_reg ={shift_register[7:4],shift_register_n[7:4],shift_register[3:0],shift_register_n[3:0]}; 
+          dac_a = {shift_register[7:4],shift_register_n[7:4],shift_register[3:0],shift_register_n[3:0]};
+        end
+        if (counter_n == 6) begin
+          dac_b = {shift_register[7:4],shift_register_n[7:4],shift_register[3:0],shift_register_n[3:0]};
+        end
+      end
+    end
+  end
+
+  always @(posedge clk_a) begin
+    if (dac_data_ready) begin
+      dac_data <= dac_data + 32'h00010002;
+    end
+  end
+
+  axi_ad3552r_if axi_ad3552r_interface (
+    .clk_in(clk_a),
+    .reset_in(reset_in),
+    .dac_data(dac_data),
+    .dac_data_valid(1'b1),
+    .address(8'h2c),
+    .data_read(data_read),
+    .data_write(data_write),
+    .ddr_sdr_n(ddr_sdr_n),
+    .reg_16b_8bn(reg_16b_8bn),
+    .transfer_data(transfer_data),
+    .stream(stream),
+    .dac_data_ready(dac_data_ready),
+    .sclk(dac_sclk),
+    .csn(dac_csn),
+    .sdio_0(dac_sdio_0),
+    .sdio_1(dac_sdio_1),
+    .sdio_2(dac_sdio_2),
+    .sdio_3(dac_sdio_3));
+
+endmodule
