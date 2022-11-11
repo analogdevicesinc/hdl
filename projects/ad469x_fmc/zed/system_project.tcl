f64830364cd46d89df0787cca822854badf0e904
Sergiu Arpadi <sergiu.arpadi@analog.com>
2022-11-11 14:57:33 +0200

    ad469x: Use axi_pwm_gen; clean-up
    Replace axi_pulse_gen with axi_pmw_gen for softare support
    considerations. Remove common/config.tcl and update project scripts
    accordingly.

--- projects/ad469x_fmc/zed/system_project.tcl
+++ projects/ad469x_fmc/zed/system_project.tcl
@@ -1,15 +1,8 @@
 source ../../../scripts/adi_env.tcl
 source ../../scripts/adi_project_xilinx.tcl
 source ../../scripts/adi_board.tcl
-source ../common/config.tcl
 
-adi_project ad469x_fmc_zed 0 [list \
-  DATA_WIDTH      [get_config_param DATA_WIDTH] \
-  ASYNC_SPI_CLK   [get_config_param ASYNC_SPI_CLK] \
-  NUM_CS          [get_config_param NUM_CS] \
-  NUM_SDI         [get_config_param NUM_SDI] \
-  SDI_DELAY       [get_config_param SDI_DELAY] \
-]
+adi_project ad469x_fmc_zed
 
 adi_project_files ad469x_fmc_zed [list \
     "../../../library/common/ad_iobuf.v" \
