3d9e0f66029b06c009f2593f7c27976ffd359c11
AndrDragomir <andrei.dragomir@analog.com>
2022-11-29 15:15:51 +0200

    adrv9009/a10soc: Fix timing

--- projects/adrv9009/a10soc/system_project.tcl
+++ projects/adrv9009/a10soc/system_project.tcl
@@ -138,5 +138,6 @@ set_instance_assignment -name IO_STANDARD "1.8 V" -to adrv9009_gpio[18]
 
 # set optimization to get a better timing closure
 set_global_assignment -name OPTIMIZATION_MODE "HIGH PERFORMANCE EFFORT"
+set_global_assignment -name PLACEMENT_EFFORT_MULTIPLIER 1.2
 
 execute_flow -compile
