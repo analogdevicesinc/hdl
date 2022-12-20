bc619aa8e3ca21633b7fff7877aa93f027c2fc54
Sergiu Arpadi <sergiu.arpadi@analog.com>
2022-12-20 20:49:04 +0200

    SPI Engine: Update spi_engine.tcl
    The SPI Engine cores were named directly inside the script and this
    would mean that for multiple SPI Engine instances IPs with the same
    name would appear. These updates will introduce the hierarchy name
    into the name given to the cores and will therefore allow for
    multiple instances of SPI Engine to be added to the same Xilinx
    project.
    
    Projects which use spi_engine.tcl will be updated to account for
    these changes.

--- projects/pulsar_adc_pmdz/common/pulsar_adc_pmdz_bd.tcl
+++ projects/pulsar_adc_pmdz/common/pulsar_adc_pmdz_bd.tcl
@@ -7,10 +7,13 @@ set data_width    32
 set async_spi_clk 1
 set num_cs        1
 set num_sdi       1
+set num_sdo       1
 set sdi_delay     1
+set echo_sclk     0
+
 set hier_spi_engine spi_pulsar_adc
 
-spi_engine_create $hier_spi_engine $data_width $async_spi_clk $num_cs $num_sdi $sdi_delay
+spi_engine_create $hier_spi_engine $data_width $async_spi_clk $num_cs $num_sdi $num_sdo $sdi_delay $echo_sclk
 
 ad_ip_instance axi_clkgen spi_clkgen
 ad_ip_parameter spi_clkgen CONFIG.CLK0_DIV 5
@@ -39,24 +42,24 @@ ad_connect spi_clk spi_clkgen/clk_0
 ad_connect spi_clk pulsar_adc_trigger_gen/ext_clk
 ad_connect $sys_cpu_clk pulsar_adc_trigger_gen/s_axi_aclk
 ad_connect sys_cpu_resetn pulsar_adc_trigger_gen/s_axi_aresetn
-ad_connect pulsar_adc_trigger_gen/pwm_0  $hier_spi_engine/offload/trigger
+ad_connect pulsar_adc_trigger_gen/pwm_0 $hier_spi_engine/trigger
 
-ad_connect axi_pulsar_adc_dma/s_axis spi_pulsar_adc/M_AXIS_SAMPLE
-ad_connect spi_pulsar_adc/m_spi pulsar_adc_spi
+ad_connect axi_pulsar_adc_dma/s_axis $hier_spi_engine/M_AXIS_SAMPLE
+ad_connect $hier_spi_engine/m_spi pulsar_adc_spi
 
-ad_connect $sys_cpu_clk spi_pulsar_adc/clk
-ad_connect spi_clk spi_pulsar_adc/spi_clk
+ad_connect $sys_cpu_clk $hier_spi_engine/clk
+ad_connect spi_clk $hier_spi_engine/spi_clk
 ad_connect spi_clk axi_pulsar_adc_dma/s_axis_aclk
-ad_connect sys_cpu_resetn spi_pulsar_adc/resetn
+ad_connect sys_cpu_resetn $hier_spi_engine/resetn
 ad_connect sys_cpu_resetn axi_pulsar_adc_dma/m_dest_axi_aresetn
 
-ad_cpu_interconnect 0x44a00000 spi_pulsar_adc/axi_regmap
+ad_cpu_interconnect 0x44a00000 $hier_spi_engine/${hier_spi_engine}_axi_regmap
 ad_cpu_interconnect 0x44a30000 axi_pulsar_adc_dma
 ad_cpu_interconnect 0x44a70000 spi_clkgen
 ad_cpu_interconnect 0x44b00000 pulsar_adc_trigger_gen
 
 ad_cpu_interrupt "ps-13" "mb-13" axi_pulsar_adc_dma/irq
-ad_cpu_interrupt "ps-12" "mb-12" /spi_pulsar_adc/irq
+ad_cpu_interrupt "ps-12" "mb-12" /$hier_spi_engine/irq
 
 ad_mem_hp1_interconnect $sys_cpu_clk sys_ps7/S_AXI_HP1
 ad_mem_hp1_interconnect $sys_cpu_clk axi_pulsar_adc_dma/m_dest_axi
