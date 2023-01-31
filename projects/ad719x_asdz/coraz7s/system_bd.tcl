ecbb9c85e224240bf5dbb8ba95e9151e3772f3dc
Iulia Moldovan <Iulia.Moldovan@analog.com>
2023-01-31 13:16:55 +0200

    projects/ad719x_asdz/coraz7s: Add make parameter and SYNCN on GPIO 33
     * Made the project parameterizable through ARDZ_PMOD_N parameter
     * Added possibility to connect through the Arduino header as well
     * Separated the top file and the constraints, for Arduino header & for PMOD JA
     * Added GPIO 33 - SYNCN, for the Arduino header connection mode
     * Added logging of make parameter in sysid string
    
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- projects/ad719x_asdz/coraz7s/system_bd.tcl
+++ projects/ad719x_asdz/coraz7s/system_bd.tcl
@@ -11,4 +11,6 @@ ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
-sysid_gen_sys_init_file
+set sys_cstring "ARDZ_PMOD_N=$ad_project_params(ARDZ_PMOD_N)"
+
+sysid_gen_sys_init_file $sys_cstring
