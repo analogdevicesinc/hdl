4be7ed517a163e67680a5f260d7487a11352705a
Liviu Adace <liviu.adace@analog.com>
2023-02-13 13:59:31 +0200

    ad7606: Added constraints according to NUM_OF_SDI

--- projects/ad7606x_fmc/zed/system_constr.tcl
+++ projects/ad7606x_fmc/zed/system_constr.tcl
@@ -29,18 +29,40 @@ switch $INTF {
     set_property -dict {PACKAGE_PIN M21     IOSTANDARD LVCMOS25} [get_ports adc_cs_n]           ; ## H10 FMC_LPC_LA04_P
     set_property -dict {PACKAGE_PIN K18     IOSTANDARD LVCMOS25} [get_ports adc_cnvst_n]        ; ## D12 FMC_LPC_LA05_N
   }
-  1 {
-    set_property -dict {PACKAGE_PIN P22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[4]]  ; ## G10 FMC_LPC_LA03_N
-    set_property -dict {PACKAGE_PIN M22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[5]]  ; ## H11 FMC_LPC_LA04_N
-    set_property -dict {PACKAGE_PIN T17     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[6]]  ; ## H14 FMC_LPC_LA07_N
-    set_property -dict {PACKAGE_PIN J22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[7]]  ; ## G13 FMC_LPC_LA08_N
-    set_property -dict {PACKAGE_PIN M20     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[0]]  ; ## G07 FMC_LPC_LA00_CC_N
-    set_property -dict {PACKAGE_PIN L22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[1]]  ; ## C11 FMC_LPC_LA06_N
-    set_property -dict {PACKAGE_PIN J18     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[2]]  ; ## D11 FMC_LPC_LA05_P
-    set_property -dict {PACKAGE_PIN R20     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[3]]  ; ## D14 FMC_LPC_LA09_P
-    set_property -dict {PACKAGE_PIN N22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdo]     ; ## G09 FMC_LPC_LA03_P
+  1 {  
 
+    if {![info exists NUM_OF_SDI]} {
+      set NUM_OF_SDI $::env(NUM_OF_SDI)
+    }
+    
+    switch $NUM_OF_SDI {
+      1 {
+        set_property -dict {PACKAGE_PIN M20     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[0]]  ; ## G07 FMC_LPC_LA00_CC_N
+      }
+      2 {
+        set_property -dict {PACKAGE_PIN M20     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[0]]  ; ## G07 FMC_LPC_LA00_CC_N
+        set_property -dict {PACKAGE_PIN L22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[1]]  ; ## C11 FMC_LPC_LA06_N
+      }
+      4 {
+        set_property -dict {PACKAGE_PIN M20     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[0]]  ; ## G07 FMC_LPC_LA00_CC_N
+        set_property -dict {PACKAGE_PIN L22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[1]]  ; ## C11 FMC_LPC_LA06_N
+        set_property -dict {PACKAGE_PIN J18     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[2]]  ; ## D11 FMC_LPC_LA05_P
+        set_property -dict {PACKAGE_PIN R20     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[3]]  ; ## D14 FMC_LPC_LA09_P
+      }
+      8 {
+        set_property -dict {PACKAGE_PIN M20     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[0]]  ; ## G07 FMC_LPC_LA00_CC_N
+        set_property -dict {PACKAGE_PIN L22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[1]]  ; ## C11 FMC_LPC_LA06_N
+        set_property -dict {PACKAGE_PIN J18     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[2]]  ; ## D11 FMC_LPC_LA05_P
+        set_property -dict {PACKAGE_PIN R20     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[3]]  ; ## D14 FMC_LPC_LA09_P
+        set_property -dict {PACKAGE_PIN P22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[4]]  ; ## G10 FMC_LPC_LA03_N
+        set_property -dict {PACKAGE_PIN M22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[5]]  ; ## H11 FMC_LPC_LA04_N
+        set_property -dict {PACKAGE_PIN T17     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[6]]  ; ## H14 FMC_LPC_LA07_N
+        set_property -dict {PACKAGE_PIN J22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdi[7]]  ; ## G13 FMC_LPC_LA08_N
+      }
+    }
+    
     set_property -dict {PACKAGE_PIN M19     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sclk]    ; ## G06 FMC_LPC_LA00_CC_P
+    set_property -dict {PACKAGE_PIN N22     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_sdo]     ; ## G09 FMC_LPC_LA03_P
     set_property -dict {PACKAGE_PIN M21     IOSTANDARD LVCMOS25} [get_ports ad7606_spi_cs]    ; ## H10 FMC_LPC_LA04_P
     set_property -dict {PACKAGE_PIN R19     IOSTANDARD LVCMOS25} [get_ports adc_cnvst_n]        ; ## C14 FMC_LPC_LA10_P
   }
