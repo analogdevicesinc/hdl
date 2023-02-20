3b056b9a20e52233d61e0eb50dd397fc5c783761
PopPaul2021 <Paul.Pop@analog.com>
2023-02-20 14:33:45 +0200

    library/common: Added DAC custom read/write interface in up_dac_common. The DAC common regmap was updated with 3 registers(rd/wr/ctrl) and 1 interface status flag for converters with custom control interface.

--- library/axi_ad5766/axi_ad5766.v
+++ library/axi_ad5766/axi_ad5766.v
@@ -1,6 +1,6 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
+// Copyright 2014 - 2023 (c) Analog Devices, Inc. All rights reserved.
 //
 // In this HDL repository, there are many different and unique modules, consisting
 // of various HDL (Verilog or VHDL) components. The individual modules are
@@ -363,6 +363,10 @@ module axi_ad5766 #(
     .dac_sync (),
     .dac_frame (),
     .dac_clksel (),
+    .dac_custom_wr(),
+    .dac_custom_rd(32'b0),
+    .dac_custom_control(),
+    .dac_status_if_busy(1'b0),
     .dac_par_type (),
     .dac_par_enb (),
     .dac_r1_mode (),
