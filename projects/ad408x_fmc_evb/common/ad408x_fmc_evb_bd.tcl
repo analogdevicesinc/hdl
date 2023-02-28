6fc859e5dfebe1de0c8a2448e2e921ddca611a7d
PopPaul2021 <Paul.Pop@analog.com>
2023-02-28 13:35:54 +0200

    projects/ad408x_fmc_evb: Initial commit for ZedBoard
    The project instantiates:
    -the axi_ad408x interface IP
    -a DMA for the 20bit conversion data stream
    -a DMA for the 80bit uncorrected sample data stream
    
    Signed-off-by: PopPaul2021 <paul.pop@analog.com>

---
+++ projects/ad408x_fmc_evb/common/ad408x_fmc_evb_bd.tcl
@@ -0,0 +1,91 @@
+###############################################################################
+## Copyright (C) 2022-2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+# ad4080 interface
+
+create_bd_port -dir I dco_p
+create_bd_port -dir I dco_n
+create_bd_port -dir I da_p
+create_bd_port -dir I da_n
+create_bd_port -dir I db_p
+create_bd_port -dir I db_n
+create_bd_port -dir I uncorrected_mode
+create_bd_port -dir I sync_n
+create_bd_port -dir O sys_cpu_out_clk
+
+# axi_ad408x
+
+ad_ip_instance axi_ad408x axi_ad4080_adc
+ad_ip_parameter axi_ad4080_adc CONFIG.NUM_OF_CHANNELS 1
+ad_ip_parameter axi_ad4080_adc CONFIG.HAS_DELAY_CTRL 1
+ad_ip_parameter axi_ad4080_adc CONFIG.DELAY_CTRL_NUM_LANES 2
+ad_ip_parameter axi_ad4080_adc CONFIG.DDR_SUPPORT 1
+
+# dma for rx1
+
+ad_ip_instance axi_dmac axi_ad4080_dma
+ad_ip_parameter axi_ad4080_dma CONFIG.DMA_TYPE_SRC 2
+ad_ip_parameter axi_ad4080_dma CONFIG.DMA_TYPE_DEST 0
+ad_ip_parameter axi_ad4080_dma CONFIG.CYCLIC 0
+ad_ip_parameter axi_ad4080_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter axi_ad4080_dma CONFIG.AXI_SLICE_SRC 0
+ad_ip_parameter axi_ad4080_dma CONFIG.AXI_SLICE_DEST 0
+ad_ip_parameter axi_ad4080_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_ad4080_dma CONFIG.DMA_DATA_WIDTH_SRC 32
+
+# dma for uncorrected
+
+ad_ip_instance axi_dmac axi_ad4080_uc_dma
+ad_ip_parameter axi_ad4080_uc_dma CONFIG.DMA_TYPE_SRC 2
+ad_ip_parameter axi_ad4080_uc_dma CONFIG.DMA_TYPE_DEST 0
+ad_ip_parameter axi_ad4080_uc_dma CONFIG.CYCLIC 0
+ad_ip_parameter axi_ad4080_uc_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter axi_ad4080_uc_dma CONFIG.AXI_SLICE_SRC 0
+ad_ip_parameter axi_ad4080_uc_dma CONFIG.AXI_SLICE_DEST 0
+ad_ip_parameter axi_ad4080_uc_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_ad4080_uc_dma CONFIG.DMA_DATA_WIDTH_SRC 128
+
+# connect interface to axi_ad4080_adc
+
+ad_connect dco_p            axi_ad4080_adc/dclk_in_p
+ad_connect dco_n            axi_ad4080_adc/dclk_in_n
+ad_connect da_p             axi_ad4080_adc/data_a_in_p
+ad_connect da_n             axi_ad4080_adc/data_a_in_n
+ad_connect db_p             axi_ad4080_adc/data_b_in_p
+ad_connect db_n             axi_ad4080_adc/data_b_in_n
+ad_connect sync_n           axi_ad4080_adc/sync_n
+ad_connect uncorrected_mode axi_ad4080_adc/uncorrected_mode
+
+ad_connect $sys_iodelay_clk axi_ad4080_adc/delay_clk
+
+# connect datapath
+
+ad_connect axi_ad4080_adc/adc_data  axi_ad4080_dma/fifo_wr_din
+ad_connect axi_ad4080_adc/adc_valid axi_ad4080_dma/fifo_wr_en
+
+ad_connect axi_ad4080_adc/adc_uncor_data  axi_ad4080_uc_dma/fifo_wr_din
+ad_connect axi_ad4080_adc/adc_uncor_valid axi_ad4080_uc_dma/fifo_wr_en
+
+# system runs on phy's received clock
+
+ad_connect axi_ad4080_adc/adc_clk axi_ad4080_dma/fifo_wr_clk
+ad_connect axi_ad4080_adc/adc_clk axi_ad4080_uc_dma/fifo_wr_clk
+ad_connect $sys_cpu_clk sys_cpu_out_clk
+
+ad_connect $sys_cpu_resetn axi_ad4080_dma/m_dest_axi_aresetn
+ad_connect $sys_cpu_resetn axi_ad4080_uc_dma/m_dest_axi_aresetn
+
+ad_cpu_interconnect 0x44A00000 axi_ad4080_adc
+ad_cpu_interconnect 0x44A30000 axi_ad4080_dma
+ad_cpu_interconnect 0x44A40000 axi_ad4080_uc_dma
+
+ad_mem_hp1_interconnect $sys_cpu_clk sys_ps7/S_AXI_HP1
+ad_mem_hp1_interconnect $sys_cpu_clk axi_ad4080_dma/m_dest_axi
+ad_mem_hp1_interconnect $sys_cpu_clk axi_ad4080_uc_dma/m_dest_axi
+
+ad_cpu_interrupt ps-13 mb-12 axi_ad4080_dma/irq
+ad_cpu_interrupt ps-12 mb-12 axi_ad4080_uc_dma/irq
+
+
---
+++ projects/ad408x_fmc_evb/zed/system_top.v
@@ -0,0 +1,346 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright (C) 2022-2024 Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+`define ECHOED_CLK
+
+module system_top (
+
+  inout   [14:0]  ddr_addr,
+  inout   [ 2:0]  ddr_ba,
+  inout           ddr_cas_n,
+  inout           ddr_ck_n,
+  inout           ddr_ck_p,
+  inout           ddr_cke,
+  inout           ddr_cs_n,
+  inout   [ 3:0]  ddr_dm,
+  inout   [31:0]  ddr_dq,
+  inout   [ 3:0]  ddr_dqs_n,
+  inout   [ 3:0]  ddr_dqs_p,
+  inout           ddr_odt,
+  inout           ddr_ras_n,
+  inout           ddr_reset_n,
+  inout           ddr_we_n,
+
+  inout           fixed_io_ddr_vrn,
+  inout           fixed_io_ddr_vrp,
+  inout   [53:0]  fixed_io_mio,
+  inout           fixed_io_ps_clk,
+  inout           fixed_io_ps_porb,
+  inout           fixed_io_ps_srstb,
+
+  inout   [31:0]  gpio_bd,
+
+  output          hdmi_out_clk,
+  output          hdmi_vsync,
+  output          hdmi_hsync,
+  output          hdmi_data_e,
+  output  [15:0]  hdmi_data,
+
+  output          i2s_mclk,
+  output          i2s_bclk,
+  output          i2s_lrclk,
+  output          i2s_sdata_out,
+  input           i2s_sdata_in,
+
+  output          spdif,
+
+  inout           iic_scl,
+  inout           iic_sda,
+  inout   [ 1:0]  iic_mux_scl,
+  inout   [ 1:0]  iic_mux_sda,
+
+  input           otg_vbusoc,
+
+  // FMC connector
+
+  // Default 100 MHz clock
+
+  input           fpgaclk_p,
+  input           fpgaclk_n,
+
+  // LVDS data interace
+
+  input           dco_p,
+  input           dco_n,
+  input           da_p,
+  input           da_n,
+  input           db_p,
+  input           db_n,
+
+  // SPI data interface
+
+  input           doa_fmc,
+  input           dob_fmc,
+  input           doc_fmc,
+  input           dod_fmc,
+
+  output          gp0_dir,
+  output          gp1_dir,
+  output          gp2_dir,
+  output          gp3_dir,
+  inout           gpio1_fmc,
+  inout           gpio2_fmc,
+  inout           gpio3_fmc,
+
+  input           pwrgd,
+  input           adf435x_lock,
+  output          en_psu,
+  output          pd_v33b,
+  output          osc_en,
+  output          ad9508_sync,
+
+`ifdef ECHOED_CLK
+  // Input for Echoed clock mode
+  input           clk_p,
+  input           clk_n,
+`else
+  // Output for self clocked mode
+  output          clk_p,
+  output          clk_n,
+  output          fpga_cnvp,
+  output          fpga_cnvn,
+`endif
+
+  // ADC SPI
+
+  input           gpio0_fmc,
+  output          sclk_src,
+  output          cs_n_src,
+  output          sdio_src,
+
+  // Clock chips SPI
+
+  input           sdo_1, //ad9508
+  output          sclk1,
+  output          sdin1,
+  output          cs1_0, //ad9508
+  output          cs1_1  //adf4350
+);
+
+  // internal signals
+
+  wire    [63:0]  gpio_i;
+  wire    [63:0]  gpio_o;
+  wire    [63:0]  gpio_t;
+
+  wire    [ 1:0]  iic_mux_scl_i_s;
+  wire    [ 1:0]  iic_mux_scl_o_s;
+  wire            iic_mux_scl_t_s;
+  wire    [ 1:0]  iic_mux_sda_i_s;
+  wire    [ 1:0]  iic_mux_sda_o_s;
+  wire            iic_mux_sda_t_s;
+
+  reg             ad9508_sync_s = 1'b0;
+  reg     [ 3:0]  sync_req_d    = 4'b0;
+  reg     [26:0]  dbg_cnt       =  'b0;
+  reg     [26:0]  dbg_cnt_f     =  'b0;
+  reg     [26:0]  dbg_cnt_100_f =  'b0;
+
+  assign ad9508_sync   = ad9508_sync_s;
+  assign gpio_i[35]    = adf435x_lock;
+  assign gpio_i[36]    = pwrgd;
+  assign gpio_i[63:37] = gpio_o[63:37];
+
+  assign gp0_dir  = 1'b0;
+  assign gp1_dir  = 1'b0;
+  assign gp2_dir  = 1'b0;
+  assign gp3_dir  = 1'b0;
+  assign en_psu   = 1'b1;
+  assign osc_en   = pwrgd;
+
+  assign sync_req = gpio_o[42];
+  assign pd_v33b  = 1'b1;
+
+  always @(posedge sys_cpu_out_clk) begin
+    sync_req_d <= {sync_req_d[2:0],sync_req};
+    if (sync_req_d[1] & ~sync_req_d[3]) begin
+      ad9508_sync_s <= 1'b0;
+    end else if (~sync_req_d[2] & sync_req_d[3]) begin
+      ad9508_sync_s <= 1'b1;
+    end
+  end
+
+  // Dummy function to prevent sys_cpu_out_clk optimization
+  // Even if the clock is not used the diff termination should help signal
+  // integrity
+
+  always @(posedge sys_cpu_out_clk) begin
+    dbg_cnt <= dbg_cnt + 1;
+  end
+
+  // Dummy function to prevent fpga_clk optimization
+  // Even if the clock is not used the diff termination should help signal
+  // integrity
+
+  always @(posedge fpga_clk) begin
+    dbg_cnt_f <= dbg_cnt_f + 1;
+  end
+
+  // Dummy function to prevent fpga_100_clk optimization
+  // Even if the clock is not used the diff termination should help signal
+  // integrity
+
+  always @(posedge fpga_100_clk) begin
+    dbg_cnt_100_f <= dbg_cnt_100_f + 1;
+  end
+
+  // instantiations
+
+  IBUFDS i_fpga_clk (
+    .I (clk_p),
+    .IB (clk_n),
+    .O (fpga_clk));
+
+  IBUFDS i_fpga_100_clk (
+    .I (fpgaclk_p),
+    .IB (fpgaclk_n),
+    .O (fpga_100_clk));
+
+  ad_iobuf #(
+    .DATA_WIDTH(2)
+  ) i_gpio_4_3_mach1 (
+    .dio_t(gpio_t[34:33]),
+    .dio_i(gpio_o[34:33]),
+    .dio_o(gpio_i[34:33]),
+    .dio_p({gpio3_fmc,gpio2_fmc}));
+
+  ad_iobuf #(
+    .DATA_WIDTH(1)
+  ) i_gpio_2_mach1 (
+    .dio_t(gpio_t[32]),
+    .dio_i(gpio_o[32]),
+    .dio_o(gpio_i[32]),
+    .dio_p(gpio1_fmc));
+
+  ad_iobuf #(
+    .DATA_WIDTH(29)
+  ) i_gpio_bd (
+    .dio_t({gpio_t[31:22],gpio_t[18:0]}),
+    .dio_i({gpio_o[31:22],gpio_o[18:0]}),
+    .dio_o({gpio_i[31:22],gpio_i[18:0]}),
+    .dio_p({gpio_bd[31:22],gpio_bd[18:0]}));
+
+  ad_iobuf #(
+    .DATA_WIDTH(2)
+  ) i_iobuf_iic_scl (
+    .dio_t ({iic_mux_scl_t_s,iic_mux_scl_t_s}),
+    .dio_i (iic_mux_scl_o_s),
+    .dio_o (iic_mux_scl_i_s),
+    .dio_p (iic_mux_scl));
+
+  ad_iobuf #(
+    .DATA_WIDTH(2)
+  ) i_iobuf_iic_sda (
+    .dio_t ({iic_mux_sda_t_s,iic_mux_sda_t_s}),
+    .dio_i (iic_mux_sda_o_s),
+    .dio_o (iic_mux_sda_i_s),
+    .dio_p (iic_mux_sda));
+
+  system_wrapper i_system_wrapper (
+    .ddr_addr (ddr_addr),
+    .ddr_ba (ddr_ba),
+    .ddr_cas_n (ddr_cas_n),
+    .ddr_ck_n (ddr_ck_n),
+    .ddr_ck_p (ddr_ck_p),
+    .ddr_cke (ddr_cke),
+    .ddr_cs_n (ddr_cs_n),
+    .ddr_dm (ddr_dm),
+    .ddr_dq (ddr_dq),
+    .ddr_dqs_n (ddr_dqs_n),
+    .ddr_dqs_p (ddr_dqs_p),
+    .ddr_odt (ddr_odt),
+    .ddr_ras_n (ddr_ras_n),
+    .ddr_reset_n (ddr_reset_n),
+    .ddr_we_n (ddr_we_n),
+    .fixed_io_ddr_vrn (fixed_io_ddr_vrn),
+    .fixed_io_ddr_vrp (fixed_io_ddr_vrp),
+    .fixed_io_mio (fixed_io_mio),
+    .fixed_io_ps_clk (fixed_io_ps_clk),
+    .fixed_io_ps_porb (fixed_io_ps_porb),
+    .fixed_io_ps_srstb (fixed_io_ps_srstb),
+    .gpio_i (gpio_i),
+    .gpio_o (gpio_o),
+    .gpio_t (gpio_t),
+    .hdmi_data (hdmi_data),
+    .hdmi_data_e (hdmi_data_e),
+    .hdmi_hsync (hdmi_hsync),
+    .hdmi_out_clk (hdmi_out_clk),
+    .hdmi_vsync (hdmi_vsync),
+    .i2s_bclk (i2s_bclk),
+    .i2s_lrclk (i2s_lrclk),
+    .i2s_mclk (i2s_mclk),
+    .i2s_sdata_in (i2s_sdata_in),
+    .i2s_sdata_out (i2s_sdata_out),
+    .iic_fmc_scl_io (iic_scl),
+    .iic_fmc_sda_io (iic_sda),
+    .iic_mux_scl_i (iic_mux_scl_i_s),
+    .iic_mux_scl_o (iic_mux_scl_o_s),
+    .iic_mux_scl_t (iic_mux_scl_t_s),
+    .iic_mux_sda_i (iic_mux_sda_i_s),
+    .iic_mux_sda_o (iic_mux_sda_o_s),
+    .iic_mux_sda_t (iic_mux_sda_t_s),
+    .otg_vbusoc (otg_vbusoc),
+    .spdif (spdif),
+    .spi0_clk_i (1'b0),
+    .spi0_clk_o (sclk_src),
+    .spi0_csn_0_o (cs_n_src),
+    .spi0_csn_1_o (),
+    .spi0_csn_2_o (),
+    .spi0_csn_i (1'b1),
+    .spi0_sdi_i (gpio0_fmc),
+    .spi0_sdo_i (1'b0),
+    .spi0_sdo_o (sdio_src),
+    .spi1_clk_i (1'b0),
+    .spi1_clk_o (sclk1),
+    .spi1_csn_0_o (cs1_0),
+    .spi1_csn_1_o (cs1_1),
+    .spi1_csn_2_o (),
+    .spi1_csn_i (1'b1),
+    .spi1_sdi_i (sdo_1),
+    .spi1_sdo_i (1'b0),
+    .spi1_sdo_o (sdin1),
+    .dco_p (dco_p),
+    .dco_n (dco_n),
+    .da_p (da_p),
+    .da_n (da_n),
+    .db_p (db_p),
+    .db_n (db_n),
+    .sync_n (ad9508_sync),
+    .sys_cpu_out_clk (sys_cpu_out_clk),
+    .uncorrected_mode (1'b0));
+
+endmodule
