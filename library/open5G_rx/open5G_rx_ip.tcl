a00b9d4fe5deaaa99427758540e665abab263359
Benjamin Menkuec <benjamin@menkuec.de>
2023-03-14 16:49:27 +0100

    add Makefile open5G_rx_constr.ttcl open5G_rx_ip.tcl

---
+++ library/open5G_rx/open5G_rx_ip.tcl
@@ -0,0 +1,85 @@
+# ip
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
+
+global VIVADO_IP_LIBRARY
+
+adi_ip_create open5G_rx
+adi_ip_files open5G_rx [list \
+  "hdl/receiver.sv" \
+  "hdl/dot_product.sv" \
+  "hdl/Peak_detector.sv" \
+  "hdl/atan.sv" \
+  "hdl/atan2.sv" \
+  "hdl/div.sv" \
+  "hdl/LFSR/LFSR.sv" \
+  "hdl/AXI_lite_interface.sv" \
+  "hdl/AXIS_FIFO.sv" \
+  "hdl/CFO_calc.sv" \
+  "hdl/frame_sync.sv" \
+  "hdl/channel_estimator.sv" \
+  "hdl/demap.sv" \
+  "hdl/PSS_correlator.sv" \
+  "hdl/PSS_detector.sv" \
+  "hdl/PSS_detector_regmap.sv" \
+  "hdl/ressource_grid_subscriber.sv" \
+  "hdl/SSS_detector.sv" \
+  "hdl/axis_axil_fifo.sv" \
+  "hdl/FFT_demod.sv" \
+  "hdl/FFT/fft/fft.v" \
+  "hdl/FFT/fft/int_fftNk.v" \
+  "hdl/FFT/fft/int_dif2_fly.v" \
+  "hdl/FFT/math/cmult/int_cmult_dsp48.v" \
+  "hdl/FFT/math/cmult/int_cmult18x25_dsp48.v" \
+  "hdl/FFT/math/cmult/int_cmult_dbl18_dsp48.v" \
+  "hdl/FFT/math/cmult/int_cmult_dbl35_dsp48.v" \
+  "hdl/FFT/math/cmult/int_cmult_trpl18_dsp48.v" \
+  "hdl/FFT/math/cmult/int_cmult_trpl52_dsp48.v" \
+  "hdl/FFT/math/mults/mlt35x25_dsp48e1.v" \
+  "hdl/FFT/math/mults/mlt35x27_dsp48e2.v" \
+  "hdl/FFT/math/mults/mlt42x18_dsp48e1.v" \
+  "hdl/FFT/math/mults/mlt44x18_dsp48e2.v" \
+  "hdl/FFT/math/mults/mlt52x25_dsp48e1.v" \
+  "hdl/FFT/math/mults/mlt52x27_dsp48e2.v" \
+  "hdl/FFT/math/mults/mlt59x18_dsp48e1.v" \
+  "hdl/FFT/math/mults/mlt61x18_dsp48e2.v" \
+  "hdl/FFT/math/int_addsub_dsp48.v" \
+  "hdl/FFT/buffers/dynamic_block_scaling.v" \
+  "hdl/FFT/buffers/inbuf_half_path.v" \
+  "hdl/FFT/buffers/outbuf_half_path.v" \
+  "hdl/FFT/buffers/int_bitrev_order.v" \
+  "hdl/FFT/twiddle/rom_twiddle_int.v" \
+  "hdl/FFT/twiddle/row_twiddle_tay.v" \
+  "hdl/FFT/delay/int_align_fft.v" \
+  "hdl/FFT/delay/int_delay_line.v" \
+  "hdl/CIC/cic_d.sv" \
+  "hdl/CIC/comb.sv" \
+  "hdl/CIC/integrator.sv" \
+  "hdl/CIC/downsampler.sv" \
+  "hdl/CIC/downsampler_variable.sv" \
+  "hdl/DDS/dds.sv" \
+  "hdl/complex_multiplier/complex_multiplier.v" \
+  "hdl/axil_interconnect_wrap_1x4.v" \
+  "hdl/verilog-axi/axil_interconnect.v" \
+  "hdl/verilog-axi/arbiter.v" \
+  "open5G_rx_constr.ttcl" ]
+
+adi_ip_properties open5G_rx
+adi_ip_infer_mm_interfaces open5G_rx
+adi_ip_ttcl open5G_rx "open5G_rx_constr.ttcl"
+
+set_property display_name "Open5G receiver" [ipx::current_core]
+set_property description "Open5G receiver" [ipx::current_core]
+
+ipx::add_bus_parameter ASSOCIATED_BUSIF [ipx::get_bus_interfaces clk_i \
+  -of_objects [ipx::current_core]]
+set_property value s_axi_if [ipx::get_bus_parameters ASSOCIATED_BUSIF \
+  -of_objects [ipx::get_bus_interfaces clk_i \
+  -of_objects [ipx::current_core]]]
+
+ipx::infer_bus_interface clk_i xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+
+
+ipx::create_xgui_files [ipx::current_core]
+ipx::save_core $cc
