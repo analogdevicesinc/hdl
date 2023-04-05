a0c382b65ee40d0041fd918342ec6999acd28ffe
Sergiu Arpadi <sergiu.arpadi@analog.com>
2023-04-05 18:14:37 +0300

    quad_iso_adc_fmc: Update tcl after rebase

--- projects/quad_iso_adc_fmc/common/quad_iso_adc_fmc_bd.tcl
+++ projects/quad_iso_adc_fmc/common/quad_iso_adc_fmc_bd.tcl
@@ -17,10 +17,13 @@ set data_width    32
 set async_spi_clk 1
 set num_cs        4
 set num_sdi       4
+set num_sdo       1
 set sdi_delay     1
+set echo_sclk     0
+
 set hier_spi_engine quad_iso_adc
 
-spi_engine_create $hier_spi_engine $data_width $async_spi_clk $num_cs $num_sdi $sdi_delay
+spi_engine_create $hier_spi_engine $data_width $async_spi_clk $num_cs $num_sdi $num_sdo $sdi_delay $echo_sclk
 
 ad_ip_instance axi_clkgen spi_clkgen
 ad_ip_parameter spi_clkgen CONFIG.CLK0_DIV 10
@@ -59,13 +62,13 @@ ad_connect spi_clk spi_clkgen/clk_0
 #ad_connect sys_cpu_resetn pulsar_adc_trigger_gen/s_axi_aresetn
 #ad_connect pulsar_adc_trigger_gen/pwm_0  $hier_spi_engine/offload/trigger
 
-ad_connect axi_qadc_dma/s_axis quad_iso_adc/M_AXIS_SAMPLE
-ad_connect quad_iso_adc/m_spi qadc_spi
+ad_connect axi_qadc_dma/s_axis $hier_spi_engine/M_AXIS_SAMPLE
+ad_connect $hier_spi_engine/m_spi qadc_spi
 
-ad_connect $sys_cpu_clk quad_iso_adc/clk
-ad_connect spi_clk quad_iso_adc/spi_clk
+ad_connect $sys_cpu_clk $hier_spi_engine/clk
+ad_connect spi_clk $hier_spi_engine/spi_clk
 ad_connect spi_clk axi_qadc_dma/s_axis_aclk
-ad_connect sys_cpu_resetn quad_iso_adc/resetn
+ad_connect sys_cpu_resetn $hier_spi_engine/resetn
 ad_connect sys_cpu_resetn axi_qadc_dma/m_dest_axi_aresetn
 
 ad_ip_instance util_reduced_logic drdy_buf
@@ -73,7 +76,7 @@ ad_ip_parameter drdy_buf CONFIG.C_OPERATION {and}
 ad_ip_parameter drdy_buf CONFIG.C_SIZE 4
 
 ad_connect qadc_drdy drdy_buf/Op1
-ad_connect drdy_buf/Res $hier_spi_engine/offload/trigger
+ad_connect drdy_buf/Res $hier_spi_engine/trigger
 
 ad_ip_instance util_reduced_logic drdy_chk
 ad_ip_parameter drdy_chk CONFIG.C_OPERATION {xor}
@@ -82,13 +85,13 @@ ad_ip_parameter drdy_chk CONFIG.C_SIZE 4
 ad_connect qadc_drdy drdy_chk/Op1
 #ad_connect drdy_buf/Res $hier_spi_engine/offload/trigger
 
-ad_cpu_interconnect 0x44a00000 quad_iso_adc/axi_regmap
+ad_cpu_interconnect 0x44a00000 $hier_spi_engine/${hier_spi_engine}_axi_regmap
 ad_cpu_interconnect 0x44a30000 axi_qadc_dma
 ad_cpu_interconnect 0x44a70000 spi_clkgen
 ad_cpu_interconnect 0x44b00000 mclk_clkgen
 #ad_cpu_interconnect 0x44b00000 pulsar_adc_trigger_gen
 
 ad_cpu_interrupt "ps-13" "mb-13" axi_qadc_dma/irq
-ad_cpu_interrupt "ps-12" "mb-12" /quad_iso_adc/irq
+ad_cpu_interrupt "ps-12" "mb-12" $hier_spi_engine/irq
 
 ad_mem_hp0_interconnect sys_cpu_clk axi_qadc_dma/m_dest_axi
