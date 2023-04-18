64bccbd8e73d60feb078be829b08956ccb0b74d3
Benjamin Menkuec <benjamin@menkuec.de>
2023-04-18 13:57:39 +0200

    rename project name from ant5G to ant_5G

--- projects/ant_5G/system_project.tcl
+++ projects/ant_5G/system_project.tcl
@@ -4,14 +4,14 @@ source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 set p_device "xc7z020clg400-2"
-adi_project ant5G
+adi_project ant_5G
 
-adi_project_files ant5G [list \
+adi_project_files ant_5G [list \
   "system_top.v" \
   "system_constr.xdc" \
   "$ad_hdl_dir/library/common/ad_iobuf.v"]
 
 set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
-adi_project_run ant5G
+adi_project_run ant_5G
 source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
 
