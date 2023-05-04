334fd123cc7256124a157e3c2ab89cbf10da2d5a
Jorge Marques <jorge.marques@analog.com>
2023-05-04 11:08:47 -0300

    i3c_controller: include _write_byte, update _read_
    Include i3c_controller_write_byte to pack u8 data into u32 SDI FIFO
    and update i3c_controller_read_byte to unpack u8 data from u32 SDO FIFO.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- library/i3c_controller/i3c_controller_host_interface/i3c_controller_read_byte.v
+++ library/i3c_controller/i3c_controller_host_interface/i3c_controller_read_byte.v
@@ -33,24 +33,24 @@
 // ***************************************************************************
 // ***************************************************************************
 /**
+ * Unpacks u32 packages into u8 stream.
  * Restrictions:
- * "u32" must have enough elements, won't stall on ~u32_valid.
- * length must be > 0
+ * "u32" must have enough elements, won't stall on ~u32_valid,
+ * with exception of the first item, when both u8_len_valid and u32_valid
+ * must be high at the same time to initiate the transfer.
+ * Length must be > 0
  */
 
 `timescale 1ns/100ps
 `default_nettype none
 
-module i3c_controller_read_byte #(
-  parameter DATA_WIDTH = 32
-) (
+module i3c_controller_read_byte (
   input  wire clk,
   input  wire resetn,
 
   output wire u32_ready,
   input  wire u32_valid,
-  input  wire [DATA_WIDTH-1:0] u32,
-  output wire reg debug_u32_underflow,
+  input  wire [31:0] u32,
 
   output wire u8_len_ready,
   input  wire u8_len_valid,
@@ -58,61 +58,62 @@ module i3c_controller_read_byte #(
 
   input  wire u8_ready,
   output wire u8_valid,
-  output wire reg [7:0] u8
+  output wire [7:0] u8,
+
+  output reg debug_u32_underflow
 );
-  reg [1:0] sm;
-  localparam [1:0]
+  reg [0:0] sm;
+  localparam [0:0]
     idle     = 0,
-    first    = 1,
-    transfer = 3;
+    transfer = 1;
 
   reg [11:0] u8_len_reg;
   reg [31:0] u32_reg;
-  reg [9:0] i;
-  reg [1:0] j;
+  wire [9:0] i;
+  wire [1:0] j;
+  reg  [1:0] c;
 
   always @(posedge clk) begin
     if (resetn) begin
       sm <= idle;
-      debug_u32_underflow <= 1'b0;
+      c <= 2'b00;
     end else begin
-        //      0  000
-        //      1  001
-        // 3 -> 2  010
-        // 4 -> 3  011
-        // 5 -> 4  100
-        //      5  101
-        //      6  110
-        //      7 1000
       case (sm)
         idle: begin
           u8_len_reg <= u8_len - 1;
-          sm <= u8_len_valid ? first : idle;
-        end
-        first: begin
+          sm <= u8_len_valid & u32_valid ? transfer : idle;
           u32_reg <= u32;
-          sm <= transfer;
+          c <= 2'b11;
         end
         transfer: begin
           if (u8_ready) begin // tick
-            if (j == 2'd0) begin
-              u32_reg <= u32;
-              if (~|i) begin
+            if (~|i && j == ~c) begin
                 sm <= idle;
-              end
+            end
+            if (c == 2'd0) begin
+              u8_len_reg <= u8_len_reg - 12'b100;
+              u32_reg <= u32;
             end else begin
               u32_reg <= u32_reg << 8;
             end
-            u8_len_reg <= u8_len_reg - 1;
+            c <= c - 1;
           end
-          u8_valid <= 1'b1;
+        end
+        default: begin
+          sm <= idle;
         end
       endcase
+    end
+  end
 
-      // Debug case
+  // Debug signals
+  always @(posedge clk) begin
+    if (resetn) begin
+      debug_u32_underflow <= 1'b0;
+    end else begin
       case (sm)
-        first: begin
-          debug_u32_underflow <= ~u32_valid;
+        idle: begin
+          debug_u32_underflow <= ~u32_valid & u8_len_valid;
         end
         transfer: begin
           if (u8_ready && |i && j == 2'd0) begin
@@ -125,7 +126,8 @@ module i3c_controller_read_byte #(
   end
 
   assign u8_len_ready = sm == idle ? 1'b1 : 1'b0;
-  assign u32_ready = (sm == first) || (sm == transfer && u8_ready && |i) ? 1'b1 : 1'b0;
+  assign u32_ready = (sm == idle && u8_len_valid & u32_valid) || (sm == transfer && u8_ready && |i && c == 2'b00) ? 1'b1 : 1'b0;
+  assign u8_valid = sm == transfer ? 1'b1 : 1'b0;
   assign j = u8_len_reg[1:0];
   assign i = u8_len_reg[11:2];
   assign u8 = u32_reg[31:24];
---
+++ library/i3c_controller/i3c_controller_host_interface/i3c_controller_write_byte.v
@@ -0,0 +1,110 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2023 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+/**
+ * Packs u8 stream into u32 packages.
+ * Restrictions:
+ * Length must be > 0
+ * At partial payload, e.g. 3-bytes, the remaining byte value is unknown.
+ */
+
+`timescale 1ns/100ps
+`default_nettype none
+
+module i3c_controller_write_byte (
+  input  wire clk,
+  input  wire resetn,
+
+  input  wire u32_ready,
+  output wire u32_valid,
+  output wire [31:0] u32,
+
+  output wire u8_len_ready,
+  input  wire u8_len_valid,
+  input  wire [11:0] u8_len,
+
+  output wire u8_ready,
+  input  wire u8_valid,
+  input  wire [7:0] u8
+);
+  reg [0:0] sm;
+  localparam [0:0]
+    idle     = 0,
+    transfer = 1;
+
+  reg [11:0] u8_len_reg;
+  reg [7:0] u32_reg [3:0];
+  reg u32_valid_ctrl;
+  reg [1:0] c;
+
+  always @(posedge clk) begin
+    if (resetn) begin
+      sm <= idle;
+      c <= 2'b00;
+      u32_valid_ctrl <= 1'b0;
+    end else begin
+      case (sm)
+        idle: begin
+          u8_len_reg <= u8_len;
+          sm <= u8_len_valid && u32_ready ? transfer : idle;
+          c <= 2'b00;
+          u32_valid_ctrl <= 1'b0;
+        end
+        transfer: begin
+          if (u8_valid) begin // tick
+            u32_valid_ctrl <= 1'b1;
+            u32_reg[c] <= u8;
+            u8_len_reg <= u8_len_reg - 1;
+            c <= c + 1;
+
+            if (~|u8_len_reg) begin
+              sm <= idle;
+            end
+          end
+        end
+        default: begin
+          sm <= idle;
+        end
+      endcase
+    end
+  end
+
+  assign u8_len_ready = sm == idle ? 1'b1 : 1'b0;
+  assign u8_ready = (sm == transfer && u32_ready) ? 1'b1 : 1'b0;
+  assign u32_valid = (sm == transfer && (c == 2'b00 || u8_len_reg == 12'd0) && u8_valid && u32_valid_ctrl) ? 1'b1 : 1'b0;
+  genvar i;
+  for (i=0; i<4; i=i+1) begin
+    assign u32[8*i+7:8*i] = u32_reg[3-i];
+  end
+endmodule
