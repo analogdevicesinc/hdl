cf90d1b3dcbd2a24132814288a9f6c98295ef753
Bogdan Luncan <bogdan.luncan@analog.com>
2023-05-08 13:01:09 +0100

    ad9081: Parameters and header update
    Signed-off-by: Bogdan Luncan <bogdan.luncan@analog.com>

--- projects/ad9081_fmca_ebz/vcu118/system_bd.tcl
+++ projects/ad9081_fmca_ebz/vcu118/system_bd.tcl
@@ -9,8 +9,12 @@ source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
 source $ad_hdl_dir/projects/ad9081_fmca_ebz/common/ad9081_fmca_ebz_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 
-ad_ip_parameter axi_mxfe_rx_jesd/rx CONFIG.NUM_INPUT_PIPELINE 2
-ad_ip_parameter axi_mxfe_tx_jesd/tx CONFIG.NUM_OUTPUT_PIPELINE 1
+if {$INTF_CFG != "TX"} {
+  ad_ip_parameter axi_mxfe_rx_jesd/rx CONFIG.NUM_INPUT_PIPELINE 2
+}
+if {$INTF_CFG != "RX"} {
+  ad_ip_parameter axi_mxfe_tx_jesd/tx CONFIG.NUM_OUTPUT_PIPELINE 1
+}
 
 set mem_init_sys_path [get_env_param ADI_PROJECT_DIR ""]mem_init_sys.txt;
 
