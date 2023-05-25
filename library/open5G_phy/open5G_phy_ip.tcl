83c409f59334e0db759d945b80e3b81c236cac8e
Benjamin Menkuec <benjamin@menkuec.de>
2023-05-25 21:51:05 +0200

    rename open5G_rx to open5G_phy

--- library/open5G_phy/open5G_phy_ip.tcl
+++ library/open5G_phy/open5G_phy_ip.tcl
@@ -12,7 +12,7 @@ exec python3 ../../submodules/open5G_rx/tools/generate_PSS_tap_file.py --PSS_LEN
 exec python3 ../../submodules/open5G_rx/tools/generate_PSS_tap_file.py --PSS_LEN=128 --TAP_DW=32 --N_id_2=1
 exec python3 ../../submodules/open5G_rx/tools/generate_PSS_tap_file.py --PSS_LEN=128 --TAP_DW=32 --N_id_2=2
 
-adi_ip_create open5G_rx
+adi_ip_create open5G_phy
 set_property part xc7z010clg400-1 [current_project]
 set proj_fileset [get_filesets sources_1]
 add_files -norecurse -scan_for_includes -fileset $proj_fileset [list \
@@ -80,12 +80,12 @@ add_files -norecurse -scan_for_includes -fileset $proj_fileset [list \
 set_property top receiver $proj_fileset
 update_compile_order -fileset sources_1
 
-adi_ip_properties_lite open5G_rx
+adi_ip_properties_lite open5G_phy
 set_property vendor_display_name Catkira [ipx::current_core]
 set_property vendor Catkira [ipx::current_core]
 set_property company_url http://www.github.com/catkira/open5G_rx [ipx::current_core]
-set_property display_name "Open5G receiver" [ipx::current_core]
-set_property description "Open5G receiver" [ipx::current_core]
+set_property display_name "Open5G_phy" [ipx::current_core]
+set_property description "Open5G PHY" [ipx::current_core]
 adi_ip_ttcl open5G_rx "open5G_rx_constr.ttcl"
 
 set project_dir [get_property DIRECTORY [current_project]]/
@@ -109,7 +109,7 @@ adi_add_bus "m_axis_out" "master" \
 		{"m_axis_out_tdata"  "TDATA"} \
 	}
 
-adi_ip_infer_mm_interfaces open5G_rx
+adi_ip_infer_mm_interfaces open5G_phy
 set memory_maps [ipx::get_memory_maps * -of_objects [ipx::current_core]]
 foreach map $memory_maps {
   ipx::remove_memory_map [lindex $map 2] [ipx::current_core]
