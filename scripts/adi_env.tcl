73a45c83c7dc6f4a9c71112c89d0d961118d5e22
Iulia Moldovan <Iulia.Moldovan@analog.com>
2023-06-27 11:59:55 +0300

    scripts/adi_env.tcl: Update to Vivado 2023.1
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- scripts/adi_env.tcl
+++ scripts/adi_env.tcl
@@ -15,7 +15,7 @@ if [info exists ::env(ADI_GHDL_DIR)] {
 }
 
 # Define the supported tool version
-set required_vivado_version "2022.2"
+set required_vivado_version "2023.1"
 if {[info exists ::env(REQUIRED_VIVADO_VERSION)]} {
   set required_vivado_version $::env(REQUIRED_VIVADO_VERSION)
 } elseif {[info exists REQUIRED_VIVADO_VERSION]} {
