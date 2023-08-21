6b3edcc943db65a81fddded4bbe451d350e3ece4
Bogdan Luncan <bogdan.luncan@analog.com>
2023-08-21 14:59:47 +0300

    Added gpios and some code cleanup

--- projects/common/fm87/system_top.v
+++ projects/common/fm87/system_top.v
@@ -1,6 +1,6 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright 2014 - 2023 (c) Analog Devices, Inc. All rights reserved.
+// Copyright 2023 - 2023 (c) Analog Devices, Inc. All rights reserved.
 //
 // In this HDL repository, there are many different and unique modules, consisting
 // of various HDL (Verilog or VHDL) components. The individual modules are
@@ -40,16 +40,15 @@ module system_top (
   // clock and resets
   input            sys_clk,
   input            hps_io_ref_clk,
-  // input            refclk_bti, // additional refclk_bti to preserve Etile XCVR
+  // input         refclk_bti, // additional refclk_bti to preserve Etile XCVR
   input            sys_resetn,
 
-  // gpio
-  input            sys_sgpio_sync,
-  input            sys_sgpio_clk,
-  input            sys_sgpi,
-  input            sys_sgpo,
-
-  input   [ 1:0]   sys_button_pio,
+  // board gpio
+  input   [12:0]   fpga_gpio,
+  input            fpga_sgpio_sync,
+  input            fpga_sgpio_clk,
+  input            fpga_sgpi,
+  output           fpga_sgpo,
 
   // hps-emif
   input            emif_hps_pll_ref_clk,
@@ -75,7 +74,7 @@ module system_top (
   input            hps_emac_rxclk,
   input            hps_emac_rxctl,
   input   [ 3:0]   hps_emac_rxd,
-  output           hps_emac_txclk,       //TODO: may need to change RMII TX CLK to be input instead, check
+  output           hps_emac_txclk,
   output           hps_emac_txctl,
   output  [ 3:0]   hps_emac_txd,
   output           hps_emac_mdc,
@@ -107,55 +106,33 @@ module system_top (
   output           hps_jtag_tdo,
   input            hps_jtag_tdi,
 
-  // hps-gpio
-  inout   [ 6:0]   hps_gpio);
+  // hps-OOBE daughter card peripherals
+  inout            hps_gpio_eth_irq,
+  inout            hps_gpio_usb_oci,
+  inout   [ 1:0]   hps_gpio_btn,
+  inout   [ 2:0]   hps_gpio_led);
 
+  // internal signals
+  wire  [63:0]  gpio_i;
+  wire  [63:0]  gpio_o;
+  wire  [ 7:0]  fpga_dipsw;
+  wire  [ 7:0]  fpga_led;
   wire          ninit_done;
   wire          sys_reset_n;
   wire          h2f_reset;
-  wire  [43:0]  stm_hw_events;
 
-  wire          hps_mem_cal_success;
-  wire          hps_mem_cal_fail;
+  // Board GPIOs
+  assign fpga_led = gpio_o[7:0];
+  assign gpio_i[ 7:0] = gpio_o[7:0];
+  assign gpio_i[15: 8] = fpga_dipsw;
+  assign gpio_i[17:16] = fpga_gpio[ 1:0]; // push buttons
+  assign gpio_i[28:18] = fpga_gpio[12:2];
 
-  wire  [ 7:0]  sys_led_pio;
-  wire  [ 7:0]  sys_dipsw_pio;
-  wire  [ 6:0]  sys_led_internal;
-  reg   [22:0]  heartbeat_count;
-  wire          heartbeat_led;
-
-  assign heartbeat_led = ~heartbeat_count[22];
-  assign sys_led_pio   = {heartbeat_led, sys_led_internal};
-
-  /*
-  wire  [ 9:0]  gpio_bd_i;
-  wire  [ 7:0]  gpio_bd_o;
-  wire  [63:0]  gpio_i;
-  wire  [63:0]  gpio_o;
-
-  // assignments
+  // Unused GPIOs
+  assign gpio_i[31:29] = gpio_o[31:29];
   assign gpio_i[63:32] = gpio_o[63:32];
 
-  // board stuff
-  assign gpio_i[31:19] = gpio_o[31:19];
-
-  assign gpio_i[18:18] = hps_mem_cal_success;
-  assign gpio_i[17:17] = hps_mem_cal_fail;
-  assign gpio_i[16:15] = gpio_bd_i[9:8]; // buttons
-  assign gpio_i[14: 7] = gpio_bd_i[7:0]; // dipsws
-  assign gpio_i[ 6: 0] = gpio_o[6:0];    // leds
-  assign gpio_bd_o     = gpio_o[6:0];
-  */
-
-  assign stm_hw_events = {14'b0, heartbeat_led, sys_led_internal, sys_dipsw_pio, sys_button_pio};
   assign sys_reset_n   = sys_resetn & ~h2f_reset & ~ninit_done;
-
-  always @(posedge sys_clk or negedge sys_reset_n) begin
-    if (~sys_reset_n)
-      heartbeat_count <= 23'd0;
-    else
-      heartbeat_count <= heartbeat_count + 23'd1;
-  end
 
   system_bd i_system_bd (
     .sys_clk_clk                               (sys_clk),
@@ -164,36 +141,29 @@ module system_top (
     .sys_rst_reset_n                           (sys_reset_n),
   //.src_prb_rst_sources_source                (1'b1), // temporary disable
     .rst_ninit_done_ninit_done                 (ninit_done),
+    .sys_gpio_bd_in_port                       (gpio_i[31: 0]),
+    .sys_gpio_bd_out_port                      (gpio_o[31: 0]),
+    .sys_gpio_in_export                        (gpio_i[63:32]),
+    .sys_gpio_out_export                       (gpio_o[63:32]),
 
-    .sys_gpio_led_in_port                      (sys_led_internal), // leds
-    .sys_gpio_led_out_port                     (sys_led_internal), // leds
-    .sys_gpio_button_export                    (sys_button_pio), // buttons
-    .sys_gpio_dipsw_export                     (sys_dipsw_pio), // dipsws
-  //.sys_gpio_bd_in_port                       (gpio_i[31:0]),
-  //.sys_gpio_bd_out_port                      (gpio_o[31:0]),
-  //.sys_gpio_in_export                        (gpio_i[63:32]),
-  //.sys_gpio_out_export                       (gpio_o[63:32]),
-
-    .emif_hps_mem_mem_ck                       (emif_hps_mem_clk_p),
-    .emif_hps_mem_mem_ck_n                     (emif_hps_mem_clk_n),
-    .emif_hps_mem_mem_a                        (emif_hps_mem_a),
-    .emif_hps_mem_mem_act_n                    (emif_hps_mem_act_n),
-    .emif_hps_mem_mem_ba                       (emif_hps_mem_ba),
-    .emif_hps_mem_mem_bg                       (emif_hps_mem_bg),
-    .emif_hps_mem_mem_cke                      (emif_hps_mem_cke),
-    .emif_hps_mem_mem_cs_n                     (emif_hps_mem_cs_n),
-    .emif_hps_mem_mem_odt                      (emif_hps_mem_odt),
-    .emif_hps_mem_mem_reset_n                  (emif_hps_mem_reset_n),
-    .emif_hps_mem_mem_par                      (emif_hps_mem_par),
-    .emif_hps_mem_mem_alert_n                  (emif_hps_mem_alert_n),
-    .emif_hps_mem_mem_dqs                      (emif_hps_mem_dqs_p),
-    .emif_hps_mem_mem_dqs_n                    (emif_hps_mem_dqs_n),
-    .emif_hps_mem_mem_dq                       (emif_hps_mem_dq),
-    .emif_hps_mem_mem_dbi_n                    (emif_hps_mem_dbi_n),
-    .emif_hps_oct_oct_rzqin                    (emif_hps_oct_rzq),
-    .emif_hps_pll_ref_clk_clk                  (emif_hps_pll_ref_clk),
- // .local_cal_success_local_cal_success       (hps_mem_cal_success),
- // .local_cal_fail_local_cal_fail             (hps_mem_cal_fail),
+    .emif_hps_ddr_mem_ck                       (emif_hps_mem_clk_p),
+    .emif_hps_ddr_mem_ck_n                     (emif_hps_mem_clk_n),
+    .emif_hps_ddr_mem_a                        (emif_hps_mem_a),
+    .emif_hps_ddr_mem_act_n                    (emif_hps_mem_act_n),
+    .emif_hps_ddr_mem_ba                       (emif_hps_mem_ba),
+    .emif_hps_ddr_mem_bg                       (emif_hps_mem_bg),
+    .emif_hps_ddr_mem_cke                      (emif_hps_mem_cke),
+    .emif_hps_ddr_mem_cs_n                     (emif_hps_mem_cs_n),
+    .emif_hps_ddr_mem_odt                      (emif_hps_mem_odt),
+    .emif_hps_ddr_mem_reset_n                  (emif_hps_mem_reset_n),
+    .emif_hps_ddr_mem_par                      (emif_hps_mem_par),
+    .emif_hps_ddr_mem_alert_n                  (emif_hps_mem_alert_n),
+    .emif_hps_ddr_mem_dqs                      (emif_hps_mem_dqs_p),
+    .emif_hps_ddr_mem_dqs_n                    (emif_hps_mem_dqs_n),
+    .emif_hps_ddr_mem_dq                       (emif_hps_mem_dq),
+    .emif_hps_ddr_mem_dbi_n                    (emif_hps_mem_dbi_n),
+    .emif_hps_oct_rzqin                        (emif_hps_oct_rzq),
+    .emif_hps_pll_ref_clk                      (emif_hps_pll_ref_clk),
 
     .sys_hps_io_EMAC0_TX_CLK                   (hps_emac_txclk),
     .sys_hps_io_EMAC0_TX_CTL                   (hps_emac_txctl),
@@ -248,31 +218,29 @@ module system_top (
     .sys_hps_h2f_cs_tdoen                      (),
     .sys_hps_h2f_cs_tms                        (1'b1),
 
-    .sys_hps_io_gpio1_io0                      (hps_gpio[0]),
-    .sys_hps_io_gpio1_io1                      (hps_gpio[1]),
-    .sys_hps_io_gpio1_io4                      (hps_gpio[2]),
-    .sys_hps_io_gpio1_io5                      (hps_gpio[3]),
-    .sys_hps_io_gpio1_io19                     (hps_gpio[4]),
-    .sys_hps_io_gpio1_io20                     (hps_gpio[5]),
-    .sys_hps_io_gpio1_io21                     (hps_gpio[6]),
+    .sys_hps_io_gpio1_io0                      (hps_gpio_eth_irq),
+    .sys_hps_io_gpio1_io1                      (hps_gpio_usb_oci),
+    .sys_hps_io_gpio1_io4                      (hps_gpio_btn[0]),
+    .sys_hps_io_gpio1_io5                      (hps_gpio_btn[1]),
+    .sys_hps_io_gpio1_io19                     (hps_gpio_led[1]),
+    .sys_hps_io_gpio1_io20                     (hps_gpio_led[0]),
+    .sys_hps_io_gpio1_io21                     (hps_gpio_led[2]),
 
     .h2f_reset_reset                           (h2f_reset),
 
     .sys_spi_MISO                              (1'b0),
     .sys_spi_MOSI                              (),
     .sys_spi_SCLK                              (),
-    .sys_spi_SS_n                              (),
+    .sys_spi_SS_n                              ());
 
-    .sys_hps_f2h_stm_hw_events_stm_hwevents    (stm_hw_events));
-    // .f2h_irq1_irq                              (f2h_irq1));
-
-  sgpio_slave sgpio_slave_inst (
-    .i_rstn     (sys_reset_n),
-    .i_clk      (sys_sgpio_clk),
-    .i_sync     (sys_sgpio_sync),
-    .i_mosi     (sys_sgpi),
-    .o_miso     (sys_sgpo),
-    .o_user_sw  (sys_dipsw_pio),
-    .i_user_led (sys_led_pio));
+  sgpio_slave sgpio_slave_inst(
+    .i_rstn     (sys_resetn),
+    .i_clk      (fpga_sgpio_clk),
+    .i_sync     (fpga_sgpio_sync),
+    .i_mosi     (fpga_sgpi),
+    .o_miso     (fpga_sgpo),
+    .o_user_sw  (fpga_dipsw),
+    .i_user_led (fpga_led)
+  );
 
 endmodule
