b5d1f758042f0164548a676e7b7c2c4324a95b36
Laez Barbosa <laez.barbosa@analog.com>
2023-09-15 14:42:08 -0300

    Add AD9081 s10soc project
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

---
+++ projects/ad9081_fmca_ebz/s10soc/system_qsys.tcl
@@ -0,0 +1,21 @@
+###############################################################################
+## Copyright (C) 2021-2023 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+## ADC FIFO depth in samples per converter
+set adc_fifo_samples_per_converter [expr $ad_project_params(RX_KS_PER_CHANNEL)*1024]
+## DAC FIFO depth in samples per converter
+set dac_fifo_samples_per_converter [expr $ad_project_params(TX_KS_PER_CHANNEL)*1024]
+
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+source $ad_hdl_dir/projects/common/s10soc/s10soc_system_qsys.tcl
+source $ad_hdl_dir/projects/common/intel/dacfifo_qsys.tcl
+source $ad_hdl_dir/projects/common/intel/adcfifo_qsys.tcl
+
+if [info exists ad_project_dir] {
+  source ../../common/ad9081_fmca_ebz_qsys.tcl
+} else {
+  source ../common/ad9081_fmca_ebz_qsys.tcl
+}
+
