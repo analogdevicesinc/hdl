d921f5fe453416bc720efe215c4bd026aba43cff
PopPaul2021 <paul.pop@analog.com>
2023-11-01 11:55:20 +0200

    projects/cn0585_fmcz: Added project for CN0584 on ZedBoard.
    The project controls 2 AD3552R digital-to-analog converters and 4 LTC2387 analog-to-digital converters.
    The reference clock for all IP's is generated by an axi_clkgen IP and is configured to output a 120MHz signal.
    The VADJ jumper needs to be placed on the 2V5 position.
    
    Signed-off-by: PopPaul2021 <paul.pop@analog.com>

---
+++ projects/cn0585_fmcz/zed/system_project.tcl
@@ -0,0 +1,18 @@
+###############################################################################
+## Copyright (C) 2022-2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source ../../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project cn0585_fmcz_zed
+
+adi_project_files cn0585_fmcz_zed [list \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"  \
+  "system_constr.xdc" \
+  "system_top.v"]
+
+adi_project_run cn0585_fmcz_zed
