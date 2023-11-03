ad6cddef21bf5248d7ad9d10b1ab9605151c43d9
Villyam <Vilmoscsaba.Jozsa@analog.com>
2023-11-03 15:09:49 +0200

    projects/scripts: Added bsp generation command to default commands.
    Signed-off-by: Villyam <Vilmoscsaba.Jozsa@analog.com>

--- projects/scripts/adi_project_lattice_bd.tcl
+++ projects/scripts/adi_project_lattice_bd.tcl
@@ -40,7 +40,11 @@ proc adi_project_bd {project_name args} {
     -board "" \
     -speed "" \
     -language "verilog" \
-    -cmd_list {{source ./system_bd.tcl}} \
+    -cmd_list {{source ./system_bd.tcl} \
+    {sbp_design save} \
+    {sbp_design pge sge \
+      -i "$ppath/$project_name/$project_name/$project_name.sbx" \
+      -o "$ppath"}} \
     {*}$args]
 
   set ppath $opt(-ppath)
