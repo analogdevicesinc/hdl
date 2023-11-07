af099ca210e1b18a57cbd0bdf7c485bb15156fc9
Jorge Marques <jorge.marques@analog.com>
2023-11-07 17:25:53 -0300

    ad7616/zcu102: Add parallel mode signals
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- projects/ad7616_sdz/zcu102/system_top_pi.v
+++ projects/ad7616_sdz/zcu102/system_top_pi.v
@@ -38,12 +38,23 @@
 module system_top (
 
   input   [12:0]  gpio_bd_i,
-  output  [ 7:0]  gpio_bd_o
+  output  [ 7:0]  gpio_bd_o,
+  inout   [31:0]  gpio_bd,
+
+  inout   [15:0]  adc_db,
+  output          adc_rd_n,
+  output          adc_wr_n,
+
+  output          adc_cs_n,
+  output          adc_reset_n,
+  output          adc_convst,
+  input           adc_busy,
+  output          adc_seq_en,
+  output  [ 1:0]  adc_hw_rngsel,
+  output  [ 2:0]  adc_chsel
 );
 
   // internal signals
-  wire    [94:0]  gpio_i;
-  wire    [94:0]  gpio_o;
 
   assign gpio_bd_o = gpio_o[7:0];
 
@@ -51,7 +62,53 @@ module system_top (
   assign gpio_i[20: 8] = gpio_bd_i;
   assign gpio_i[ 7: 0] = gpio_o[ 7: 0];
 
+  // internal signals
+
+  wire    [94:0]  gpio_i;
+  wire    [94:0]  gpio_o;
+  wire    [94:0]  gpio_t;
+
+  wire            adc_db_t;
+  wire    [15:0]  adc_db_o;
+  wire    [15:0]  adc_db_i;
+
+  genvar i;
+
   // instantiations
+
+  ad_iobuf #(
+    .DATA_WIDTH(7)
+  ) i_iobuf_adc_cntrl (
+    .dio_t ({gpio_t[43:41], gpio_t[37], gpio_t[35:33]}),
+    .dio_i ({gpio_o[43:41], gpio_o[37], gpio_o[35:33]}),
+    .dio_o ({gpio_i[43:41], gpio_i[37], gpio_i[35:33]}),
+    .dio_p ({adc_reset_n,        // 43
+             adc_hw_rngsel,      // 42:41
+             adc_seq_en,         // 37
+             adc_chsel}));       // 35:33
+
+  assign gpio_i[63:44] = gpio_o[63:44];
+  assign gpio_i[40:38] = gpio_o[40:38];
+  assign gpio_i[36] = gpio_o[36];
+
+  generate
+    for (i = 0; i < 16; i = i + 1) begin: adc_db_io
+      ad_iobuf i_iobuf_adc_db (
+        .dio_t(adc_db_t),
+        .dio_i(adc_db_o[i]),
+        .dio_o(adc_db_i[i]),
+        .dio_p(adc_db[i]));
+    end
+  endgenerate
+
+  ad_iobuf #(
+    .DATA_WIDTH(15)
+  ) i_iobuf_gpio (
+    .dio_t(gpio_t[14:0]),
+    .dio_i(gpio_o[14:0]),
+    .dio_o(gpio_i[14:0]),
+    .dio_p(gpio_bd));
+
   system_wrapper i_system_wrapper (
     .gpio_i (gpio_i),
     .gpio_o (gpio_o),
@@ -64,6 +121,14 @@ module system_top (
     .spi1_csn (1'b1),
     .spi1_miso (1'b0),
     .spi1_mosi (),
-    .spi1_sclk ());
+    .spi1_sclk (),
+    .rx_cnvst (adc_convst),
+    .rx_cs_n (adc_cs_n),
+    .rx_busy (adc_busy),
+    .rx_db_o (adc_db_o),
+    .rx_db_i (adc_db_i),
+    .rx_db_t (adc_db_t),
+    .rx_rd_n (adc_rd_n),
+    .rx_wr_n (adc_wr_n));
 
 endmodule
