f1c8bd36fbe89be8b606b605e61f9bff033e265b
PopPaul2021 <Paul.Pop@analog.com>
2023-11-23 10:49:45 +0200

    projects/common/ctpnxe: Second GPIO controller and constraints comments
    Signed-off-by: PopPaul2021 <Paul.Pop@analog.com>

--- projects/common/ctpnxe/ctpnxe_system_bd.tcl
+++ projects/common/ctpnxe/ctpnxe_system_bd.tcl
@@ -17,7 +17,7 @@ adi_ip_instance -vlnv {latticesemi.com:ip:cpu0:2.4.0} \
     "SIMULATION": false,
     "DEBUG_ENABLE": true,
     "M_STANDALONE": true,
-    "IRQ_NUM": 4
+    "IRQ_NUM": 6
   } \
   -ip_iname "cpu0_inst"
 adi_ip_instance -vlnv {latticesemi.com:ip:gpio0:1.6.1} \
@@ -27,6 +27,13 @@ adi_ip_instance -vlnv {latticesemi.com:ip:gpio0:1.6.1} \
     "DIRECTION_DEF_VAL_INPUT": "FF"
   } \
   -ip_iname "gpio0_inst"
+  adi_ip_instance -vlnv {latticesemi.com:ip:gpio1:1.6.1} \
+  -ip_path "$ip_download_path/latticesemi.com_ip_gpio_1.6.1" \
+  -ip_params {
+    "IO_LINES_COUNT": 32,
+    "DIRECTION_DEF_VAL_INPUT": "FF"
+  } \
+  -ip_iname "gpio1_inst"
 adi_ip_instance -vlnv {latticesemi.com:ip:uart0:1.3.0} \
   -ip_path "$ip_download_path/latticesemi.com_ip_uart_1.3.0" \
   -ip_params {
@@ -36,7 +43,11 @@ adi_ip_instance -vlnv {latticesemi.com:ip:uart0:1.3.0} \
 adi_ip_instance -vlnv {latticesemi.com:ip:spi0:1.4.1} \
   -ip_path "$ip_download_path/latticesemi.com_ip_spi_master_1.4.1" \
   -ip_params {
-    "SYS_CLOCK_FREQ": 108
+    "DATA_WIDTH": 8,
+    "CPOL": 1,
+    "CPHA": 1,
+    "FIFO_DEPTH": 256,
+    "SYS_CLOCK_FREQ": 108.0
   } \
   -ip_iname "spi0_inst"
 adi_ip_instance -vlnv {latticesemi.com:ip:i2c0:1.5.0} \
@@ -71,7 +82,7 @@ adi_ip_instance -vlnv {latticesemi.com:module:ahbl0:1.3.0} \
   -ip_path "$preinst_ip_mod_dir/ip/common/ahb_lite_interconnect" \
   -ip_params {
     "TOTAL_MASTER_CNT": 1,
-    "TOTAL_SLAVE_CNT": 3
+    "TOTAL_SLAVE_CNT": 4
   } \
   -ip_iname "ahbl0_inst"
 adi_ip_instance -vlnv {latticesemi.com:module:ahbl2apb0:1.1.0} \
@@ -84,7 +95,7 @@ adi_ip_instance -vlnv {latticesemi.com:module:apb0:1.2.0} \
   -ip_path "$preinst_ip_mod_dir/ip/common/apb_interconnect" \
   -ip_params {
     "TOTAL_MASTER_CNT": 1,
-    "TOTAL_SLAVE_CNT": 3
+    "TOTAL_SLAVE_CNT": 4
   } \
   -ip_iname "apb0_inst"
 adi_ip_instance -vlnv {latticesemi.com:ip:sysmem0:1.1.2} \
@@ -92,8 +103,7 @@ adi_ip_instance -vlnv {latticesemi.com:ip:sysmem0:1.1.2} \
   -ip_params {
     "ADDR_DEPTH": 8192,
     "PORT_COUNT": 2,
-    "INIT_MEM": true,
-    "INIT_FILE": "../../riscvmc.mem",
+    "INIT_MEM": false,
     "REGMODE_S0": true,
     "REGMODE_S1": true
   } \
@@ -109,6 +119,7 @@ sbp_add_port -direction in rstn_i
 sbp_add_port -direction in rxd_i
 sbp_add_port -direction out txd_o
 sbp_add_port -from 31 -to 0 -direction inout gpio
+sbp_add_port -from 31 -to 0 -direction inout gpio_eval
 
 sbp_add_port -direction out mosi_o
 sbp_add_port -direction out sclk_o
@@ -118,16 +129,6 @@ sbp_add_port -from 0 -to 0 -direction out ssn_o
 sbp_add_port -direction inout scl_io
 sbp_add_port -direction inout sda_io
 
-sbp_connect_net "$project_name/cpu0_inst/system_resetn_o" \
-  "$project_name/ahbl2apb0_inst/presetn_i" \
-  "$project_name/ahbl2apb0_inst/rst_n_i" \
-  "$project_name/apb0_inst/apb_presetn_i" \
-  "$project_name/uart0_inst/rst_n_i" \
-  "$project_name/ahbl0_inst/ahbl_hresetn_i" \
-  "$project_name/gpio0_inst/resetn_i" \
-  "$project_name/sysmem0_inst/ahbl_hresetn_i" \
-  "$project_name/spi0_inst/rst_n_i" \
-  "$project_name/i2c0_inst/rst_n_i"
 
 sbp_connect_net "$project_name/equation_module0_inst/O" \
   "$project_name/cpu0_inst/rst_n_i"
@@ -135,18 +136,12 @@ sbp_connect_net "$project_name/equation_module0_inst/O" \
 sbp_connect_net "$project_name/osc0_inst/hf_clk_out_o" \
   "$project_name/pll0_inst/clki_i"
 
-sbp_connect_net "$project_name/pll0_inst/clkop_o" \
-  "$project_name/ahbl2apb0_inst/clk_i" \
-  "$project_name/ahbl0_inst/ahbl_hclk_i" \
-  "$project_name/cpu0_inst/clk_i" \
-  "$project_name/sysmem0_inst/ahbl_hclk_i"  \
-  "$project_name/spi0_inst/clk_i"
-
 sbp_connect_net "$project_name/pll0_inst/clkos_o" \
   "$project_name/ahbl2apb0_inst/pclk_i" \
   "$project_name/apb0_inst/apb_pclk_i" \
   "$project_name/uart0_inst/clk_i" \
   "$project_name/gpio0_inst/clk_i" \
+  "$project_name/gpio1_inst/clk_i" \
   "$project_name/i2c0_inst/clk_i"
 
 sbp_connect_net "$project_name/pll0_inst/lock_o" \
@@ -162,6 +157,8 @@ sbp_connect_net "$project_name/uart0_inst/txd_o" \
 
 sbp_connect_net "$project_name/gpio0_inst/gpio_io" \
   "$project_name/gpio"
+sbp_connect_net "$project_name/gpio1_inst/gpio_io" \
+  "$project_name/gpio_eval"
 
 sbp_connect_net "$project_name/i2c0_inst/scl_io" \
   "$project_name/scl_io"
@@ -191,6 +188,8 @@ sbp_connect_interface_net "$project_name/apb0_inst/APB_M01" \
   "$project_name/gpio0_inst/APB_S0"
 sbp_connect_interface_net "$project_name/apb0_inst/APB_M02" \
   "$project_name/i2c0_inst/APB_S0"
+sbp_connect_interface_net "$project_name/apb0_inst/APB_M03" \
+  "$project_name/gpio1_inst/APB_S0"
 sbp_connect_interface_net "$project_name/cpu0_inst/AHBL_M0_INSTR" \
   "$project_name/sysmem0_inst/AHBL_S0"
 sbp_connect_interface_net "$project_name/cpu0_inst/AHBL_M1_DATA" \
@@ -203,20 +202,24 @@ sbp_connect_interface_net "$project_name/spi0_inst/INTR" \
   "$project_name/cpu0_inst/IRQ_S2"
 sbp_connect_interface_net "$project_name/i2c0_inst/INTR" \
   "$project_name/cpu0_inst/IRQ_S3"
+sbp_connect_interface_net "$project_name/gpio1_inst/INTR" \
+  "$project_name/cpu0_inst/IRQ_S4"
 
 sbp_connect_constant -constant 1'B1 "$project_name/osc0_inst/hf_out_en_i"
 
+sbp_assign_addr_seg -offset 'h00000000 "$project_name/ahbl0_inst/AHBL_M00" \
+  "$project_name/sysmem0_inst/AHBL_S1"
 sbp_assign_addr_seg -offset 'h00000000 "$project_name/cpu0_inst/AHBL_M0_INSTR" \
   "$project_name/sysmem0_inst/AHBL_S0"
+sbp_assign_addr_seg -offset 'h00008000 "$project_name/apb0_inst/APB_M00" \
+  "$project_name/uart0_inst/APB_S0"
 sbp_assign_addr_seg -offset 'h00008400 "$project_name/apb0_inst/APB_M01" \
   "$project_name/gpio0_inst/APB_S0"
 sbp_assign_addr_seg -offset 'h00008800 "$project_name/apb0_inst/APB_M02" \
   "$project_name/i2c0_inst/APB_S0"
 sbp_assign_addr_seg -offset 'h00008C00 "$project_name/ahbl0_inst/AHBL_M02" \
   "$project_name/spi0_inst/AHBL_S0"
-sbp_assign_addr_seg -offset 'h00000000 "$project_name/ahbl0_inst/AHBL_M00" \
-  "$project_name/sysmem0_inst/AHBL_S1"
-sbp_assign_addr_seg -offset 'h00008000 "$project_name/apb0_inst/APB_M00" \
-  "$project_name/uart0_inst/APB_S0"
+sbp_assign_addr_seg -offset 'h00018000 "$project_name/apb0_inst/APB_M03" \
+  "$project_name/gpio1_inst/APB_S0"
 
 #sbp_design auto_assign_addresses
