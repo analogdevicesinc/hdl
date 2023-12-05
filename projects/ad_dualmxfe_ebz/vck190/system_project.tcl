fb9d8d2d43b73b0e3cc6bebe37774d43980f1f34
Iulia Moldovan <Iulia.Moldovan@analog.com>
2023-12-05 09:58:59 +0200

    system_project.tcl: Remove empty lines
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- projects/ad_dualmxfe_ebz/vck190/system_project.tcl
+++ projects/ad_dualmxfe_ebz/vck190/system_project.tcl
@@ -43,19 +43,16 @@ adi_project ad_dualmxfe_ebz_vck190 0 [list \
   RX_LANE_RATE         [get_env_param RX_LANE_RATE        10 ] \
   TX_LANE_RATE         [get_env_param TX_LANE_RATE        10 ] \
   REF_CLK_RATE         [get_env_param REF_CLK_RATE       250 ] \
-
   RX_JESD_M            [get_env_param RX_JESD_M            8 ] \
   RX_JESD_L            [get_env_param RX_JESD_L            8 ] \
   RX_JESD_S            [get_env_param RX_JESD_S            1 ] \
   RX_JESD_NP           [get_env_param RX_JESD_NP          16 ] \
   RX_NUM_LINKS         [get_env_param RX_NUM_LINKS         2 ] \
-
   TX_JESD_M            [get_env_param TX_JESD_M            8 ] \
   TX_JESD_L            [get_env_param TX_JESD_L            8 ] \
   TX_JESD_S            [get_env_param TX_JESD_S            1 ] \
   TX_JESD_NP           [get_env_param TX_JESD_NP          16 ] \
   TX_NUM_LINKS         [get_env_param TX_NUM_LINKS         2 ] \
-
   RX_KS_PER_CHANNEL    [get_env_param RX_KS_PER_CHANNEL   64 ] \
   TX_KS_PER_CHANNEL    [get_env_param TX_KS_PER_CHANNEL   64 ] \
 ]
