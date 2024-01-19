1124117b6af54a7cd3443b07875c8c7d5319effa
Bogdan Luncan <bogdan.luncan@analog.com>
2024-01-19 12:56:37 +0200

    ad9082: vpk180: Initial commit
    Signed-off-by: Bogdan Luncan <bogdan.luncan@analog.com>

---
+++ projects/ad9082_fmca_ebz/vpk180/system_bd.tcl
@@ -0,0 +1,6 @@
+###############################################################################
+## Copyright (C) 2024-2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source $ad_hdl_dir/projects/ad9081_fmca_ebz/vpk180/system_bd.tcl
