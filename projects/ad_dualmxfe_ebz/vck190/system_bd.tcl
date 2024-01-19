32408bb547aa706b8f2fa24c1acf9a692cb15f7c
Iulia Moldovan <Iulia.Moldovan@analog.com>
2024-01-19 12:35:28 +0200

    system_bd.tcl: Add logging of make parameters
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- projects/ad_dualmxfe_ebz/vck190/system_bd.tcl
+++ projects/ad_dualmxfe_ebz/vck190/system_bd.tcl
@@ -16,9 +16,27 @@ source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 
 set mem_init_sys_path [get_env_param ADI_PROJECT_DIR ""]mem_init_sys.txt;
 
-#system ID
-ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+# system ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 10
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/$mem_init_sys_path"
-ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 10
 
-sysid_gen_sys_init_file
+set sys_cstring "$ad_project_params(JESD_MODE)\
+RX:RATE=$ad_project_params(RX_LANE_RATE)\
+M=$ad_project_params(RX_JESD_M)\
+L=$ad_project_params(RX_JESD_L)\
+S=$ad_project_params(RX_JESD_S)\
+NP=$ad_project_params(RX_JESD_NP)\
+LINKS=$ad_project_params(RX_NUM_LINKS)\
+KS/CH=$ad_project_params(RX_KS_PER_CHANNEL)\
+TX:RATE=$ad_project_params(TX_LANE_RATE)\
+M=$ad_project_params(TX_JESD_M)\
+L=$ad_project_params(TX_JESD_L)\
+S=$ad_project_params(TX_JESD_S)\
+NP=$ad_project_params(TX_JESD_NP)\
+LINKS=$ad_project_params(TX_NUM_LINKS)\
+KS/CH=$ad_project_params(TX_KS_PER_CHANNEL)\
+REF_CLK_RATE:$ad_project_params(REF_CLK_RATE)\
+GEN_LINK_CLK:$ad_project_params(GENERATE_LINK_CLK)"
+
+sysid_gen_sys_init_file $sys_cstring 10
