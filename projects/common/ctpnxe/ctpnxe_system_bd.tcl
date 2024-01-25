65ace2516da75445f71846dcd4da3ac69e6d3b98
Villyam <Vilmoscsaba.Jozsa@analog.com>
2024-01-25 18:40:33 +0200

    projects/common/ctpnxe: Updating riscv-mc base desing for 2023.2 tools.
    Updating IP versions for latests.
    
    Signed-off-by: Villyam <Vilmoscsaba.Jozsa@analog.com>

--- projects/common/ctpnxe/ctpnxe_system_bd.tcl
+++ projects/common/ctpnxe/ctpnxe_system_bd.tcl
@@ -11,8 +11,8 @@ set ip_download_path ${env(USERPROFILE)}/PropelIPLocal
 set conf_dir $ad_hdl_dir/projects/common/ctpnxe/ipcfg
 
 ## configure ip components and add to design. #################################
-adi_ip_instance -vlnv {latticesemi.com:ip:cpu0:2.4.0} \
-  -ip_path "$ip_download_path/latticesemi.com_ip_riscv_mc_2.4.0" \
+adi_ip_instance -vlnv {latticesemi.com:ip:cpu0:2.5.0} \
+  -ip_path "$ip_download_path/latticesemi.com_ip_riscv_mc_2.5.0" \
   -ip_params {
     "SIMULATION": false,
     "DEBUG_ENABLE": true,
@@ -20,16 +20,16 @@ adi_ip_instance -vlnv {latticesemi.com:ip:cpu0:2.4.0} \
     "IRQ_NUM": 5
   } \
   -ip_iname "cpu0_inst"
-adi_ip_instance -vlnv {latticesemi.com:ip:gpio0:1.6.1} \
-  -ip_path "$ip_download_path/latticesemi.com_ip_gpio_1.6.1" \
+adi_ip_instance -vlnv {latticesemi.com:ip:gpio0:1.6.2} \
+  -ip_path "$ip_download_path/latticesemi.com_ip_gpio_1.6.2" \
   -ip_params {
     "IO_LINES_COUNT": 32,
     "EXTERNAL_BUF": true,
     "DIRECTION_DEF_VAL_INPUT": "00FFFFFF"
   } \
   -ip_iname "gpio0_inst"
-adi_ip_instance -vlnv {latticesemi.com:ip:gpio1:1.6.1} \
-  -ip_path "$ip_download_path/latticesemi.com_ip_gpio_1.6.1" \
+adi_ip_instance -vlnv {latticesemi.com:ip:gpio1:1.6.2} \
+  -ip_path "$ip_download_path/latticesemi.com_ip_gpio_1.6.2" \
   -ip_params {
     "IO_LINES_COUNT": 32,
     "EXTERNAL_BUF": true,
@@ -57,19 +57,12 @@ adi_ip_instance -vlnv {latticesemi.com:ip:i2c0:1.5.0} \
   } \
   -ip_iname "i2c0_inst"
 
-adi_ip_instance -vlnv {latticesemi.com:module:pll0:1.7.0} \
+adi_ip_instance -vlnv {latticesemi.com:module:pll0:1.8.0} \
   -ip_path "$preinst_ip_mod_dir/ip/lifcl/pll" \
   -ip_params {
-    "gui_en_frac_n": false,
-    "gui_en_ssc": false,
-    "gui_en_int_fbkdel_sel": false,
     "gui_refclk_freq": 125.0,
     "gui_clk_op_freq": 75.0,
-    "gui_clk_op_tol": 0.0,
-    "gui_clk_os_en": false,
-    "gui_clk_os_freq": 25.0,
-    "gui_en_pll_reset": true,
-    "gui_en_pll_lock": true
+    "gui_clk_os_en": false
   } \
   -ip_iname "pll0_inst"
 
@@ -93,7 +86,7 @@ adi_ip_instance -vlnv {latticesemi.com:module:apb0:1.2.0} \
     "TOTAL_SLAVE_CNT": 4
   } \
   -ip_iname "apb0_inst"
-adi_ip_instance -vlnv {latticesemi.com:ip:sysmem0:1.1.2} \
+adi_ip_instance -vlnv {latticesemi.com:ip:sysmem0:2.1.0} \
   -ip_path "$preinst_ip_mod_dir/ip/common/system_memory" \
   -ip_params {
     "ADDR_DEPTH": 32768,
@@ -104,7 +97,7 @@ adi_ip_instance -vlnv {latticesemi.com:ip:sysmem0:1.1.2} \
     "REGMODE_S1": true
   } \
   -ip_iname "sysmem0_inst"
-adi_ip_instance -vlnv {latticesemi.com:ip:isr_ram0:1.1.2} \
+adi_ip_instance -vlnv {latticesemi.com:ip:isr_ram0:2.1.0} \
   -ip_path "$preinst_ip_mod_dir/ip/common/system_memory" \
   -ip_params {
     "ADDR_DEPTH": 8192,
@@ -247,6 +240,7 @@ sbp_connect_interface_net "$project_name/i2c0_inst/INTR" \
   "$project_name/cpu0_inst/IRQ_S3"
 
 # sbp_assign_addr_seg -offset 'h00190000 "$project_name/ahbl0_inst/AHBL_M00" \
+
 sbp_assign_addr_seg -offset 'h00000000 "$project_name/ahbl0_inst/AHBL_M00" \
   "$project_name/isr_ram0_inst/AHBL_S0"
 sbp_assign_addr_seg -offset 'h000C0000 "$project_name/ahbl0_inst/AHBL_M01" \
