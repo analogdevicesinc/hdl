eb22c9ae401c2af2bba70230b5b3f1e6e91182ba
Istvan-Zsolt Szekely <istvan.szekely@analog.com>
2024-02-21 12:00:21 +0000

    ad_mem: Added initialization with 0 at startup for simulations
    Signed-off-by: Istvan-Zsolt Szekely <istvan.szekely@analog.com>

--- library/common/ad_mem.v
+++ library/common/ad_mem.v
@@ -54,6 +54,14 @@ module ad_mem #(
   (* ram_style = "block" *)
   reg         [(DATA_WIDTH-1):0]      m_ram[0:((2**ADDRESS_WIDTH)-1)];
 
+  integer i;
+  initial
+    for (i = 0; i < 2 ** ADDRESS_WIDTH; i = i+1)
+      m_ram[i] = {DATA_WIDTH{1'b0}};
+
+  initial
+    doutb <= {DATA_WIDTH{1'b0}};
+
   always @(posedge clka) begin
     if (wea == 1'b1) begin
       m_ram[addra] <= dina;
