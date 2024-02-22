b27ec7319e25ea1d86d6a8a71476684df662dfd6
Jorge Marques <jorge.marques@analog.com>
2024-02-22 11:49:07 -0300

    deploy: be0e2809e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
