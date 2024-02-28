9fcb3095c91373df14dd156edde4db7210872b67
Cristian Mihai Popa <cristianmihai.popa@analog.com>
2024-02-28 15:19:21 +0200

    projects/ad9747: Initial commit
    Signed-off-by: Cristian Mihai Popa <cristianmihai.popa@analog.com>

---
+++ projects/ad9747_ebz/zed/system_project.tcl
@@ -0,0 +1,16 @@
+###############################################################################
+## Copyright (C) 2022-2023 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source ../../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project template_zed
+adi_project_files template_zed [list \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" \
+  "system_top.v" ]
+
+adi_project_run template_zed
