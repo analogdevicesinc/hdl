a68368e61de4a6229b85de4032afff23fe7286ec
PIoandan <128376819+PIoandan@users.noreply.github.com>
2024-03-06 17:28:43 +0200

    hdl: Zed-AD7768: Wideband fixed bug (#1281)
    * AD7768_zed: Fix wideband filter bug
    
    In SPI control mode, when not used as GPIO the FILTER pin and when a
    crystal is used as the clock source, this pin must be set to 1.
    The START pin must be tied to a logic 1 through a pull-up resistor, when
    it is not used.

--- projects/ad7768evb/zed/system_bd.tcl
+++ projects/ad7768evb/zed/system_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2014-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2014-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
--- projects/ad7768evb/zed/system_constr.xdc
+++ projects/ad7768evb/zed/system_constr.xdc
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2019-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2019-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
