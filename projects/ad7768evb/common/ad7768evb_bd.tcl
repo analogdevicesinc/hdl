ce35fc9b68f3f50218b1455a6d3f2096327a57d9
Ioan-daniel Pop <Pop.Ioan-daniel@analog.com>
2024-03-06 13:33:13 +0200

    AD7768: Update license headers

--- projects/ad7768evb/common/ad7768evb_bd.tcl
+++ projects/ad7768evb/common/ad7768evb_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2015-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2015-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
