6ee0e9f5dc7cb1dfe3193bace1a5526b25f1e5fc
Villyam <Vilmoscsaba.Jozsa@analog.com>
2024-03-07 11:55:02 +0200

    library/scripts: Modified addif to use only with manual portlist.
    Added addifa to use with -mod_data filter by interface prefix
    (-v_name) and exeption port list -exept_pl to exclude reset and
    clk.
    
    Signed-off-by: Villyam <Vilmoscsaba.Jozsa@analog.com>

--- library/scripts/test.tcl
+++ library/scripts/test.tcl
@@ -328,6 +328,7 @@ namespace eval ipl {
             -group2 "" \
             -macro_name "" \
         {*}$args]
+        # -optional for entering exact xml attribute string
 
         set optl {
             -id
@@ -412,6 +413,7 @@ namespace eval ipl {
             -attribute "" \
             -port_type "" \
         {*}$args]
+        # -optional for entering exact xml attribute string
 
         set ip $opt(-ip)
         set id $opt(-id)
@@ -671,7 +673,9 @@ namespace eval ipl {
         # do some tasks
         return $ip
     }
-
+# to do:
+# -addif only by list of logical and pysical port names
+# -addifmd call addif and use module data to generate the logical an physical port pairs
     proc addif {args} {
         array set opt [list -ip "$::ipl::ip" \
             -name "" \
@@ -679,25 +683,19 @@ namespace eval ipl {
             -description "" \
             -bus_type "" \
             -abstraction_ref "" \
-            -mod_data "" \
-            -if_name "" \
-            -exep_ports "" \
             -master_slave "" \
+            -portmap "" \
         {*}$args]
         
         set c {"}
         set ip $opt(-ip)
-        set mod_data $opt(-mod_data)
         set name $opt(-name)
-        set if_name $opt(-if_name)
         set display_name $opt(-display_name)
-        set exep_ports $opt(-exep_ports)
         set description $opt(-description)
         set bus_type $opt(-bus_type)
         set abst_ref $opt(-abstraction_ref)
         set master_slave $opt(-master_slave)
-
-        set ports [dict get $mod_data portlist]
+        set portmap $opt(-portmap)
 
         set bif $::ipl::busInterface_desc
         set bif [ipl::setncont {} lsccip:name $name $bif]
@@ -707,24 +705,20 @@ namespace eval ipl {
         set bif [ipl::setatts lsccip:abstractionTypes/lsccip:abstractionType lsccip:abstractionRef [list {0} $abst_ref] $bif]
         set bif [ipl::setnname {} lsccip:master_slave lsccip:$master_slave $bif]
 
-        set plist {}
-        foreach line $ports {
-            set reg [list ${if_name}_.+]
-            set pname [dict get $line name]
-            if {[lsearch $exep_ports $pname] == -1 && [regexp $reg $pname]} {
-                set logic [string toupper [string map [list ${if_name}_ ""] $pname]]
-                set pmap [ipl::setncont lsccip:logicalPort lsccip:name $logic $::ipl::portMap_desc]
-                set pmap [ipl::setncont lsccip:physicalPort lsccip:name $pname $pmap]
-                set bif [ipl::setnode lsccip:abstractionTypes/lsccip:abstractionType/lsccip:portMaps $pname $pmap $bif]
-                set ip [ipl::setport -ip $ip -id $pname -bus_interface bus_interface=${c}$name$c]
-            }
+        foreach line $portmap {
+            set pname [lindex $line 0]
+            set logic [lindex $line 1]
+            set pmap [ipl::setncont lsccip:logicalPort lsccip:name $logic $::ipl::portMap_desc]
+            set pmap [ipl::setncont lsccip:physicalPort lsccip:name $pname $pmap]
+            set bif [ipl::setnode lsccip:abstractionTypes/lsccip:abstractionType/lsccip:portMaps $pname $pmap $bif]
+            set ip [ipl::setport -ip $ip -id $pname -bus_interface bus_interface=${c}$name$c]
         }
         set bifs [ipl::getnode {} busInterfaces_desc $ip]
         if {$bifs == ""} {
             set bifs $::ipl::busInterfaces_desc
         }
         set bifsl [lindex $bifs 3]
-        dict set bifsl $if_name $bif
+        dict set bifsl $name $bif
         lset bifs 3 $bifsl
         set ip [ipl::setnode {} busInterfaces_desc $bifs $ip]
 
@@ -732,6 +726,51 @@ namespace eval ipl {
         # - manual portlist/portmap
         # - update ports attributes with the interface attribute
         return $ip
+    }
+    proc addifa {args} {
+        array set opt [list -ip "$::ipl::ip" \
+            -name "" \
+            -display_name "" \
+            -description "" \
+            -bus_type "" \
+            -abstraction_ref "" \
+            -master_slave "" \
+            -mod_data "" \
+            -v_name "" \
+            -exept_pl "" \
+        {*}$args]
+
+        set optl {
+            -ip
+            -name
+            -display_name
+            -description
+            -bus_type
+            -abstraction_ref
+            -master_slave
+        }
+        set argl {}
+        foreach op $optl {
+            set argl [list {*}$argl $op $opt($op)]
+        }
+ 
+        set mod_data $opt(-mod_data)
+        set v_name $opt(-v_name)
+        set exept_pl $opt(-exept_pl)
+
+        set ports [dict get $mod_data portlist]
+
+        set portmap {}
+        foreach line $ports {
+            set regx [list ${v_name}_.+]
+            set pname [dict get $line name]
+            if {[lsearch $exept_pl $pname] == -1 && [regexp $regx $pname]} {
+                set logic [string toupper [string map [list ${v_name}_ ""] $pname]]
+                set portmap [list {*}$portmap [list $pname $logic]]
+            }
+        }
+        # puts $portmap
+        return [ipl::addif {*}$argl -portmap $portmap]
     }
 
 # create an ignore ports or ignore interface option
@@ -829,8 +868,8 @@ namespace eval ipl {
         # ipl::xmlgen $ip
         set bustype {library="AMBA4" name="AXI4-Lite" vendor="amba.com" version="r0p0"}
         set abstref {library="AMBA4" name="AXI4-Lite_rtl" vendor="amba.com" version="r0p0"}
-        set ip [ipl::addif -ip $ip -mod_data $mod_data -name s_axi -if_name s_axi \
-            -exep_ports [list s_axi_aclk s_axi_aresetn] \
+        set ip [ipl::addifa -ip $ip -mod_data $mod_data -name s_axi -v_name s_axi \
+            -exept_pl [list s_axi_aclk s_axi_aresetn] \
             -display_name s_axi \
             -description s_axi \
             -bus_type $bustype \
@@ -838,22 +877,22 @@ namespace eval ipl {
             -master_slave slave]
         set bustype {library="AMBA4" name="AXI4" vendor="amba.com" version="r0p0"}
         set abstref {library="AMBA4" name="AXI4_rtl" vendor="amba.com" version="r0p0"}
-        set ip [ipl::addif -ip $ip -mod_data $mod_data -name m_dest_axi -if_name m_dest_axi \
-            -exep_ports [list m_dest_axi_aclk m_dest_axi_aresetn] \
+        set ip [ipl::addifa -ip $ip -mod_data $mod_data -name m_dest_axi -v_name m_dest_axi \
+            -exept_pl [list m_dest_axi_aclk m_dest_axi_aresetn] \
             -display_name m_dest_axi \
             -description m_dest_axi \
             -bus_type $bustype \
             -abstraction_ref $abstref \
             -master_slave master]
-        set ip [ipl::addif -ip $ip -mod_data $mod_data -name m_src_axi -if_name m_src_axi \
-            -exep_ports [list m_src_axi_aclk m_src_axi_aresetn] \
+        set ip [ipl::addifa -ip $ip -mod_data $mod_data -name m_src_axi -v_name m_src_axi \
+            -exept_pl [list m_src_axi_aclk m_src_axi_aresetn] \
             -display_name m_src_axi \
             -description m_src_axi \
             -bus_type $bustype \
             -abstraction_ref $abstref \
             -master_slave master]
-        set ip [ipl::addif -ip $ip -mod_data $mod_data -name m_sg_axi -if_name m_sg_axi \
-            -exep_ports [list m_sg_axi_aclk m_sg_axi_aresetn] \
+        set ip [ipl::addifa -ip $ip -mod_data $mod_data -name m_sg_axi -v_name m_sg_axi \
+            -exept_pl [list m_sg_axi_aclk m_sg_axi_aresetn] \
             -display_name m_sg_axi \
             -description m_sg_axi \
             -bus_type $bustype \
