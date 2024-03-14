7d4abcc0e3fc363ec593ac8e1770ddc603a97e08
Cristian Mihai Popa <cristianmihai.popa@analog.com>
2024-03-14 14:14:11 +0200

    projects/ad9747: added base design tcl script
    Signed-off-by: Cristian Mihai Popa <cristianmihai.popa@analog.com>

---
+++ projects/ad9747_ebz/common/ad9747_ebz_bd.tcl
@@ -0,0 +1,9 @@
+###############################################################################
+## Copyright (C) 2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+# dac interface
+
+
+# dac peripherals
