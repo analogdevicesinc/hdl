c7876eb0bb0197833d2478ec8e1656c39cd4a72f
AndreiGrozav <andrei.grozav@analog.com>
2024-03-26 16:45:51 +0200

    ad485x family support
    Rename files and modules from axi_ad4858 to axi_ad485x
    Updates to support:
    ad4851
    ad4852
    ad4853
    ad4854
    ad4855
    ad4856
    ad4857
    ad4858
    
    Signed-off-by: AndreiGrozav <andrei.grozav@analog.com>

--- library/axi_ad485x/axi_ad485x_lvds.v
+++ library/axi_ad485x/axi_ad485x_lvds.v
@@ -1,6 +1,6 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright (C) 2023 Analog Devices, Inc. All rights reserved.
+// Copyright (C) 2023-2024 Analog Devices, Inc. All rights reserved.
 //
 // In this HDL repository, there are many different and unique modules, consisting
 // of various HDL (Verilog or VHDL) components. The individual modules are
@@ -35,12 +35,13 @@
 
 `timescale 1ns/100ps
 
-module axi_ad4858_lvds #(
+module axi_ad485x_lvds #(
 
   parameter FPGA_TECHNOLOGY = 0,
+  parameter RESOLUTION = 20,
   parameter DELAY_REFCLK_FREQ = 200,
   parameter IODELAY_ENABLE = 1,
-  parameter NEG_EDGE = 1
+  parameter N_CHANNELS = 8
 ) (
 
   input                   rst,
@@ -83,8 +84,15 @@ module axi_ad4858_lvds #(
   output                  delay_locked
 );
 
-  localparam  DW = 32;
-  localparam  BW = DW - 1;
+  localparam DW  = (32 * N_CHANNELS);
+  localparam HDW = (DW / 2)-1;
+  localparam BW  = DW - 1;
+  localparam FBW = DW + 31;
+  localparam HBW = ((DW + 32) / 2)-1;
+
+  localparam [5:0] PACKET_1 = RESOLUTION == 16 ? 16 : 20;
+  localparam [5:0] PACKET_2 = RESOLUTION == 16 ? 24 : 24;
+  localparam [5:0] PACKET_3 = RESOLUTION == 16 ? 24 : 32;
 
   // internal registers
 
@@ -92,8 +100,8 @@ module axi_ad4858_lvds #(
   reg        [  5:0]  packet_cnt_length;
   reg        [ 15:0]  crc_data_length;
 
-  reg        [143:0]  rx_data_pos;
-  reg        [143:0]  rx_data_neg;
+  reg        [HBW:0]  rx_data_pos;
+  reg        [HBW:0]  rx_data_neg;
 
   reg        [  5:0]  data_counter = 'h0;
   reg        [  3:0]  ch_counter = 'h0;
@@ -109,8 +117,8 @@ module axi_ad4858_lvds #(
   reg        [ 31:0]  busy_measure_value;
   reg        [ 31:0]  busy_measure_value_plus;
 
-  reg        [287:0]  adc_data_store;
-  reg        [287:0]  adc_data_init;
+  reg        [FBW:0]  adc_data_store;
+  reg        [FBW:0]  adc_data_init;
   reg                 aquire_data;
   reg                 capture_complete_init;
   reg                 capture_complete_d;
@@ -122,14 +130,14 @@ module axi_ad4858_lvds #(
   reg                 adc_valid_init_d;
   reg                 adc_valid_init_d2;
 
-  reg        [255:0]  adc_data_0;
-  reg        [255:0]  adc_data_1;
-  reg        [255:0]  adc_data_2;
+  reg        [ BW:0]  adc_data_0;
+  reg        [ BW:0]  adc_data_1;
+  reg        [ BW:0]  adc_data_2;
   reg                 crc_enable_window;
   reg                 run_crc;
   reg                 run_crc_d;
-  reg        [287:0]  crc_data_in;
-  reg        [287:0]  crc_data_in_sh;
+  reg        [FBW:0]  crc_data_in;
+  reg        [FBW:0]  crc_data_in_sh;
   reg        [ 15:0]  crc_cnt;
   reg        [  7:0]  data_in_byte;
 
@@ -173,17 +181,17 @@ module axi_ad4858_lvds #(
   wire       [  8:0]  ch_index_24_pack [8:0];
   wire       [  8:0]  ch_index_32_pack [8:0];
 
-  wire       [143:0]  rx_data_pos_s;
-  wire       [143:0]  rx_data_neg_s;
+  wire       [HBW:0]  rx_data_pos_s;
+  wire       [HBW:0]  rx_data_neg_s;
 
   // assgments
 
   genvar j;
   generate
-    for (j = 0; j <= 8; j = j + 1) begin
-      assign ch_index_20_pack [j] = j * 20;
-      assign ch_index_24_pack [j] = j * 24;
-      assign ch_index_32_pack [j] = j * 32;
+    for (j = 0; j <= N_CHANNELS; j = j + 1) begin
+      assign ch_index_20_pack [j] = j * PACKET_1;
+      assign ch_index_24_pack [j] = j * PACKET_2;
+      assign ch_index_32_pack [j] = j * PACKET_3;
     end
   endgenerate
 
@@ -197,9 +205,9 @@ module axi_ad4858_lvds #(
   always @(posedge clk) begin
     packet_format <= packet_format_in;
     if (start_transfer) begin
-      packet_cnt_length <= packet_format == 2'd0 ? 6'd20 - 6'd4 :
-                           packet_format == 2'd1 ? 6'd24 - 6'd4 : 6'd32 - 6'd4;
-      max_channel_transfer <= adc_crc_enable ? 8 : 7;
+      packet_cnt_length <= packet_format == 2'd0 ? PACKET_1 - 6'd4 :
+                           packet_format == 2'd1 ? PACKET_2 - 6'd4 : PACKET_3 - 6'd4;
+      max_channel_transfer <= adc_crc_enable ? N_CHANNELS : N_CHANNELS - 1;
       crc_enable_window <= adc_crc_enable;
     end
   end
@@ -321,18 +329,18 @@ module axi_ad4858_lvds #(
     .delay_locked (delay_locked));
 
   always @(posedge scko) begin
-    rx_data_pos <= {rx_data_pos[142:0], sdo_p_s};
+    rx_data_pos <= {rx_data_pos[HBW-1:0], sdo_p_s};
   end
   always @(negedge scko) begin
-    rx_data_neg <= {rx_data_neg[142:0], sdo_n_s};
+    rx_data_neg <= {rx_data_neg[HBW-1:0], sdo_n_s};
   end
 
-  assign rx_data_pos_s = {rx_data_pos[142:0], sdo_p_s};
-  assign rx_data_neg_s = {rx_data_neg[142:0], sdo_n_s};
+  assign rx_data_pos_s = {rx_data_pos[HBW-1:0], sdo_p_s};
+  assign rx_data_neg_s = {rx_data_neg[HBW-1:0], sdo_n_s};
 
   genvar i;
   generate
-    for (i = 0; i <= 288 - 2; i = i + 2) begin
+    for (i = 0; i <= FBW - 1; i = i + 2) begin
       always @(posedge clk) begin
         if (capture_complete) begin
           adc_data_init[i+:2] <= {rx_data_pos_s[i>>1], rx_data_neg_s[i>>1]};
@@ -349,62 +357,94 @@ module axi_ad4858_lvds #(
     adc_data_store <= adc_data_init; // relax timing
   end
 
-  always @(posedge clk) begin
-    if (start_transfer_d) begin
-      ch_7_index <= crc_enable_window ? 1 : 0;
-      ch_6_index <= crc_enable_window ? 2 : 1;
-      ch_5_index <= crc_enable_window ? 3 : 2;
-      ch_4_index <= crc_enable_window ? 4 : 3;
-      ch_3_index <= crc_enable_window ? 5 : 4;
-      ch_2_index <= crc_enable_window ? 6 : 5;
-      ch_1_index <= crc_enable_window ? 7 : 6;
-      ch_0_index <= crc_enable_window ? 8 : 7;
+  generate
+    if (N_CHANNELS == 8) begin
+
+      always @(posedge clk) begin
+        if (start_transfer_d) begin
+          ch_7_index <= crc_enable_window ? 1 : 0;
+          ch_6_index <= crc_enable_window ? 2 : 1;
+          ch_5_index <= crc_enable_window ? 3 : 2;
+          ch_4_index <= crc_enable_window ? 4 : 3;
+          ch_3_index <= crc_enable_window ? 5 : 4;
+          ch_2_index <= crc_enable_window ? 6 : 5;
+          ch_1_index <= crc_enable_window ? 7 : 6;
+          ch_0_index <= crc_enable_window ? 8 : 7;
+        end
+      end
+
+      always @(posedge clk) begin
+        ch_7_base <= packet_format == 0 ? ch_index_20_pack[ch_7_index] :
+                     packet_format == 1 ? ch_index_24_pack[ch_7_index] : ch_index_32_pack[ch_7_index];
+        ch_6_base <= packet_format == 0 ? ch_index_20_pack[ch_6_index] :
+                     packet_format == 1 ? ch_index_24_pack[ch_6_index] : ch_index_32_pack[ch_6_index];
+        ch_5_base <= packet_format == 0 ? ch_index_20_pack[ch_5_index] :
+                     packet_format == 1 ? ch_index_24_pack[ch_5_index] : ch_index_32_pack[ch_5_index];
+        ch_4_base <= packet_format == 0 ? ch_index_20_pack[ch_4_index] :
+                     packet_format == 1 ? ch_index_24_pack[ch_4_index] : ch_index_32_pack[ch_4_index];
+
+        if (RESOLUTION == 16) begin
+          adc_data_0[255:128] <= {16'b0,adc_data_store[ch_7_base+:16],
+                                  16'b0,adc_data_store[ch_6_base+:16],
+                                  16'b0,adc_data_store[ch_5_base+:16],
+                                  16'b0,adc_data_store[ch_4_base+:16]};
+        end else begin
+          adc_data_0[255:128] <= {12'b0,adc_data_store[ch_7_base+:20],
+                                  12'b0,adc_data_store[ch_6_base+:20],
+                                  12'b0,adc_data_store[ch_5_base+:20],
+                                  12'b0,adc_data_store[ch_4_base+:20]};
+        end
+        adc_data_1[255:128] <= {8'b0,adc_data_store[ch_7_base+:24],
+                                8'b0,adc_data_store[ch_6_base+:24],
+                                8'b0,adc_data_store[ch_5_base+:24],
+                                8'b0,adc_data_store[ch_4_base+:24]};
+        adc_data_2[255:128] <= {adc_data_store[ch_7_base+:32],
+                                adc_data_store[ch_6_base+:32],
+                                adc_data_store[ch_5_base+:32],
+                                adc_data_store[ch_4_base+:32]};
+      end
+    end else begin
+      always @(posedge clk) begin
+        if (start_transfer_d) begin
+          ch_3_index <= crc_enable_window ? 1 : 0;
+          ch_2_index <= crc_enable_window ? 2 : 1;
+          ch_1_index <= crc_enable_window ? 3 : 2;
+          ch_0_index <= crc_enable_window ? 4 : 3;
+        end
+      end
     end
 
-    ch_7_base <= packet_format == 0 ? ch_index_20_pack[ch_7_index] :
-                 packet_format == 1 ? ch_index_24_pack[ch_7_index] : ch_index_32_pack[ch_7_index];
-    ch_6_base <= packet_format == 0 ? ch_index_20_pack[ch_6_index] :
-                 packet_format == 1 ? ch_index_24_pack[ch_6_index] : ch_index_32_pack[ch_6_index];
-    ch_5_base <= packet_format == 0 ? ch_index_20_pack[ch_5_index] :
-                 packet_format == 1 ? ch_index_24_pack[ch_5_index] : ch_index_32_pack[ch_5_index];
-    ch_4_base <= packet_format == 0 ? ch_index_20_pack[ch_4_index] :
-                 packet_format == 1 ? ch_index_24_pack[ch_4_index] : ch_index_32_pack[ch_4_index];
-    ch_3_base <= packet_format == 0 ? ch_index_20_pack[ch_3_index] :
-                 packet_format == 1 ? ch_index_24_pack[ch_3_index] : ch_index_32_pack[ch_3_index];
-    ch_2_base <= packet_format == 0 ? ch_index_20_pack[ch_2_index] :
-                 packet_format == 1 ? ch_index_24_pack[ch_2_index] : ch_index_32_pack[ch_2_index];
-    ch_1_base <= packet_format == 0 ? ch_index_20_pack[ch_1_index] :
-                 packet_format == 1 ? ch_index_24_pack[ch_1_index] : ch_index_32_pack[ch_1_index];
-    ch_0_base <= packet_format == 0 ? ch_index_20_pack[ch_0_index] :
-                 packet_format == 1 ? ch_index_24_pack[ch_0_index] : ch_index_32_pack[ch_0_index];
-  end
+    always @(posedge clk) begin
+      ch_3_base <= packet_format == 0 ? ch_index_20_pack[ch_3_index] :
+                   packet_format == 1 ? ch_index_24_pack[ch_3_index] : ch_index_32_pack[ch_3_index];
+      ch_2_base <= packet_format == 0 ? ch_index_20_pack[ch_2_index] :
+                   packet_format == 1 ? ch_index_24_pack[ch_2_index] : ch_index_32_pack[ch_2_index];
+      ch_1_base <= packet_format == 0 ? ch_index_20_pack[ch_1_index] :
+                   packet_format == 1 ? ch_index_24_pack[ch_1_index] : ch_index_32_pack[ch_1_index];
+      ch_0_base <= packet_format == 0 ? ch_index_20_pack[ch_0_index] :
+                   packet_format == 1 ? ch_index_24_pack[ch_0_index] : ch_index_32_pack[ch_0_index];
 
-  always @(posedge clk) begin
-    adc_data_0 <={12'b0,adc_data_store[ch_7_base+:20],
-                  12'b0,adc_data_store[ch_6_base+:20],
-                  12'b0,adc_data_store[ch_5_base+:20],
-                  12'b0,adc_data_store[ch_4_base+:20],
-                  12'b0,adc_data_store[ch_3_base+:20],
-                  12'b0,adc_data_store[ch_2_base+:20],
-                  12'b0,adc_data_store[ch_1_base+:20],
-                  12'b0,adc_data_store[ch_0_base+:20]};
-    adc_data_1 <={8'b0,adc_data_store[ch_7_base+:24],
-                  8'b0,adc_data_store[ch_6_base+:24],
-                  8'b0,adc_data_store[ch_5_base+:24],
-                  8'b0,adc_data_store[ch_4_base+:24],
-                  8'b0,adc_data_store[ch_3_base+:24],
-                  8'b0,adc_data_store[ch_2_base+:24],
-                  8'b0,adc_data_store[ch_1_base+:24],
-                  8'b0,adc_data_store[ch_0_base+:24]};
-    adc_data_2 <={adc_data_store[ch_7_base+:32],
-                  adc_data_store[ch_6_base+:32],
-                  adc_data_store[ch_5_base+:32],
-                  adc_data_store[ch_4_base+:32],
-                  adc_data_store[ch_3_base+:32],
-                  adc_data_store[ch_2_base+:32],
-                  adc_data_store[ch_1_base+:32],
-                  adc_data_store[ch_0_base+:32]};
-  end
+      if (RESOLUTION == 16) begin
+        adc_data_0[127:0] <= {16'b0,adc_data_store[ch_3_base+:16],
+                              16'b0,adc_data_store[ch_2_base+:16],
+                              16'b0,adc_data_store[ch_1_base+:16],
+                              16'b0,adc_data_store[ch_0_base+:16]};
+      end else if (RESOLUTION == 20) begin
+        adc_data_0[127:0] <= {12'b0,adc_data_store[ch_3_base+:20],
+                              12'b0,adc_data_store[ch_2_base+:20],
+                              12'b0,adc_data_store[ch_1_base+:20],
+                              12'b0,adc_data_store[ch_0_base+:20]};
+      end
+      adc_data_1[127:0] <= {8'b0,adc_data_store[ch_3_base+:24],
+                            8'b0,adc_data_store[ch_2_base+:24],
+                            8'b0,adc_data_store[ch_1_base+:24],
+                            8'b0,adc_data_store[ch_0_base+:24]};
+      adc_data_2[127:0] <= {adc_data_store[ch_3_base+:32],
+                            adc_data_store[ch_2_base+:32],
+                            adc_data_store[ch_1_base+:32],
+                            adc_data_store[ch_0_base+:32]};
+    end
+  endgenerate
 
   always @(posedge clk) begin
     if (crc_enable_window == 1'b1) begin
@@ -439,12 +479,30 @@ module axi_ad4858_lvds #(
 
   always @(posedge clk) begin
     if (adc_valid_init_d == 1) begin
-      if (packet_format == 0) begin
-        crc_data_in <= {adc_data_store[179:0], 108'd0};
-      end else if (packet_format == 1) begin
-        crc_data_in <= {adc_data_store[215:0], 72'd0};
-      end else  begin
-        crc_data_in <= {adc_data_store};
+      if (N_CHANNELS == 8) begin
+        if (packet_format == 0) begin
+          if (RESOLUTION == 16) begin
+            crc_data_in <= {adc_data_store[143:0], 144'd0};
+          end else if (RESOLUTION == 20) begin
+            crc_data_in <= {adc_data_store[179:0], 108'd0};
+          end
+        end else if (packet_format == 1) begin
+          crc_data_in <= {adc_data_store[215:0], 72'd0};
+        end else  begin
+          crc_data_in <= {adc_data_store};
+        end
+      end else begin // quad channel
+        if (packet_format == 0) begin
+          if (RESOLUTION == 16) begin
+            crc_data_in <= {adc_data_store[79:0], 80'd0};
+          end else if (RESOLUTION == 20) begin
+            crc_data_in <= {4'd0, adc_data_store[99:0], 56'd0};
+          end
+        end else if (packet_format == 1) begin
+          crc_data_in <= {adc_data_store[119:0], 40'd0};
+        end else  begin
+          crc_data_in <= {adc_data_store};
+        end
       end
     end
   end
@@ -457,8 +515,21 @@ module axi_ad4858_lvds #(
   // multiple of 8. So, we will feed 184 bits, which is a multiple of 8.
   // Last extra 4 bits entering the checker being 0, will not affect the result
   always @(posedge clk) begin
-    crc_data_length <= packet_format == 2'd0 ? 16'd176 : // 184-8
-                       packet_format == 2'd1 ? 16'd208 : 16'd280;
+    if (N_CHANNELS == 8) begin
+      if (RESOLUTION == 16) begin
+        crc_data_length <= packet_format == 2'd0 ? 16'd144 : 16'd208;
+      end else if (RESOLUTION == 20) begin
+        crc_data_length <= packet_format == 2'd0 ? 16'd176 : // 184-8
+                           packet_format == 2'd1 ? 16'd208 : 16'd280;
+      end
+    end else begin // quad channel
+      if (RESOLUTION == 16) begin
+        crc_data_length <= packet_format == 2'd0 ? 16'd80 : 16'd120;
+      end else if (RESOLUTION == 20) begin
+        crc_data_length <= packet_format == 2'd0 ? 16'd104 :
+                           packet_format == 2'd1 ? 16'd120 : 16'd160;
+      end
+    end
   end
 
   always @(posedge clk) begin
@@ -488,7 +559,7 @@ module axi_ad4858_lvds #(
 
   always @(posedge clk) begin
     if (run_crc == 1'b1) begin
-      data_in_byte <= crc_data_in_sh[287:280];
+      data_in_byte <= crc_data_in_sh[FBW:FBW-7];
     end else begin
       data_in_byte <= 8'd0;
     end
@@ -507,7 +578,7 @@ module axi_ad4858_lvds #(
     end
   end
 
-  axi_ad4858_crc i_ad4858_crc_8 (
+  axi_ad485x_crc i_ad485x_crc_8 (
     .rst (crc_reset),
     .clk (clk),
     .crc_en (crc_enable),
