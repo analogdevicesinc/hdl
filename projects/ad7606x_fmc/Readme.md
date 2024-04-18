da0c422690f8ef4eccf6c26fe7ef53214fe53b9f
Ioan-daniel Pop <Pop.Ioan-daniel@analog.com>
2024-04-18 08:51:17 +0300

    V12: Ad7606 serial/parallel interface
    Fix whitespaces. Update readme.
    
    Signed-off-by: Ioan-daniel Pop <Pop.Ioan-daniel@analog.com>

--- projects/ad7606x_fmc/Readme.md
+++ projects/ad7606x_fmc/Readme.md
@@ -24,3 +24,16 @@ EXT_CLK - Defines the external clock option for the ADC clock: 0 - No, 1 - Yes.
 
 For the serial interface, the following parameters will be used in make command: DEV_CONFIG, INTF, NUM_OF_SDI.
 For the parallel interface, the following parameters will be used in make command: DEV_CONFIG, INTF,EXT_CLK.
+
+**Example:**
+
+make DEV_CONFIG=0 INTF=1 NUM_OF_SDI=1
+make DEV_CONFIG=1 INTF=1 NUM_OF_SDI=2
+make DEV_CONFIG=2 INTF=1 NUM_OF_SDI=1
+make DEV_CONFIG=2 INTF=1 NUM_OF_SDI=2
+make DEV_CONFIG=2 INTF=1 NUM_OF_SDI=4
+make DEV_CONFIG=2 INTF=1 NUM_OF_SDI=8
+make DEV_CONFIG=0 INTF=0 EXT_CLK=0
+make DEV_CONFIG=1 INTF=0 EXT_CLK=0
+make DEV_CONFIG=2 INTF=0 EXT_CLK=0
+...
--- projects/ad7606x_fmc/common/ad7606x_bd.tcl
+++ projects/ad7606x_fmc/common/ad7606x_bd.tcl
@@ -83,35 +83,35 @@ switch $INTF {
       ad_ip_parameter adc_clk_generator CONFIG.CLKIN1_UI_JITTER 0
       ad_ip_parameter adc_clk_generator CONFIG.PRIM_IN_FREQ 100.000
 
-      ad_connect  sys_cpu_clk adc_clk_generator/clk_in1
-      ad_connect  sys_cpu_resetn adc_clk_generator/resetn
-      ad_connect  adc_clk_generator/clk_out1 axi_ad7606x/external_clk
+      ad_connect sys_cpu_clk adc_clk_generator/clk_in1
+      ad_connect sys_cpu_resetn adc_clk_generator/resetn
+      ad_connect adc_clk_generator/clk_out1 axi_ad7606x/external_clk
     } else {
-      ad_connect  sys_cpu_clk axi_ad7606x/external_clk
+      ad_connect sys_cpu_clk axi_ad7606x/external_clk
     }
 
     # interface connections
-    ad_connect  rx_db_o axi_ad7606x/rx_db_o
-    ad_connect  rx_db_i axi_ad7606x/rx_db_i
-    ad_connect  rx_db_t axi_ad7606x/rx_db_t
-    ad_connect  rx_rd_n axi_ad7606x/rx_rd_n
-    ad_connect  rx_wr_n axi_ad7606x/rx_wr_n
+    ad_connect rx_db_o axi_ad7606x/rx_db_o
+    ad_connect rx_db_i axi_ad7606x/rx_db_i
+    ad_connect rx_db_t axi_ad7606x/rx_db_t
+    ad_connect rx_rd_n axi_ad7606x/rx_rd_n
+    ad_connect rx_wr_n axi_ad7606x/rx_wr_n
     
-    ad_connect  rx_cs_n axi_ad7606x/rx_cs_n
-    ad_connect  rx_cnvst_n ad7606_pwm_gen/pwm_0
-    ad_connect  rx_busy axi_ad7606x/rx_busy
-    ad_connect  rx_first_data axi_ad7606x/first_data
+    ad_connect rx_cs_n axi_ad7606x/rx_cs_n
+    ad_connect rx_cnvst_n ad7606_pwm_gen/pwm_0
+    ad_connect rx_busy axi_ad7606x/rx_busy
+    ad_connect rx_first_data axi_ad7606x/first_data
 
-    ad_connect  sys_cpu_clk axi_ad7606x_dma/s_axi_aclk
-    ad_connect  sys_cpu_clk ad7606_pwm_gen/s_axi_aclk
-    ad_connect  sys_cpu_resetn ad7606_pwm_gen/s_axi_aresetn
+    ad_connect sys_cpu_clk axi_ad7606x_dma/s_axi_aclk
+    ad_connect sys_cpu_clk ad7606_pwm_gen/s_axi_aclk
+    ad_connect sys_cpu_resetn ad7606_pwm_gen/s_axi_aresetn
 
-    ad_connect  axi_ad7606x/adc_clk ad7606x_adc_pack/clk
-    ad_connect  axi_ad7606x/adc_clk axi_ad7606x_dma/fifo_wr_clk
-    ad_connect  axi_ad7606x/adc_reset ad7606x_adc_pack/reset
-    ad_connect  axi_ad7606x/adc_valid ad7606x_adc_pack/fifo_wr_en
-    ad_connect  ad7606x_adc_pack/packed_fifo_wr axi_ad7606x_dma/fifo_wr
-    ad_connect  ad7606x_adc_pack/fifo_wr_overflow axi_ad7606x/adc_dovf
+    ad_connect axi_ad7606x/adc_clk ad7606x_adc_pack/clk
+    ad_connect axi_ad7606x/adc_clk axi_ad7606x_dma/fifo_wr_clk
+    ad_connect axi_ad7606x/adc_reset ad7606x_adc_pack/reset
+    ad_connect axi_ad7606x/adc_valid ad7606x_adc_pack/fifo_wr_en
+    ad_connect ad7606x_adc_pack/packed_fifo_wr axi_ad7606x_dma/fifo_wr
+    ad_connect ad7606x_adc_pack/fifo_wr_overflow axi_ad7606x/adc_dovf
 
     for {set i 0} {$i < 8} {incr i} {
       ad_connect axi_ad7606x/adc_data_$i ad7606x_adc_pack/fifo_wr_data_$i
@@ -119,9 +119,9 @@ switch $INTF {
     }
 
     # interconnect
-    ad_cpu_interconnect  0x44a00000 axi_ad7606x
-    ad_cpu_interconnect  0x44a30000 axi_ad7606x_dma
-    ad_cpu_interconnect  0x44b00000 ad7606_pwm_gen
+    ad_cpu_interconnect 0x44a00000 axi_ad7606x
+    ad_cpu_interconnect 0x44a30000 axi_ad7606x_dma
+    ad_cpu_interconnect 0x44b00000 ad7606_pwm_gen
 
     # memory interconnect
     ad_mem_hp1_interconnect sys_cpu_clk sys_ps7/S_AXI_HP1
