e9fb752e599394fa53c8f58fb94dc0022000e285
Villyam <Vilmoscsaba.Jozsa@analog.com>
2024-04-19 14:20:33 +0300

    projects/common/lfcpnx: Adding base design files for Lattice LFCPNX-EVN board.
    * system_project_pb.tcl
        - Used to build Propel Builder project (block design).
        - Linked in project-lattice.mk, run by propelbld | propelbldwrap.
        - There are use case options documented in comments in this file.
    
    * system_project.tcl
        - Used to build Radiant project.
        - Linked in project-lattice.mk, run by pnmainc | radiantc.
        - There are use case options documented in comments in this file.
    
    * system_pb.tcl
        - Linker script for the project, sourced in adi_project_pb procedure that is
        called in system_project_pb.tcl and it is defined in adi_project_lattice_pb.tcl.
        - Used to link lfcpnx_system_pb.tcl base design.
        in this specific case.
    
    * Base design periferals are common for both designs:
        - gpio
        - spi
        - i2c
        - uart
    
    * lfcpnx_system_pb.tcl
        - Base design with riscv-rx softcore cpu option that has AXI interface.
        - The uart is built into riscv-rx cpu.
        Note: - the design in some cases wasn't working with the EBR (Block Ram) tcm
        memory type. I changed the tcm IP memory type to Large Ram (LRAM) and now
        works. I think there is some bug somewhere in tcm memory module generation,
        but it builds without error in both cases.
    
    Note:
        - I added an external timer IP to the design which can be enabled by setting
        the timer_en to 1 in the system_pb.tcl.
        - The Propel Builder tool currently has compatibility issues in WSL. The IP
        configuration does not work, nor in GUI neither from script.
    
    * lfcpnx_system_constr.pdc
        - Constraint file for the design.
    
    * system_top.v
        - Default wrapper for the design.
    
    * Makefile
        - Sets the dependencies for the project.
    
    Signed-off-by: Villyam <Vilmoscsaba.Jozsa@analog.com>

---
+++ projects/common/lfcpnx/lfcpnx_system_pb.tcl
@@ -0,0 +1,317 @@
+###############################################################################
+## Copyright (C) 2023-2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+########### This file contains only the tcl commands for importing lib ########
+########### components and the assembly of the design. ########################
+
+set preinst_ip_mod_dir ${env(TOOLRTF)}
+set conf_dir $ad_hdl_dir/projects/common/lfcpnx/ipcfg
+# If you want to use file path for the adi_ip_instance or adi_ip_update, use the
+# -ip_path option and the file path to the ip. The built in modules and IPs can
+# be found in the $preinst_ip_mod_dir, the downloaded IPs can be found somewhere
+# in the <path_to>/PropelIPLocal directory.
+# You can also use the exact text from the <instance_name>.cfg file from an
+# already instatiated IP folder from the
+# <project_name>/<project_name>/<.lib|lib>/<module|ip>/<instance_name><version>
+# folder to configure an IP. For that you have to use the -ip_params option.
+# set ip_download_path ${env(USERPROFILE)}/PropelIPLocal # by default on windows
+
+## configure ip components and add to design. #################################
+adi_ip_instance -vlnv {latticesemi.com:ip:cpu0:2.3.0} \
+  -meta_vlnv {latticesemi.com:ip:riscv_rtos:2.3.0} \
+  -cfg_value {
+    TCM_ENABLE: true,
+    IRQ_NUM: 16,
+    UART_EN: true
+  } \
+  -ip_iname "cpu0_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:gpio0:1.6.2} \
+  -meta_vlnv {latticesemi.com:ip:gpio:1.6.2} \
+  -cfg_value {
+    IO_LINES_COUNT: 32,
+    EXTERNAL_BUF: true,
+    DIRECTION_DEF_VAL_INPUT: 00FFFFFF
+  } \
+  -ip_iname "gpio0_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:gpio1:1.6.2} \
+  -meta_vlnv {latticesemi.com:ip:gpio:1.6.2} \
+  -cfg_value {
+    IO_LINES_COUNT: 32,
+    EXTERNAL_BUF: true,
+    DIRECTION_DEF_VAL_INPUT: 00000000
+  } \
+  -ip_iname "gpio1_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:spi0:2.1.0} \
+  -meta_vlnv {latticesemi.com:ip:spi_controller:2.1.0} \
+  -cfg_value {
+    DATA_WIDTH: 8,
+    FIFO_DEPTH: 256,
+    SYS_CLOCK_FREQ: 100
+  } \
+  -ip_iname "spi0_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:i2c0:2.0.1} \
+  -meta_vlnv {latticesemi.com:ip:i2c_controller:2.0.1} \
+  -cfg_value {
+    SYS_CLOCK_FREQ: 100
+  } \
+  -ip_iname "i2c0_inst"
+adi_ip_instance -vlnv {latticesemi.com:module:pll0:1.8.0} \
+  -meta_vlnv {latticesemi.com:module:pll:1.8.0} \
+  -cfg_value {
+    gui_refclk_freq: 125.0,
+    gui_clk_os_en: true,
+    gui_clk_os_byp: false,
+    gui_clk_os_freq: 10.0
+  } \
+  -ip_iname "pll0_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:axi_interc0:1.2.2} \
+  -meta_vlnv {latticesemi.com:ip:axi_interconnect:1.2.2} \
+  -cfg_value {
+    TOTAL_EXTMAS_CNT: 1,
+    TOTAL_EXTSLV_CNT: 4,
+    EXT_MAS_AXI_ID_WIDTH: 1,
+    EXT_SLV_AXI_ID_WIDTH: 8
+  } \
+  -ip_iname "axi_interc0_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:axi_ahb0:1.1.1} \
+  -meta_vlnv {latticesemi.com:ip:axi2ahb_bridge:1.1.1} \
+  -cfg_value {} \
+  -ip_iname "axi_ahb0_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:axi_apb0:1.1.1} \
+  -meta_vlnv {latticesemi.com:ip:axi2apb_bridge:1.1.1} \
+  -cfg_value {} \
+  -ip_iname "axi_apb0_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:axi_apb1:1.1.1} \
+  -meta_vlnv {latticesemi.com:ip:axi2apb_bridge:1.1.1} \
+  -cfg_value {} \
+  -ip_iname "axi_apb1_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:axi_apb2:1.1.1} \
+  -meta_vlnv {latticesemi.com:ip:axi2apb_bridge:1.1.1} \
+  -cfg_value {} \
+  -ip_iname "axi_apb2_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:sysmem0:2.1.0} \
+  -meta_vlnv {latticesemi.com:ip:system_memory:2.1.0} \
+  -cfg_value {
+    INTERFACE: AXI4,
+    ADDR_DEPTH: 8192,
+    ID_WIDTH: 1,
+    REGMODE_S0: true
+  } \
+  -ip_iname "sysmem0_inst"
+adi_ip_instance -vlnv {latticesemi.com:ip:tcm0:1.3.9} \
+  -meta_vlnv {latticesemi.com:ip:localbus_tcm:1.3.9} \
+  -cfg_value {
+    PORT_COUNT: 2,
+    ADDR_DEPTH_A: 16384,
+    ADDR_DEPTH_B: 16384,
+    MEM_TYPE: LRAM
+  } \
+  -ip_iname "tcm0_inst"
+
+sbp_add_gluelogic -name equation_module_inst \
+  -logicinfo [sbp_create_glue_logic equation equation_module {} { {
+    "expr": "A & B",
+    "module_name": "equation_module0"
+  }}]
+
+sbp_add_port -direction in clk_125
+sbp_add_port -direction in rstn_i
+sbp_add_port -direction in rxd_i
+sbp_add_port -direction out txd_o
+
+sbp_add_port -from 31 -to 0 -direction out gpio0_o
+sbp_add_port -from 31 -to 0 -direction in gpio0_i
+sbp_add_port -from 31 -to 0 -direction out gpio0_en_o
+
+sbp_add_port -from 31 -to 0 -direction out gpio1_o
+sbp_add_port -from 31 -to 0 -direction in gpio1_i
+sbp_add_port -from 31 -to 0 -direction out gpio1_en_o
+
+sbp_add_port -direction out mosi_o
+sbp_add_port -direction out sclk_o
+sbp_add_port -direction in  miso_i
+sbp_add_port -from 0 -to 0 -direction out ssn_o
+
+sbp_add_port -direction inout scl_io
+sbp_add_port -direction inout sda_io
+
+sbp_connect_net "$project_name/equation_module_inst/A" \
+  "$project_name/pll0_inst/lock_o"
+sbp_connect_net "$project_name/equation_module_inst/B" \
+  "$project_name/pll0_inst/rstn_i" \
+  "$project_name/rstn_i"
+sbp_connect_net "$project_name/equation_module_inst/O" \
+  "$project_name/cpu0_inst/rstn_i"
+
+sbp_connect_net "$project_name/cpu0_inst/system_resetn_o" \
+  "$project_name/sysmem0_inst/axi_resetn_i" \
+  "$project_name/tcm0_inst/sys_rst_n" \
+  "$project_name/axi_interc0_inst/axi_aresetn_i" \
+  "$project_name/axi_ahb0_inst/aresetn_i" \
+  "$project_name/axi_apb0_inst/aresetn_i" \
+  "$project_name/axi_apb1_inst/aresetn_i" \
+  "$project_name/axi_apb2_inst/aresetn_i" \
+  "$project_name/spi0_inst/rst_n_i" \
+  "$project_name/i2c0_inst/rst_n_i" \
+  "$project_name/gpio0_inst/resetn_i" \
+  "$project_name/gpio1_inst/resetn_i"
+
+sbp_connect_net "$project_name/clk_125" \
+  "$project_name/pll0_inst/clki_i"
+
+sbp_connect_net "$project_name/pll0_inst/clkos_o" \
+  "$project_name/cpu0_inst/clk_realtime_i"
+
+sbp_connect_net "$project_name/pll0_inst/clkop_o" \
+  "$project_name/cpu0_inst/clk_system_i" \
+  "$project_name/sysmem0_inst/axi_aclk_i" \
+  "$project_name/axi_interc0_inst/axi_aclk_i" \
+  "$project_name/tcm0_inst/sys_clk" \
+  "$project_name/axi_ahb0_inst/aclk_i" \
+  "$project_name/axi_apb0_inst/aclk_i" \
+  "$project_name/axi_apb1_inst/aclk_i" \
+  "$project_name/axi_apb2_inst/aclk_i" \
+  "$project_name/spi0_inst/clk_i" \
+  "$project_name/i2c0_inst/clk_i" \
+  "$project_name/gpio0_inst/clk_i" \
+  "$project_name/gpio1_inst/clk_i"
+
+sbp_connect_net "$project_name/cpu0_inst/uart_rxd_i" \
+  "$project_name/rxd_i"
+sbp_connect_net "$project_name/cpu0_inst/uart_txd_o" \
+  "$project_name/txd_o"
+
+sbp_connect_net "$project_name/gpio0_inst/gpio_i" \
+  "$project_name/gpio0_i"
+sbp_connect_net "$project_name/gpio0_inst/gpio_o" \
+  "$project_name/gpio0_o"
+sbp_connect_net "$project_name/gpio0_inst/gpio_en_o" \
+  "$project_name/gpio0_en_o"
+
+sbp_connect_net "$project_name/gpio1_inst/gpio_i" \
+  "$project_name/gpio1_i"
+sbp_connect_net "$project_name/gpio1_inst/gpio_o" \
+  "$project_name/gpio1_o"
+sbp_connect_net "$project_name/gpio1_inst/gpio_en_o" \
+  "$project_name/gpio1_en_o"
+
+sbp_connect_net "$project_name/i2c0_inst/scl_io" \
+  "$project_name/scl_io"
+sbp_connect_net "$project_name/i2c0_inst/sda_io" \
+  "$project_name/sda_io"
+
+sbp_connect_net "$project_name/spi0_inst/mosi_o" \
+  "$project_name/mosi_o"
+sbp_connect_net "$project_name/spi0_inst/miso_i" \
+  "$project_name/miso_i"
+sbp_connect_net "$project_name/spi0_inst/sclk_o" \
+  "$project_name/sclk_o"
+sbp_connect_net "$project_name/spi0_inst/ssn_o" \
+  "$project_name/ssn_o"
+
+sbp_connect_interface_net "$project_name/axi_interc0_inst/AXI_M00" \
+  "$project_name/axi_ahb0_inst/AXI4_S"
+sbp_connect_interface_net "$project_name/axi_interc0_inst/AXI_M01" \
+  "$project_name/axi_apb0_inst/AXI4_S"
+sbp_connect_interface_net "$project_name/axi_interc0_inst/AXI_M02" \
+  "$project_name/axi_apb1_inst/AXI4_S"
+sbp_connect_interface_net "$project_name/axi_interc0_inst/AXI_M03" \
+  "$project_name/axi_apb2_inst/AXI4_S"
+sbp_connect_interface_net "$project_name/axi_ahb0_inst/AHBL_M" \
+  "$project_name/spi0_inst/AHBL_S0"
+sbp_connect_interface_net "$project_name/cpu0_inst/AXI_M_INSTR" \
+  "$project_name/sysmem0_inst/AXI_S0"
+sbp_connect_interface_net "$project_name/axi_apb0_inst/APB3_M" \
+  "$project_name/i2c0_inst/APB_S0"
+sbp_connect_interface_net "$project_name/axi_apb1_inst/APB3_M" \
+  "$project_name/gpio0_inst/APB_S0"
+sbp_connect_interface_net "$project_name/axi_apb2_inst/APB3_M" \
+  "$project_name/gpio1_inst/APB_S0"
+sbp_connect_interface_net "$project_name/cpu0_inst/AXI_M_DATA" \
+  "$project_name/axi_interc0_inst/AXI_S00"
+sbp_connect_interface_net "$project_name/cpu0_inst/LOCAL_BUS_M_DATA" \
+  "$project_name/tcm0_inst/LOCAL_BUS_IF_S0"
+sbp_connect_interface_net "$project_name/cpu0_inst/LOCAL_BUS_M_INSTR" \
+  "$project_name/tcm0_inst/LOCAL_BUS_IF_S1"
+sbp_connect_interface_net "$project_name/spi0_inst/INTR" \
+  "$project_name/cpu0_inst/IRQ_S2"
+sbp_connect_interface_net "$project_name/i2c0_inst/INTR" \
+  "$project_name/cpu0_inst/IRQ_S3"
+sbp_connect_interface_net "$project_name/gpio0_inst/INTR" \
+  "$project_name/cpu0_inst/IRQ_S4"
+sbp_connect_interface_net "$project_name/gpio1_inst/INTR" \
+  "$project_name/cpu0_inst/IRQ_S5"
+
+sbp_assign_addr_seg -offset 'h10004000 "$project_name/axi_apb2_inst/APB3_M" \
+  "$project_name/gpio1_inst/APB_S0"
+sbp_assign_addr_seg -offset 'h10003000 "$project_name/axi_apb1_inst/APB3_M" \
+  "$project_name/gpio0_inst/APB_S0"
+sbp_assign_addr_seg -offset 'h10002000 "$project_name/axi_apb0_inst/APB3_M" \
+  "$project_name/i2c0_inst/APB_S0"
+sbp_assign_addr_seg -offset 'h10000000 "$project_name/axi_ahb0_inst/AHBL_M" \
+  "$project_name/spi0_inst/AHBL_S0"
+sbp_assign_addr_seg -offset 'h00000000 "$project_name/cpu0_inst/LOCAL_BUS_M_DATA" \
+  "$project_name/tcm0_inst/LOCAL_BUS_IF_S0"
+sbp_assign_addr_seg -offset 'h00200000 "$project_name/cpu0_inst/AXI_M_INSTR" \
+  "$project_name/sysmem0_inst/AXI_S0"
+sbp_assign_addr_seg -offset 'h00000000 "$project_name/cpu0_inst/LOCAL_BUS_M_INSTR" \
+  "$project_name/tcm0_inst/LOCAL_BUS_IF_S1"
+
+if {$timer_en == 1} {
+  adi_ip_update -vlnv {latticesemi.com:ip:axi_interc0:1.2.2} \
+    -meta_vlnv {latticesemi.com:ip:axi_interconnect:1.2.2} \
+    -cfg_value {
+      TOTAL_EXTMAS_CNT: 1,
+      TOTAL_EXTSLV_CNT: 5,
+      EXT_MAS_AXI_ID_WIDTH: 1,
+      EXT_SLV_AXI_ID_WIDTH: 8
+    } \
+    -ip_iname "axi_interc0_inst"
+
+  adi_ip_instance -vlnv {latticesemi.com:ip:axi_apb3:1.1.0} \
+    -meta_vlnv {latticesemi.com:ip:axi2apb_bridge:1.1.0} \
+    -cfg_value {} \
+    -ip_iname "axi_apb3_inst"
+
+  adi_ip_instance -vlnv {latticesemi.com:ip:timer0:1.3.0} \
+    -meta_vlnv {latticesemi.com:ip:gp_timer:1.3.0} \
+    -cfg_value {
+      t1_cnt_up: count-up,
+      T1_PERIOD_WIDTH: 32,
+      t1_period_val: 0,
+      t1_dis_pscaler: false,
+      t2_cnt_up: count-up,
+      T2_PERIOD_WIDTH: 32,
+      t2_period_val: 0,
+      t2_dis_pscaler: false,
+      t3_cnt_up: count-up,
+      T3_PERIOD_WIDTH: 32,
+      t3_period_val: 0,
+      t4_cnt_up: count-up,
+      T4_PERIOD_WIDTH: 32,
+      t4_period_val: 0
+    } \
+    -ip_iname "timer0_inst"
+
+  sbp_connect_net -name [sbp_get_nets -from $project_name/pll0_inst *clkop*] \
+    "$project_name/timer0_inst/clk_i" \
+    "$project_name/axi_apb3_inst/aclk_i"
+
+  sbp_connect_net -name [sbp_get_nets -from $project_name/cpu0_inst *resetn*] \
+    "$project_name/timer0_inst/rst_n_i" \
+    "$project_name/axi_apb3_inst/aresetn_i"
+
+  sbp_connect_interface_net "$project_name/axi_interc0_inst/AXI_M04" \
+    "$project_name/axi_apb3_inst/AXI4_S"
+
+  sbp_connect_interface_net "$project_name/axi_apb3_inst/APB3_M" \
+    "$project_name/timer0_inst/APB_S0"
+
+  sbp_connect_interface_net "$project_name/timer0_inst/INTR" \
+    "$project_name/cpu0_inst/IRQ_S6"
+
+  sbp_assign_addr_seg -offset 'h10005000 "$project_name/axi_apb3_inst/APB3_M" \
+    "$project_name/timer0_inst/APB_S0"
+}
---
+++ projects/common/lfcpnx/system_pb.tcl
@@ -0,0 +1,11 @@
+###############################################################################
+## Copyright (C) 2023-2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+set timer_en 0
+source $ad_hdl_dir/projects/common/lfcpnx/lfcpnx_system_pb.tcl
+
+#system ID # to be added
