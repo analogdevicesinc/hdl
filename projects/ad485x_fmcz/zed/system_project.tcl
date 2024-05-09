e366c6f93fbf90cd14d5dbe986a74ef07c32b69b
AndreiGrozav <andrei.grozav@analog.com>
2024-05-09 16:59:29 +0300

    ad485x_fmcz: Rename ad4858_fmcz to ad485x_fmcz
    Update after rename of axi_ad4858 -> axi_ad485x.
    Separate quad and octa channel constraints from common tcl constr file to avoid
    critical warnings and errors when the design is opend in GUI and
    modified an built.
    
    Signed-off-by: AndreiGrozav <andrei.grozav@analog.com>

--- projects/ad485x_fmcz/zed/system_project.tcl
+++ projects/ad485x_fmcz/zed/system_project.tcl
@@ -1,5 +1,5 @@
-###############################################################################
-## Copyright (C) 2023 Analog Devices, Inc. All rights reserved.
+##############################################################################
+## Copyright (C) 2023-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -9,30 +9,48 @@ source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 # if the interface is not build defined, set CMOS as default inferface
 # make LVDS_CMOS_N=1 for LVDS interface
-set LVDS_CMOS_N  0
+set LVDS_CMOS_N 0
 if [info exists ::env(LVDS_CMOS_N)] {
   set LVDS_CMOS_N $::env(LVDS_CMOS_N)
 } else {
   set env(LVDS_CMOS_N) $LVDS_CMOS_N
 }
 
-adi_project ad4858_fmcz_zed 0 [list \
+set DEVICE "AD4858"
+if [info exists ::env(DEVICE)] {
+  set DEVICE $::env(DEVICE)
+} else {
+  set env(DEVICE) $DEVICE
+}
+
+adi_project ad485x_fmcz_zed 0 [list \
   LVDS_CMOS_N     $LVDS_CMOS_N \
+  DEVICE          $DEVICE \
 ]
 
 if {$LVDS_CMOS_N == "0"} {
-  adi_project_files {} [list \
-    "system_top_cmos.v" \
-  ]
+  source ../common/config.tcl
+  if {$numb_of_lanes == "4"} {
+    adi_project_files {} [list \
+      "system_top_cmos_quad.v" \
+      "system_constr_cmos_quad.xdc" \
+    ]
+  } else {
+    adi_project_files {} [list \
+      "system_top_cmos_octa.v" \
+      "system_constr_cmos_octa.xdc" \
+    ]
+  }
 } else {
   adi_project_files {} [list \
     "system_top_lvds.v" \
+    "system_constr_lvds.xdc" \
   ]
 }
 
 adi_project_files {} [list \
   "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "system_constr.tcl"]
+]
 
-adi_project_run ad4858_fmcz_zed
+adi_project_run ad485x_fmcz_zed
