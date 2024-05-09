985102e0df70c0b6c4ffcc52e435059906c56087
Laez Barbosa <laez.barbosa@analog.com>
2024-05-09 17:18:49 -0300

    Fix mem_init error (#1221)
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

--- projects/ad9081_fmca_ebz/s10soc/system_qsys.tcl
+++ projects/ad9081_fmca_ebz/s10soc/system_qsys.tcl
@@ -19,3 +19,17 @@ if [info exists ad_project_dir] {
   source ../common/ad9081_fmca_ebz_qsys.tcl
 }
 
+#system ID
+
+if {[info exists ::env(ADI_PROJECT_DIR)]} {
+  set mem_init_sys_file_path "$::env(ADI_PROJECT_DIR)mem_init_sys.txt";
+} else {
+  set mem_init_sys_file_path mem_init_sys.txt;
+}
+
+set_instance_parameter_value axi_sysid_0 {ROM_ADDR_BITS} {9}
+set_instance_parameter_value rom_sys_0 {ROM_ADDR_BITS} {9}
+
+set_instance_parameter_value rom_sys_0 {PATH_TO_FILE} $mem_init_sys_file_path
+
+sysid_gen_sys_init_file
