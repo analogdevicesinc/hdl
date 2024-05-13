9960c6eb8dc9eba66bd4985cd255650c96ff67f7
Bogdan Luncan <bogdan.luncan@analog.com>
2024-05-13 15:12:33 +0300

    ad9081: a10soc: Updates to support FM87 changes
    Signed-off-by: Bogdan Luncan <bogdan.luncan@analog.com>

--- projects/ad9081_fmca_ebz/a10soc/system_qsys.tcl
+++ projects/ad9081_fmca_ebz/a10soc/system_qsys.tcl
@@ -13,6 +13,7 @@ source $ad_hdl_dir/projects/common/a10soc/a10soc_system_qsys.tcl
 source $ad_hdl_dir/projects/common/intel/dacfifo_qsys.tcl
 source $ad_hdl_dir/projects/common/intel/adcfifo_qsys.tcl
 
+set TRANSCEIVER_TYPE "SX"
 if [info exists ad_project_dir] {
   source ../../common/ad9081_fmca_ebz_qsys.tcl
 } else {
@@ -26,4 +27,3 @@ set_instance_parameter_value rom_sys_0 {ROM_ADDR_BITS} {9}
 set_instance_parameter_value rom_sys_0 {PATH_TO_FILE} "[pwd]/mem_init_sys.txt"
 
 sysid_gen_sys_init_file;
-
