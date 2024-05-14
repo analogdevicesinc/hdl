12f6c85a5a860155ae0009dde0e1872d7cccf211
Bogdan Luncan <bogdan.luncan@analog.com>
2024-05-14 13:13:16 +0300

    scripts:adi_env: Change the default version of Quartus Standard to 23.std1.1 and Quartus Pro 24.2
    Signed-off-by: Bogdan Luncan <bogdan.luncan@analog.com>

--- projects/ad777x_ardz/de10nano/system_project.tcl
+++ projects/ad777x_ardz/de10nano/system_project.tcl
@@ -3,7 +3,7 @@
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
-set REQUIRED_QUARTUS_VERSION 22.1std.0
+set REQUIRED_QUARTUS_VERSION 23.1std.1
 set QUARTUS_PRO_ISUSED 0
 source ../../../scripts/adi_env.tcl
 source ../../scripts/adi_project_intel.tcl
@@ -22,7 +22,7 @@ set_location_assignment PIN_U14  -to adc_data_in[2] ; ##   P5.5 Arduino_IO04
 set_location_assignment PIN_AG13 -to adc_data_in[3] ; ##   P5.1 Arduino_IO00
 
 set_location_assignment PIN_AF13 -to sdp_convst     ; ##   P5.2 Arduino_IO01
-set_location_assignment PIN_AH8  -to start_n        ; ##   P5.8 Arduino_IO07              
+set_location_assignment PIN_AH8  -to start_n        ; ##   P5.8 Arduino_IO07
 set_location_assignment PIN_AG10 -to reset_n        ; ##   P5.3 Arduino_IO02
 set_location_assignment PIN_AG9  -to sdp_mclk       ; ##   P5.4 Arduino_IO03
 
@@ -31,23 +31,23 @@ set_location_assignment PIN_AG16 -to spi_mosi       ; ##   P4.4 Arduino_IO11
 set_location_assignment PIN_AH11 -to spi_miso       ; ##   P4.5 Arduino_IO12
 set_location_assignment PIN_AH12 -to spi_clk        ; ##   P4.5 Arduino_IO13
 
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_clk_in   
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_ready_in 
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_clk_in
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_ready_in
 
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_data_in[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_data_in[1]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_data_in[2]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_data_in[3]
 
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdp_convst   
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to start_n      
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to reset_n      
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdp_convst   
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdp_convst
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to start_n
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to reset_n
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdp_convst
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdp_mclk
 
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi_csn      
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi_mosi     
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi_miso     
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi_clk      
-       
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi_csn
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi_mosi
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi_miso
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi_clk
+
 execute_flow -compile
