e8ee3f2cab85c5a2f801615237570eac42471c17
AndrDragomir <andrei.dragomir@analog.com>
2024-06-27 16:44:27 +0300

    adrv9371_kcu105: Fix timing
    Signed-off-by: AndrDragomir <andrei.dragomir@analog.com>

--- projects/adrv9371x/kcu105/system_bd.tcl
+++ projects/adrv9371x/kcu105/system_bd.tcl
@@ -31,4 +31,3 @@ ad_ip_parameter util_ad9371_xcvr CONFIG.QPLL_REFCLK_DIV 1
 ad_ip_parameter util_ad9371_xcvr CONFIG.CPLL_CFG0 0x67f8
 ad_ip_parameter util_ad9371_xcvr CONFIG.CPLL_CFG1 0xa4ac
 ad_ip_parameter util_ad9371_xcvr CONFIG.CPLL_CFG2 0x0007
-
