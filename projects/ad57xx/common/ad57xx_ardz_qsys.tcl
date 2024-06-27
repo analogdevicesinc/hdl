dc66d41a0a8405ce2b1ad60b0fd2ff2d3916364a
Laez Barbosa <laez.barbosa@analog.com>
2024-06-27 17:28:56 -0300

    ad57xx: add generic SPI project
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

---
+++ projects/ad57xx/common/ad57xx_ardz_qsys.tcl
@@ -0,0 +1,15 @@
+###############################################################################
+## Copyright (C) 2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+# ad57xx
+
+# resets
+
+# interrupts
+
+# cpu interconnects
+
+# mem interconnects
+
