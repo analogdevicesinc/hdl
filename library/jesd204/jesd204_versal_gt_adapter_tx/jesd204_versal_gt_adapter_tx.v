82d94ae9d15c9d3179057b14fdb4a0988b99cdb2
Bogdan Luncan <bogdan.luncan@analog.com>
2024-07-02 16:39:55 +0300

    library:jesd204:jesd204_gt_adapter: Register inputs and outputs
    Signed-off-by: Bogdan Luncan <bogdan.luncan@analog.com>

--- library/jesd204/jesd204_versal_gt_adapter_tx/jesd204_versal_gt_adapter_tx.v
+++ library/jesd204/jesd204_versal_gt_adapter_tx/jesd204_versal_gt_adapter_tx.v
@@ -1,6 +1,6 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright (C) 2017-2019, 2021 Analog Devices, Inc. All rights reserved.
+// Copyright (C) 2017-2019, 2021, 2024 Analog Devices, Inc. All rights reserved.
 // SPDX short identifier: ADIJESD204
 // ***************************************************************************
 // ***************************************************************************
@@ -8,8 +8,8 @@
 `timescale 1ns/100ps
 
 module  jesd204_versal_gt_adapter_tx (
-  output  [127 : 0] txdata,
-  output    [5 : 0] txheader,
+  output reg [127 : 0] txdata,
+  output reg   [5 : 0] txheader,
 
   // Interface to Link layer core
   input      [63:0] tx_data,
@@ -24,8 +24,10 @@ module  jesd204_versal_gt_adapter_tx (
     assign tx_data_flip[63-i] = tx_data[i];
   end
 
-  assign txdata = {64'b0,tx_data_flip};
   // Flip header bits and data
-  assign txheader = {4'b0,tx_header[0],tx_header[1]};
+  always @(posedge usr_clk) begin
+    txdata <= {64'b0, tx_data_flip};
+    txheader <= {4'b0, tx_header[0], tx_header[1]};
+  end
 
 endmodule
