1fbd43e467bd5cb751d36c854ca41a2090435082
Bogdan Luncan <bogdan.luncan@analog.com>
2024-07-02 16:39:55 +0300

    library:jesd204:jesd204_gt_adapter: Register inputs and outputs
    Signed-off-by: Bogdan Luncan <bogdan.luncan@analog.com>

--- library/jesd204/jesd204_versal_gt_adapter_rx/jesd204_versal_gt_adapter_rx.v
+++ library/jesd204/jesd204_versal_gt_adapter_rx/jesd204_versal_gt_adapter_rx.v
@@ -1,6 +1,6 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright (C) 2017, 2018, 2020-2022 Analog Devices, Inc. All rights reserved.
+// Copyright (C) 2017, 2018, 2020-2022, 2024 Analog Devices, Inc. All rights reserved.
 // SPDX short identifier: ADIJESD204
 // ***************************************************************************
 // ***************************************************************************
@@ -21,13 +21,25 @@ module  jesd204_versal_gt_adapter_rx (
   input           usr_clk
 );
 
+  wire       rxgearboxslip_s;
+  reg [63:0] rxdata_d;
+  reg [ 1:0] rxheader_d;
+  reg        rxgearboxslip_d;
+  reg [ 1:0] rxheadervalid_d;
+
+  always @(posedge usr_clk) begin
+    rxdata_d <= rxdata[63:0];
+    rxheader_d <= rxheader[1:0];
+    rxgearboxslip_d <= rxgearboxslip_s;
+  end
+
   // Sync header alignment
   wire rx_bitslip_req_s;
-  reg [4:0] rx_bitslip_done_cnt = 'h0;
+  reg [5:0] rx_bitslip_done_cnt = 'h0;
   always @(posedge usr_clk) begin
-    if (rx_bitslip_done_cnt[4]) begin
+    if (rx_bitslip_done_cnt[5]) begin
       rx_bitslip_done_cnt <= 'b0;
-    end else if (rx_bitslip_req_s & ~rx_bitslip_done_cnt[4]) begin
+    end else if (rx_bitslip_req_s & ~rx_bitslip_done_cnt[5]) begin
       rx_bitslip_done_cnt <= rx_bitslip_done_cnt + 1;
     end
   end
@@ -36,12 +48,14 @@ module  jesd204_versal_gt_adapter_rx (
   always @(posedge usr_clk) begin
      rx_bitslip_req_s_d <= rx_bitslip_req_s;
   end
-  assign rxgearboxslip = rx_bitslip_req_s & ~rx_bitslip_req_s_d;
+
+  assign rxgearboxslip_s = rx_bitslip_req_s & ~rx_bitslip_req_s_d;
+  assign rxgearboxslip = rxgearboxslip_d;
 
   wire [63:0] rxdata_flip;
   genvar i;
   for (i = 0; i < 64; i=i+1) begin
-    assign rxdata_flip[63-i] = rxdata[i];
+    assign rxdata_flip[63-i] = rxdata_d[i];
   end
 
   // Sync header alignment
@@ -49,9 +63,9 @@ module  jesd204_versal_gt_adapter_rx (
     .clk(usr_clk),
     .reset(~rxheadervalid[0]),
     // Flip header bits and data
-    .i_data({rxheader[0],rxheader[1],rxdata_flip[63:0]}),
+    .i_data({rxheader_d[0],rxheader_d[1],rxdata_flip[63:0]}),
     .i_slip(rx_bitslip_req_s),
-    .i_slip_done(rx_bitslip_done_cnt[4]),
+    .i_slip_done(rx_bitslip_done_cnt[5]),
     .o_data(rx_data),
     .o_header(rx_header),
     .o_block_sync(rx_block_sync));
