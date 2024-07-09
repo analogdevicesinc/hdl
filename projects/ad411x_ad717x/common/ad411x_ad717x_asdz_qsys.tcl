b6791fc769f77fadf981799e294b43d1dc520ef6
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2024-07-09 11:00:03 +0300

    ad411x_ad717x: Requested changes
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/ad411x_ad717x/common/ad411x_ad717x_asdz_qsys.tcl
+++ projects/ad411x_ad717x/common/ad411x_ad717x_asdz_qsys.tcl
@@ -134,7 +134,7 @@ ad_cpu_interconnect 0x00030000 axi_spi_engine_0.s_axi
 # dma interconnect
 ad_dma_interconnect axi_dmac_0.m_dest_axi
 
-#interrupts
+# interrupts
 
 ad_cpu_interrupt 4 axi_dmac_0.interrupt_sender
 ad_cpu_interrupt 5 axi_spi_engine_0.interrupt_sender
--- projects/ad411x_ad717x/de10nano/system_project.tcl
+++ projects/ad411x_ad717x/de10nano/system_project.tcl
@@ -46,42 +46,4 @@ set_location_assignment PIN_AH9   -to i2c_sda       ; ##   Arduino_IO14
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to i2c_scl
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to i2c_sda
 
-# Arduino shield connections on de10_nano
-
-                  ####################################
-                  #                                  #       ## JP2 ##
-                  #                             *1   #    1  Arduino_IO15
-                  #                             *2   #    2  Arduino_IO14
-                  #                             *3   #    3  Arduino_VREF
-   ## JP5 ##      #                             *4   #    4  Arduino_GND
- # 1  NC          #                             *5   #    5  Arduino_IO13
- # 2  VCC3P3      #     *1                      *6   #    6  Arduino_IO12
- # 3  Reset_n     #     *2                      *7   #    7  Arduino_IO11
- # 4  VCC3P3      #     *3                  JP2 *8   #    8  Arduino_IO10
- # 5  VCC5        #     *4                      *9   #    9  Arduino_IO9
- # 6  GND         # JP5 *5                      *10  #    10 Arduino_IO8
- # 7  GND         #     *6                           #
- # 8  VCC9        #     *7                           #       ## JP3 ##
-                  #     *8                      *1   #    1  Arduino_IO7
-                  #                             *2   #    2  Arduino_IO6
-   ## JP6 ##      #     *1                      *3   #    3  Arduino_IO5
- # 1 ADC_IN0      #     *2                      *4   #    4  Arduino_IO4
- # 2 ADC_IN1      # JP6 *3          531     JP3 *5   #    5  Arduino_IO3
- # 3 ADC_IN2      #     *4      JP4 ***         *6   #    6  Arduino_IO2
- # 4 ADC_IN3      #     *5          ***         *7   #    7  Arduino_IO1
- # 5 ADC_IN4      #     *6          642         *8   #    8  Arduino_IO0
- # 6 ADC_IN5      #                                  #
-                   #                                #
-                    #                              #
-                     ##############################
-
-                                ## JP4 ##
-                              # 1 Arduino_I12
-                              # 2 VCC5
-                              # 3 Arduino_I13
-                              # 4 Arduino_IO11
-                              # 5 Arduino_Reset_n
-                              # 6 GND
-
-
 execute_flow -compile
