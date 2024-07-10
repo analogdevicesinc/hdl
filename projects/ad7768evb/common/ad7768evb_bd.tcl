spaceinventor/adhdl

projects: Update to use the generic sync signal
Signed-off-by: Ionut Podgoreanu <ionut.podgoreanu@analog.com>

projects/ad7768evb/common/ad7768evb_bd.tcl
@@ -66,13 +66,14 @@ ad_connect  ad7768_dma/m_dest_axi_aresetn     sys_cpu_resetn
 ad_connect  ad7768_dma/fifo_wr_clk            axi_ad7768_adc/adc_clk                
 ad_connect  ad7768_dma/fifo_wr_en             axi_ad7768_adc/adc_valid              
 ad_connect  ad7768_dma/fifo_wr_din            axi_ad7768_adc/adc_data  
-ad_connect  ad7768_dma/fifo_wr_sync           axi_ad7768_adc/adc_sync             
+ad_connect  ad7768_dma/sync                   axi_ad7768_adc/adc_sync             
 
 #parallel DMA
 
 ad_connect  ad7768_dma_2/m_dest_axi_aresetn   sys_cpu_resetn                        
 ad_connect  ad7768_dma_2/fifo_wr_clk          axi_ad7768_adc/adc_clk                
 ad_connect  ad7768_dma_2/fifo_wr              util_ad7768_adc_pack/packed_fifo_wr   
+ad_connect  ad7768_dma_2/sync                 util_ad7768_adc_pack/packed_fifo_wr_sync
 
 # interrupts
 

projects/adrv9009/common/adrv9009_qsys.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2018-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2018-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -236,7 +236,7 @@ set_instance_parameter_value axi_adrv9009_rx_dma {DMA_TYPE_SRC} {2}
 set_instance_parameter_value axi_adrv9009_rx_dma {FIFO_SIZE} {16}
 add_connection adrv9009_rx_jesd204.link_clk axi_adrv9009_rx_dma.if_fifo_wr_clk
 add_connection axi_adrv9009_rx_cpack.if_packed_fifo_wr_en axi_adrv9009_rx_dma.if_fifo_wr_en
-add_connection axi_adrv9009_rx_cpack.if_packed_fifo_wr_sync axi_adrv9009_rx_dma.if_fifo_wr_sync
+add_connection axi_adrv9009_rx_cpack.if_packed_fifo_wr_sync axi_adrv9009_rx_dma.if_sync
 add_connection axi_adrv9009_rx_cpack.if_packed_fifo_wr_data axi_adrv9009_rx_dma.if_fifo_wr_din
 add_connection axi_adrv9009_rx_dma.if_fifo_wr_overflow axi_adrv9009_rx_cpack.if_packed_fifo_wr_overflow
 add_connection sys_clk.clk axi_adrv9009_rx_dma.s_axi_clock
@@ -259,7 +259,7 @@ set_instance_parameter_value axi_adrv9009_rx_os_dma {DMA_TYPE_SRC} {2}
 set_instance_parameter_value axi_adrv9009_rx_os_dma {FIFO_SIZE} {16}
 add_connection adrv9009_rx_os_jesd204.link_clk axi_adrv9009_rx_os_dma.if_fifo_wr_clk
 add_connection axi_adrv9009_rx_os_cpack.if_packed_fifo_wr_en axi_adrv9009_rx_os_dma.if_fifo_wr_en
-add_connection axi_adrv9009_rx_os_cpack.if_packed_fifo_wr_sync  axi_adrv9009_rx_os_dma.if_fifo_wr_sync
+add_connection axi_adrv9009_rx_os_cpack.if_packed_fifo_wr_sync  axi_adrv9009_rx_os_dma.if_sync
 add_connection axi_adrv9009_rx_os_cpack.if_packed_fifo_wr_data axi_adrv9009_rx_os_dma.if_fifo_wr_din
 add_connection axi_adrv9009_rx_os_dma.if_fifo_wr_overflow axi_adrv9009_rx_os_cpack.if_packed_fifo_wr_overflow
 add_connection sys_clk.clk axi_adrv9009_rx_os_dma.s_axi_clock

projects/adrv9026/common/adrv9026_bd.tcl
@@ -200,6 +200,7 @@ ad_connect  rx_adrv9026_tpl_core/adc_dovf util_adrv9026_rx_cpack/fifo_wr_overflo
 
 ad_connect  core_clk axi_adrv9026_rx_dma/fifo_wr_clk
 ad_connect  util_adrv9026_rx_cpack/packed_fifo_wr axi_adrv9026_rx_dma/fifo_wr
+ad_connect  util_adrv9026_rx_cpack/packed_fifo_wr_sync axi_adrv9026_rx_dma/sync
 
 # interconnect (cpu)
 

projects/adrv9361z7035/common/adrv9361z7035_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2016-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2016-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -301,6 +301,7 @@ ad_ip_parameter axi_ad9361_adc_dma CONFIG.DMA_2D_TRANSFER 0
 ad_ip_parameter axi_ad9361_adc_dma CONFIG.DMA_DATA_WIDTH_SRC 64
 ad_connect util_ad9361_divclk/clk_out axi_ad9361_adc_dma/fifo_wr_clk
 ad_connect util_ad9361_adc_pack/packed_fifo_wr axi_ad9361_adc_dma/fifo_wr
+ad_connect util_ad9361_adc_pack/packed_fifo_wr_sync axi_ad9361_adc_dma/sync
 ad_connect sys_cpu_resetn axi_ad9361_adc_dma/m_dest_axi_aresetn
 
 # dac-path rfifo

projects/cn0579/common/cn0579_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2023-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -58,6 +58,7 @@ ad_connect axi_ad77684_adc/adc_dovf            cn0579_adc_pack/fifo_wr_overflow
 ad_connect  cn0579_dma/m_dest_axi_aresetn       sys_cpu_resetn                   
 ad_connect  cn0579_dma/fifo_wr_clk              axi_ad77684_adc/adc_clk                
 ad_connect  cn0579_dma/fifo_wr                  cn0579_adc_pack/packed_fifo_wr   
+ad_connect  cn0579_dma/sync                     cn0579_adc_pack/packed_fifo_wr_sync
 
 # interrupts
 

projects/cn0579/common/cn0579_qsys.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2023-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -44,7 +44,7 @@ set_instance_parameter_value cn0579_dma {DMA_TYPE_SRC} {2}
 
 add_connection axi_ad77684_adc.if_adc_clk                 cn0579_dma.if_fifo_wr_clk
 add_connection cn0579_adc_pack.if_packed_fifo_wr_en       cn0579_dma.if_fifo_wr_en
-add_connection cn0579_adc_pack.if_packed_fifo_wr_sync     cn0579_dma.if_fifo_wr_sync
+add_connection cn0579_adc_pack.if_packed_fifo_wr_sync     cn0579_dma.if_sync
 add_connection cn0579_adc_pack.if_packed_fifo_wr_data     cn0579_dma.if_fifo_wr_din
 add_connection cn0579_adc_pack.if_packed_fifo_wr_overflow cn0579_dma.if_fifo_wr_overflow
 

projects/fmcomms5/common/fmcomms5_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2014-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2014-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -193,6 +193,7 @@ ad_ip_parameter axi_ad9361_adc_dma CONFIG.DMA_DATA_WIDTH_SRC 128
 ad_ip_parameter axi_ad9361_adc_dma CONFIG.DMA_DATA_WIDTH_DEST 64
 ad_connect util_ad9361_divclk/clk_out axi_ad9361_adc_dma/fifo_wr_clk
 ad_connect util_ad9361_adc_pack/packed_fifo_wr axi_ad9361_adc_dma/fifo_wr
+ad_connect util_ad9361_adc_pack/packed_fifo_wr_sync axi_ad9361_adc_dma/sync
 ad_connect $sys_dma_resetn axi_ad9361_adc_dma/m_dest_axi_aresetn
 
 # dac-path rfifo

analogdevicesinc/hdl

projects/ad7768evb/common/ad7768evb_bd.tcl
@@ -66,13 +66,14 @@ ad_connect  ad7768_dma/m_dest_axi_aresetn     sys_cpu_resetn
 ad_connect  ad7768_dma/fifo_wr_clk            axi_ad7768_adc/adc_clk                
 ad_connect  ad7768_dma/fifo_wr_en             axi_ad7768_adc/adc_valid              
 ad_connect  ad7768_dma/fifo_wr_din            axi_ad7768_adc/adc_data  
-ad_connect  ad7768_dma/fifo_wr_sync           axi_ad7768_adc/adc_sync             
+ad_connect  ad7768_dma/sync                   axi_ad7768_adc/adc_sync             
 
 #parallel DMA
 
 ad_connect  ad7768_dma_2/m_dest_axi_aresetn   sys_cpu_resetn                        
 ad_connect  ad7768_dma_2/fifo_wr_clk          axi_ad7768_adc/adc_clk                
 ad_connect  ad7768_dma_2/fifo_wr              util_ad7768_adc_pack/packed_fifo_wr   
+ad_connect  ad7768_dma_2/sync                 util_ad7768_adc_pack/packed_sync
 
 # interrupts
 

projects/adrv9009/common/adrv9009_qsys.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2018-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2018-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -236,7 +236,7 @@ set_instance_parameter_value axi_adrv9009_rx_dma {DMA_TYPE_SRC} {2}
 set_instance_parameter_value axi_adrv9009_rx_dma {FIFO_SIZE} {16}
 add_connection adrv9009_rx_jesd204.link_clk axi_adrv9009_rx_dma.if_fifo_wr_clk
 add_connection axi_adrv9009_rx_cpack.if_packed_fifo_wr_en axi_adrv9009_rx_dma.if_fifo_wr_en
-add_connection axi_adrv9009_rx_cpack.if_packed_fifo_wr_sync axi_adrv9009_rx_dma.if_fifo_wr_sync
+add_connection axi_adrv9009_rx_cpack.if_packed_sync axi_adrv9009_rx_dma.if_sync
 add_connection axi_adrv9009_rx_cpack.if_packed_fifo_wr_data axi_adrv9009_rx_dma.if_fifo_wr_din
 add_connection axi_adrv9009_rx_dma.if_fifo_wr_overflow axi_adrv9009_rx_cpack.if_packed_fifo_wr_overflow
 add_connection sys_clk.clk axi_adrv9009_rx_dma.s_axi_clock
@@ -259,7 +259,7 @@ set_instance_parameter_value axi_adrv9009_rx_os_dma {DMA_TYPE_SRC} {2}
 set_instance_parameter_value axi_adrv9009_rx_os_dma {FIFO_SIZE} {16}
 add_connection adrv9009_rx_os_jesd204.link_clk axi_adrv9009_rx_os_dma.if_fifo_wr_clk
 add_connection axi_adrv9009_rx_os_cpack.if_packed_fifo_wr_en axi_adrv9009_rx_os_dma.if_fifo_wr_en
-add_connection axi_adrv9009_rx_os_cpack.if_packed_fifo_wr_sync  axi_adrv9009_rx_os_dma.if_fifo_wr_sync
+add_connection axi_adrv9009_rx_os_cpack.if_packed_sync  axi_adrv9009_rx_os_dma.if_sync
 add_connection axi_adrv9009_rx_os_cpack.if_packed_fifo_wr_data axi_adrv9009_rx_os_dma.if_fifo_wr_din
 add_connection axi_adrv9009_rx_os_dma.if_fifo_wr_overflow axi_adrv9009_rx_os_cpack.if_packed_fifo_wr_overflow
 add_connection sys_clk.clk axi_adrv9009_rx_os_dma.s_axi_clock

projects/adrv9026/common/adrv9026_bd.tcl
@@ -200,6 +200,7 @@ ad_connect  rx_adrv9026_tpl_core/adc_dovf util_adrv9026_rx_cpack/fifo_wr_overflo
 
 ad_connect  core_clk axi_adrv9026_rx_dma/fifo_wr_clk
 ad_connect  util_adrv9026_rx_cpack/packed_fifo_wr axi_adrv9026_rx_dma/fifo_wr
+ad_connect  util_adrv9026_rx_cpack/packed_sync axi_adrv9026_rx_dma/sync
 
 # interconnect (cpu)
 

projects/adrv9361z7035/common/adrv9361z7035_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2016-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2016-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -301,6 +301,7 @@ ad_ip_parameter axi_ad9361_adc_dma CONFIG.DMA_2D_TRANSFER 0
 ad_ip_parameter axi_ad9361_adc_dma CONFIG.DMA_DATA_WIDTH_SRC 64
 ad_connect util_ad9361_divclk/clk_out axi_ad9361_adc_dma/fifo_wr_clk
 ad_connect util_ad9361_adc_pack/packed_fifo_wr axi_ad9361_adc_dma/fifo_wr
+ad_connect util_ad9361_adc_pack/packed_sync axi_ad9361_adc_dma/sync
 ad_connect sys_cpu_resetn axi_ad9361_adc_dma/m_dest_axi_aresetn
 
 # dac-path rfifo

projects/cn0579/common/cn0579_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2023-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -58,6 +58,7 @@ ad_connect axi_ad77684_adc/adc_dovf            cn0579_adc_pack/fifo_wr_overflow
 ad_connect  cn0579_dma/m_dest_axi_aresetn       sys_cpu_resetn                   
 ad_connect  cn0579_dma/fifo_wr_clk              axi_ad77684_adc/adc_clk                
 ad_connect  cn0579_dma/fifo_wr                  cn0579_adc_pack/packed_fifo_wr   
+ad_connect  cn0579_dma/sync                     cn0579_adc_pack/packed_sync
 
 # interrupts
 

projects/cn0579/common/cn0579_qsys.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2023-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -44,7 +44,7 @@ set_instance_parameter_value cn0579_dma {DMA_TYPE_SRC} {2}
 
 add_connection axi_ad77684_adc.if_adc_clk                 cn0579_dma.if_fifo_wr_clk
 add_connection cn0579_adc_pack.if_packed_fifo_wr_en       cn0579_dma.if_fifo_wr_en
-add_connection cn0579_adc_pack.if_packed_fifo_wr_sync     cn0579_dma.if_fifo_wr_sync
+add_connection cn0579_adc_pack.if_packed_sync             cn0579_dma.if_sync
 add_connection cn0579_adc_pack.if_packed_fifo_wr_data     cn0579_dma.if_fifo_wr_din
 add_connection cn0579_adc_pack.if_packed_fifo_wr_overflow cn0579_dma.if_fifo_wr_overflow
 

projects/fmcomms5/common/fmcomms5_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2014-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2014-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -193,6 +193,7 @@ ad_ip_parameter axi_ad9361_adc_dma CONFIG.DMA_DATA_WIDTH_SRC 128
 ad_ip_parameter axi_ad9361_adc_dma CONFIG.DMA_DATA_WIDTH_DEST 64
 ad_connect util_ad9361_divclk/clk_out axi_ad9361_adc_dma/fifo_wr_clk
 ad_connect util_ad9361_adc_pack/packed_fifo_wr axi_ad9361_adc_dma/fifo_wr
+ad_connect util_ad9361_adc_pack/packed_sync axi_ad9361_adc_dma/sync
 ad_connect $sys_dma_resetn axi_ad9361_adc_dma/m_dest_axi_aresetn
 
 # dac-path rfifo