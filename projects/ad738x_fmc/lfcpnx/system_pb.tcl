3062300060209e81ee86c7f5d7f33fa482c74881
Villyam <Vilmoscsaba.Jozsa@analog.com>
2024-07-11 10:28:47 +0300

    projects: Initial commit for ad738x_fmc_lfcpnx.
    Signed-off-by: Villyam <Vilmoscsaba.Jozsa@analog.com>

---
+++ projects/ad738x_fmc/lfcpnx/system_pb.tcl
@@ -0,0 +1,10 @@
+###############################################################################
+## Copyright (C) 2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+set timer_en 0
+source $ad_hdl_dir/projects/common/lfcpnx/lfcpnx_system_pb.tcl
+source $ad_hdl_dir/projects/ad738x_fmc/common/ad738x_pb.tcl
