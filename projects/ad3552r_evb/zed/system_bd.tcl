2f6c305b56b801d02d6aaf6ba048e4e3a2ecd937
sarpadi <sergiu.arpadi@analog.com>
2024-07-25 13:46:56 +0300

    ad3552r: Cosmetic changes

--- projects/ad3552r_evb/zed/system_bd.tcl
+++ projects/ad3552r_evb/zed/system_bd.tcl
@@ -1,10 +1,13 @@
 ###############################################################################
-## Copyright (C) 2022-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2022-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+# block design
+source ../common/ad3552r_evb_bd.tcl
 
 #system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
@@ -12,5 +15,3 @@ ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
 sysid_gen_sys_init_file
-
-source ../common/ad3552r_evb_bd.tcl
