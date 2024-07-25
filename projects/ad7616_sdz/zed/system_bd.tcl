b036d2096be3db9ff6c1842b2d7ca13184713380
sarpadi <sergiu.arpadi@analog.com>
2024-07-25 10:59:16 +0300

    ad7616: Fix sysid
    Rewrite system_bd.tcl to fix sysid cstring and for uniformity reasons
    Project only has one build param (SER_PAR)
    
    Signed-off-by: sarpadi <sergiu.arpadi@analog.com>

--- projects/ad7616_sdz/zed/system_bd.tcl
+++ projects/ad7616_sdz/zed/system_bd.tcl
@@ -6,28 +6,21 @@
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 
-#system ID
-ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
-ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_sys.txt"
-ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
-
-if {$ad_project_params(SI_OR_PI) == 0} {
-    set C_SI_OR_PI "SI"
-} else {
-    set C_SI_OR_PI "PI"
-}
-
-# system level parameters
-set SER_PAR_N $ad_project_params(SER_PAR_N)
-
-set sys_cstring "$C_SI_OR_PI\
-SER_PAR_N=$SER_PAR_N\
-ADC_SAMPLING_RATE=$adc_sampling_rate"
-
-sysid_gen_sys_init_file $sys_cstring
-
 adi_project_files ad7616_sdz_zed [list \
-	"../../../library/common/ad_edge_detect.v" \
-	"../../../library/util_cdc/sync_bits.v"]
+       "../../../library/common/ad_edge_detect.v" \
+       "../../../library/util_cdc/sync_bits.v"]
 
 source ../common/ad7616_bd.tcl
+
+#system ID
+
+set mem_init_sys_path [get_env_param ADI_PROJECT_DIR ""]mem_init_sys.txt;
+
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/$mem_init_sys_path"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+
+set SER_PAR_N [get_env_param SER_PAR_N 1]
+
+set sys_cstring "SER_PAR_N=$SER_PAR_N"
+sysid_gen_sys_init_file $sys_cstring
