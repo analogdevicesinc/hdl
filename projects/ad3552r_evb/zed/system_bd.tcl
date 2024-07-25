f50f0cb3b6a9e540c66e366bb269a8a53b7a49e3
sarpadi <sergiu.arpadi@analog.com>
2024-07-25 10:56:22 +0300

    ad3552r_evb: Fix sysid
    This project does not have build parameters
    
    Signed-off-by: sarpadi <sergiu.arpadi@analog.com>

--- projects/ad3552r_evb/zed/system_bd.tcl
+++ projects/ad3552r_evb/zed/system_bd.tcl
@@ -11,12 +11,6 @@ ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
-if {$ad_project_params(SI_OR_PI) == 0} {
-    set sys_cstring "SI"
-} else {
-    set sys_cstring "PI"
-}
-
-sysid_gen_sys_init_file $sys_cstring
+sysid_gen_sys_init_file
 
 source ../common/ad3552r_evb_bd.tcl
