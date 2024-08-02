21b56ac22d8a185d279ceca3247254d9033b6d92
sarpadi <sergiu.arpadi@analog.com>
2024-08-02 15:26:03 +0300

    ad7606x: Update project
    Simplify parameters. EXT_CLK parameter was useless.The AD7606X IP Core
    does not implment any CDC to allow for clocking options. SPI Engine
    option will stick to 32 bits only to cover all parts. We do not need to
    build for different number of channels, software can disable them if
    needed.In all, the number of bitstreams is greatly reduced and the build
    process should be more straight forward. Fix license headers. Update sysid
    script for uniformity
    
    Signed-off-by: sarpadi <sergiu.arpadi@analog.com>
    (cherry picked from commit e24715251e10c36e994a6884fd5b14c8edd1239a)

--- projects/ad7606x_fmc/common/ad7606x_bd.tcl
+++ projects/ad7606x_fmc/common/ad7606x_bd.tcl
@@ -4,18 +4,15 @@
 ###############################################################################
 
 # system level parameters
-set DEV_CONFIG $ad_project_params(DEV_CONFIG)
 set INTF $ad_project_params(INTF)
 set NUM_OF_SDI $ad_project_params(NUM_OF_SDI)
-set ADC_N_BITS [expr {$DEV_CONFIG == 2 ? 18 : 16}]
+set ADC_N_BITS $ad_project_params(ADC_N_BITS)
 set ADC_TO_DMA_N_BITS [expr {$ADC_N_BITS == 16 ? 16 : 32}]
-set EXT_CLK $ad_project_params(EXT_CLK)
 set TOTAL_N_BITS_DMA [expr {$ADC_TO_DMA_N_BITS*8}]
 
-puts "build parameters: DEV_CONFIG: $DEV_CONFIG"
 puts "build parameters: INTF: $INTF"
 puts "build parameters: NUM_OF_SDI: $NUM_OF_SDI"
-puts "build parameters: EXT_CLK: $EXT_CLK"
+puts "build parameters: ADC_N_BITS: $ADC_N_BITS"
 
 # control lines
 create_bd_port -dir I rx_busy
@@ -23,6 +20,7 @@ create_bd_port -dir O rx_cnvst_n
 
 # axi_pwm_gen
 ad_ip_instance axi_pwm_gen ad7606_pwm_gen
+ad_ip_parameter ad7606_pwm_gen CONFIG.N_PWMS 1
 
 # dma
 ad_ip_instance axi_dmac axi_ad7606x_dma
@@ -46,21 +44,13 @@ switch $INTF {
 
     # instantiation
     ad_ip_instance axi_ad7606x axi_ad7606x
-    ad_ip_parameter axi_ad7606x CONFIG.DEV_CONFIG $DEV_CONFIG
     ad_ip_parameter axi_ad7606x CONFIG.ADC_N_BITS $ADC_N_BITS
     ad_ip_parameter axi_ad7606x CONFIG.ADC_TO_DMA_N_BITS $ADC_TO_DMA_N_BITS
-    ad_ip_parameter axi_ad7606x CONFIG.EXTERNAL_CLK $EXT_CLK
 
     # axi_pwm_gen
     ad_ip_parameter ad7606_pwm_gen CONFIG.ASYNC_CLK_EN 0
-    ad_ip_parameter ad7606_pwm_gen CONFIG.N_PWMS 1
-    if {$DEV_CONFIG == 0} {
-      ad_ip_parameter ad7606_pwm_gen CONFIG.PULSE_0_WIDTH 124
-      ad_ip_parameter ad7606_pwm_gen CONFIG.PULSE_0_PERIOD 125
-    } else {
-      ad_ip_parameter ad7606_pwm_gen CONFIG.PULSE_0_WIDTH 99
-      ad_ip_parameter ad7606_pwm_gen CONFIG.PULSE_0_PERIOD 100
-    }
+    ad_ip_parameter ad7606_pwm_gen CONFIG.PULSE_0_WIDTH 124
+    ad_ip_parameter ad7606_pwm_gen CONFIG.PULSE_0_PERIOD 125
 
     # dma
     ad_ip_parameter axi_ad7606x_dma CONFIG.DMA_TYPE_SRC 2
@@ -69,26 +59,6 @@ switch $INTF {
     ad_ip_instance util_cpack2 ad7606x_adc_pack
     ad_ip_parameter ad7606x_adc_pack CONFIG.NUM_OF_CHANNELS 8
     ad_ip_parameter ad7606x_adc_pack CONFIG.SAMPLE_DATA_WIDTH $ADC_TO_DMA_N_BITS
-
-    if {$EXT_CLK == 1} {
-      # use Xilinx's clocking wizard in order to generate the clock from the CPU clock, this being then assigned to the adc_clk in the axi_ad7606x IP
-      ad_ip_instance clk_wiz adc_clk_generator
-      ad_ip_parameter adc_clk_generator CONFIG.PRIMITIVE PLL
-      ad_ip_parameter adc_clk_generator CONFIG.RESET_TYPE ACTIVE_LOW
-      ad_ip_parameter adc_clk_generator CONFIG.USE_LOCKED false
-      ad_ip_parameter adc_clk_generator CONFIG.CLKOUT1_REQUESTED_OUT_FREQ 100.000
-      ad_ip_parameter adc_clk_generator CONFIG.CLKOUT1_REQUESTED_PHASE 0.000
-      ad_ip_parameter adc_clk_generator CONFIG.CLKOUT1_REQUESTED_DUTY_CYCLE 50.000
-      ad_ip_parameter adc_clk_generator CONFIG.PRIM_SOURCE Global_buffer
-      ad_ip_parameter adc_clk_generator CONFIG.CLKIN1_UI_JITTER 0
-      ad_ip_parameter adc_clk_generator CONFIG.PRIM_IN_FREQ 100.000
-
-      ad_connect sys_cpu_clk adc_clk_generator/clk_in1
-      ad_connect sys_cpu_resetn adc_clk_generator/resetn
-      ad_connect adc_clk_generator/clk_out1 axi_ad7606x/external_clk
-    } else {
-      ad_connect sys_cpu_clk axi_ad7606x/external_clk
-    }
 
     # interface connections
     ad_connect rx_db_o axi_ad7606x/rx_db_o
@@ -120,8 +90,6 @@ switch $INTF {
 
     # interconnect
     ad_cpu_interconnect 0x44a00000 axi_ad7606x
-    ad_cpu_interconnect 0x44a30000 axi_ad7606x_dma
-    ad_cpu_interconnect 0x44b00000 ad7606_pwm_gen
 
     # memory interconnect
     ad_mem_hp1_interconnect sys_cpu_clk sys_ps7/S_AXI_HP1
@@ -136,10 +104,17 @@ switch $INTF {
     # instantiation
     create_bd_intf_port -mode Master -vlnv analog.com:interface:spi_engine_rtl:1.0 ad7606_spi
 
+    ad_ip_instance axi_clkgen spi_clkgen
+    ad_ip_parameter spi_clkgen CONFIG.CLK0_DIV 5
+    ad_ip_parameter spi_clkgen CONFIG.VCO_DIV 1
+    ad_ip_parameter spi_clkgen CONFIG.VCO_MUL 6
+
+    ad_connect $sys_cpu_clk spi_clkgen/clk
+
     # spi engine
     source $ad_hdl_dir/library/spi_engine/scripts/spi_engine.tcl
 
-    set data_width    $ADC_TO_DMA_N_BITS
+    set data_width    32
     set async_spi_clk 1
     set num_cs        1
     set num_sdi       $NUM_OF_SDI
@@ -149,12 +124,6 @@ switch $INTF {
     set hier_spi_engine spi_ad7606
 
     spi_engine_create $hier_spi_engine $data_width $async_spi_clk $num_cs $num_sdi $num_sdo $sdi_delay
-
-    # axi_clkgen
-    ad_ip_instance axi_clkgen spi_clkgen
-    ad_ip_parameter spi_clkgen CONFIG.CLK0_DIV 5
-    ad_ip_parameter spi_clkgen CONFIG.VCO_DIV 1
-    ad_ip_parameter spi_clkgen CONFIG.VCO_MUL 6
 
     # axi_pwm_gen
     ad_ip_parameter ad7606_pwm_gen CONFIG.PULSE_0_PERIOD 120
@@ -182,7 +151,6 @@ switch $INTF {
     ad_ip_parameter axi_ad7606x_dma CONFIG.DMA_DATA_WIDTH_SRC [expr $data_width * $num_sdi]
 
     # interface connections
-    ad_connect $sys_cpu_clk spi_clkgen/clk
     ad_connect $sys_cpu_clk $hier_spi_engine/clk
     ad_connect $sys_cpu_clk ad7606_pwm_gen/s_axi_aclk
 
@@ -203,9 +171,7 @@ switch $INTF {
 
     # interconnect
     ad_cpu_interconnect 0x44a00000 $hier_spi_engine/${hier_spi_engine}_axi_regmap
-    ad_cpu_interconnect 0x44a30000 axi_ad7606x_dma
     ad_cpu_interconnect 0x44a70000 spi_clkgen
-    ad_cpu_interconnect 0x44b00000 ad7606_pwm_gen
 
     # interrupts
     ad_cpu_interrupt ps-13 mb-13 axi_ad7606x_dma/irq
@@ -216,3 +182,6 @@ switch $INTF {
     ad_mem_hp1_interconnect $sys_cpu_clk axi_ad7606x_dma/m_dest_axi
     }
 }
+
+ad_cpu_interconnect 0x44a30000 axi_ad7606x_dma
+ad_cpu_interconnect 0x44b00000 ad7606_pwm_gen
