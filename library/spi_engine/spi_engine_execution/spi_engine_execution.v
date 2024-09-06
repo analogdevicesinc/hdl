d845ac3a826e185979b7d97e3789978a2ba1a6a0
Laez Barbosa <laez.barbosa@analog.com>
2024-09-06 11:50:37 -0300

    SPI Engine: decouple SDO data handshake from control
    SDO data can now be clocked in independently from the offload trigger.
    This allows lower latencies for executing transfers, since the data can be
    obtained from the DMA before the trigger. It also better separates the command
    path from the data path.
    
    SDO data source behavior is altered: previously, with SDO_STREAMING=1 the SPI
    Engine would prioritize the SDO memory and switch to AXI Streaming when empty.
    This unused automatic switching is now removed: if the SPI Engine is built with
    SDO_STREAMING=1 it will only get SDO data from AXI Streaming, and with
    SDO_STREAMING=0 it will only use the SDO memory.
    
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

--- library/spi_engine/spi_engine_execution/spi_engine_execution.v
+++ library/spi_engine/spi_engine_execution/spi_engine_execution.v
@@ -132,6 +132,8 @@ module spi_engine_execution #(
 
   reg sdo_enabled = 1'b0;
   reg sdi_enabled = 1'b0;
+  wire sdo_enabled_io;
+  wire sdi_enabled_io;
 
   wire sdo_int_s;
 
@@ -165,7 +167,7 @@ module spi_engine_execution #(
 
   wire end_of_sdi_latch;
 
-  wire sample_sdo;
+  wire sdo_io_ready;
 
   (* direct_enable = "yes" *) wire cs_gen;
 
@@ -194,7 +196,6 @@ module spi_engine_execution #(
     .sdo_idle_state(sdo_idle_state),
     .left_aligned(left_aligned),
     .word_length(word_length),
-    .sample_sdo(sample_sdo),
     .sdo_io_ready(sdo_io_ready),
     .transfer_active(transfer_active),
     .trigger_tx(trigger_tx),
@@ -202,8 +203,6 @@ module spi_engine_execution #(
     .first_bit(first_bit),
     .cs_activate(cs_activate),
     .end_of_sdi_latch(end_of_sdi_latch));
-
-  assign sample_sdo = sdo_data_valid && ((trigger_tx && last_bit) || (wait_for_io || exec_transfer_cmd));
 
   assign cs_gen = inst_d1 == CMD_CHIPSELECT
                   && ((cs_sleep_counter_compare == 1'b1) || cs_sleep_early_exit)
@@ -217,6 +216,8 @@ module spi_engine_execution #(
       sdi_enabled <= cmd[9];
     end
   end
+  assign sdo_enabled_io = (exec_transfer_cmd) ? cmd[8] : sdo_enabled;
+  assign sdi_enabled_io = (exec_transfer_cmd) ? cmd[9] : sdi_enabled;
 
   always @(posedge clk) begin
     if (cmd_ready & cmd_valid)
@@ -388,9 +389,9 @@ module spi_engine_execution #(
   assign sync = cmd_d1[7:0];
 
   assign io_ready1 = (sdi_data_valid == 1'b0 || sdi_data_ready == 1'b1) &&
-          (sdo_enabled == 1'b0 || last_transfer == 1'b1 || sdo_io_ready == 1'b1);
+          (sdo_enabled_io == 1'b0 || sdo_io_ready == 1'b1);
   assign io_ready2 = (sdi_enabled == 1'b0 || sdi_data_ready == 1'b1) &&
-          (sdo_enabled == 1'b0 || last_transfer == 1'b1 || sdo_data_valid == 1'b1);
+          (sdo_enabled_io == 1'b0 || last_transfer == 1'b1 || sdo_io_ready == 1'b1);
 
   always @(posedge clk) begin
     if (idle == 1'b1) begin
@@ -409,14 +410,11 @@ module spi_engine_execution #(
       wait_for_io <= 1'b0;
     end else begin
       if (exec_transfer_cmd == 1'b1) begin
-        wait_for_io <= 1'b1;
-        transfer_active <= 1'b0;
+        wait_for_io <= !io_ready1;
+        transfer_active <= io_ready1;
       end else if (wait_for_io == 1'b1 && io_ready1 == 1'b1) begin
         wait_for_io <= 1'b0;
-        if (last_transfer == 1'b0)
-          transfer_active <= 1'b1;
-        else
-          transfer_active <= 1'b0;
+        transfer_active <= !last_transfer;
       end else if (transfer_active == 1'b1 && end_of_word == 1'b1) begin
         if (last_transfer == 1'b1 || io_ready2 == 1'b0)
           transfer_active <= 1'b0;
