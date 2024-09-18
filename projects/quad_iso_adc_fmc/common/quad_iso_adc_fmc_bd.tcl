400797085b396ada1beac5b02420b4957f7fec73
sarpadi <sergiu.arpadi@analog.com>
2024-09-18 17:22:33 +0300

    quad_iso_adc: SPI_CLK is 120MHz for timing purposes

--- projects/quad_iso_adc_fmc/common/quad_iso_adc_fmc_bd.tcl
+++ projects/quad_iso_adc_fmc/common/quad_iso_adc_fmc_bd.tcl
@@ -18,11 +18,19 @@ set hier_spi_engine quad_iso_adc
 
 spi_engine_create $hier_spi_engine $data_width $async_spi_clk $num_cs $num_sdi $num_sdo $sdi_delay $echo_sclk
 
+ad_ip_instance axi_clkgen spi_clkgen
+ad_ip_parameter spi_clkgen CONFIG.CLK0_DIV 10
+ad_ip_parameter spi_clkgen CONFIG.VCO_DIV 1
+ad_ip_parameter spi_clkgen CONFIG.VCO_MUL 12
+
+ad_connect $sys_cpu_clk spi_clkgen/clk
+ad_connect spi_clk spi_clkgen/clk_0
 
 ad_ip_instance axi_clkgen mclk_clkgen
 ad_ip_parameter mclk_clkgen CONFIG.CLK0_DIV 60
 ad_ip_parameter mclk_clkgen CONFIG.VCO_DIV 1
 ad_ip_parameter mclk_clkgen CONFIG.VCO_MUL 30
+
 # dma to receive data stream
 ad_ip_instance axi_dmac axi_qadc_dma
 ad_ip_parameter axi_qadc_dma CONFIG.DMA_TYPE_SRC 1
@@ -38,14 +46,15 @@ ad_ip_parameter axi_qadc_dma CONFIG.DMA_DATA_WIDTH_DEST 64
 ad_connect qadc_mclk_refclk mclk_clkgen/clk
 ad_connect qadc_xtal2_mclk mclk_clkgen/clk_0
 
-
+ad_connect spi_clk $hier_spi_engine/spi_clk
+ad_connect spi_clk axi_qadc_dma/s_axis_aclk
 
 ad_connect axi_qadc_dma/s_axis $hier_spi_engine/M_AXIS_SAMPLE
 ad_connect $hier_spi_engine/m_spi qadc_spi
 
 ad_connect $sys_cpu_clk $hier_spi_engine/clk
-ad_connect mclk_clkgen/clk_0 $hier_spi_engine/spi_clk
-ad_connect mclk_clkgen/clk_0 axi_qadc_dma/s_axis_aclk
+#ad_connect mclk_clkgen/clk_0 $hier_spi_engine/spi_clk
+#ad_connect mclk_clkgen/clk_0 axi_qadc_dma/s_axis_aclk
 ad_connect sys_cpu_resetn $hier_spi_engine/resetn
 ad_connect sys_cpu_resetn axi_qadc_dma/m_dest_axi_aresetn
 
@@ -64,7 +73,8 @@ ad_connect qadc_drdy drdy_chk/Op1
 
 ad_cpu_interconnect 0x44a00000 $hier_spi_engine/${hier_spi_engine}_axi_regmap
 ad_cpu_interconnect 0x44a30000 axi_qadc_dma
-ad_cpu_interconnect 0x44a70000 mclk_clkgen
+ad_cpu_interconnect 0x44a70000 spi_clkgen
+ad_cpu_interconnect 0x44b00000 mclk_clkgen
 
 ad_cpu_interrupt "ps-13" "mb-13" axi_qadc_dma/irq
 ad_cpu_interrupt "ps-12" "mb-12" $hier_spi_engine/irq
