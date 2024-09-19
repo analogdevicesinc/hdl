d8bd7d37619deae498ef3b0a23ac3975d03a85a9
Bogdan Luncan <bogdan.luncan@analog.com>
2024-09-19 10:40:20 +0300

    projects: ad9082: vhk158: Add initial design
    Signed-off-by: Bogdan Luncan <bogdan.luncan@analog.com>

---
+++ projects/ad9082_fmca_ebz/vhk158/system_bd.tcl
@@ -0,0 +1,10 @@
+###############################################################################
+## Copyright (C) 2024-2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+adi_project_files ad9082_fmca_ebz_vhk158 [list \
+  "$ad_hdl_dir/library/util_cdc/sync_bits.v" \
+]
+
+source $ad_hdl_dir/projects/ad9081_fmca_ebz/vhk158/system_bd.tcl
