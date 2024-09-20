5265d7ef2b3938b45c2119fd98e5acca8cf5d957
Iulia Moldovan <Iulia.Moldovan@analog.com>
2024-09-20 17:00:07 +0300

    docs/library: Add axi_clock_monitor IP doc & Update IP GUI
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- library/axi_clock_monitor/axi_clock_monitor_ip.tcl
+++ library/axi_clock_monitor/axi_clock_monitor_ip.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2022-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2022-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -16,41 +16,48 @@ adi_ip_files axi_clock_monitor [list \
 
 adi_ip_properties axi_clock_monitor
 
-set_property company_url {https://wiki.analog.com/resources/fpga/docs/axi_clock_monitor} [ipx::current_core]
+set cc [ipx::current_core]
+set_property display_name {AXI Clock Monitor} $cc
+set_property company_url {https://analogdevicesinc.github.io/hdl/library/axi_clock_monitor/index.html} $cc
 
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 1} \
-  [ipx::get_ports *_1* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_1* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 2} \
-  [ipx::get_ports *_2* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_2* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 3} \
-  [ipx::get_ports *_3* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_3* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 4} \
-  [ipx::get_ports *_4* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_4* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 5} \
-  [ipx::get_ports *_5* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_5* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 6} \
-  [ipx::get_ports *_6* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_6* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 7} \
-  [ipx::get_ports *_7* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_7* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 8 } \
-  [ipx::get_ports *_8*  -of_objects [ipx::current_core]]
+  [ipx::get_ports *_8*  -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 9 } \
-  [ipx::get_ports *_9*  -of_objects [ipx::current_core]]
+  [ipx::get_ports *_9*  -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 10} \
-  [ipx::get_ports *_10* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_10* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 11} \
-  [ipx::get_ports *_11* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_11* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 12} \
-  [ipx::get_ports *_12* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_12* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 13} \
-  [ipx::get_ports *_13* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_13* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 14} \
-  [ipx::get_ports *_14* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_14* -of_objects $cc]
 set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CLOCKS')) > 15} \
-  [ipx::get_ports *_15* -of_objects [ipx::current_core]]
+  [ipx::get_ports *_15* -of_objects $cc]
 
-ipx::infer_bus_interface reset xilinx.com:signal:reset_rtl:1.0 [ipx::current_core]
+set_property widget {textEdit} [ipgui::get_guiparamspec -name "NUM_OF_CLOCKS" -component [ipx::current_core] ]
+set_property -dict [list \
+  "value_validation_type" "range_long" \
+  "value_validation_range_minimum" "1" \
+  "value_validation_range_maximum" "16"
+] [ipx::get_user_parameters NUM_OF_CLOCKS -of_objects $cc]
 
-ipx::save_core [ipx::current_core]
+ipx::infer_bus_interface reset xilinx.com:signal:reset_rtl:1.0 $cc
 
-
+ipx::save_core $cc
