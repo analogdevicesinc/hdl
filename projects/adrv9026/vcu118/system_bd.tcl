a37a588802d241e744a1e1ed9d6b7e8efc80bc8e
AndrDragomir <andrei.dragomir@analog.com>
2024-09-25 14:44:18 +0300

    adrv9026: Add vcu118 support
    Signed-off-by: AndrDragomir <andrei.dragomir@analog.com>

---
+++ projects/adrv9026/vcu118/system_bd.tcl
@@ -0,0 +1,22 @@
+###############################################################################
+## Copyright (C) 2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+## FIFO depth is 18Mb - 1M samples
+set dac_fifo_address_width 17
+
+source $ad_hdl_dir/projects/common/vcu118/vcu118_system_bd.tcl
+source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+set mem_init_sys_path [get_env_param ADI_PROJECT_DIR ""]mem_init_sys.txt;
+
+#system ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/$mem_init_sys_path"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+
+sysid_gen_sys_init_file
+
+source ../common/adrv9026_bd.tcl
