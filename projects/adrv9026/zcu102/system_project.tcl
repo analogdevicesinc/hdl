d146370c10fdd55156de2bafdd9b24292c01b6e1
AndrDragomir <andrei.dragomir@analog.com>
2024-10-07 14:26:57 +0300

    adrv9026/zcu102: Update build parameters
    Signed-off-by: AndrDragomir <andrei.dragomir@analog.com>

--- projects/adrv9026/zcu102/system_project.tcl
+++ projects/adrv9026/zcu102/system_project.tcl
@@ -8,12 +8,17 @@ source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 adi_project adrv9026_zcu102 0 [list \
-  TX_JESD_M       [get_env_param TX_JESD_M       8 ] \
-  TX_JESD_L       [get_env_param TX_JESD_L       4 ] \
-  TX_JESD_S       [get_env_param TX_JESD_S       1 ] \
-  RX_JESD_M       [get_env_param RX_JESD_M       8 ] \
-  RX_JESD_L       [get_env_param RX_JESD_L       4 ] \
-  RX_JESD_S       [get_env_param RX_JESD_S       1 ] \
+  JESD_MODE           [get_env_param JESD_MODE      8B10B ] \
+  TX_LANE_RATE        [get_env_param TX_LANE_RATE    9.83 ] \
+  RX_LANE_RATE        [get_env_param RX_LANE_RATE    9.83 ] \
+  TX_NUM_LINKS        [get_env_param RX_NUM_LINKS       1 ] \
+  RX_NUM_LINKS        [get_env_param RX_NUM_LINKS       1 ] \
+  TX_JESD_M           [get_env_param TX_JESD_M          8 ] \
+  TX_JESD_L           [get_env_param TX_JESD_L          4 ] \
+  TX_JESD_S           [get_env_param TX_JESD_S          1 ] \
+  RX_JESD_M           [get_env_param RX_JESD_M          8 ] \
+  RX_JESD_L           [get_env_param RX_JESD_L          4 ] \
+  RX_JESD_S           [get_env_param RX_JESD_S          1 ] \
 ]
 adi_project_files adrv9026_zcu102 [list \
   "system_top.v" \
