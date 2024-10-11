f7e9e1e01780a11d8ad618505f49146b434fe3a7
Laez Barbosa <laez.barbosa@analog.com>
2024-10-11 14:21:55 -0300

    SPI Engine: simplify interconnect
    Make the interconnect use an external signal for switching between
    sources, so it becomes just a collection of muxes, reducing latency.
    This signal is generated based on the software-controlled offload
    enable, but is sourced from the offload module to avoid unnecessary
    CDCs.
    
    This commit makes the use of SYNC optional.
    
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

--- library/spi_engine/spi_engine_offload/spi_engine_offload_ip.tcl
+++ library/spi_engine/spi_engine_offload/spi_engine_offload_ip.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2015-2024 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2015-2025 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -77,6 +77,14 @@ adi_add_bus "s_axis_sdo" "slave" \
 	[list {"s_axis_sdo_ready" "TREADY"} \
 	  {"s_axis_sdo_valid" "TVALID"} \
 	  {"s_axis_sdo_data" "TDATA"}]
+
+adi_add_bus "m_interconnect_ctrl" "master" \
+	"analog.com:interface:spi_engine_interconnect_ctrl_rtl:1.0" \
+	"analog.com:interface:spi_engine_interconnect_ctrl:1.0" \
+	{ \
+		{"interconnect_dir" "interconnect_dir"} \
+	}
+adi_add_bus_clock "spi_clk" "m_interconnect_ctrl" "resetn"
 
 adi_add_bus_clock "spi_clk" "spi_engine_ctrl:offload_sdi:s_axis_sdo" "spi_resetn"
 adi_add_bus_clock "ctrl_clk" "spi_engine_offload_ctrl"
