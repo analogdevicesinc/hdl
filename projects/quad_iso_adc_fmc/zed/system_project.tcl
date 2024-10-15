d2fdb03379e2714a84ddc8b46da68d5b877c028c
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2024-10-15 14:04:51 +0300

    projects/quad_iso_adc: Add NUM_OF_SDI parameter for build
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/quad_iso_adc_fmc/zed/system_project.tcl
+++ projects/quad_iso_adc_fmc/zed/system_project.tcl
@@ -3,7 +3,8 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project quad_iso_adc_fmc_zed
+adi_project quad_iso_adc_fmc_zed 0 [list \
+  NUM_OF_SDI [get_env_param NUM_OF_SDI 4]]
 
 adi_project_files quad_iso_adc_fmc_zed [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
