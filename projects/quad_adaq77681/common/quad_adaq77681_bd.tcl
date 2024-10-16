fb8638126e546f0e008094b2eec874799a376dd6
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2024-10-16 16:10:54 +0300

    projects/quad_ad77681: Rename the project
    Rename the project and update the files that are affected.
    
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/quad_adaq77681/common/quad_adaq77681_bd.tcl
+++ projects/quad_adaq77681/common/quad_adaq77681_bd.tcl
@@ -5,11 +5,11 @@ set NUM_OF_SDI  $ad_project_params(NUM_OF_SDI)
 
 puts "build parameters: NUM_OF_SDI: $NUM_OF_SDI"
 
-create_bd_intf_port -mode Master -vlnv analog.com:interface:spi_engine_rtl:1.0 qadc_spi
+create_bd_intf_port -mode Master -vlnv analog.com:interface:spi_engine_rtl:1.0 quad_adaq77681_spi
 
-create_bd_port -dir I -from 3 -to 0 qadc_drdy
-create_bd_port -dir I               qadc_mclk_refclk
-create_bd_port -dir O               qadc_xtal2_mclk
+create_bd_port -dir I -from 3 -to 0 quad_adaq77681_drdy
+create_bd_port -dir I               quad_adaq77681_mclk_refclk
+create_bd_port -dir O               quad_adaq77681_xtal2_mclk
 
 set data_width    32
 set async_spi_clk 1
@@ -19,7 +19,7 @@ set num_sdo       1
 set sdi_delay     1
 set echo_sclk     0
 
-set hier_spi_engine quad_iso_adc
+set hier_spi_engine quad_adaq77681
 
 spi_engine_create $hier_spi_engine $data_width $async_spi_clk $num_cs $num_sdi $num_sdo $sdi_delay $echo_sclk
 
@@ -48,14 +48,14 @@ ad_ip_parameter axi_qadc_dma CONFIG.DMA_2D_TRANSFER 0
 ad_ip_parameter axi_qadc_dma CONFIG.DMA_DATA_WIDTH_SRC [expr $NUM_OF_SDI * $data_width]
 ad_ip_parameter axi_qadc_dma CONFIG.DMA_DATA_WIDTH_DEST 64
 
-ad_connect qadc_mclk_refclk mclk_clkgen/clk
-ad_connect qadc_xtal2_mclk mclk_clkgen/clk_0
+ad_connect quad_adaq77681_mclk_refclk mclk_clkgen/clk
+ad_connect quad_adaq77681_xtal2_mclk mclk_clkgen/clk_0
 
 ad_connect spi_clk $hier_spi_engine/spi_clk
 ad_connect spi_clk axi_qadc_dma/s_axis_aclk
 
 ad_connect axi_qadc_dma/s_axis $hier_spi_engine/M_AXIS_SAMPLE
-ad_connect $hier_spi_engine/m_spi qadc_spi
+ad_connect $hier_spi_engine/m_spi quad_adaq77681_spi
 
 ad_connect $sys_cpu_clk $hier_spi_engine/clk
 #ad_connect mclk_clkgen/clk_0 $hier_spi_engine/spi_clk
@@ -67,14 +67,14 @@ ad_ip_instance util_reduced_logic drdy_buf
 ad_ip_parameter drdy_buf CONFIG.C_OPERATION {and}
 ad_ip_parameter drdy_buf CONFIG.C_SIZE 4
 
-ad_connect qadc_drdy drdy_buf/Op1
+ad_connect quad_adaq77681_drdy drdy_buf/Op1
 ad_connect drdy_buf/Res $hier_spi_engine/trigger
 
 ad_ip_instance util_reduced_logic drdy_chk
 ad_ip_parameter drdy_chk CONFIG.C_OPERATION {xor}
 ad_ip_parameter drdy_chk CONFIG.C_SIZE 4
 
-ad_connect qadc_drdy drdy_chk/Op1
+ad_connect quad_adaq77681_drdy drdy_chk/Op1
 
 ad_cpu_interconnect 0x44a00000 $hier_spi_engine/${hier_spi_engine}_axi_regmap
 ad_cpu_interconnect 0x44a30000 axi_qadc_dma
--- projects/quad_adaq77681/zed/system_project.tcl
+++ projects/quad_adaq77681/zed/system_project.tcl
@@ -3,14 +3,14 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project quad_iso_adc_fmc_zed 0 [list \
+adi_project quad_adaq77681_zed 0 [list \
   NUM_OF_SDI [get_env_param NUM_OF_SDI 4]]
 
-adi_project_files quad_iso_adc_fmc_zed [list \
+adi_project_files quad_adaq77681_zed [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "system_top.v" \
   "system_constr.xdc" \
   "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"]
 
-adi_project_run quad_iso_adc_fmc_zed
+adi_project_run quad_adaq77681_zed
 
