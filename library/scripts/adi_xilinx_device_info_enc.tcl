c06cefa5df2359709f4cb987b0b48ced941c45a7
PopPaul2021 <paul.pop@analog.com>
2024-10-22 17:52:25 +0300

    projects/ad9213_ad4080: Initial commit
    Signed-off-by: PopPaul2021 <paul.pop@analog.com>

--- library/scripts/adi_xilinx_device_info_enc.tcl
+++ library/scripts/adi_xilinx_device_info_enc.tcl
@@ -116,7 +116,7 @@ proc adi_device_spec {cellpath param} {
       FPGA_TECHNOLOGY {
           switch  -regexp -- $part {
              ^xc7          {set series_name 7series}
-             ^xczu         {set series_name ultrascale+}
+             ^xc[zv]u      {set series_name ultrascale+}
              ^x.zu..?p     {set series_name ultrascale+}
              ^xck26        {set series_name ultrascale+}
              ^xc.u         {set series_name ultrascale }
