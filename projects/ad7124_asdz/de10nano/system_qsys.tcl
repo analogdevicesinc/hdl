afdf96430bc87c0ccb446485473092808a4a9bc9
sarpadi <sergiu.arpadi@analog.com>
2024-10-28 13:14:30 +0200

    ad7124/de10nano: Change SYS SPI ref clock to 80MHz
    Signed-off-by: sarpadi <sergiu.arpadi@analog.com>

--- projects/ad7124_asdz/de10nano/system_qsys.tcl
+++ projects/ad7124_asdz/de10nano/system_qsys.tcl
@@ -14,6 +14,14 @@ set_instance_parameter_value sys_gpio_in {irqType} {EDGE}
 set_instance_parameter_value sys_spi {clockPolarity} {0}
 set_instance_parameter_value sys_spi {targetClockRate} {20000000.0}
 
+# change sys spi ref clock to 80MHz
+
+remove_connection sys_clk.clk/sys_spi.clk
+remove_connection sys_clk.clk_reset/sys_spi.reset
+
+add_connection sys_dma_clk.clk sys_spi.clk
+add_connection sys_dma_clk.clk_reset sys_spi.reset
+
 #system ID
 set_instance_parameter_value axi_sysid_0 {ROM_ADDR_BITS} {9}
 set_instance_parameter_value rom_sys_0 {ROM_ADDR_BITS} {9}
