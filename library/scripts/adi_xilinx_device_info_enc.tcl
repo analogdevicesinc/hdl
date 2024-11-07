412a71aaf5f6b890d6d35fd53f65820b9769dac5
PopPaul2021 <paul.pop@analog.com>
2024-11-07 16:59:32 +0200

    library/scripts/adi_xilinx_device_info_enc: Indentation fix
    Signed-off-by: PopPaul2021 <paul.pop@analog.com>

--- library/scripts/adi_xilinx_device_info_enc.tcl
+++ library/scripts/adi_xilinx_device_info_enc.tcl
@@ -117,7 +117,7 @@ proc adi_device_spec {cellpath param} {
           switch  -regexp -- $part {
              ^xc7          {set series_name 7series}
              ^xczu         {set series_name ultrascale+}
-             ^xcvu.?.p    {set series_name ultrascale+}
+             ^xcvu.?.p     {set series_name ultrascale+}
              ^x.zu..?p     {set series_name ultrascale+}
              ^xck26        {set series_name ultrascale+}
              ^xc.u         {set series_name ultrascale }
