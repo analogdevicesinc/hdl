eca8ed9126faa5096ac9d9395c151a5633b55a50
ladace <84384593+ladace@users.noreply.github.com>
2024-11-19 10:39:25 +0200

    AD469x_fmc: Initial version for Coraz7s and DE-10Nano (#1463)
    * ad469x_evb: Rename and update project
    
    Renamed ad469x_fmc to ad469x_evb to cover new carriers
    Clean and update Zed project
    Initial version for Coraz7s
    Initial version for DE-10Nano
    
    Signed-off-by: Liviu 'Ceshu' Adace <liviu.adace@analog.com>
    
    * docs/ad469x_evb: Update documentation and diagrams
    
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>
    Signed-off-by: sarpadi <sergiu.arpadi@analog.com>
    Signed-off-by: Liviu 'Ceshu' Adace <liviu.adace@analog.com>
    
    ---------
    
    Signed-off-by: Liviu 'Ceshu' Adace <liviu.adace@analog.com>
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>
    Signed-off-by: sarpadi <sergiu.arpadi@analog.com>

--- projects/ad469x_evb/zed/system_bd.tcl
+++ projects/ad469x_evb/zed/system_bd.tcl
@@ -6,30 +6,18 @@
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 
-# specify the spi reference clock frequency in MHz
-set spi_clk_ref_frequency 160
-
-# specify ADC resolution -- supported resolutions 16 bits
-set adc_resolution 16
-
-# specify ADC sampling rate in samples/seconds
-set adc_sampling_rate 1000000
-
-adi_project_files ad469x_fmc_zed [list \
+adi_project_files ad469x_evb_zed [list \
 	"../../../library/common/ad_edge_detect.v" \
 	"../../../library/util_cdc/sync_bits.v" \
 ]
 
 source ../common/ad469x_bd.tcl
 
-
 #system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
-set sys_cstring "SPI_CLK_FREQ=$spi_clk_ref_frequency\
-ADC_RESOLUTION=$adc_resolution\
-SAMPLING_RATE=$adc_sampling_rate"
+set sys_cstring "SPI_4WIRE=$ad_project_params(SPI_4WIRE)"
 
 sysid_gen_sys_init_file $sys_cstring
