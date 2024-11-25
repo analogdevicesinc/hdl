a9713e2364c07fa239fd801997e1c4e0092799ab
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2024-11-25 13:37:51 +0200

    daq3: Replace adcfifo/dacfifo with data_offload
    Signed-off-by: Ionut Podgoreanu <ionut.podgoreanu@analog.com>

--- projects/daq3/zcu102/system_bd.tcl
+++ projects/daq3/zcu102/system_bd.tcl
@@ -1,15 +1,9 @@
 ###############################################################################
-## Copyright (C) 2014-2025 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2014-2026 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
-## FIFO depth is 8Mb - 500k samples
-set dac_fifo_address_width 16
-
-## NOTE: With this configuration the #36Kb BRAM utilization is at ~28%
-
 source $ad_hdl_dir/projects/common/zcu102/zcu102_system_bd.tcl
-source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
 source ../common/daq3_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 
@@ -23,8 +17,7 @@ L=$ad_project_params(RX_JESD_L)\
 S=$ad_project_params(RX_JESD_S)\
 TX:M=$ad_project_params(TX_JESD_M)\
 L=$ad_project_params(TX_JESD_L)\
-S=$ad_project_params(TX_JESD_S)\
-DAC_FIFO_ADDR_WIDTH=$dac_fifo_address_width"
+S=$ad_project_params(TX_JESD_S)"
 
 sysid_gen_sys_init_file $sys_cstring
 
@@ -33,8 +26,6 @@ ad_ip_parameter util_daq3_xcvr CONFIG.CPLL_CFG0 0x03fe
 ad_ip_parameter util_daq3_xcvr CONFIG.CPLL_CFG1 0x0021
 ad_ip_parameter util_daq3_xcvr CONFIG.CPLL_CFG2 0x0203
 
-create_bd_port -dir I dac_fifo_bypass
-
 ad_ip_parameter axi_ad9152_dma CONFIG.FIFO_SIZE 32
 ad_ip_parameter axi_ad9152_dma CONFIG.AXI_SLICE_SRC 1
 ad_ip_parameter axi_ad9152_dma CONFIG.AXI_SLICE_DEST 1
@@ -42,9 +33,8 @@ ad_ip_parameter axi_ad9152_dma CONFIG.CYCLIC 1
 ad_ip_parameter axi_ad9152_dma CONFIG.MAX_BYTES_PER_BURST 256
 
 ad_ip_parameter axi_ad9680_dma CONFIG.DMA_TYPE_SRC 2
-ad_ip_parameter axi_ad9680_dma CONFIG.FIFO_SIZE 32
 ad_ip_parameter axi_ad9680_dma CONFIG.DMA_DATA_WIDTH_DEST 128
-ad_ip_parameter axi_ad9680_dma CONFIG.DMA_DATA_WIDTH_SRC 128
+ad_ip_parameter axi_ad9680_dma CONFIG.FIFO_SIZE 32
 ad_ip_parameter axi_ad9680_dma CONFIG.AXI_SLICE_SRC 1
 ad_ip_parameter axi_ad9680_dma CONFIG.AXI_SLICE_DEST 1
 ad_ip_parameter axi_ad9680_dma CONFIG.MAX_BYTES_PER_BURST 256
@@ -69,16 +59,13 @@ ad_connect sys_cpu_resetn sys_dma_rstgen/ext_reset_in
 ad_connect sys_dma_reset sys_dma_rstgen/peripheral_reset
 ad_connect sys_dma_resetn sys_dma_rstgen/peripheral_aresetn
 
-ad_connect sys_dma_clk axi_ad9152_fifo/dma_clk
-ad_connect sys_dma_reset axi_ad9152_fifo/dma_rst
-ad_connect sys_dma_clk axi_ad9152_dma/m_axis_aclk
 ad_connect sys_dma_resetn axi_ad9152_dma/m_src_axi_aresetn
-ad_connect axi_ad9152_fifo/bypass dac_fifo_bypass
+ad_connect util_daq3_xcvr/tx_out_clk_0 axi_ad9152_dma/m_axis_aclk
+ad_connect axi_ad9152_upack/s_axis axi_ad9152_dma/m_axis
 
 ad_connect sys_dma_resetn axi_ad9680_dma/m_dest_axi_aresetn
-ad_connect axi_ad9680_dma/fifo_wr_clk util_daq3_xcvr/rx_out_clk_0
+ad_connect util_daq3_xcvr/rx_out_clk_0 axi_ad9680_dma/fifo_wr_clk
 ad_connect axi_ad9680_cpack/packed_fifo_wr axi_ad9680_dma/fifo_wr
-ad_connect axi_ad9680_cpack/fifo_wr_overflow axi_ad9680_tpl_core/adc_dovf
 
 ad_mem_hp0_interconnect sys_cpu_clk sys_ps8/S_AXI_HP0
 ad_mem_hp0_interconnect sys_cpu_clk axi_ad9680_xcvr/m_axi
