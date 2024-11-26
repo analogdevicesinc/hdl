a60b44cd5c489ed1133b4a91523a5689dda865fd
Iulia Moldovan <Iulia.Moldovan@analog.com>
2024-11-26 09:29:03 +0200

    remove lidar and tia related stuff
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- projects/ad_fmclidar1_ebz/zc706/system_top.v
+++ projects/ad_fmclidar1_ebz/zc706/system_top.v
@@ -184,7 +184,7 @@ module system_top (
   // laser driver
 
   OBUFDS i_obufds_laser_driver (
-    .I (laser_driver),
+    .I (1'b0),
     .O (laser_driver_p),
     .OB (laser_driver_n));
 
@@ -266,10 +266,6 @@ module system_top (
     .rx_sync_0 (rx_sync),
     .rx_sysref_0 (rx_sysref),
     .spdif (spdif),
-    .laser_driver (laser_driver),
-    .laser_driver_en_n (laser_driver_en_n),
-    .laser_driver_otw_n (laser_driver_otw_n),
-    .tia_chsel (tia_chsel),
     .iic_dac_scl_io (afe_dac_scl),
     .iic_dac_sda_io (afe_dac_sda),
     .spi0_clk_i (spi_adc_clk),
