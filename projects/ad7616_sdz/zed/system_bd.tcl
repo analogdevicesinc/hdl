baf640d16354f86d8950b8ec5de476cf6a354bdb
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2024-11-28 16:18:21 +0200

    projects: ad7616: Project development
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/ad7616_sdz/zed/system_bd.tcl
+++ projects/ad7616_sdz/zed/system_bd.tcl
@@ -19,6 +19,6 @@ ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
-set sys_cstring "SER_PAR_N=$ad_project_params(SER_PAR_N)"
+set sys_cstring "INTF=$ad_project_params(INTF)"
 
 sysid_gen_sys_init_file $sys_cstring
--- projects/ad7616_sdz/zed/system_project.tcl
+++ projects/ad7616_sdz/zed/system_project.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2019-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2019-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -16,10 +16,10 @@ source $ad_hdl_dir/projects/scripts/adi_board.tcl
 #   How to use over-writable parameters from the environment:
 #
 #    e.g.
-#      make SER_PAR_N=0
+#      make INTF=0
 #
-#    SER_PAR_N  - Defines the interface type (serial OR parallel)
-#               - Default value is 1
+#    INTF  - Defines the interface type (serial OR parallel)
+#          - Default value is 0
 #
 # LEGEND: Serial    - 1
 #         Parallel  - 0
@@ -31,21 +31,17 @@ source $ad_hdl_dir/projects/scripts/adi_board.tcl
 #
 ##--------------------------------------------------------------
 
-if {[info exists ::env(SER_PAR_N)]} {
-  set S_SER_PAR_N [get_env_param SER_PAR_N 0]
-} elseif {![info exists SER_PAR_N]} {
-  set S_SER_PAR_N 1
-}
+set INTF [get_env_param INTF 0]
 
 adi_project ad7616_sdz_zed 0 [list \
-  SER_PAR_N  $S_SER_PAR_N \
+   INTF $INTF \
 ]
 
 adi_project_files ad7616_sdz_zed [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"]
 
-switch $S_SER_PAR_N {
+switch $INTF {
   1 {
     adi_project_files ad7616_sdz_zed [list \
       "system_top_si.v" \
