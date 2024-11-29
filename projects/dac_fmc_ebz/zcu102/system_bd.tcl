fab28492aa8b1d62d439595edc51673fd477336d
PopPaul2021 <93918446+PopPaul2021@users.noreply.github.com>
2024-11-29 15:00:26 +0200

    Fixes for AD9164 on ZCU102
    * Fixes and features for AD9164 on ZCU102
    
    Changes made:
    
    - changing the source DMA DATA WIDTH to 128 on ZCU102
    - change the LANE_INVERT value to 240(0xF0) which is specific for the
       AD9161, AD9162, AD9163 and AD9164.
    - the ADI_LANE_RATE parameter is added on ZCU102 project which
       can take the 12.5GHz OR 15.4GHz(default) values.
    - If the parameters are not listed in any of the supported modes, 
      the user can configure them through make parameters.
      make ADI_DAC_DEVICE=AD9164 M=1 L=8 S=4 F=1 HD=1 N=16 NP=16
      make ADI_DAC_DEVICE=AD9164 ADI_DAC_MODE=08 M=1 S=4
      This will result in the same configuration: M=1 L=8 S=4 F=1 HD=1 N=16 NP=16
    
    Signed-off-by: PopPaul2021 <paul.pop@analog.com>

--- projects/dac_fmc_ebz/zcu102/system_bd.tcl
+++ projects/dac_fmc_ebz/zcu102/system_bd.tcl
@@ -1,36 +1,66 @@
 ###############################################################################
-## Copyright (C) 2019-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2019-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
 set dac_fifo_address_width 13
 
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 source $ad_hdl_dir/projects/common/zcu102/zcu102_system_bd.tcl
 source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
 source ../common/dac_fmc_ebz_bd.tcl
-source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+if {[info exists ::env(ADI_LANE_RATE)]} {
+  set ADI_LANE_RATE [get_env_param ADI_LANE_RATE 15.4]
+} elseif {![info exists ADI_LANE_RATE]} {
+  set ADI_LANE_RATE 15.4
+}
+
+set ADI_DEVICE_CODE $ad_project_params(DEVICE_CODE)
+
+# Common for both 12.5 and 15.4 GHz Lane Rate
 
 ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_REFCLK_DIV 1
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG2 0xFC0
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG3 0x120
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG0 0x333C
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_FBDIV 40
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG4 0x45
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG1 0xD038
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG1_G3 0xD038
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG2_G3 0xFC0
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TX_CLK25_DIV 15
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TX_OUT_DIV 1
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TXPI_CFG 0x0
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.A_TXDIFFCTRL 0xC
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TX_PI_BIASSET 3
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.POR_CFG 0x0
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.PPF0_CFG 0xF00
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CP 0xFF
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CP_G3 0xF
-ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_LPF 0x31D
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG3       0x120
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_FBDIV      40
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG1       0xD038
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG1_G3    0xD038
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TX_OUT_DIV      1
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TXPI_CFG        0x0
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.A_TXDIFFCTRL    0xC
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.POR_CFG         0x0
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CP         0xFF
+ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CP_G3      0xF
 
-ad_ip_parameter dac_jesd204_link/tx CONFIG.SYSREF_IOB false
+if { $ADI_LANE_RATE == 15.4 } {
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG2       0xFC0
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG0       0x333C
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG4       0x45
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG2_G3    0xFC0
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TX_CLK25_DIV    15
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TX_PI_BIASSET   3
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.PPF0_CFG        0xF00
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_LPF        0x31D
+} elseif { $ADI_LANE_RATE == 12.5 } {
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG2       0xFC1
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG0       0x331C
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG4       0x4
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_CFG2_G3    0xFC1
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TX_CLK25_DIV    13
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.TX_PI_BIASSET   2
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.PPF0_CFG        0x800
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.PPF1_CFG        0x600
+    ad_ip_parameter util_dac_jesd204_xcvr CONFIG.QPLL_LPF        0x37F
+} else {
+    error "ADI_LANE_RATE must be either 12.5 GHz or 15.4GHz"
+}
+
+ad_ip_parameter  dac_jesd204_link/tx   CONFIG.SYSREF_IOB         false
+ad_ip_parameter  dac_dma               CONFIG.DMA_DATA_WIDTH_SRC 128
+
+if { $ADI_DEVICE_CODE == 3 } {
+  ad_ip_parameter  util_dac_jesd204_xcvr CONFIG.TX_LANE_INVERT     240
+}
 
 #system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
@@ -44,9 +74,10 @@ L=$ad_project_params(JESD_L)\
 S=$ad_project_params(JESD_S)\
 NP=$ad_project_params(JESD_NP)\
 LINKS=$ad_project_params(NUM_LINKS)\
-DEVICE_CODE=$ad_project_params(DEVICE_CODE)\
+DEVICE_CODE=$ADI_DEVICE_CODE\
 DAC_DEVICE=$ADI_DAC_DEVICE\
 DAC_MODE=$ADI_DAC_MODE\
+ADI_LANE_RATE=$ADI_LANE_RATE\
 DAC_FIFO_ADDR_WIDTH=$dac_fifo_address_width"
 
 sysid_gen_sys_init_file $sys_cstring
