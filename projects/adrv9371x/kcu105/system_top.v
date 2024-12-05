1ebf581687ffa4b3d890e203bfe1f403c0a2f863
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2024-12-05 16:37:56 +0200

    adrv9371x: Replace dacfifo with data_offload
    Signed-off-by: Ionut Podgoreanu <ionut.podgoreanu@analog.com>

--- projects/adrv9371x/kcu105/system_top.v
+++ projects/adrv9371x/kcu105/system_top.v
@@ -1,6 +1,6 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright (C) 2017-2023 Analog Devices, Inc. All rights reserved.
+// Copyright (C) 2017-2025 Analog Devices, Inc. All rights reserved.
 //
 // In this HDL repository, there are many different and unique modules, consisting
 // of various HDL (Verilog or VHDL) components. The individual modules are
@@ -239,7 +239,6 @@ module system_top (
   assign gpio_i[63:60] = gpio_o[63:60];
 
   system_wrapper i_system_wrapper (
-    .dac_fifo_bypass (gpio_o[60]),
     .adc_fir_filter_active (gpio_o[61]),
     .dac_fir_filter_active (gpio_o[62]),
     .c0_ddr4_act_n (ddr4_act_n),
