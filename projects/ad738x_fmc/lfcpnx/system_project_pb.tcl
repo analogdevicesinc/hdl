d774c8910b4ab9b1e7c7938085a1a531913b871d
Villyam <Vilmoscsaba.Jozsa@analog.com>
2024-12-06 12:33:43 +0200

    ad738x_fmc/lfcpnx: Adding suport for Lattice LFCPNX-EVN carrier.
    Signed-off-by: Villyam <Vilmoscsaba.Jozsa@analog.com>

---
+++ projects/ad738x_fmc/lfcpnx/system_project_pb.tcl
@@ -0,0 +1,9 @@
+###############################################################################
+## Copyright (C) 2024 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source ../../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_lattice_pb.tcl
+
+adi_project_pb ad738x_fmc_lfcpnx
