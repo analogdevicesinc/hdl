f80ee3b22fa1640c26204cc07b9b598486bd7bef
Iulia Moldovan <Iulia.Moldovan@analog.com>
2025-01-13 15:29:19 +0200

    projects/pulsar_adc: Fix project name
    * Changed from pulsar_adc_pmdz to pulsar_adc as the folder name
    
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- projects/pulsar_adc/zed/system_project.tcl
+++ projects/pulsar_adc/zed/system_project.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2021-2024 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2021-2025 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -26,35 +26,34 @@ source $ad_hdl_dir/projects/scripts/adi_board.tcl
 set FMC_N_PMOD [get_env_param FMC_N_PMOD 1]
 set SPI_OP_MODE [get_env_param SPI_OP_MODE 0]
 
-adi_project pulsar_adc_pmdz_zed 0 [list \
+adi_project pulsar_adc_zed 0 [list \
   FMC_N_PMOD    [get_env_param FMC_N_PMOD  1] \
   SPI_OP_MODE   [get_env_param SPI_OP_MODE 0] ]
 
-adi_project_files pulsar_adc_pmdz_zed [list \
+adi_project_files pulsar_adc_zed [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" ]
 
 if {$FMC_N_PMOD == 0} {
-  adi_project_files pulsar_adc_pmdz_zed [list \
+  adi_project_files pulsar_adc_zed [list \
     "system_top_pmod.v" \
     "system_constr_pmod.xdc" ]
 } elseif {$FMC_N_PMOD == 1} {
-    adi_project_files pulsar_adc_pmdz_zed [list \
+    adi_project_files pulsar_adc_zed [list \
       "system_top_fmc.v" \
       "system_constr_fmc.xdc" ]
     if {$SPI_OP_MODE == 0} {
-    adi_project_files pulsar_adc_pmdz_zed [list \
+    adi_project_files pulsar_adc_zed [list \
       "system_constr_fmc_sm0.xdc" ]
     } elseif {$SPI_OP_MODE == 1} {
-    adi_project_files pulsar_adc_pmdz_zed [list \
+    adi_project_files pulsar_adc_zed [list \
       "system_constr_fmc_sm1.xdc" ]
     } elseif {$SPI_OP_MODE == 2} {
-    adi_project_files pulsar_adc_pmdz_zed [list \
+    adi_project_files pulsar_adc_zed [list \
       "system_constr_fmc_sm2.xdc" ]
     }
 } else {
   return -code error [format "ERROR: Invalid eval board type! ..."]
 }
 
-adi_project_run pulsar_adc_pmdz_zed
-
+adi_project_run pulsar_adc_zed
