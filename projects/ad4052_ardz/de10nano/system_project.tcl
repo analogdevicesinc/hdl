ddef02443a23a966e27ef578fb4e192f63f8aa35
bluncan <bogdan.luncan@analog.com>
2025-01-27 15:51:39 +0200

    projects: ad4052_ardz: de10nano: system_project: Updated quartus version to 23.1std
    Signed-off-by: bluncan <bogdan.luncan@analog.com>

--- projects/ad4052_ardz/de10nano/system_project.tcl
+++ projects/ad4052_ardz/de10nano/system_project.tcl
@@ -19,26 +19,26 @@ set_location_assignment PIN_AG11  -to i2c_scl     ; ## Arduino_SCL
 
 # ad4052 interface
 
-set_location_assignment PIN_AH12 -to adc_spi_sclk ; ## Arduino_IO13   
-set_location_assignment PIN_AH11 -to adc_spi_sdi  ; ## Arduino_IO12   
-set_location_assignment PIN_AG16 -to adc_spi_sdo  ; ## Arduino_IO11   
-set_location_assignment PIN_AF15 -to adc_spi_cs   ; ## Arduino_IO10   
-set_location_assignment PIN_AG8  -to adc_cnv      ; ## Arduino_IO06   
-set_location_assignment PIN_AE15 -to adc_gp0      ; ## Arduino_IO09   
-set_location_assignment PIN_AF17 -to adc_gp1      ; ## Arduino_IO08   
+set_location_assignment PIN_AH12 -to adc_spi_sclk ; ## Arduino_IO13
+set_location_assignment PIN_AH11 -to adc_spi_sdi  ; ## Arduino_IO12
+set_location_assignment PIN_AG16 -to adc_spi_sdo  ; ## Arduino_IO11
+set_location_assignment PIN_AF15 -to adc_spi_cs   ; ## Arduino_IO10
+set_location_assignment PIN_AG8  -to adc_cnv      ; ## Arduino_IO06
+set_location_assignment PIN_AE15 -to adc_gp0      ; ## Arduino_IO09
+set_location_assignment PIN_AF17 -to adc_gp1      ; ## Arduino_IO08
 
 
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to i2c_scl
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to i2c_sda
 
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_spi_sclk
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_spi_sdi 
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_spi_sdo 
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_spi_cs  
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_cnv 
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_gp0     
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_gp1     
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_spi_sdi
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_spi_sdo
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_spi_cs
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_cnv
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_gp0
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to adc_gp1
 
 set_global_assignment -name OPTIMIZATION_MODE "HIGH PERFORMANCE EFFORT"
-       
+
 execute_flow -compile
