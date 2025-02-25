565a8c1c5bc4ced41adfab9c7e9beda642d0ae06
Laez Barbosa <laez.barbosa@analog.com>
2025-02-25 13:43:09 -0300

    Revert "library: fix wrong reset polarity role for altera IP"
    This reverts commit 47a9cee44637b7d1472b5b13f8de54768bf5fc37.

--- library/util_sigma_delta_spi/util_sigma_delta_spi_hw.tcl
+++ library/util_sigma_delta_spi/util_sigma_delta_spi_hw.tcl
@@ -27,8 +27,8 @@ proc p_elaboration {} {
 
 # clock and reset interface
 
-  ad_interface clock    clk    input 1
-  ad_interface reset-n  resetn input 1 if_clk
+  ad_interface clock clk    input 1
+  ad_interface reset resetn input 1 if_clk
 
   ad_interface signal spi_active input 1 active
   ad_interface signal data_ready output 1 if_pwm
