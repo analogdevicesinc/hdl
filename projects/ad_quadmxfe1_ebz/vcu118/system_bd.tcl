df0801608f7534dda00c11799801d264b6e6d103
Ionut Podgoreanu <ionut.podgoreanu@analog.com>
2025-03-21 11:48:10 +0200

    ad_quadmxfe1_ebz: Use variable Data Offload size
    Signed-off-by: Ionut Podgoreanu <ionut.podgoreanu@analog.com>

--- projects/ad_quadmxfe1_ebz/vcu118/system_bd.tcl
+++ projects/ad_quadmxfe1_ebz/vcu118/system_bd.tcl
@@ -3,12 +3,10 @@
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
-## Offload attributes
-set adc_offload_type 0                   ; ## BRAM
-set adc_offload_size [expr 2*1024*1024]  ; ## 2 MB
-
-set dac_offload_type 0                   ; ## BRAM
-set dac_offload_size [expr 2*1024*1024]  ; ## 2 MB
+## ADC FIFO depth in samples per converter
+set adc_fifo_samples_per_converter [expr $ad_project_params(RX_KS_PER_CHANNEL)*1024]
+## DAC FIFO depth in samples per converter
+set dac_fifo_samples_per_converter [expr $ad_project_params(TX_KS_PER_CHANNEL)*1024]
 
 source $ad_hdl_dir/projects/common/vcu118/vcu118_system_bd.tcl
 source ../common/ad_quadmxfe1_ebz_bd.tcl
