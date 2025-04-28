aa9d711eec512cac6359e63c5ede7ad00dbd056f
AndreiGrozav <andrei.grozav@analog.com>
2025-04-28 15:07:23 +0300

    ad485x: Update
    Remove the rest of the unused ports.
    Fix typo, in parameter settings.
    
    Signed-off-by: AndreiGrozav <andrei.grozav@analog.com>

--- projects/ad485x_fmcz/zed/system_bd.tcl
+++ projects/ad485x_fmcz/zed/system_bd.tcl
@@ -15,11 +15,20 @@ delete_bd_objs [get_bd_nets spi1_clk_i_1] \
                [get_bd_nets spi1_csn_i_1] \
                [get_bd_nets sys_ps7_SPI1_SS_O] \
                [get_bd_nets sys_ps7_SPI1_SS1_O] \
-               [get_bd_nets sys_ps7_SPI1_SS2_O]
+               [get_bd_nets sys_ps7_SPI1_SS2_O] \
+               [get_bd_ports spi1_clk_i] \
+               [get_bd_ports spi1_sdi_i] \
+               [get_bd_ports spi1_csn_i] \
+               [get_bd_ports spi1_sdo_i] \
+               [get_bd_ports spi1_csn_1_o] \
+               [get_bd_ports spi1_csn_2_o] \
+               [get_bd_ports spi1_clk_o] \
+               [get_bd_ports spi1_csn_0_o] \
+               [get_bd_ports spi1_sdo_o]
 
 # Connect SPI1 to MIO pins JE1 PMOD
-ad_ip_parameter sys_ps7 PCW_SPI1_SPI1_IO {MIO 10 .. 15}
-ad_ip_parameter sys_ps7 PCW_QSPI_GRP_SINGLE_SS_ENABLE 1
+ad_ip_parameter sys_ps7 CONFIG.PCW_SPI1_SPI1_IO {MIO 10 .. 15}
+ad_ip_parameter sys_ps7 CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE 1
 
 #system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
