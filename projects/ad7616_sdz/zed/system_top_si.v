a4fa5ca58abce8dad7d2cbf45d4aafb8ecc09781
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2025-05-05 14:30:43 +0300

    projects/ad7616: Add NUM_OF_SDI build parameter
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/ad7616_sdz/zed/system_top_si.v
+++ projects/ad7616_sdz/zed/system_top_si.v
@@ -1,6 +1,6 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright (C) 2014-2023 Analog Devices, Inc. All rights reserved.
+// Copyright (C) 2014-2025 Analog Devices, Inc. All rights reserved.
 //
 // In this HDL repository, there are many different and unique modules, consisting
 // of various HDL (Verilog or VHDL) components. The individual modules are
@@ -35,81 +35,73 @@
 
 `timescale 1ns/100ps
 
-module system_top (
-
-  inout       [14:0]      ddr_addr,
-  inout       [ 2:0]      ddr_ba,
+module system_top #(
+  parameter NUM_OF_SDI = 2
+) (
+  inout  [14:0]           ddr_addr,
+  inout  [ 2:0]           ddr_ba,
   inout                   ddr_cas_n,
   inout                   ddr_ck_n,
   inout                   ddr_ck_p,
   inout                   ddr_cke,
   inout                   ddr_cs_n,
-  inout       [ 3:0]      ddr_dm,
-  inout       [31:0]      ddr_dq,
-  inout       [ 3:0]      ddr_dqs_n,
-  inout       [ 3:0]      ddr_dqs_p,
+  inout  [ 3:0]           ddr_dm,
+  inout  [31:0]           ddr_dq,
+  inout  [ 3:0]           ddr_dqs_n,
+  inout  [ 3:0]           ddr_dqs_p,
   inout                   ddr_odt,
   inout                   ddr_ras_n,
   inout                   ddr_reset_n,
   inout                   ddr_we_n,
-
   inout                   fixed_io_ddr_vrn,
   inout                   fixed_io_ddr_vrp,
-  inout       [53:0]      fixed_io_mio,
+  inout  [53:0]           fixed_io_mio,
   inout                   fixed_io_ps_clk,
   inout                   fixed_io_ps_porb,
   inout                   fixed_io_ps_srstb,
-
-  inout       [31:0]      gpio_bd,
-
+  inout  [31:0]           gpio_bd,
   output                  hdmi_out_clk,
   output                  hdmi_vsync,
   output                  hdmi_hsync,
   output                  hdmi_data_e,
-  output      [15:0]      hdmi_data,
-
+  output [15:0]           hdmi_data,
   output                  i2s_mclk,
   output                  i2s_bclk,
   output                  i2s_lrclk,
   output                  i2s_sdata_out,
   input                   i2s_sdata_in,
-
   output                  spdif,
-
   inout                   iic_scl,
   inout                   iic_sda,
-  inout       [ 1:0]      iic_mux_scl,
-  inout       [ 1:0]      iic_mux_sda,
-
+  inout  [ 1:0]           iic_mux_scl,
+  inout  [ 1:0]           iic_mux_sda,
   input                   otg_vbusoc,
-
   output                  ad7616_spi_sclk,
   output                  ad7616_spi_sdo,
-  input       [ 1:0]      ad7616_spi_sdi,
+  input  [NUM_OF_SDI-1:0] ad7616_spi_sdi,
   output                  ad7616_spi_cs,
-
   output                  adc_reset_n,
   output                  adc_cnvst,
   input                   adc_busy,
   output                  adc_seq_en,
-  output      [ 1:0]      adc_hw_rngsel,
-  output      [ 2:0]      adc_chsel,
+  output [ 1:0]           adc_hw_rngsel,
+  output [ 2:0]           adc_chsel,
   output                  adc_crcen,
   output                  adc_burst,
-  output      [ 2:0]      adc_os
+  output [ 2:0]           adc_os
 );
 
   // internal signals
 
-  wire    [63:0]  gpio_i;
-  wire    [63:0]  gpio_o;
-  wire    [63:0]  gpio_t;
-  wire    [ 1:0]  iic_mux_scl_i_s;
-  wire    [ 1:0]  iic_mux_scl_o_s;
-  wire            iic_mux_scl_t_s;
-  wire    [ 1:0]  iic_mux_sda_i_s;
-  wire    [ 1:0]  iic_mux_sda_o_s;
-  wire            iic_mux_sda_t_s;
+  wire   [63:0]           gpio_i;
+  wire   [63:0]           gpio_o;
+  wire   [63:0]           gpio_t;
+  wire   [ 1:0]           iic_mux_scl_i_s;
+  wire   [ 1:0]           iic_mux_scl_o_s;
+  wire                    iic_mux_scl_t_s;
+  wire   [ 1:0]           iic_mux_sda_i_s;
+  wire   [ 1:0]           iic_mux_sda_o_s;
+  wire                    iic_mux_sda_t_s;
 
   // instantiations
 
@@ -119,13 +111,13 @@ module system_top (
     .dio_t (gpio_t[43:32]),
     .dio_i (gpio_o[43:32]),
     .dio_o (gpio_i[43:32]),
-    .dio_p ({adc_reset_n,        // 43
-             adc_hw_rngsel,      // 42:41
-             adc_os,             // 40:38
-             adc_seq_en,         // 37
-             adc_burst,          // 36
-             adc_chsel,          // 35:33
-             adc_crcen}));       // 32
+    .dio_p ({adc_reset_n,   // 43
+             adc_hw_rngsel, // 42:41
+             adc_os,        // 40:38
+             adc_seq_en,    // 37
+             adc_burst,     // 36
+             adc_chsel,     // 35:33
+             adc_crcen}));  // 32
 
   ad_iobuf #(
     .DATA_WIDTH(32)
