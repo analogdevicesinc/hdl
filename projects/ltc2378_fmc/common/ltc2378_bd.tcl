18667ea5122f7735b30deaa8918654a83b13124c
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2025-05-22 11:26:11 +0300

    projects/ltc2378: Project development
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/ltc2378_fmc/common/ltc2378_bd.tcl
+++ projects/ltc2378_fmc/common/ltc2378_bd.tcl
@@ -6,6 +6,7 @@
 create_bd_intf_port -mode Master -vlnv analog.com:interface:spi_engine_rtl:1.0 ltc2378_spi
 
 create_bd_port -dir I ltc2378_busy
+create_bd_port -dir O ltc2378_cnv
 
 source $ad_hdl_dir/library/spi_engine/scripts/spi_engine.tcl
 
@@ -47,12 +48,12 @@ ad_connect busy_sync/out_resetn $hier_spi_engine/${hier_spi_engine}_axi_regmap/s
 ad_connect spi_clk busy_sync/out_clk
 ad_connect busy_sync/in_bits ltc2378_busy
 ad_connect busy_sync/out_bits busy_capture/signal_in
-ad_connect busy_capture/signal_out $hier_spi_engine/trigger
+ad_connect busy_capture/signal_out $hier_spi_engine/${hier_spi_engine}_offload/trigger
 
 ad_connect spi_clk ltc2378_trigger_gen/ext_clk
 ad_connect $sys_cpu_clk ltc2378_trigger_gen/s_axi_aclk
 ad_connect sys_cpu_resetn ltc2378_trigger_gen/s_axi_aresetn
-ad_connect ltc2378_trigger_gen/pwm_0 $hier_spi_engine/trigger
+ad_connect ltc2378_trigger_gen/pwm_0 ltc2378_cnv
 
 # dma to receive data stream
 ad_ip_instance axi_dmac ltc2378_dma
@@ -70,6 +71,7 @@ ad_connect ltc2378_dma/s_axis $hier_spi_engine/M_AXIS_SAMPLE
 ad_connect $hier_spi_engine/m_spi ltc2378_spi
 ad_connect spi_clk $hier_spi_engine/spi_clk
 ad_connect sys_cpu_resetn $hier_spi_engine/resetn
+ad_connect spi_clk ltc2378_dma/s_axis_aclk
 ad_connect sys_cpu_resetn ltc2378_dma/m_dest_axi_aresetn
 
 # AXI address definitions
