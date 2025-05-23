4b249c85c8c6610cc48aec827581d987e368642e
Laez Barbosa <laez.barbosa@analog.com>
2025-05-23 14:38:25 -0300

    projects/ad4052/de10nano: fix timing
    remove pipelining in system interconnect
    
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

--- projects/ad4052_ardz/de10nano/system_qsys.tcl
+++ projects/ad4052_ardz/de10nano/system_qsys.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2024 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2024-2025 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -13,3 +13,6 @@ set_instance_parameter_value rom_sys_0 {ROM_ADDR_BITS} {9}
 set_instance_parameter_value rom_sys_0 {PATH_TO_FILE} "$mem_init_sys_file_path/mem_init_sys.txt"
 
 sysid_gen_sys_init_file
+
+# Set a lower limit than the default of 4 for pipeline stages on the memnory-mapped interconnect
+set_interconnect_requirement {$system} {qsys_mm.maxAdditionalLatency} {1}
