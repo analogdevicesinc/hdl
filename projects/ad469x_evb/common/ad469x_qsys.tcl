ea6077e4b6821fe7e75a1a9aa6c3d0bbd47ef728
Laez Barbosa <laez.barbosa@analog.com>
2025-05-30 15:43:28 -0300

    projects/ad469x_evb: change to pll-provided 160mhz clock
    Project was trying to use a HPS-generated 160MHz, which is not allowed and
    defaults to a maximum 100MHz clock.
    
    Signed-off-by: Laez Barbosa <laez.barbosa@analog.com>

--- projects/ad469x_evb/common/ad469x_qsys.tcl
+++ projects/ad469x_evb/common/ad469x_qsys.tcl
@@ -6,9 +6,6 @@
 # receive dma
 set_instance_parameter_value sys_hps {F2SDRAM_Width} {128 64}
 
-# set spi_clk to 160MHz
-set_instance_parameter_value sys_hps {S2FCLK_USER0CLK_FREQ} {160.0}
-
 add_instance axi_dmac_0 axi_dmac
 set_instance_parameter_value axi_dmac_0 {DMA_TYPE_SRC} {1}
 set_instance_parameter_value axi_dmac_0 {DMA_TYPE_DEST} {0}
@@ -65,6 +62,47 @@ set_instance_parameter_value ad469x_trigger_gen {N_PWMS} {1}
 set_instance_parameter_value ad469x_trigger_gen {PULSE_0_PERIOD} {160}
 set_instance_parameter_value ad469x_trigger_gen {PULSE_0_WIDTH} {1}
 
+# spi_clk pll
+
+add_instance spi_clk_pll altera_pll
+set_instance_parameter_value spi_clk_pll {gui_feedback_clock} {Global Clock}
+set_instance_parameter_value spi_clk_pll {gui_operation_mode} {direct}
+set_instance_parameter_value spi_clk_pll {gui_number_of_clocks} {1}
+set_instance_parameter_value spi_clk_pll {gui_output_clock_frequency0} {160}
+set_instance_parameter_value spi_clk_pll {gui_phase_shift0} {0}
+set_instance_parameter_value spi_clk_pll {gui_phase_shift1} {0}
+set_instance_parameter_value spi_clk_pll {gui_phase_shift_deg0} {0.0}
+set_instance_parameter_value spi_clk_pll {gui_phase_shift_deg1} {0.0}
+set_instance_parameter_value spi_clk_pll {gui_phout_division} {1}
+set_instance_parameter_value spi_clk_pll {gui_pll_auto_reset} {Off}
+set_instance_parameter_value spi_clk_pll {gui_pll_bandwidth_preset} {Auto}
+set_instance_parameter_value spi_clk_pll {gui_pll_mode} {Fractional-N PLL}
+set_instance_parameter_value spi_clk_pll {gui_ps_units0} {ps}
+set_instance_parameter_value spi_clk_pll {gui_refclk_switch} {0}
+set_instance_parameter_value spi_clk_pll {gui_reference_clock_frequency} {50.0}
+set_instance_parameter_value spi_clk_pll {gui_switchover_delay} {0}
+set_instance_parameter_value spi_clk_pll {gui_en_reconf} {1}
+
+add_instance spi_clk_pll_reconfig altera_pll_reconfig
+set_instance_parameter_value spi_clk_pll_reconfig {ENABLE_BYTEENABLE} {0}
+set_instance_parameter_value spi_clk_pll_reconfig {ENABLE_MIF} {0}
+set_instance_parameter_value spi_clk_pll_reconfig {MIF_FILE_NAME} {}
+
+add_connection spi_clk_pll.reconfig_from_pll spi_clk_pll_reconfig.reconfig_from_pll
+set_connection_parameter_value spi_clk_pll.reconfig_from_pll/spi_clk_pll_reconfig.reconfig_from_pll endPort {}
+set_connection_parameter_value spi_clk_pll.reconfig_from_pll/spi_clk_pll_reconfig.reconfig_from_pll endPortLSB {0}
+set_connection_parameter_value spi_clk_pll.reconfig_from_pll/spi_clk_pll_reconfig.reconfig_from_pll startPort {}
+set_connection_parameter_value spi_clk_pll.reconfig_from_pll/spi_clk_pll_reconfig.reconfig_from_pll startPortLSB {0}
+set_connection_parameter_value spi_clk_pll.reconfig_from_pll/spi_clk_pll_reconfig.reconfig_from_pll width {0}
+
+add_connection spi_clk_pll.reconfig_to_pll spi_clk_pll_reconfig.reconfig_to_pll
+set_connection_parameter_value spi_clk_pll.reconfig_to_pll/spi_clk_pll_reconfig.reconfig_to_pll endPort {}
+set_connection_parameter_value spi_clk_pll.reconfig_to_pll/spi_clk_pll_reconfig.reconfig_to_pll endPortLSB {0}
+set_connection_parameter_value spi_clk_pll.reconfig_to_pll/spi_clk_pll_reconfig.reconfig_to_pll startPort {}
+set_connection_parameter_value spi_clk_pll.reconfig_to_pll/spi_clk_pll_reconfig.reconfig_to_pll startPortLSB {0}
+set_connection_parameter_value spi_clk_pll.reconfig_to_pll/spi_clk_pll_reconfig.reconfig_to_pll width {0}
+
+
 # exported interface
 
 add_interface ad469x_spi_sclk       clock source
@@ -87,29 +125,34 @@ add_connection axi_spi_engine_0.if_spi_resetn reset_bridge_0.in_reset
 
 # clocks
 
-add_interface ad469x_spi_clk        clock source
-set_interface_property ad469x_spi_clk     EXPORT_OF clock_bridge_0.out_clk
+add_interface ad469x_spi_clk            clock source
+set_interface_property ad469x_spi_clk   EXPORT_OF clock_bridge_0.out_clk
 
+
+add_connection sys_clk.clk spi_clk_pll.refclk
+add_connection sys_clk.clk spi_clk_pll_reconfig.mgmt_clk
 add_connection sys_clk.clk ad469x_trigger_gen.s_axi_clock
 add_connection sys_clk.clk axi_spi_engine_0.s_axi_clock
 add_connection sys_clk.clk axi_dmac_0.s_axi_clock
 
-add_connection sys_dma_clk.clk ad469x_trigger_gen.if_ext_clk
-add_connection sys_dma_clk.clk spi_engine_execution_0.if_clk
-add_connection sys_dma_clk.clk spi_engine_interconnect_0.if_clk
-add_connection sys_dma_clk.clk axi_spi_engine_0.if_spi_clk
-add_connection sys_dma_clk.clk spi_engine_offload_0.if_ctrl_clk
-add_connection sys_dma_clk.clk spi_engine_offload_0.if_spi_clk
-add_connection sys_dma_clk.clk axi_dmac_0.if_s_axis_aclk
+add_connection spi_clk_pll.outclk0 ad469x_trigger_gen.if_ext_clk
+add_connection spi_clk_pll.outclk0 spi_engine_execution_0.if_clk
+add_connection spi_clk_pll.outclk0 spi_engine_interconnect_0.if_clk
+add_connection spi_clk_pll.outclk0 axi_spi_engine_0.if_spi_clk
+add_connection spi_clk_pll.outclk0 spi_engine_offload_0.if_ctrl_clk
+add_connection spi_clk_pll.outclk0 spi_engine_offload_0.if_spi_clk
+add_connection spi_clk_pll.outclk0 axi_dmac_0.if_s_axis_aclk
+add_connection spi_clk_pll.outclk0 clock_bridge_0.in_clk
+
 add_connection sys_dma_clk.clk axi_dmac_0.m_dest_axi_clock
-add_connection sys_dma_clk.clk clock_bridge_0.in_clk
 
 # resets
 
-add_connection sys_clk.clk_reset ad469x_trigger_gen.s_axi_reset
-
+add_connection sys_clk.clk_reset spi_clk_pll.reset
+add_connection sys_clk.clk_reset spi_clk_pll_reconfig.mgmt_reset
 add_connection sys_clk.clk_reset axi_spi_engine_0.s_axi_reset
 add_connection sys_clk.clk_reset axi_dmac_0.s_axi_reset
+add_connection sys_clk.clk_reset ad469x_trigger_gen.s_axi_reset
 
 add_connection axi_spi_engine_0.if_spi_resetn spi_engine_execution_0.if_resetn
 add_connection axi_spi_engine_0.if_spi_resetn spi_engine_interconnect_0.if_resetn
