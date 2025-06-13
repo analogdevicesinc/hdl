de7b168b56837f67d1c5299b997fcd2453b1ad55
PopPaul2021 <paul.pop@analog.com>
2025-06-13 16:38:25 +0300

    projects/ad488x_fmc_evb: Add EVAL-AD4880 support
    Signed-off-by: PopPaul2021 <paul.pop@analog.com>

---
+++ projects/ad488x_fmc_evb/README.md
@@ -0,0 +1,15 @@
+# AD488X-FMC-EVB HDL Project
+
+- Evaluation board product page: [EVAL-AD4880-FMC](https://www.analog.com/eval-ad4880-fmc)
+- System documentation: TO BE ADDED
+- HDL project documentation: https://analogdevicesinc.github.io/hdl/projects/ad488x_fmc_evb/index.html
+
+## Supported parts
+
+| Part name                               | Description                           |
+|-----------------------------------------|---------------------------------------|
+| [AD4880](https://www.analog.com/ad4880) | Dual Channel 20-Bit, 40 MSPS, SAR ADC |
+
+## Building the project
+
+Please enter the folder for the FPGA carrier you want to use and read the README.md.
---
+++ projects/ad488x_fmc_evb/zed/system_project.tcl
@@ -0,0 +1,17 @@
+###############################################################################
+## Copyright (C) 2025 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source ../../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project ad488x_fmc_evb_zed
+adi_project_files ad488x_fmc_evb_zed [list \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" \
+  "system_constr.xdc" \
+  "system_top.v" ]
+
+adi_project_run ad488x_fmc_evb_zed
