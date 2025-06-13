6463ac024d886cdb25817099580f4897376478cd
PopPaul2021 <paul.pop@analog.com>
2025-06-13 12:33:17 +0300

    projects/admx6001_ebz: Add reference design for ADMX6001-EBZ
    The projects instantiates the JESD204b Framework for the AD9213
    converter and the AXI-AD408X custom IP for the AD4080 converter.
    The data paths are synchronized by one of the HMC7044 channels.
    
    Signed-off-by: PopPaul2021 <paul.pop@analog.com>

---
+++ projects/admx6001_ebz/README.md
@@ -0,0 +1,16 @@
+# ADMX6001-EBZ HDL Project
+
+- Evaluation board product page: [EVAL-ADMX6001-EBZ](https://www.analog.com/eval-admx6001-ebz)
+- System documentation: TO BE ADDED
+- HDL project documentation: https://analogdevicesinc.github.io/hdl/projects/admx6001_ebz/index.html
+
+## Supported parts
+
+| Part name                               | Description                                                  |
+|-----------------------------------------|--------------------------------------------------------------|
+| [AD4080](https://www.analog.com/ad4080) | 20-Bit, 40 MSPS, Differential SAR ADC                        |
+| [AD9213](https://www.analog.com/ad9213) | 12-Bit, 10.25 GSPS, JESD204B, RF Analog-to-Digital Converter |
+
+## Building the project
+
+Please enter the folder for the FPGA carrier you want to use and read the README.md.
---
+++ projects/admx6001_ebz/vcu118/system_project.tcl
@@ -0,0 +1,22 @@
+###############################################################################
+## Copyright (C) 2025 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source ../../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project admx6001_ebz_vcu118
+
+adi_project_files admx6001_ebz_vcu118 [list \
+  "$ad_hdl_dir/library/common/ad_3w_spi.v" \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/vcu118/vcu118_system_constr.xdc" \
+  "system_constr.xdc" \
+  "system_top.v" ]
+
+## To improve timing in DDR4 MIG
+set_property strategy Performance_SpreadSLLs [get_runs impl_1]
+
+adi_project_run admx6001_ebz_vcu118
