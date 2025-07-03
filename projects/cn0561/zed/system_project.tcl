94b69766d1b0aae013eb8273d4bb1d288b200c9b
Capota Bianca <bianca-ramona.capota@analog.com>
2025-07-03 14:29:04 +0300

    projects/cn0561:Update VADJ value in constraints for ZED

--- projects/cn0561/zed/system_project.tcl
+++ projects/cn0561/zed/system_project.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2022-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2022-2023, 2025 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -12,7 +12,6 @@ adi_project cn0561_zed
 adi_project_files cn0561_zed [list \
     "$ad_hdl_dir/library/common/ad_iobuf.v" \
     "system_top.v" \
-    "system_constr.xdc" \
-    "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"]
+    "system_constr.xdc" ]
 
 adi_project_run cn0561_zed
