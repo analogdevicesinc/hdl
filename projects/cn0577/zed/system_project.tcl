6d09b9a53dc080afb48e30de0fa7ebb00a8b9124
Iulia Moldovan <Iulia.Moldovan@analog.com>
2025-07-07 12:05:07 +0300

    projects/cn0577 & library/axi_ltc2387: Add make parameters
    - projects/cn0577: Added make parameters ADC_RES (the ADC input
      resolution) and TWOLANES (two-lane mode or one-lane mode). Also fixed
      the sysid parameters logging
    - library/axi_ltc2387/axi_ltc2387_if.v: Changed the parameter name RESOLUTION
      to ADC_RES, as it was redundant
    
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- projects/cn0577/zed/system_project.tcl
+++ projects/cn0577/zed/system_project.tcl
@@ -1,14 +1,28 @@
 ###############################################################################
-## Copyright (C) 2022-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2022-2023, 2025 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
-# load script
+# load scripts
 source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project cn0577_zed
+# TWOLANES: parameter describing the number of lanes
+# - 1: in two-lane mode (default)
+# - 0: in one-lane mode
+#
+# ADC_RES: parameter describing the ADC input resolution
+# - 18: 18 bits (default)
+# - 16: 16 bits
+#
+# in one-lane mode (TWOLANES=0), only the 18-bit resolution is supported! (ADC_RES=16)
+
+adi_project cn0577_zed 0 [list \
+  TWOLANES  [get_env_param TWOLANES  1 ] \
+  ADC_RES   [get_env_param ADC_RES  18 ] \
+]
+
 adi_project_files cn0577_zed [list \
   "system_top.v" \
   "system_constr.xdc" \
