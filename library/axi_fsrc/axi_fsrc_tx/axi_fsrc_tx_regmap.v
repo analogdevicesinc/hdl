4b68412fb736debcc8c92c45613428cfac3217a5
Jorge Marques <jorge.marques@analog.com>
2025-07-07 18:30:34 +0200

    axi_fsrc: Remove sync_bits from slow clk
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- library/axi_fsrc/axi_fsrc_tx/axi_fsrc_tx_regmap.v
+++ library/axi_fsrc/axi_fsrc_tx/axi_fsrc_tx_regmap.v
@@ -48,7 +48,7 @@ module axi_fsrc_tx_regmap #(
   output        stop,
   output        change_rate,
   output        accum_set,
-  output [15:0] conv_mask,
+  output reg [15:0] conv_mask,
   output [ACCUM_WIDTH-1:0] accum_add_val,
   output reg [NUM_SAMPLES-1:0][ACCUM_WIDTH-1:0] accum_set_val,
 
@@ -68,18 +68,12 @@ module axi_fsrc_tx_regmap #(
   output reg              up_rack
 );
 
-  wire  [3:0] accum_set_val_addr;
-  wire [63:0] accum_set_val_s;
-  wire accum_set_val_apply;
-  wire [31:0] enable_s;
-  wire [31:0] ctrl_transmit;
-  wire [31:0] conv_mask_s;
-  wire [31:0] up_debug_flags_s;
-
   wire stop_s;
   wire start_s;
   wire change_rate_s;
   wire accum_set_s;
+  wire [31:0] debug_flags_s;
+  wire [ACCUM_WIDTH-1:0] accum_set_val_s;
 
   reg stop_reg;
   reg start_reg;
@@ -87,63 +81,63 @@ module axi_fsrc_tx_regmap #(
   reg accum_set_reg;
 
   // internal registers
-  reg [31:0] up_scratch_s;
-  reg [31:0] up_enable_s;
-  reg [31:0] up_ctrl_transmit_s;
-  reg [31:0] up_conv_mask_s;
-  reg [31:0] up_accum_add_val_0_s;
-  reg [31:0] up_accum_add_val_1_s;
-  reg  [3:0] up_accum_set_val_addr_s;
-  reg        up_accum_set_val_apply_s;
-  reg [31:0] up_accum_set_val_s;
-  reg [31:0] up_accum_set_val_2_s;
+  reg [31:0] scratch_reg;
+  reg [31:0] enable_reg;
+  reg [31:0] ctrl_transmit_reg;
+  reg [31:0] conv_mask_reg;
+  reg [31:0] accum_add_val_0_reg;
+  reg [31:0] accum_add_val_1_reg;
+  reg  [3:0] accum_set_val_addr_reg;
+  reg        accum_set_val_apply_reg;
+  reg [31:0] accum_set_val_0_reg;
+  reg [31:0] accum_set_val_1_reg;
 
   always @(posedge up_clk) begin
     if (up_rstn == 0) begin
       up_wack <= 'd0;
-      up_scratch_s <= 'd0;
-      up_enable_s <= 'd0;
-      up_ctrl_transmit_s <= 'd0;
-      up_conv_mask_s <= 'd0;
-      up_accum_add_val_0_s <= 'd0;
-      up_accum_add_val_1_s <= 'd0;
-      up_accum_set_val_addr_s <= 'd0;
-      up_accum_set_val_apply_s <= 1'b0;
-      up_accum_set_val_s <= 'd0;
-      up_accum_set_val_2_s <= 'd0;
+      scratch_reg <= 'd0;
+      enable_reg <= 'd0;
+      ctrl_transmit_reg <= 'd0;
+      conv_mask_reg <= 'd0;
+      accum_add_val_0_reg <= 'd0;
+      accum_add_val_1_reg <= 'd0;
+      accum_set_val_addr_reg <= 'd0;
+      accum_set_val_apply_reg <= 1'b0;
+      accum_set_val_0_reg <= 'd0;
+      accum_set_val_1_reg <= 'd0;
     end else begin
       up_wack <= up_wreq;
       if ((up_wreq == 1'b1) && (up_waddr == 14'h2)) begin
-        up_scratch_s <= up_wdata;
+        scratch_reg <= up_wdata;
       end
       if ((up_wreq == 1'b1) && (up_waddr == 14'h4)) begin
-        up_enable_s <= up_wdata;
+        enable_reg <= up_wdata;
       end
       if ((up_wreq == 1'b1) && (up_waddr == 14'h5)) begin
-        up_ctrl_transmit_s <= up_wdata;
+        ctrl_transmit_reg <= up_wdata;
       end else begin
-        up_ctrl_transmit_s <= 'd0;
+        ctrl_transmit_reg <= 'd0;
       end
       if ((up_wreq == 1'b1) && (up_waddr == 14'h6)) begin
-       up_conv_mask_s <= up_wdata;
+       conv_mask_reg <= up_wdata[15:0];
       end
       if ((up_wreq == 1'b1) && (up_waddr == 14'h7)) begin
-        up_accum_add_val_0_s <= up_wdata;
+        accum_add_val_0_reg <= up_wdata;
       end
       if ((up_wreq == 1'b1) && (up_waddr == 14'h8)) begin
-        up_accum_add_val_1_s <= up_wdata;
+        accum_add_val_1_reg <= up_wdata;
       end
       if ((up_wreq == 1'b1) && (up_waddr == 14'h9)) begin
-        up_accum_set_val_addr_s <= up_wdata[3:0];
-        up_accum_set_val_apply_s <= 1'b1;
+        accum_set_val_addr_reg <= up_wdata[3:0];
+        accum_set_val_apply_reg <= 1'b1;
       end
       if ((up_wreq == 1'b1) && (up_waddr == 14'ha)) begin
-        up_accum_set_val_s <= up_wdata;
-        up_accum_set_val_apply_s <= 1'b0;
+        accum_set_val_0_reg <= up_wdata;
+        accum_set_val_apply_reg <= 1'b0;
       end
       if ((up_wreq == 1'b1) && (up_waddr == 14'hb)) begin
-        up_accum_set_val_2_s <= up_wdata;
-        up_accum_set_val_apply_s <= 1'b0;
+        accum_set_val_1_reg <= up_wdata;
+        accum_set_val_apply_reg <= 1'b0;
       end
     end
   end
@@ -158,18 +152,18 @@ module axi_fsrc_tx_regmap #(
         case (up_raddr)
           14'h0:   up_rdata <= CORE_VERSION;
           14'h1:   up_rdata <= ID;
-          14'h2:   up_rdata <= up_scratch_s;
+          14'h2:   up_rdata <= scratch_reg;
           14'h3:   up_rdata <= CORE_MAGIC;
-          14'h4:   up_rdata <= up_enable_s;
-          14'h5:   up_rdata <= up_ctrl_transmit_s;
-          14'h6:   up_rdata <= up_conv_mask_s;
-          14'h7:   up_rdata <= up_accum_add_val_0_s;
-          14'h8:   up_rdata <= up_accum_add_val_1_s;
-          14'h9:   up_rdata <= {28'b0, up_accum_set_val_addr_s};
-          14'ha:   up_rdata <= up_accum_set_val_s;
-          14'hb:   up_rdata <= up_accum_set_val_2_s;
+          14'h4:   up_rdata <= enable_reg;
+          14'h5:   up_rdata <= ctrl_transmit_reg;
+          14'h6:   up_rdata <= conv_mask_reg;
+          14'h7:   up_rdata <= accum_add_val_0_reg;
+          14'h8:   up_rdata <= accum_add_val_1_reg;
+          14'h9:   up_rdata <= {28'b0, accum_set_val_addr_reg};
+          14'ha:   up_rdata <= accum_set_val_0_reg;
+          14'hb:   up_rdata <= accum_set_val_1_reg;
           14'hc:   up_rdata <= ACCUM_WIDTH;
-          14'hd:   up_rdata <= up_debug_flags_s;
+          14'hd:   up_rdata <= debug_flags_s;
           default: up_rdata <= 0;
         endcase
       end else begin
@@ -181,56 +175,17 @@ module axi_fsrc_tx_regmap #(
   sync_bits #(
     .NUM_OF_BITS (32),
     .ASYNC_CLK (1)
-  ) enable_sync (
-    .in_bits(up_enable_s),
-    .out_clk(clk),
-    .out_bits(enable_s));
-
-  sync_bits #(
-    .NUM_OF_BITS (32),
-    .ASYNC_CLK (1)
-  ) ctrl_transmit_sync (
-    .in_bits(up_ctrl_transmit_s),
-    .out_clk(clk),
-    .out_bits(ctrl_transmit));
-
-  sync_bits #(
-    .NUM_OF_BITS (32),
-    .ASYNC_CLK (1)
-  ) conv_mask_sync (
-    .in_bits(up_conv_mask_s),
-    .out_clk(clk),
-    .out_bits(conv_mask_s));
-
-  sync_bits #(
-    .NUM_OF_BITS (ACCUM_WIDTH),
-    .ASYNC_CLK (1)
-  ) accum_add_val_sync_0 (
-    .in_bits({up_accum_add_val_1_s, up_accum_add_val_0_s}),
-    .out_clk(clk),
-    .out_bits(accum_add_val));
-
-  sync_bits #(
-    .NUM_OF_BITS (ACCUM_WIDTH + 5),
-    .ASYNC_CLK (1)
-  ) accum_set_val_sync (
-    .in_bits({up_accum_set_val_2_s, up_accum_set_val_s, up_accum_set_val_addr_s, up_accum_set_val_apply_s}),
-    .out_clk(clk),
-    .out_bits({accum_set_val_s, accum_set_val_addr, accum_set_val_apply}));
-
-  sync_bits #(
-    .NUM_OF_BITS (32),
-    .ASYNC_CLK (1)
   ) debug_flags_sync (
     .in_bits(debug_flags),
+    .out_resetn (~reset),
     .out_clk(up_clk),
-    .out_bits(up_debug_flags_s));
+    .out_bits(debug_flags_s));
 
   genvar j;
   for (j=0; j < NUM_SAMPLES; j=j+1) begin : set_val_gen
     always @(posedge clk) begin
       // Write value first then address to apply
-      if (j == accum_set_val_addr && accum_set_val_apply) begin
+      if (j == accum_set_val_addr_reg && accum_set_val_apply_reg) begin
         accum_set_val[j] <= accum_set_val_s;
       end
     end
@@ -243,13 +198,14 @@ module axi_fsrc_tx_regmap #(
     stop_reg <= stop_s;
   end
 
-  assign enable          = enable_s[0];
-  assign ext_trig_en     = enable_s[1];
-  assign start_s         = ctrl_transmit[0];
-  assign stop_s          = ctrl_transmit[1];
-  assign accum_set_s     = ctrl_transmit[2];
-  assign change_rate_s   = ctrl_transmit[3];
-  assign conv_mask       = conv_mask_s[15:0];
+  assign enable        = enable_reg[0];
+  assign ext_trig_en   = enable_reg[1];
+  assign start_s       = ctrl_transmit_reg[0];
+  assign stop_s        = ctrl_transmit_reg[1];
+  assign accum_set_s   = ctrl_transmit_reg[2];
+  assign change_rate_s = ctrl_transmit_reg[3];
+  assign accum_add_val = {accum_add_val_1_reg, accum_add_val_0_reg};
+  assign accum_set_val_s = {accum_set_val_1_reg, accum_set_val_0_reg};
 
   assign change_rate = change_rate_s && ~change_rate_reg; // should go to apollo?
   assign accum_set = accum_set_s && ~accum_set_reg;
