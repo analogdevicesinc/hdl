136a4eb7ebea828a817cdd92ef01c7d670697fda
Istvan-Zsolt Szekely <istvan.szekely@analog.com>
2025-07-15 11:24:45 +0300

    library/util_axis_fifo: Full/empty FIFO information for both interfaces in case of CDC
    Signed-off-by: Istvan-Zsolt Szekely <istvan.szekely@analog.com>

--- library/util_axis_fifo/util_axis_fifo_address_generator.v
+++ library/util_axis_fifo/util_axis_fifo_address_generator.v
@@ -50,6 +50,8 @@ module util_axis_fifo_address_generator #(
   output m_axis_valid,
   output m_axis_empty,
   output m_axis_almost_empty,
+  output m_axis_full,
+  output m_axis_almost_full,
   output [ADDRESS_WIDTH-1:0] m_axis_raddr,
   output [ADDRESS_WIDTH-1:0] m_axis_level,
 
@@ -59,8 +61,10 @@ module util_axis_fifo_address_generator #(
   input s_axis_aresetn,
   output s_axis_ready,
   input s_axis_valid,
-  output  s_axis_full,
-  output  s_axis_almost_full,
+  output s_axis_empty,
+  output s_axis_almost_empty,
+  output s_axis_full,
+  output s_axis_almost_full,
   output [ADDRESS_WIDTH-1:0] s_axis_waddr,
   output [ADDRESS_WIDTH-1:0] s_axis_room
 );
@@ -150,6 +154,8 @@ module util_axis_fifo_address_generator #(
   //------------------------------------------------------------------------------
 
   wire [ADDRESS_WIDTH:0] s_axis_fifo_fill = s_axis_waddr_reg - s_axis_raddr_reg;
+  assign s_axis_empty = (s_axis_fifo_fill == {ADDRESS_WIDTH{1'b0}});
+  assign s_axis_almost_empty = s_axis_fifo_fill < ALMOST_EMPTY_THRESHOLD;
   assign s_axis_full = (s_axis_fifo_fill == {ADDRESS_WIDTH{1'b1}});
   assign s_axis_almost_full = s_axis_fifo_fill > {1'b0, ~ALMOST_FULL_THRESHOLD};
   assign s_axis_ready = ~s_axis_full;
@@ -164,8 +170,10 @@ module util_axis_fifo_address_generator #(
   //------------------------------------------------------------------------------
 
   wire [ADDRESS_WIDTH:0] m_axis_fifo_fill = m_axis_waddr_reg - m_axis_raddr_reg;
-  assign m_axis_empty = m_axis_fifo_fill == 0;
+  assign m_axis_empty = (m_axis_fifo_fill == {ADDRESS_WIDTH{1'b0}});
   assign m_axis_almost_empty = (m_axis_fifo_fill < ALMOST_EMPTY_THRESHOLD);
+  assign m_axis_full = (m_axis_fifo_fill == {ADDRESS_WIDTH{1'b1}});
+  assign m_axis_almost_full = (m_axis_fifo_fill > {1'b0, ~ALMOST_FULL_THRESHOLD});
   assign m_axis_valid = ~m_axis_empty;
   assign m_axis_level = m_axis_fifo_fill;
 
