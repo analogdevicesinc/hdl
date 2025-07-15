c1ba60026ed0fccf1fc2e0ae5ec4d3427f5f4f5d
Liviu 'Ceshu' Adace <liviu.adace@analog.com>
2025-07-15 18:11:47 +0300

    projects: ad9740_fmc: Initial version of AD974x DACs
    Signed-off-by: Liviu 'Ceshu' Adace <liviu.adace@analog.com>

---
+++ projects/ad9740_fmc/common/ad9740_fmc_bd.tcl
@@ -0,0 +1,62 @@
+###############################################################################
+## Copyright (C) 2025 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+# bd ports
+
+create_bd_port -dir I ad9740_clk
+create_bd_port -dir O ad9740_valid
+create_bd_port -dir I ad9740_ready
+create_bd_port -dir O -from 15 -to 0 ad9740_data
+
+# dma to send sample data
+
+ad_ip_instance axi_dmac ad9740_dma
+ad_ip_parameter ad9740_dma CONFIG.DMA_TYPE_SRC 0
+ad_ip_parameter ad9740_dma CONFIG.DMA_TYPE_DEST 1
+ad_ip_parameter ad9740_dma CONFIG.CYCLIC 0
+ad_ip_parameter ad9740_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter ad9740_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter ad9740_dma CONFIG.DMA_DATA_WIDTH_SRC 32
+ad_ip_parameter ad9740_dma CONFIG.DMA_DATA_WIDTH_DEST 16
+
+ad_ip_instance axi_ad974x ad9740_dac
+
+# clocks
+
+ad_connect ad9740_clk ad9740_dma/m_axis_aclk
+ad_connect ad9740_clk ad9740_dac/dac_clk
+
+# resets
+
+ad_connect sys_rstgen/peripheral_aresetn ad9740_dma/m_src_axi_aresetn
+
+# data path
+
+ad_connect ad9740_dma/m_axis ad9740_dac/s_axis
+ad_connect ad9740_dma/m_axis_valid ad9740_valid
+ad_connect ad9740_dma/m_axis_ready ad9740_ready
+
+# Tie unused inputs to GND
+
+ad_connect ad9740_dac/valid_in_dma_sec GND
+ad_connect ad9740_dac/data_in_a GND
+ad_connect ad9740_dac/data_in_b GND
+ad_connect ad9740_dac/valid_in_a GND
+ad_connect ad9740_dac/valid_in_b GND
+ad_connect ad9740_dac/external_sync GND
+
+# AXI address definitions
+
+ad_cpu_interconnect 0x44a40000 ad9740_dma
+ad_cpu_interconnect 0x44a70000 ad9740_dac
+
+# interrupts
+
+ad_cpu_interrupt "ps-13" "mb-13" ad9740_dma/irq
+
+# memory interconnects
+
+ad_mem_hp1_interconnect $sys_cpu_clk sys_ps7/S_AXI_HP1
+ad_mem_hp1_interconnect $sys_cpu_clk ad9740_dma/m_src_axi
