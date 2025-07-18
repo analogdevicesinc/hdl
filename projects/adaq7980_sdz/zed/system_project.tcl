7b2e2394e0917f3fe7d69217073484583910c35f
Elena-Hadarau_adi <Elena.Hadarau@analog.com>
2025-07-18 17:23:47 +0300

    projects/adaq7980_sdz: Fix constraint file
    Signed-off-by: Elena-Hadarau_adi <Elena.Hadarau@analog.com>

--- projects/adaq7980_sdz/zed/system_project.tcl
+++ projects/adaq7980_sdz/zed/system_project.tcl
@@ -13,7 +13,7 @@ adi_project_files adaq7980_sdz_zed [list \
     "$ad_hdl_dir/library/common/ad_iobuf.v" \
     "system_top.v" \
     "system_constr.xdc" \
-    "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"]
+    ]
 
 adi_project_run adaq7980_sdz_zed
 
