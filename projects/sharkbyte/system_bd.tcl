ee9bbf9eb6c53c056b19dcba036c26f3f598006b
PopPaul2021 <paul.pop@analog.com>
2025-07-31 16:49:06 +0300

    checkpoint
    Signed-off-by: PopPaul2021 <paul.pop@analog.com>

--- projects/sharkbyte/system_bd.tcl
+++ projects/sharkbyte/system_bd.tcl
@@ -140,7 +140,7 @@ ad_ip_parameter hmcad15xx_a2_dma CONFIG.MAX_BYTES_PER_BURST 4096
 ad_ip_instance axi_hmcad15xx axi_hmcad15xx_a1_adc
 ad_ip_parameter axi_hmcad15xx_a1_adc CONFIG.IODELAY_CTRL 1
 ad_ip_parameter axi_hmcad15xx_a1_adc CONFIG.NUM_CHANNELS 4
-ad_ip_parameter axi_hmcad15xx_a1_adc CONFIG.POLARITY_MASK 255
+ad_ip_parameter axi_hmcad15xx_a1_adc CONFIG.POLARITY_MASK 239
 
 ad_connect axi_hmcad15xx_a1_adc/s_axi_aclk    sys_cpu_clk
 ad_connect axi_hmcad15xx_a1_adc/clk_in_p      clk_in_a1_p
