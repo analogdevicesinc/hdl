4392c503ce7fcb4bfa4bf456155a2fd52699bcde
caosjr <carlos.souza@analog.com>
2025-08-05 16:26:10 -0300

    library/spi_engine: update spi lane mask instruction
    There is a different register for SDI lane mask and SDO
    lane mask;
    Update the NUM_OF_SDI parameter to NUM_OF_SDIO parameter to reflect
    that both SDI and SDO are symmetrical. The control over them is
    through the SDI/SDO lane masks.
    
    Inserts a ready signal for when the valid_indices
    has finished its inner logic. This avoid the possible
    issue where the latency of the command is smaller than
    this logic. This could only happen in the FIFO mode.
    
    Signed-off-by: Carlos Souza <carlos.souza@analog.com>

--- library/spi_engine/spi_engine_interconnect/spi_engine_interconnect_ip.tcl
+++ library/spi_engine/spi_engine_interconnect/spi_engine_interconnect_ip.tcl
@@ -88,13 +88,13 @@ set_property -dict [list \
  ] \
  [ipx::get_user_parameters DATA_WIDTH -of_objects $cc]
 
-## NUM_OF_SDI
+## NUM_OF_SDIO
 set_property -dict [list \
   "value_validation_type" "range_long" \
   "value_validation_range_minimum" "1" \
   "value_validation_range_maximum" "8" \
  ] \
- [ipx::get_user_parameters NUM_OF_SDI -of_objects $cc]
+ [ipx::get_user_parameters NUM_OF_SDIO -of_objects $cc]
 
 ## Customize IP Layout
 
@@ -115,11 +115,11 @@ set_property -dict [list \
   "tooltip" "\[DATA_WIDTH\] Define the data interface width"
 ] [ipgui::get_guiparamspec -name "DATA_WIDTH" -component $cc]
 
-ipgui::add_param -name "NUM_OF_SDI" -component $cc -parent $general_group
+ipgui::add_param -name "NUM_OF_SDIO" -component $cc -parent $general_group
 set_property -dict [list \
-  "display_name" "Number of MISO lines" \
-  "tooltip" "\[NUM_OF_SDI\] Define the number of MISO lines" \
-] [ipgui::get_guiparamspec -name "NUM_OF_SDI" -component $cc]
+  "display_name" "Number of MISO/MOSI lines" \
+  "tooltip" "\[NUM_OF_SDIO\] Define the number of MISO/MOSI lines" \
+] [ipgui::get_guiparamspec -name "NUM_OF_SDIO" -component $cc]
 
 ## Create and save the XGUI file
 ipx::create_xgui_files $cc
