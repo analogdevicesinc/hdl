41bafd994a1bd9063de38a912d80e294f1968c22
Bogdan Luncan <bogdan.luncan@analog.com>
2025-08-05 17:39:39 +0300

    library: jesd204: axi_jesd204: Bump core version
    Signed-off-by: Bogdan Luncan <bogdan.luncan@analog.com>

--- library/jesd204/axi_jesd204_tx/axi_jesd204_tx.v
+++ library/jesd204/axi_jesd204_tx/axi_jesd204_tx.v
@@ -83,7 +83,7 @@ module axi_jesd204_tx #(
   input [31:0] status_synth_params2
 );
 
-  localparam PCORE_VERSION = 32'h00010661; // 1.06.a
+  localparam PCORE_VERSION = 32'h00010761; // 1.07.a
   localparam PCORE_MAGIC = 32'h32303454; // 204T
 
   localparam DATA_PATH_WIDTH_LOG2 = (DATA_PATH_WIDTH == 8) ? 3 : 2;
