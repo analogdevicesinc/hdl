15deab144cdb923ea3c683798b5e0f56b838a101
caosjr <carlos.souza@analog.com>
2025-08-07 12:10:44 -0300

    projects: updated NUM_OF_SDI parameter to NUM_OF_SDIO
    Updated the projects to use NUM_OF_SDIO parameter. It is the same
    project, no functionality has changed.
    
    Signed-off-by: Carlos Souza <carlos.souza@analog.com>

--- projects/ad7616_sdz/zed/system_bd.tcl
+++ projects/ad7616_sdz/zed/system_bd.tcl
@@ -20,6 +20,6 @@ ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
 set sys_cstring "INTF=$ad_project_params(INTF)\
-NUM_OF_SDI=$ad_project_params(NUM_OF_SDI)"
+NUM_OF_SDIO=$ad_project_params(NUM_OF_SDIO)"
 
 sysid_gen_sys_init_file $sys_cstring
