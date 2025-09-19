a468d6d87ec35bf288f7e566588e184180ed0abe
Stanca Pop <stanca.pop@analog.com>
2025-09-19 17:12:43 +0300

    adaq2387x: Select if MMCM is used at make
    Signed-off-by: Stanca Pop <stanca.pop@analog.com>

--- projects/adaq2387x/zed/system_bd.tcl
+++ projects/adaq2387x/zed/system_bd.tcl
@@ -14,6 +14,7 @@ ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
 set sys_cstring "TWOLANES=$ad_project_params(TWOLANES) \
-ADC_RES=$ad_project_params(ADC_RES)"
+ADC_RES=$ad_project_params(ADC_RES) \
+USE_MMCM=$ad_project_params(USE_MMCM)"
 
 sysid_gen_sys_init_file $sys_cstring
