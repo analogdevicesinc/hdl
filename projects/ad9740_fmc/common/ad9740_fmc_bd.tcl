160265d54fd9ce526487b510bebc88c22ec3aea5
Liviu 'Ceshu' Adace <liviu.adace@analog.com>
2025-09-23 17:01:36 +0300

    ad9740/zed: Initial commit
    Signed-off-by: Liviu 'Ceshu' Adace <liviu.adace@analog.com>
    Signed-off-by: sarpadi <sergiu.arpadi@analog.com>

---
+++ projects/ad9740_fmc/common/ad9740_fmc_bd.tcl
@@ -0,0 +1,87 @@
+###############################################################################
+## Copyright (C) 2025 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+# bd ports
+
+create_bd_port -dir I ad9740_clk
+create_bd_port -dir O -from 13 -to 0 ad9740_data
+
+# dma
+
+ad_ip_instance axi_dmac ad9740_dma
+ad_ip_parameter ad9740_dma CONFIG.DMA_TYPE_SRC 0
+ad_ip_parameter ad9740_dma CONFIG.DMA_TYPE_DEST 2
+ad_ip_parameter ad9740_dma CONFIG.CYCLIC 1
+ad_ip_parameter ad9740_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter ad9740_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter ad9740_dma CONFIG.DMA_DATA_WIDTH_SRC 64
+ad_ip_parameter ad9740_dma CONFIG.DMA_DATA_WIDTH_DEST 16
+
+# ad9740
+
+ad_ip_instance axi_ad9740 ad9740_dac
+
+set DEVICE $ad_project_params(DEVICE)
+
+# Set DAC resolution based on DEVICE parameter
+# DEVICE must be set by the calling script (system_bd.tcl or system_project.tcl)
+switch $DEVICE {
+  "AD9748" {
+    ad_ip_parameter ad9740_dac CONFIG.DAC_RESOLUTION 8
+    puts "common bd tcl: detected AD9748"
+  }
+  "AD9740" {
+    ad_ip_parameter ad9740_dac CONFIG.DAC_RESOLUTION 10
+    puts "common bd tcl: detected AD9740"
+  }
+  "AD9742" {
+    ad_ip_parameter ad9740_dac CONFIG.DAC_RESOLUTION 12
+    puts "common bd tcl: detected AD9742"
+  }
+  "AD9744" {
+    ad_ip_parameter ad9740_dac CONFIG.DAC_RESOLUTION 14
+    puts "common bd tcl: detected AD9744"
+  }
+  default {
+    ad_ip_parameter ad9740_dac CONFIG.DAC_RESOLUTION 14
+    puts "common bd tcl: detected default AD9744"
+  }
+}
+
+# clocks
+
+#ad_connect ad9740_clk ad9740_dma/m_axis_aclk
+ad_connect ad9740_clk ad9740_dac/dac_clk
+
+# resets
+
+ad_connect sys_rstgen/peripheral_aresetn ad9740_dma/m_src_axi_aresetn
+
+# data path
+
+connect_bd_net [get_bd_pins ad9740_dma/fifo_rd_dout] [get_bd_pins ad9740_dac/dma_data]
+connect_bd_net [get_bd_pins ad9740_dma/fifo_rd_valid] [get_bd_pins ad9740_dac/dma_valid]
+
+connect_bd_net [get_bd_ports ad9740_clk] [get_bd_pins ad9740_dma/fifo_rd_clk]
+
+connect_bd_net [get_bd_pins ad9740_dma/fifo_rd_en] [get_bd_pins ad9740_dac/dma_ready]
+
+
+#ad_connect ad9740_dma/m_axis ad9740_dac/s_axis
+ad_connect ad9740_dac/dac_data ad9740_data
+
+# AXI address definitions
+
+ad_cpu_interconnect 0x44a40000 ad9740_dma
+ad_cpu_interconnect 0x44a70000 ad9740_dac
+
+# interrupts
+
+ad_cpu_interrupt "ps-13" "mb-13" ad9740_dma/irq
+
+# memory interconnects
+
+ad_mem_hp1_interconnect $sys_cpu_clk sys_ps7/S_AXI_HP1
+ad_mem_hp1_interconnect $sys_cpu_clk ad9740_dma/m_src_axi
---
+++ projects/ad9740_fmc/zed/system_top.v
@@ -0,0 +1,246 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright (C) 2025 Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/main/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module system_top #(
+  parameter DEVICE = "AD9744"
+) (
+
+  inout   [14:0]  ddr_addr,
+  inout   [ 2:0]  ddr_ba,
+  inout           ddr_cas_n,
+  inout           ddr_ck_n,
+  inout           ddr_ck_p,
+  inout           ddr_cke,
+  inout           ddr_cs_n,
+  inout   [ 3:0]  ddr_dm,
+  inout   [31:0]  ddr_dq,
+  inout   [ 3:0]  ddr_dqs_n,
+  inout   [ 3:0]  ddr_dqs_p,
+  inout           ddr_odt,
+  inout           ddr_ras_n,
+  inout           ddr_reset_n,
+  inout           ddr_we_n,
+
+  inout           fixed_io_ddr_vrn,
+  inout           fixed_io_ddr_vrp,
+  inout   [53:0]  fixed_io_mio,
+  inout           fixed_io_ps_clk,
+  inout           fixed_io_ps_porb,
+  inout           fixed_io_ps_srstb,
+
+  inout   [31:0]  gpio_bd,
+
+  output          hdmi_out_clk,
+  output          hdmi_vsync,
+  output          hdmi_hsync,
+  output          hdmi_data_e,
+  output  [15:0]  hdmi_data,
+
+  output          spdif,
+
+  output          i2s_mclk,
+  output          i2s_bclk,
+  output          i2s_lrclk,
+  output          i2s_sdata_out,
+  input           i2s_sdata_in,
+
+  inout           iic_scl,
+  inout           iic_sda,
+  inout   [ 1:0]  iic_mux_scl,
+  inout   [ 1:0]  iic_mux_sda,
+
+  input           otg_vbusoc,
+
+  // adf4351 interface
+
+  output          adf4351_ce,
+  output          adf4351_clk,
+  output          adf4351_csn,
+  output          adf4351_mosi,
+
+  // ad9740 interface
+
+  input           ad9740_clk_p,
+  input           ad9740_clk_n,
+  output  [13:0]  ad9740_data
+);
+
+  // internal signals
+
+  wire    [63:0]  gpio_i;
+  wire    [63:0]  gpio_o;
+  wire    [63:0]  gpio_t;
+  wire    [ 1:0]  iic_mux_scl_i_s;
+  wire    [ 1:0]  iic_mux_scl_o_s;
+  wire            iic_mux_scl_t_s;
+  wire    [ 1:0]  iic_mux_sda_i_s;
+  wire    [ 1:0]  iic_mux_sda_o_s;
+  wire            iic_mux_sda_t_s;
+  wire            ad9740_clk_ds;
+  wire            ad9740_clk;
+  wire    [13:0]  ad9740_data_int;
+
+  // instantiations
+
+  assign gpio_i[63:32] = gpio_o[63:32];
+  assign adf4351_ce = 1'b1;
+
+  generate
+    if (DEVICE == "AD9744") begin
+      // AD9744: 14-bit DAC - use all bits
+      assign ad9740_data = ad9740_data_int [13:0];
+    end else if (DEVICE == "AD9742") begin
+      // AD9742: 12-bit DAC - MSB aligned, use upper 12 bits, tie lower 2 bits to 0
+      // FIX: Take upper 12 bits from internal 14-bit bus (was taking lower 12 bits)
+      assign ad9740_data[13:2] = ad9740_data_int[13:2];
+      assign ad9740_data[1:0] = 2'b00;
+    end else if (DEVICE == "AD9740") begin
+      // AD9740: 10-bit DAC - MSB aligned, use upper 10 bits, tie lower 4 bits to 0
+      // FIX: Take upper 10 bits from internal 14-bit bus (was taking lower 10 bits)
+      assign ad9740_data[13:4] = ad9740_data_int[13:4];
+      assign ad9740_data[3:0] = 4'b0000;
+    end else if (DEVICE == "AD9748") begin
+      // AD9748: 8-bit DAC - MSB aligned, use upper 8 bits, tie lower 6 bits to 0
+      // FIX: Take upper 8 bits from internal 14-bit bus (was taking lower 8 bits)
+      assign ad9740_data[13:6] = ad9740_data_int[13:6];
+      assign ad9740_data[5:0] = 6'b000000;
+    end else begin
+      // Default case: assume full 14-bit width
+      assign ad9740_data = ad9740_data_int;
+    end
+  endgenerate
+
+  IBUFDS i_ad9740_clk_ibuf_ds (
+    .I (ad9740_clk_p),
+    .IB (ad9740_clk_n),
+    .O (ad9740_clk_ds));
+
+  BUFG i_ad9740_clk_ibuf (
+    .I (ad9740_clk_ds),
+    .O (ad9740_clk));
+
+  ad_iobuf #(
+    .DATA_WIDTH(32)
+  ) i_iobuf (
+    .dio_t(gpio_t[31:0]),
+    .dio_i(gpio_o[31:0]),
+    .dio_o(gpio_i[31:0]),
+    .dio_p(gpio_bd));
+
+  ad_iobuf #(
+    .DATA_WIDTH(2)
+  ) i_iic_mux_scl (
+    .dio_t({iic_mux_scl_t_s, iic_mux_scl_t_s}),
+    .dio_i(iic_mux_scl_o_s),
+    .dio_o(iic_mux_scl_i_s),
+    .dio_p(iic_mux_scl));
+
+  ad_iobuf #(
+    .DATA_WIDTH(2)
+  ) i_iic_mux_sda (
+    .dio_t({iic_mux_sda_t_s, iic_mux_sda_t_s}),
+    .dio_i(iic_mux_sda_o_s),
+    .dio_o(iic_mux_sda_i_s),
+    .dio_p(iic_mux_sda));
+
+  system_wrapper i_system_wrapper (
+    .ad9740_clk (ad9740_clk),
+    .ad9740_data (ad9740_data_int),
+    .ddr_addr (ddr_addr),
+    .ddr_ba (ddr_ba),
+    .ddr_cas_n (ddr_cas_n),
+    .ddr_ck_n (ddr_ck_n),
+    .ddr_ck_p (ddr_ck_p),
+    .ddr_cke (ddr_cke),
+    .ddr_cs_n (ddr_cs_n),
+    .ddr_dm (ddr_dm),
+    .ddr_dq (ddr_dq),
+    .ddr_dqs_n (ddr_dqs_n),
+    .ddr_dqs_p (ddr_dqs_p),
+    .ddr_odt (ddr_odt),
+    .ddr_ras_n (ddr_ras_n),
+    .ddr_reset_n (ddr_reset_n),
+    .ddr_we_n (ddr_we_n),
+    .fixed_io_ddr_vrn (fixed_io_ddr_vrn),
+    .fixed_io_ddr_vrp (fixed_io_ddr_vrp),
+    .fixed_io_mio (fixed_io_mio),
+    .fixed_io_ps_clk (fixed_io_ps_clk),
+    .fixed_io_ps_porb (fixed_io_ps_porb),
+    .fixed_io_ps_srstb (fixed_io_ps_srstb),
+    .gpio_i (gpio_i),
+    .gpio_o (gpio_o),
+    .gpio_t (gpio_t),
+    .hdmi_data (hdmi_data),
+    .hdmi_data_e (hdmi_data_e),
+    .hdmi_hsync (hdmi_hsync),
+    .hdmi_out_clk (hdmi_out_clk),
+    .hdmi_vsync (hdmi_vsync),
+    .i2s_bclk (i2s_bclk),
+    .i2s_lrclk (i2s_lrclk),
+    .i2s_mclk (i2s_mclk),
+    .i2s_sdata_in (i2s_sdata_in),
+    .i2s_sdata_out (i2s_sdata_out),
+    .iic_fmc_scl_io (iic_scl),
+    .iic_fmc_sda_io (iic_sda),
+    .iic_mux_scl_i (iic_mux_scl_i_s),
+    .iic_mux_scl_o (iic_mux_scl_o_s),
+    .iic_mux_scl_t (iic_mux_scl_t_s),
+    .iic_mux_sda_i (iic_mux_sda_i_s),
+    .iic_mux_sda_o (iic_mux_sda_o_s),
+    .iic_mux_sda_t (iic_mux_sda_t_s),
+    .otg_vbusoc (otg_vbusoc),
+    .spdif (spdif),
+    .spi0_clk_i (1'b0),
+    .spi0_clk_o (adf4351_clk),
+    .spi0_csn_0_o (adf4351_csn),
+    .spi0_csn_1_o (),
+    .spi0_csn_2_o (),
+    .spi0_csn_i (1'b1),
+    .spi0_sdi_i (1'b0),
+    .spi0_sdo_i (1'b0),
+    .spi0_sdo_o (adf4351_mosi),
+    .spi1_clk_i (1'b0),
+    .spi1_clk_o (),
+    .spi1_csn_0_o (),
+    .spi1_csn_1_o (),
+    .spi1_csn_2_o (),
+    .spi1_csn_i (1'b1),
+    .spi1_sdi_i (1'b0),
+    .spi1_sdo_i (1'b0),
+    .spi1_sdo_o ());
+
+endmodule
