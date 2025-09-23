760e8bc4e5dfa930c7ee65aa8363f91abea39abc
Liviu 'Ceshu' Adace <liviu.adace@analog.com>
2025-09-23 11:41:22 +0300

    projects: ad738x_fmc: Fix system ID to reflect the actual build parameters
    Signed-off-by: Liviu 'Ceshu' Adace <liviu.adace@analog.com>

--- projects/ad738x_fmc/zed/system_bd.tcl
+++ projects/ad738x_fmc/zed/system_bd.tcl
@@ -6,24 +6,15 @@
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 
-# specify ADC resolution -- the design supports 16/14/12 bit resolutions
-set adc_resolution 16
+# block design
+source ../common/ad738x_bd.tcl
 
-# specify the number of active channel -- 1 or 2 or 4
-set adc_num_of_channels 2
-
-# specify ADC sampling rate in sample/seconds -- default is 3 MSPS
-set adc_sampling_rate 3000000
-
-#system ID
+# system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
-set sys_cstring "ADC_RESOLUTION=$adc_resolution\
-ADC_NUM_OF_CHANNELS=$adc_num_of_channels\
-ADC_SAMPLING_RATE=$adc_sampling_rate"
+set sys_cstring "ALERT_SPI_N=$ad_project_params(ALERT_SPI_N)\
+NUM_OF_SDI=$ad_project_params(NUM_OF_SDI)"
 
 sysid_gen_sys_init_file $sys_cstring
-
-source ../common/ad738x_bd.tcl
