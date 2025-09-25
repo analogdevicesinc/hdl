991b4591037291e78414b48ee029561aa659e2c5
Pedro Mendonca <Pedro.Mendonca@analog.com>
2025-09-25 16:53:23 -0300

    daq2: deploy auto timing fix
    - added auto timing fix (ATF) script as post route script
    - changed strategy to Congestion_SpreadLogic_high
    - refactored system_project.tcl as Factory Method
    - updated header copyright year
    
    Signed-off-by: Pedro Mendonca <Pedro.Mendonca@analog.com>

--- projects/daq2/kcu105/system_project.tcl
+++ projects/daq2/kcu105/system_project.tcl
@@ -1,11 +1,13 @@
 ###############################################################################
-## Copyright (C) 2014-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2014-2023, 2025 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
 source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
+set ADI_POST_ROUTE_SCRIPT [file normalize $ad_hdl_dir/projects/scripts/auto_timing_fix_xilinx.tcl]
+set BOARD_NAME kcu105
 
 # get_env_param retrieves parameter value from the environment if exists,
 # other case use the default value
@@ -13,14 +15,14 @@ source $ad_hdl_dir/projects/scripts/adi_board.tcl
 #   Use over-writable parameters from the environment.
 #
 #    e.g.
-#      make RX_JESD_L=4 RX_JESD_M=2 TX_JESD_L=4 TX_JESD_M=2 
+#      make RX_JESD_L=4 RX_JESD_M=2 TX_JESD_L=4 TX_JESD_M=2
 
 # Parameter description:
 #   [RX/TX]_JESD_M : Number of converters per link
 #   [RX/TX]_JESD_L : Number of lanes per link
 #   [RX/TX]_JESD_S : Number of samples per frame
 
-adi_project daq2_kcu105 0 [list \
+adi_project daq2_${BOARD_NAME} 0 [list \
   RX_JESD_M    [get_env_param RX_JESD_M    2 ] \
   RX_JESD_L    [get_env_param RX_JESD_L    4 ] \
   RX_JESD_S    [get_env_param RX_JESD_S    1 ] \
@@ -29,14 +31,15 @@ adi_project daq2_kcu105 0 [list \
   TX_JESD_S    [get_env_param TX_JESD_S    1 ] \
 ]
 
-adi_project_files daq2_kcu105 [list \
+adi_project_files daq2_${BOARD_NAME} [list \
   "../common/daq2_spi.v" \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "$ad_hdl_dir/projects/common/kcu105/kcu105_system_constr.xdc" ]
+  "$ad_hdl_dir/projects/common/${BOARD_NAME}/${BOARD_NAME}_system_constr.xdc" ]
 
-## To improve timing in DDR4 MIG
-set_property strategy Performance_RefinePlacement [get_runs impl_1]
+## daq2 design is presenting hold time violations on some paths
+## set the strategy to spread logic and help with hold time fixes
+set_property strategy Congestion_SpreadLogic_high [get_runs impl_1]
 
-adi_project_run daq2_kcu105
+adi_project_run daq2_${BOARD_NAME}
