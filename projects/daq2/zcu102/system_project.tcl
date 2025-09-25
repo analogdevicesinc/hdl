73d1d5d967d55bf238a1e39bad57cafccccebfe9
Pedro Mendonca <Pedro.Mendonca@analog.com>
2025-09-25 16:55:22 -0300

    projects/daq2: auto timing fix added as post route script
    Signed-off-by: Pedro Mendonca <Pedro.Mendonca@analog.com>

--- projects/daq2/zcu102/system_project.tcl
+++ projects/daq2/zcu102/system_project.tcl
@@ -6,6 +6,7 @@
 source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
+set ADI_POST_ROUTE_SCRIPT [file normalize $ad_hdl_dir/scripts/auto_timing_fix.tcl]
 set BOARD_NAME zcu102
 
 # get_env_param retrieves parameter value from the environment if exists,
