ba61f00c8871adf540a44887f4a7ad490aea02d3
Pedro Mendonca <Pedro.Mendonca@analog.com>
2025-09-25 16:24:44 -0300

    projects/fmcomms5: added ATF as post-route script
    Signed-off-by: Pedro Mendonca <Pedro.Mendonca@analog.com>

--- projects/fmcomms5/zc706/system_project.tcl
+++ projects/fmcomms5/zc706/system_project.tcl
@@ -6,6 +6,7 @@
 source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
+set ADI_POST_ROUTE_SCRIPT [file normalize $ad_hdl_dir/scripts/auto_timing_fix.tcl]
 
 adi_project fmcomms5_zc706
 adi_project_files fmcomms5_zc706 [list \
