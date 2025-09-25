8c4ef13849ebe2bec041976602acb50dbd42a0e7
Pedro Mendonca <Pedro.Mendonca@analog.com>
2025-09-25 16:28:00 -0300

    projects/fmcomms5: refactor system_project.tcl as Factory Method
    Signed-off-by: Pedro Mendonca <Pedro.Mendonca@analog.com>

--- projects/fmcomms5/zc702/system_project.tcl
+++ projects/fmcomms5/zc702/system_project.tcl
@@ -7,14 +7,15 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 set ADI_POST_ROUTE_SCRIPT [file normalize $ad_hdl_dir/scripts/auto_timing_fix.tcl]
+set BOARD_NAME zc702
 
-adi_project fmcomms5_zc702
-adi_project_files fmcomms5_zc702 [list \
+adi_project fmcomms5_${BOARD_NAME}
+adi_project_files fmcomms5_${BOARD_NAME} [list \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "$ad_hdl_dir/projects/common/zc702/zc702_system_constr.xdc" ]
+  "$ad_hdl_dir/projects/common/${BOARD_NAME}/${BOARD_NAME}_system_constr.xdc" ]
 
-adi_project_run fmcomms5_zc702
+adi_project_run fmcomms5_${BOARD_NAME}
 source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
 
