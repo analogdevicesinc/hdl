4dbb6d74e4cbfab3cdb77e952d7332777ddea05b
Stanca Pop <stanca.pop@analog.com>
2025-10-08 10:42:49 +0300

    dbg: axi_ltc2387: Remove dco from ILA core

--- library/axi_ltc2387/axi_ltc2387_ip.tcl
+++ library/axi_ltc2387/axi_ltc2387_ip.tcl
@@ -11,7 +11,7 @@ adi_ip_create axi_ltc2387
 create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name my_ila
 set_property -dict [list CONFIG.C_MONITOR_TYPE {Native}] [get_ips my_ila]
 set_property -dict [list CONFIG.C_EN_STRG_QUAL {1}] [get_ips my_ila]
-set_property -dict [list CONFIG.C_NUM_OF_PROBES {12}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_NUM_OF_PROBES {10}] [get_ips my_ila]
 set_property -dict [list CONFIG.C_DATA_DEPTH {4096}] [get_ips my_ila]
 set_property -dict [list CONFIG.C_TRIGIN_EN {false}] [get_ips my_ila]
 set_property -dict [list CONFIG.C_PROBE0_WIDTH {1}] [get_ips my_ila]
@@ -21,11 +21,9 @@ set_property -dict [list CONFIG.C_PROBE3_WIDTH {1}] [get_ips my_ila]
 set_property -dict [list CONFIG.C_PROBE4_WIDTH {16}] [get_ips my_ila]
 set_property -dict [list CONFIG.C_PROBE5_WIDTH {1}] [get_ips my_ila]
 set_property -dict [list CONFIG.C_PROBE6_WIDTH {1}] [get_ips my_ila]
-set_property -dict [list CONFIG.C_PROBE7_WIDTH {1}] [get_ips my_ila]
-set_property -dict [list CONFIG.C_PROBE8_WIDTH {1}] [get_ips my_ila]
-set_property -dict [list CONFIG.C_PROBE9_WIDTH {18}] [get_ips my_ila]
-set_property -dict [list CONFIG.C_PROBE10_WIDTH {9}] [get_ips my_ila]
-set_property -dict [list CONFIG.C_PROBE11_WIDTH {9}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE7_WIDTH {18}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE8_WIDTH {9}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE9_WIDTH {9}] [get_ips my_ila]
 generate_target {all} [get_files <ip_core_name>.srcs/sources_1/ip/my_ila/my_ila.xci]
 
 adi_ip_files axi_ltc2387 [list \
