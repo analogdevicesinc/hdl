80c1c207321c9c851e99aa8da8332ad704e2d529
Liviu 'Ceshu' Adace <liviu.adace@analog.com>
2025-11-14 12:46:34 +0200

    projects: ad9740_fmc: Fixed constraints and other
    Signed-off-by: Liviu 'Ceshu' Adace <liviu.adace@analog.com>

--- projects/ad9740_fmc/zed/system_project.tcl
+++ projects/ad9740_fmc/zed/system_project.tcl
@@ -7,9 +7,31 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-
-
-set DEVICE [get_env_param DEVICE "AD9740"]
+# The get_env_param procedure retrieves parameter value from the environment if exists,
+# other case returns the default value specified in its second parameter field.
+#
+#   How to use over-writable parameters from the environment:
+#
+#    e.g.
+#      make DEVICE="AD9740"
+#
+#
+# Parameter description:
+#
+# DEVICE : DAC Part for which the project is compiled
+#
+#   AD9740 (Default)
+#   AD9742
+#   AD9744
+#   AD9748
+#
+# Example:
+#
+#   make DEVICE="AD9740"
+#   make DEVICE="AD9742"
+#   make DEVICE="AD9744"
+#   make DEVICE="AD9748"
+#
 
 adi_project ad9740_fmc_zed 0 [list \
   DEVICE [get_env_param DEVICE "AD9740"] \
@@ -25,25 +47,24 @@ adi_project_files ad9740_fmc_zed [list \
     "AD9740" {
       adi_project_files ad9740_fmc_zed [list \
         "ad9744_constr.xdc" ]
-  }
+    }
     "AD9742" {
       adi_project_files ad9740_fmc_zed [list \
         "ad9744_constr.xdc" ]
-  }
+    }
     "AD9744" {
       adi_project_files ad9740_fmc_zed [list \
         "ad9744_constr.xdc" ]
-  }
+    }
     "AD9748" {
       adi_project_files ad9740_fmc_zed [list \
         "ad9744_constr.xdc" ]
+    }
+    default {
+      adi_project_files ad9740_fmc_zed [list \
+        "ad9740_constr.xdc" ]
+    }
   }
-  default {
-    adi_project_files ad9740_fmc_zed [list \
-      "ad9740_constr.xdc" ]
-  }
-}
-
-
 
 adi_project_run ad9740_fmc_zed
+
