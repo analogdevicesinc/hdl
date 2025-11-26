0edbc1dfc2ed10d980d858a138f44a1fb7fafcd7
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2025-11-26 15:45:27 +0200

    projects/ada4356: Integrate AXI_TDD in HDL design
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/ada4355_fmc/zed/system_project.tcl
+++ projects/ada4355_fmc/zed/system_project.tcl
@@ -8,28 +8,36 @@ source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 # Parameter description
-# BUFMRCE_EN - Enable BUFMRCE buffer
+# BUFMRCE_EN - Enable BUFMRCE buffer (ADA4355 variant)
+# TDD_EN     - Enable TDD controller for LiDAR timing
 
 set BUFMRCE_EN [get_env_param BUFMRCE_EN 0]
+set TDD_EN [get_env_param TDD_EN 0]
+
+if {$BUFMRCE_EN == 1 && $TDD_EN == 1} {
+  error "ERROR: TDD_EN=1 is not supported with BUFMRCE_EN=1 (ADA4355 variant)"
+}
 
 adi_project ada4355_fmc_zed 0 [list \
-  BUFMRCE_EN $BUFMRCE_EN ]
+  BUFMRCE_EN $BUFMRCE_EN \
+  TDD_EN $TDD_EN ]
 
 adi_project_files ada4355_fmc_zed [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" ]
 
-switch $BUFMRCE_EN {
-  0 {
-    adi_project_files ada4355_fmc_zed [list \
-      "system_constr.xdc" \
-      "system_top.v" ]
-  }
-  1 {
-    adi_project_files ada4355_fmc_zed [list \
-      "system_constr_bufmrce_en.xdc" \
-      "system_top_bufmrce_en.v" ]
-  }
+if {$BUFMRCE_EN == 1} {
+  adi_project_files ada4355_fmc_zed [list \
+    "system_constr_bufmrce_en.xdc" \
+    "system_top_bufmrce_en.v" ]
+} elseif {$TDD_EN == 1} {
+  adi_project_files ada4355_fmc_zed [list \
+    "system_constr.xdc" \
+    "system_top_tdd.v" ]
+} else {
+  adi_project_files ada4355_fmc_zed [list \
+    "system_constr.xdc" \
+    "system_top.v" ]
 }
 
 adi_project_run ada4355_fmc_zed
