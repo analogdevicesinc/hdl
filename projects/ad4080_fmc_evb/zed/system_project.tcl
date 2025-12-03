53ac22e8e70d37a4183f8e8ad0e16b2687dad360
Villyam <Vilmoscsaba.Jozsa@analog.com>
2025-12-03 17:56:26 +0200

    ad408x: Updates for lower resolution sampling.
    Signed-off-by: Villyam <Vilmoscsaba.Jozsa@analog.com>

--- projects/ad4080_fmc_evb/zed/system_project.tcl
+++ projects/ad4080_fmc_evb/zed/system_project.tcl
@@ -7,11 +7,15 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project ad408x_fmc_evb_zed
-adi_project_files ad408x_fmc_evb_zed [list \
+set ADC_N_BITS [get_env_param ADC_N_BITS 20]
+
+adi_project ad4080_fmc_evb_zed 0 [list \
+  ADC_N_BITS $ADC_N_BITS \
+]
+adi_project_files ad4080_fmc_evb_zed [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" \
   "system_constr.xdc" \
   "system_top.v" ]
 
-adi_project_run ad408x_fmc_evb_zed
+adi_project_run ad4080_fmc_evb_zed
