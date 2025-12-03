aa4c6742664da8b1bede1dba8c1688575b831055
Villyam <Vilmoscsaba.Jozsa@analog.com>
2025-12-03 17:56:26 +0200

    ad408x: Updates for lower resolution sampling.
    Signed-off-by: Villyam <Vilmoscsaba.Jozsa@analog.com>

--- projects/ad408x_fmc_evb/common/ad408x_fmc_evb_bd.tcl
+++ projects/ad408x_fmc_evb/common/ad408x_fmc_evb_bd.tcl
@@ -3,7 +3,14 @@
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
-# ad4080 interface
+set ADC_N_BITS $ad_project_params(ADC_N_BITS)
+if {$ADC_N_BITS <= 16} {
+    set DMA_DATA_WIDTH_SRC 16
+} else {
+    set DMA_DATA_WIDTH_SRC 32
+}
+
+# ad408x interface
 
 create_bd_port -dir I dco_p
 create_bd_port -dir I dco_n
@@ -29,52 +36,53 @@ ad_connect fpga_100_clk  ad408x_clock_monitor/clock_1
 
 # axi_ad408x
 
-ad_ip_instance axi_ad408x axi_ad4080_adc
+ad_ip_instance axi_ad408x axi_ad408x_adc
+ad_ip_parameter axi_ad408x_adc CONFIG.ADC_N_BITS $ADC_N_BITS
 
 # dma for rx data
 
-ad_ip_instance axi_dmac axi_ad4080_dma
-ad_ip_parameter axi_ad4080_dma CONFIG.DMA_TYPE_SRC 2
-ad_ip_parameter axi_ad4080_dma CONFIG.DMA_TYPE_DEST 0
-ad_ip_parameter axi_ad4080_dma CONFIG.CYCLIC 0
-ad_ip_parameter axi_ad4080_dma CONFIG.SYNC_TRANSFER_START 0
-ad_ip_parameter axi_ad4080_dma CONFIG.AXI_SLICE_SRC 1
-ad_ip_parameter axi_ad4080_dma CONFIG.AXI_SLICE_DEST 0
-ad_ip_parameter axi_ad4080_dma CONFIG.DMA_2D_TRANSFER 0
-ad_ip_parameter axi_ad4080_dma CONFIG.DMA_DATA_WIDTH_SRC 32
-ad_ip_parameter axi_ad4080_dma CONFIG.DMA_DATA_WIDTH_DEST 64
+ad_ip_instance axi_dmac axi_ad408x_dma
+ad_ip_parameter axi_ad408x_dma CONFIG.DMA_TYPE_SRC 2
+ad_ip_parameter axi_ad408x_dma CONFIG.DMA_TYPE_DEST 0
+ad_ip_parameter axi_ad408x_dma CONFIG.CYCLIC 0
+ad_ip_parameter axi_ad408x_dma CONFIG.SYNC_TRANSFER_START 0
+ad_ip_parameter axi_ad408x_dma CONFIG.AXI_SLICE_SRC 1
+ad_ip_parameter axi_ad408x_dma CONFIG.AXI_SLICE_DEST 0
+ad_ip_parameter axi_ad408x_dma CONFIG.DMA_2D_TRANSFER 0
+ad_ip_parameter axi_ad408x_dma CONFIG.DMA_DATA_WIDTH_SRC $DMA_DATA_WIDTH_SRC
+ad_ip_parameter axi_ad408x_dma CONFIG.DMA_DATA_WIDTH_DEST 64
 
-# connect interface to axi_ad4080_adc
+# connect interface to axi_ad408x_adc
 
-ad_connect dco_p                axi_ad4080_adc/dclk_in_p
-ad_connect dco_n                axi_ad4080_adc/dclk_in_n
-ad_connect da_p                 axi_ad4080_adc/data_a_in_p
-ad_connect da_n                 axi_ad4080_adc/data_a_in_n
-ad_connect db_p                 axi_ad4080_adc/data_b_in_p
-ad_connect db_n                 axi_ad4080_adc/data_b_in_n
-ad_connect sync_n               axi_ad4080_adc/sync_n
-ad_connect cnv_in_p             axi_ad4080_adc/cnv_in_p
-ad_connect cnv_in_n             axi_ad4080_adc/cnv_in_n
-ad_connect filter_data_ready_n  axi_ad4080_adc/filter_data_ready_n
-ad_connect $sys_iodelay_clk     axi_ad4080_adc/delay_clk
+ad_connect dco_p                axi_ad408x_adc/dclk_in_p
+ad_connect dco_n                axi_ad408x_adc/dclk_in_n
+ad_connect da_p                 axi_ad408x_adc/data_a_in_p
+ad_connect da_n                 axi_ad408x_adc/data_a_in_n
+ad_connect db_p                 axi_ad408x_adc/data_b_in_p
+ad_connect db_n                 axi_ad408x_adc/data_b_in_n
+ad_connect sync_n               axi_ad408x_adc/sync_n
+ad_connect cnv_in_p             axi_ad408x_adc/cnv_in_p
+ad_connect cnv_in_n             axi_ad408x_adc/cnv_in_n
+ad_connect filter_data_ready_n  axi_ad408x_adc/filter_data_ready_n
+ad_connect $sys_iodelay_clk     axi_ad408x_adc/delay_clk
 
 # connect datapath
 
-ad_connect axi_ad4080_adc/adc_data  axi_ad4080_dma/fifo_wr_din
-ad_connect axi_ad4080_adc/adc_valid axi_ad4080_dma/fifo_wr_en
-ad_connect axi_ad4080_adc/adc_dovf  axi_ad4080_dma/fifo_wr_overflow
+ad_connect axi_ad408x_adc/adc_data  axi_ad408x_dma/fifo_wr_din
+ad_connect axi_ad408x_adc/adc_valid axi_ad408x_dma/fifo_wr_en
+ad_connect axi_ad408x_adc/adc_dovf  axi_ad408x_dma/fifo_wr_overflow
 
 # system runs on phy's received clock
 
-ad_connect axi_ad4080_adc/adc_clk axi_ad4080_dma/fifo_wr_clk
+ad_connect axi_ad408x_adc/adc_clk axi_ad408x_dma/fifo_wr_clk
 
-ad_connect $sys_cpu_resetn axi_ad4080_dma/m_dest_axi_aresetn
+ad_connect $sys_cpu_resetn axi_ad408x_dma/m_dest_axi_aresetn
 
-ad_cpu_interconnect 0x44A00000 axi_ad4080_adc
-ad_cpu_interconnect 0x44A30000 axi_ad4080_dma
+ad_cpu_interconnect 0x44A00000 axi_ad408x_adc
+ad_cpu_interconnect 0x44A30000 axi_ad408x_dma
 ad_cpu_interconnect 0x44A40000 ad408x_clock_monitor
 
 ad_mem_hp1_interconnect $sys_cpu_clk sys_ps7/S_AXI_HP1
-ad_mem_hp1_interconnect $sys_cpu_clk axi_ad4080_dma/m_dest_axi
+ad_mem_hp1_interconnect $sys_cpu_clk axi_ad408x_dma/m_dest_axi
 
-ad_cpu_interrupt ps-13 mb-12 axi_ad4080_dma/irq
+ad_cpu_interrupt ps-13 mb-12 axi_ad408x_dma/irq
