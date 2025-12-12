10edd0df4252073c977e823e37085238742d6e45
Villyam <Vilmoscsaba.Jozsa@analog.com>
2025-12-12 12:08:16 +0200

    Projects: Updating project names for Makefile generator.
    Signed-off-by: Villyam <Vilmoscsaba.Jozsa@analog.com>

--- projects/daq2/zcu102/system_project.tcl
+++ projects/daq2/zcu102/system_project.tcl
@@ -7,7 +7,6 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 set ADI_POST_ROUTE_SCRIPT [file normalize $ad_hdl_dir/projects/scripts/auto_timing_fix_xilinx.tcl]
-set BOARD_NAME zcu102
 
 # get_env_param retrieves parameter value from the environment if exists,
 # other case use the default value.
@@ -58,7 +57,7 @@ set xcvr_config_paths [adi_xcvr_project [list \
 #   [RX/TX]_JESD_L : Number of lanes per link
 #   [RX/TX]_JESD_S : Number of samples per frame
 
-adi_project daq2_${BOARD_NAME} 0 [list \
+adi_project daq2_zcu102 0 [list \
   RX_JESD_M    [get_env_param RX_JESD_M    2 ] \
   RX_JESD_L    [get_env_param RX_JESD_L    4 ] \
   RX_JESD_S    [get_env_param RX_JESD_S    1 ] \
@@ -67,15 +66,15 @@ adi_project daq2_${BOARD_NAME} 0 [list \
   TX_JESD_S    [get_env_param TX_JESD_S    1 ] \
 ]
 
-adi_project_files daq2_${BOARD_NAME} [list \
+adi_project_files daq2_zcu102 [list \
   "../common/daq2_spi.v" \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "$ad_hdl_dir/projects/common/${BOARD_NAME}/${BOARD_NAME}_system_constr.xdc" ]
+  "$ad_hdl_dir/projects/common/zcu102/zcu102_system_constr.xdc" ]
 
 ## daq2 design is presenting hold time violations on some paths
 ## set the strategy to spread logic and help with hold time fixes
 set_property strategy Congestion_SpreadLogic_high [get_runs impl_1]
 
-adi_project_run daq2_${BOARD_NAME}
+adi_project_run daq2_zcu102
--- projects/fmcomms2/zed/system_project.tcl
+++ projects/fmcomms2/zed/system_project.tcl
@@ -7,18 +7,17 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 set ADI_POST_ROUTE_SCRIPT [file normalize $ad_hdl_dir/projects/scripts/auto_timing_fix_xilinx.tcl]
-set BOARD_NAME zed
 
-adi_project fmcomms2_${BOARD_NAME}
-adi_project_files fmcomms2_${BOARD_NAME} [list \
+adi_project fmcomms2_zed
+adi_project_files fmcomms2_zed [list \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "$ad_hdl_dir/projects/common/${BOARD_NAME}/${BOARD_NAME}_system_constr.xdc" ]
+  "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" ]
 
 ## fmcomms2 design is presenting hold time violations on some paths
 ## set the strategy to spread logic and help with hold time fixes
 set_property strategy Congestion_SpreadLogic_high [get_runs impl_1]
 
-adi_project_run fmcomms2_${BOARD_NAME}
+adi_project_run fmcomms2_zed
 source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
