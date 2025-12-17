fb8023ed9927cbec328944c6a0793caf53f3a7d2
Stanca Pop <stanca.pop@analog.com>
2025-12-17 11:48:02 +0200

    projects/adaq23875: Add adaq2387x support
    Signed-off-by: Stanca Pop <stanca.pop@analog.com>

---
+++ projects/adaq23875/zed/system_project.tcl
@@ -0,0 +1,57 @@
+###############################################################################
+## Copyright (C) 2022-2023, 2025 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+# load scripts
+source ../../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+# TWOLANES: parameter describing the number of lanes
+# - 1: in two-lane mode (default)
+# - 0: in one-lane mode
+#
+# ADC_RES: parameter describing the ADC input resolution, thus selecting
+#          between ADAQ23878 (18-bit, default) and ADAQ23875/ADAQ23876 (16-bit)
+# - 18: 18 bits, ADAQ23878 (default)
+# - 16: 16 bits, ADAQ23875 & ADAQ23876
+#
+# USE_MMCM: parameter used to select between the 100MHz ref_clk or passing it
+#           through a clk_wizard and increase it to 120MHz
+# - 1: use the default clocking scheme
+# - 0: use the clk_wizard to increase the clk frequency
+#
+# The valid configurations for each supported evaluation board, depending on
+# the above parameters, are:
+#
+# Eval board | ADC_RES | TWOLANES | USE_MMCM |
+# ============================================
+# ADAQ23875  | 16      | 0 or 1   | 0 or 1   |
+# ADAQ23876  | 16      | 0 or 1   | 0 or 1   |
+# ADAQ23878  | 18      | 0 or 1   | 0 or 1   |
+
+adi_project adaq23875_zed 0 [list \
+  TWOLANES  [get_env_param TWOLANES  1 ] \
+  ADC_RES   [get_env_param ADC_RES  18 ] \
+  USE_MMCM  [get_env_param USE_MMCM  0 ]]
+
+# Base files
+set base_files [list \
+  "system_top.v" \
+  "system_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/ad_data_clk.v" \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc"]
+
+# Append timing constraint file based on USE_MMCM
+if { [get_env_param USE_MMCM 0] == 1 } {
+  lappend base_files "timing_mmcm.xdc"
+} else {
+  lappend base_files "timing_default.xdc"
+}
+
+# Register final list
+adi_project_files adaq23875_zed $base_files
+
+adi_project_run adaq23875_zed
