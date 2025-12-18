a994d958bc651e8a7470c5fd03be036a2bd3f4d5
Pedro Mendonca <Pedro.Mendonca@analog.com>
2025-12-18 10:20:31 -0300

    adrv936x replace route_design.tcl with ATF
    Signed-off-by: Pedro Mendonca <Pedro.Mendonca@analog.com>

--- projects/adrv9364z7020/ccbob_lvds/system_project.tcl
+++ projects/adrv9364z7020/ccbob_lvds/system_project.tcl
@@ -6,7 +6,7 @@
 source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
-set ADI_POST_ROUTE_POD_PRE_SCRIPT [file normalize $ad_hdl_dir/projects/scripts/route_design.tcl]
+set ADI_POST_ROUTE_SCRIPT [file normalize $ad_hdl_dir/projects/scripts/auto_timing_fix_xilinx.tcl]
 
 adi_project_create adrv9364z7020_ccbob_lvds 0 {} "xc7z020clg400-1"
 adi_project_files adrv9364z7020_ccbob_lvds [list \
