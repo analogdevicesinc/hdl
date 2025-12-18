7f2c93e1e6b0f8cb29f46be6414e6fe6d58540f5
Pedro Mendonca <Pedro.Mendonca@analog.com>
2025-12-18 10:18:42 -0300

    removed route_design.tcl
    Signed-off-by: Pedro Mendonca <Pedro.Mendonca@analog.com>

--- projects/scripts/route_design.tcl
+++
@@ -1,7 +0,0 @@
-# This script is used to fix some hold timing issues presented in more recent
-# versions of vivado. This remedy was given by AMD employee through the xilinx
-# forums.
-# References:
-# https://adaptivesupport.amd.com/s/question/0D5Pd00000pVOyuKAG/unexpected-hold-errors-when-moving-to-vivado-20251?language=en_US
-# https://adaptivesupport.amd.com/s/question/0D5Pd0000153gqkKAA/persistent-hold-timing-violations-after-upgrading-to-vivado-2025x-physoptdesign-holdfix-ineffective?language=en_US
-route_design
