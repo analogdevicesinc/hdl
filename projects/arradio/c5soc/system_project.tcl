3bdb9b9184ba5280085dba8f9b31f6a779b8f191
Bogdan Luncan <bogdan.luncan@analog.com>
2026-01-15 11:21:46 +0200

    projects: intel: Remove Quartus standard version from system_project.tcl
    Signed-off-by: Bogdan Luncan <bogdan.luncan@analog.com>

--- projects/arradio/c5soc/system_project.tcl
+++ projects/arradio/c5soc/system_project.tcl
@@ -1,10 +1,8 @@
 ###############################################################################
-## Copyright (C) 2014-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2014-2023, 2025-2026 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
-set REQUIRED_QUARTUS_VERSION 24.1std.0
-set QUARTUS_PRO_ISUSED 0
 source ../../../scripts/adi_env.tcl
 source ../../scripts/adi_project_intel.tcl
 
