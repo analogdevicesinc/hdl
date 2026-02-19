17518c650f2568d8b4df256063f650a83eb155f3
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2026-02-19 16:30:09 +0200

    ada4356: Fix guidline check errors
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/ada4355_fmc/zed/system_top_tdd.v
+++ projects/ada4355_fmc/zed/system_top_tdd.v
@@ -1,6 +1,6 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright (C) 2025 Analog Devices, Inc. All rights reserved.
+// Copyright (C) 2025-2026 Analog Devices, Inc. All rights reserved.
 //
 // In this HDL repository, there are many different and unique modules, consisting
 // of various HDL (Verilog or VHDL) components. The individual modules are
@@ -122,7 +122,6 @@ module system_top (
   output        sclk_pot,
   output        mosi_pot,
   output        csb_apd_pot
-
 );
 
   // internal signals
