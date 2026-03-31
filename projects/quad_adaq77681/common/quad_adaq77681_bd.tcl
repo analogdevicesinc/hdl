caadc4043e1ea80e622077ee76413bf6d8c5e866
caosjr <carlos.souza@analog.com>
2026-03-31 17:18:46 -0300

    projects/quad_adaq77681: update project
    * Inserts EN_SINGLE_CS for enabling a single CS for
      the four devices.
    * Uses mclk as the input for the mclk_clkgen. Adapts the
      XDC file accordingly.
    * Inserts a ready pin for the GPIO[45].
    
    Signed-off-by: Carlos Souza <carlos.souza@analog.com>

--- projects/quad_adaq77681/common/quad_adaq77681_bd.tcl
+++ projects/quad_adaq77681/common/quad_adaq77681_bd.tcl
@@ -1,14 +1,16 @@
 source $ad_hdl_dir/library/spi_engine/scripts/spi_engine.tcl
 
 # system level parameters
-set NUM_OF_SDIO  $ad_project_params(NUM_OF_SDIO)
-set NUM_OF_CS  $ad_project_params(NUM_OF_CS)
+set NUM_OF_SDIO    $ad_project_params(NUM_OF_SDIO)
+set NUM_OF_CS      $ad_project_params(NUM_OF_CS)
+set EN_SINGLE_CS   $ad_project_params(EN_SINGLE_CS)
 
 puts "build parameters: NUM_OF_SDIO: $NUM_OF_SDIO"
 
 create_bd_intf_port -mode Master -vlnv analog.com:interface:spi_engine_rtl:1.0 quad_adaq77681_spi
 
 create_bd_port -dir I -from 3 -to 0 quad_adaq77681_drdy
+create_bd_port -dir O               quad_adaq77681_out_drdy
 create_bd_port -dir I               quad_adaq77681_mclk_refclk
 create_bd_port -dir O               quad_adaq77681_xtal2_mclk
 
@@ -25,18 +27,16 @@ set echo_sclk        0
 spi_engine_create $hier_spi_engine $data_width $async_spi_clk $offload_en $num_cs $num_sdi $num_sdo $sdi_delay $echo_sclk
 ad_ip_parameter $hier_spi_engine/${hier_spi_engine}_offload CONFIG.ASYNC_TRIG 1
 
-ad_ip_instance axi_clkgen spi_clkgen
-ad_ip_parameter spi_clkgen CONFIG.CLK0_DIV 10
-ad_ip_parameter spi_clkgen CONFIG.VCO_DIV 1
-ad_ip_parameter spi_clkgen CONFIG.VCO_MUL 12
-
-ad_connect $sys_cpu_clk spi_clkgen/clk
-ad_connect spi_clk spi_clkgen/clk_0
-
 ad_ip_instance axi_clkgen mclk_clkgen
-ad_ip_parameter mclk_clkgen CONFIG.CLK0_DIV 60
+ad_ip_parameter mclk_clkgen CONFIG.CLK0_DIV 6
 ad_ip_parameter mclk_clkgen CONFIG.VCO_DIV 1
-ad_ip_parameter mclk_clkgen CONFIG.VCO_MUL 30
+ad_ip_parameter mclk_clkgen CONFIG.VCO_MUL 42
+
+ad_connect quad_adaq77681_mclk_refclk mclk_clkgen/clk
+#mclk 16MHz, spi_clk == mclk * 7 == 112MHz
+ad_connect spi_clk mclk_clkgen/clk_0
+#loopback of the xtal_mclk to the output mclk
+ad_connect quad_adaq77681_xtal2_mclk quad_adaq77681_mclk_refclk
 
 # dma to receive data stream
 ad_ip_instance axi_dmac axi_qadc_dma
@@ -49,9 +49,6 @@ ad_ip_parameter axi_qadc_dma CONFIG.AXI_SLICE_DEST 1
 ad_ip_parameter axi_qadc_dma CONFIG.DMA_2D_TRANSFER 0
 ad_ip_parameter axi_qadc_dma CONFIG.DMA_DATA_WIDTH_SRC [expr $NUM_OF_SDIO * $data_width]
 ad_ip_parameter axi_qadc_dma CONFIG.DMA_DATA_WIDTH_DEST 64
-
-ad_connect quad_adaq77681_mclk_refclk mclk_clkgen/clk
-ad_connect quad_adaq77681_xtal2_mclk mclk_clkgen/clk_0
 
 ad_connect spi_clk $hier_spi_engine/spi_clk
 ad_connect spi_clk axi_qadc_dma/s_axis_aclk
@@ -71,6 +68,7 @@ ad_ip_parameter drdy_buf CONFIG.C_SIZE 4
 
 ad_connect quad_adaq77681_drdy drdy_buf/Op1
 ad_connect drdy_buf/Res $hier_spi_engine/trigger
+ad_connect drdy_buf/Res quad_adaq77681_out_drdy
 
 ad_ip_instance util_reduced_logic drdy_chk
 ad_ip_parameter drdy_chk CONFIG.C_OPERATION {xor}
@@ -80,7 +78,7 @@ ad_connect quad_adaq77681_drdy drdy_chk/Op1
 
 ad_cpu_interconnect 0x44a00000 $hier_spi_engine/${hier_spi_engine}_axi_regmap
 ad_cpu_interconnect 0x44a30000 axi_qadc_dma
-ad_cpu_interconnect 0x44a70000 spi_clkgen
+# ad_cpu_interconnect 0x44a70000 spi_clkgen
 ad_cpu_interconnect 0x44b00000 mclk_clkgen
 
 ad_cpu_interrupt "ps-13" "mb-13" axi_qadc_dma/irq
