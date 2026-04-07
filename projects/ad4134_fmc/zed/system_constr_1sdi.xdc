73bf9c5364a85bc58c6194ae3aaf2fb37628b8c2
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2026-04-07 17:23:11 +0300

    projects/ad4134_fmc: Add timing constraints/MUX between SDO and DOUT1
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/ad4134_fmc/zed/system_constr_1sdi.xdc
+++ projects/ad4134_fmc/zed/system_constr_1sdi.xdc
@@ -5,4 +5,4 @@
 
 # Constraints for single-channel mode (NUM_OF_SDI=1)
 
-set_property -dict {PACKAGE_PIN M20 IOSTANDARD LVCMOS18 IOB TRUE} [get_ports ad4134_din[0]]; ## FMC_LPC_LA00_N_CC
+set_property -dict {PACKAGE_PIN M20 IOSTANDARD LVCMOS18} [get_ports ad4134_din[0]]; ## FMC_LPC_LA00_N_CC
--- projects/ad4134_fmc/zed/system_project.tcl
+++ projects/ad4134_fmc/zed/system_project.tcl
@@ -13,8 +13,6 @@ source $ad_hdl_dir/projects/scripts/adi_board.tcl
 #
 #   1 - Minimum I/O mode (Format 0x0): single-channel daisy-chain, all data
 #       multiplexed on DOUT0/DEC3_SDO, DCLK externally tied to FORMAT1/SCLK
-#   2 - Dual-channel daisy-chain mode (Format 0x1): data from Ch0/Ch1 on DOUT0,
-#       data from Ch2/Ch3 on DOUT1
 #   4 - Quad-channel parallel output mode (Format 0x2): each ADC channel has a
 #       dedicated data output pin (DOUT0..DOUT3)
 #
@@ -33,10 +31,6 @@ switch $NUM_OF_SDI {
   1 {
     adi_project_files ad4134_fmc_zed [list \
       "system_constr_1sdi.xdc" ]
-  }
-  2 {
-    adi_project_files ad4134_fmc_zed [list \
-      "system_constr_2sdi.xdc" ]
   }
   4 {
     adi_project_files ad4134_fmc_zed [list \
