aa2fab5dc13516307cb8f4081573eb7b26dd13b2
Pop Paul <paul.pop@analog.com>
2026-04-20 18:11:34 +0300

    projects/hmcad1520_ebz: Add support for the HMCAD1520-EBZ project
    The reference design that captures data from the HMCAD1520 or HMCAD1511
    was added for all 8/12/14 bit resolution modes.
    
    Signed-off-by: Pop Paul <paul.pop@analog.com>

---
+++ projects/hmcad1520_ebz/zed/system_project.tcl
@@ -0,0 +1,17 @@
+###############################################################################
+## Copyright (C) 2025-2026 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source ../../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project hmcad1520_ebz_zed
+adi_project_files hmcad1520_ebz_zed [list \
+  "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "system_constr.xdc" \
+  "system_top.v" ]
+
+adi_project_run hmcad1520_ebz_zed
