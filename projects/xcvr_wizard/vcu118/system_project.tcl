0b0889ab0bd49c92e370c35c3625152d5b271e57
Elena-Hadarau_adi <Elena.Hadarau@analog.com>
2026-05-29 11:54:13 +0300

    xcvr_wizard: Extend for jesd204c
    Signed-off-by: Elena-Hadarau_adi <Elena.Hadarau@analog.com>

--- projects/xcvr_wizard/vcu118/system_project.tcl
+++ projects/xcvr_wizard/vcu118/system_project.tcl
@@ -11,9 +11,11 @@ source $ad_hdl_dir/projects/scripts/adi_board.tcl
 # LANE_RATE: Value of lane rate [gbps]
 # REF_CLK: Value of the reference clock [MHz] (usually LANE_RATE/20 or LANE_RATE/40)
 # PLL_TYPE: The PLL used for driving the link [CPLL/QPLL0/QPLL1]
+# JESD_MODE: Used link layer encoder mode [64B66B - JESD 204C, 8B10B - JESD 204B]
 
 adi_project xcvr_wizard_vcu118 0 [list \
   LANE_RATE   [get_env_param LANE_RATE        10 ] \
   REF_CLK     [get_env_param REF_CLK         500 ] \
   PLL_TYPE    [get_env_param PLL_TYPE      QPLL0 ] \
+  JESD_MODE   [get_env_param JESD_MODE      8B10B] \
 ]
