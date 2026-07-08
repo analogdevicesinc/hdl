de54970eb0198d6a68cc1792bcf5455144a56d6d
mandrica <maria-zoita.andrica@analog.com>
2026-07-08 13:06:38 +0300

    projects/emg: Add Emg FMC evaluation board project
    Zedboard (zed) carrier support for the Emg customer evaluation board
     (RevB 088765) featuring dual AD4134 ADCs and 8-channel amplifier
     SPI interface.
    
      Block design:
      - SPI Engine (8 SDI, 32-bit, offload) for AD4134 parallel data capture
      - AXI DMA (256b src / 128b dst) on HP2
      - AXI ClkGen (100 MHz) and PWM-based ODR generator (~1.5 MSPS)
      - PS SPI0 for ADC configuration, PS SPI1 for amplifier configuration
      - FCLK_CLK2 (50 MHz) for SDPCLK reference clock
    
      Top-level:
      - AD4134 data interface (8x DOUT, DCLK, ODR) on FMC Bank 34
      - AD4134 GPIO (reset, power-down, mode, PINBSPI, DCLKMODE)
      - Amplifier SPI (8x CS via GPIO[58:51], SDI/SDO/SCLK via PS SPI1)
        on FMC Bank 35
      - All FMC pins at LVCMOS18 (VADJ = 1.8V)
    
    Signed-off-by: mandrica <maria-zoita.andrica@analog.com>

---
+++ projects/emg/zed/system_project.tcl
@@ -0,0 +1,17 @@
+###############################################################################
+## Copyright (C) 2019-2026 Analog Devices, Inc. All rights reserved.
+### SPDX short identifier: ADIBSD
+###############################################################################
+
+source ../../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project emg_zed
+
+adi_project_files emg_zed [list \
+    "$ad_hdl_dir/library/common/ad_iobuf.v" \
+    "system_top.v" \
+    "system_constr.xdc"]
+
+adi_project_run emg_zed
