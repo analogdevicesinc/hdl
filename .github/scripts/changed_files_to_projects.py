cfa5e1de9b1de88a5c79c89f7addba27f00e7337
Jorge Marques <jorge.marques@analog.com>
2026-07-26 19:10:27 +0200

    .github/changed_files_to_projects: Filter sdk kind
    Allow to filter all project of a sdk like altera if NO_ALTERA is set.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- .github/scripts/changed_files_to_projects.py
+++ .github/scripts/changed_files_to_projects.py
@@ -43,11 +43,34 @@ def get_touched_projects():
     touched = set()
     stdin = sys.stdin.read()
     stdin = json.loads(stdin) if stdin.startswith('[') else stdin.split()
+
+    arch_deps = {}
+    for lname, ldef in libs.items():
+        for k in ['XILINX_DEPS', 'INTEL_DEPS', 'LATTICE_DEPS']:
+            for f in ldef.get(k, []):
+                arch = k.split('_')[0].lower()
+                arch_deps.setdefault(os.path.normpath(f), set()).add(arch)
+
+    skip_sdk = set()
+    if os.environ.get('NO_AMD_XILINX') is not None:
+        skip_sdk.add('xilinx')
+    if os.environ.get('NO_LATTICE') is not None:
+        skip_sdk.add('lattice')
+    if os.environ.get('NO_ALTERA') is not None:
+        skip_sdk.add('intel')
+
     for cf in stdin:
         norm = os.path.normpath(cf)
         touched |= file2proj.get(norm, set())
         for lib in library_deps.get(norm, []):
-            touched |= lib2proj.get(lib, set())
+            dep_archs = arch_deps.get(norm, set())
+            for pname in lib2proj.get(lib, set()):
+                proj_kind = projs[pname].get('sdk_kind')
+                if proj_kind in skip_sdk:
+                    continue
+                if dep_archs and proj_kind and proj_kind not in dep_archs:
+                    continue
+                touched.add(pname)
 
     return json.dumps(sorted([t for t in touched if t in proj_paths or t]))
 
