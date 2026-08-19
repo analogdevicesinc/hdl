12ba36e85bb5085b4420924a7df059a790ceec64
Stanca Pop <stanca.pop@analog.com>
2026-08-19 11:26:42 +0300

    ad4858_dual: Rename project to match folder name
    Signed-off-by: Stanca Pop <stanca.pop@analog.com>

--- projects/ad4858_fmcz_dual/zcu102/system_project.tcl
+++ projects/ad4858_fmcz_dual/zcu102/system_project.tcl
@@ -4,15 +4,15 @@ source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 set LVDS_CMOS_N 1
 
-adi_project ad4858_fmcz_zcu102 0 [list \
+adi_project ad4858_fmcz_dual_zcu102 0 [list \
   LVDS_CMOS_N     $LVDS_CMOS_N \
 ]
 
 set top_file [list "system_top_lvds.v" "system_constr_lvds.xdc"]
 
-adi_project_files ad4858_fmcz_zcu102 [linsert $top_file 0 \
+adi_project_files ad4858_fmcz_dual_zcu102 [linsert $top_file 0 \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/zcu102/zcu102_system_constr.xdc" ]
 
-adi_project_run ad4858_fmcz_zcu102
+adi_project_run ad4858_fmcz_dual_zcu102
 
