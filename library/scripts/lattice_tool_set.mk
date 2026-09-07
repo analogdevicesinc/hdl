1962a20b9a3d1397dd671f8d3642b76c04c5cdb2
tambaru <iamtambaru@gmail.com>
2026-09-07 14:58:24 +0300

    library/scripts: Fix Tcl namespace resolution and export Lattice env paths
    - adi_ip_lattice.tcl: Declare PropelIPLocal_path and AXI port lists using
      the 'variable' keyword, and link PropelIPLocal_path inside procedures.
    - adi_ip_lattice.tcl: Use global qualification (::env, ::ipl::inclid,
      and ::ipl::*) to prevent variable lookup failures under strict Tcl 9.0 rules.
    - lattice_tool_set.mk: Replace unexpanded tilde (~) with $(HOME) for POSIX
      environments and export LATTICE_DEFAULT_INTERFACE_PATH and
      LATTICE_DEFAULT_IP_PATH so they propagate to child Tcl processes.
    
    Signed-off-by: Tamerlan Baziev <iamtambaru@gmail.com>

--- library/scripts/lattice_tool_set.mk
+++ library/scripts/lattice_tool_set.mk
@@ -3,16 +3,16 @@
 ## SPDX short identifier: BSD-1-Clause
 ####################################################################################
 
-LATTICE_IP_TOOL ?= tclsh
+LATTICE_IP_TOOL := tclsh
 
 CYGPATH_VERSION := $(shell command -v cygpath --version)
 
 ifeq ($(LATTICE_DEFAULT_PATHS),1)
 ifneq ($(CYGPATH_VERSION),)
-LATTICE_DEFAULT_INTERFACE_PATH := $(shell cygpath -H)/$(shell whoami)/PropelIPLocal/interfaces
-LATTICE_DEFAULT_IP_PATH := $(shell cygpath -H)/$(shell whoami)/PropelIPLocal
+export LATTICE_DEFAULT_INTERFACE_PATH := $(shell cygpath -H)/$(shell whoami)/PropelIPLocal/interfaces
+export LATTICE_DEFAULT_IP_PATH := $(shell cygpath -H)/$(shell whoami)/PropelIPLocal
 else
-LATTICE_DEFAULT_INTERFACE_PATH := ~/PropelIPLocal/interfaces
-LATTICE_DEFAULT_IP_PATH := ~/PropelIPLocal
+export LATTICE_DEFAULT_INTERFACE_PATH := $(HOME)/PropelIPLocal/interfaces
+export LATTICE_DEFAULT_IP_PATH := $(HOME)/PropelIPLocal
 endif
 endif
