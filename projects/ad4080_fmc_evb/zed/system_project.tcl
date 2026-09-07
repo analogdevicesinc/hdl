caddda60cf2df7fd232184ea6f4f6cd1f3b9ddbd
Tudor Gadalean <Tudor.Gadalean@analog.com>
2026-09-07 16:34:35 +0300

    projects/ad4080_fmc_evb: Add optional SPI slave for AD4080
    Add an optional readout path to fetch ADC samples using an external SPI
    master connected over the ZedBoard's PMOD JB connector.
    
    When SPI_SLAVE=1, the block design instantiates the spi_slave IP, which
    buffers ADC samples in an asynchronous FIFO and streams them out on a
    slave SPI interface, with a data_ready flag to signal available samples.
    
    Signed-off-by: Tudor Gadalean <Tudor.Gadalean@analog.com>

--- projects/ad4080_fmc_evb/zed/system_project.tcl
+++ projects/ad4080_fmc_evb/zed/system_project.tcl
@@ -6,16 +6,25 @@
 source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
+set ADI_POST_ROUTE_SCRIPT [file normalize $ad_hdl_dir/projects/scripts/auto_timing_fix_xilinx.tcl]
 
 set ADC_N_BITS [get_env_param ADC_N_BITS 20]
+set SPI_SLAVE [get_env_param SPI_SLAVE 0]
 
 adi_project ad4080_fmc_evb_zed 0 [list \
   ADC_N_BITS $ADC_N_BITS \
+  SPI_SLAVE $SPI_SLAVE \
 ]
+
 adi_project_files ad4080_fmc_evb_zed [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/zed/zed_system_constr.xdc" \
   "system_constr.xdc" \
   "system_top.v" ]
+
+if {$SPI_SLAVE == 1} {
+  adi_project_files ad4080_fmc_evb_zed [list "system_constr_spi_slave.xdc" ]
+  set_property verilog_define {SPI_SLAVE} [current_fileset]
+}
 
 adi_project_run ad4080_fmc_evb_zed
