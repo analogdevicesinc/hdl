0341c9b3bc2b280b55aeed9f3319d9866b3b271d
AndreiGrozav <andrei.grozav@analog.com>
2026-09-11 17:24:51 +0300

    admfm8000_evalz: Add ADC info in sysid
    Signed-off-by: AndreiGrozav <andrei.grozav@analog.com>

--- projects/admfm8000_evalz/zed/system_bd.tcl
+++ projects/admfm8000_evalz/zed/system_bd.tcl
@@ -13,4 +13,6 @@ ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "$mem_init_sys_file_path/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 
-sysid_gen_sys_init_file
+set sys_cstring "ADC_N_BITS=$ADC_N_BITS"
+
+sysid_gen_sys_init_file $sys_cstring
