66e5a7ce4be4c49480c823e3dd618a90d99e317c
Ioanpaul.Catuna <ictuna@romlxdevvm2.adlk.analog.com>
2026-09-15 18:54:51 +0300

    projects: ad_xband16_ebz: Added location constraints for JESD204 channels. Reduced offload memory size

--- projects/ad_xband16_ebz/vcu118/system_project.tcl
+++ projects/ad_xband16_ebz/vcu118/system_project.tcl
@@ -69,8 +69,8 @@ adi_project ad_xband16_ebz_vcu118 0 [list \
   TX_JESD_S         [get_env_param TX_JESD_S               1 ] \
   TX_JESD_NP        [get_env_param TX_JESD_NP             16 ] \
   TX_NUM_LINKS      [get_env_param TX_NUM_LINKS            4 ] \
-  RX_KS_PER_CHANNEL [get_env_param RX_KS_PER_CHANNEL      16 ] \
-  TX_KS_PER_CHANNEL [get_env_param TX_KS_PER_CHANNEL      16 ] \
+  RX_KS_PER_CHANNEL [get_env_param RX_KS_PER_CHANNEL       8 ] \
+  TX_KS_PER_CHANNEL [get_env_param TX_KS_PER_CHANNEL       8 ] \
   DO_HAS_BYPASS     [get_env_param DO_HAS_BYPASS           0 ] \
 ]
 
