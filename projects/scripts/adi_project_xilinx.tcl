d169cb8f04f4899db56cec68f29739d32ac9f5c0
mandrica <maria-zoita.andrica@analog.com>
2026-09-21 16:16:49 +0300

    common/vpk120: Add support for vpk120
    Add the template project for the VPK120 evaluation board
    (xcvp1202-vsva2785-2MP-e-S).
    
    Board configuration:
      - LPDDR4 clock is 200.321 MHz (4.992 ns), so the NoC uses
        MC_FREQ_SEL SYS_CLK_FROM_MEMORY_CLK
      - PMC_SD1_SLOT_TYPE is {SD 3.0}
      - sys_clk, GPIO LED, DIP switch and push button pins per schematic
    
    Pin assignments are taken from the VPK120 Rev B02 master XDC and
    schematic.
    
    Signed-off-by: mandrica <maria-zoita.andrica@analog.com>

--- projects/scripts/adi_project_xilinx.tcl
+++ projects/scripts/adi_project_xilinx.tcl
@@ -157,6 +157,10 @@ proc adi_project {project_name {mode 0} {parameter_list {}} } {
     set device "xcvp1802-lsvc4072-2MP-e-S"
     set board [lindex [lsearch -all -inline [get_board_parts] *vpk180*] end]
   }
+  if [regexp "_vpk120" $project_name] {
+    set device "xcvp1202-vsva2785-2MP-e-S"
+    set board [lindex [lsearch -all -inline [get_board_parts] *vpk120*] end]
+  }
   if [regexp "_vc709" $project_name] {
     set device "xc7vx690tffg1761-2"
     set board [lindex [lsearch -all -inline [get_board_parts] *vc709*] end]
